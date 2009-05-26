d11b4da9370e70c2e81e15bf793837d1d6d2b031
Carl Hartung <carlhartung@gmail.com>
2009-05-26 23:19:38 +0000

    fixing warnings.

--- src/org/google/android/odk/MainMenu.java
+++ src/org/google/android/odk/MainMenu.java
@@ -25,7 +25,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.Toast;
 
 /**
  * Responsible for displaying buttons to launch the major activities. Also
