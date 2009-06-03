c60dfd3facb353469ef88f31f85b14193b1b7b78
Carl Hartung <carlhartung@gmail.com>
2009-06-03 00:27:14 +0000

    cleaning up warnings.

--- src/org/google/android/odk/FormEntry.java
+++ src/org/google/android/odk/FormEntry.java
@@ -17,7 +17,6 @@
 package org.google.android.odk;
 
 import java.io.File;
-import java.util.Set;
 
 import org.google.android.odk.FormLoader.LoadingState;
 
--- src/org/google/android/odk/widgets/VideoWidget.java
+++ src/org/google/android/odk/widgets/VideoWidget.java
@@ -25,7 +25,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
