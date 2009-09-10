3be9facfaeef0aa6d1618b87b1ac196c95b5cf67
Yaw Anokwa <yanokwa@gmail.com>
2009-09-10 17:21:17 +0000

    odk-collect: fixes issue 59

--- src/org/odk/collect/android/prefs/GlobalPreferences.java
+++ src/org/odk/collect/android/prefs/GlobalPreferences.java
@@ -60,21 +60,21 @@ public class GlobalPreferences extends PreferenceActivity implements
         if (s.endsWith("/")) {
             s = s.substring(0, s.lastIndexOf("/"));
         }
-        etp.setText(s);
+        etp.setSummary(s);
     }
 
 
     private void updateUsername() {
         EditTextPreference etp =
                 (EditTextPreference) this.getPreferenceScreen().findPreference(KEY_USERNAME);
-        etp.setText(etp.getText());
+        etp.setSummary(etp.getText());
     }
 
 
     private void updatePassword() {
         EditTextPreference etp =
                 (EditTextPreference) this.getPreferenceScreen().findPreference(KEY_PASSWORD);
-        etp.setText(etp.getText());
+        etp.setSummary(etp.getText());
     }
 
 
