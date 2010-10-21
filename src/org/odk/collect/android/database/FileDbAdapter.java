52fd0f2650a945cdece0bed62187bc647b432da3
Yaw Anokwa <yanokwa@gmail.com>
2010-10-21 07:23:59 -0700

    closing db adapters

--- src/org/odk/collect/android/database/FileDbAdapter.java
+++ src/org/odk/collect/android/database/FileDbAdapter.java
@@ -478,6 +478,7 @@ public class FileDbAdapter {
 
             // clean up adapter
             if (c != null) {
+                c.deactivate();
                 c.close();
             }
         }
