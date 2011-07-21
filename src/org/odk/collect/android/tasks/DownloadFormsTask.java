e596ce5a41adb83698459f5042aedb71bda508af
carlhartung <devnull@localhost>
2011-07-21 12:04:27 -0700

    making the download error relative to the url, not the attempted file

--- src/org/odk/collect/android/tasks/DownloadFormsTask.java
+++ src/org/odk/collect/android/tasks/DownloadFormsTask.java
@@ -263,7 +263,7 @@ public class DownloadFormsTask extends
             if (statusCode != 200) {
                 String errMsg =
                     Collect.getInstance()
-                            .getString(R.string.file_fetch_failed, f.getAbsolutePath(),
+                            .getString(R.string.file_fetch_failed, downloadUrl,
                                 response.getStatusLine().getReasonPhrase(), statusCode);
                 Log.e(t, errMsg);
                 throw new Exception(errMsg);
