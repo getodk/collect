162d03660668484ab90e23576b5cdeb0f0060b6c
mitchellsundt <mitchellsundt@gmail.com>
2012-07-02 11:07:38 -0700

    Report only the root cause of the failure, not the entire failure chain.

--- src/org/odk/collect/android/utilities/WebUtils.java
+++ src/org/odk/collect/android/utilities/WebUtils.java
@@ -417,11 +417,11 @@ public final class WebUtils {
         } catch (Exception e) {
             e.printStackTrace();
             String cause;
-            if (e.getCause() != null) {
-                cause = e.getCause().toString();
-            } else {
-                cause = e.toString();
+            Throwable c = e;
+            while ( c.getCause() != null ) {
+            	c = c.getCause();
             }
+            cause = c.toString();
             String error = "Error: " + cause + " while accessing " + u.toString();
 
             Log.w(t, error);
