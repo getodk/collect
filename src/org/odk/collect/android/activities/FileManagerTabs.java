e4fa101b953dbf5e88ba768cf72cebcca520485f
mitchellsundt <mitchellsundt@gmail.com>
2013-06-20 17:04:25 -0700

    Changes to fix type conversion and Force Closed in (at least) target build 4.x systems.

--- src/org/odk/collect/android/activities/FileManagerTabs.java
+++ src/org/odk/collect/android/activities/FileManagerTabs.java
@@ -1,11 +1,11 @@
 /*
  * Copyright (C) 2009 University of Washington
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -21,6 +21,8 @@ import android.app.TabActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TabHost;
@@ -48,12 +50,12 @@ public class FileManagerTabs extends TabActivity {
 
         final TabHost tabHost = getTabHost();
         tabHost.setBackgroundColor(Color.WHITE);
-        tabHost.getTabWidget().setBackgroundColor(Color.BLACK);
+        tabHost.getTabWidget().setBackgroundColor(Color.DKGRAY);
 
         Intent remote = new Intent(this, DataManagerList.class);
         tabHost.addTab(tabHost.newTabSpec(DATA_TAB).setIndicator(getString(R.string.data))
                 .setContent(remote));
-        
+
         Intent local = new Intent(this, FormManagerList.class);
         tabHost.addTab(tabHost.newTabSpec(FORMS_TAB).setIndicator(getString(R.string.forms))
                 .setContent(local));
@@ -63,27 +65,29 @@ public class FileManagerTabs extends TabActivity {
         TabWidget tw = (TabWidget) ll.getChildAt(0);
 
         int fontsize = Collect.getQuestionFontsize();
-        
-        RelativeLayout rllf = (RelativeLayout) tw.getChildAt(0);
+
+        ViewGroup rllf = (ViewGroup) tw.getChildAt(0);
         mTVFF = (TextView) rllf.getChildAt(1);
         mTVFF.setTextSize(fontsize);
+        mTVFF.setTextColor(Color.WHITE);
         mTVFF.setPadding(0, 0, 0, 6);
 
-        RelativeLayout rlrf = (RelativeLayout) tw.getChildAt(1);
+        ViewGroup rlrf = (ViewGroup) tw.getChildAt(1);
         mTVDF = (TextView) rlrf.getChildAt(1);
         mTVDF.setTextSize(fontsize);
+        mTVDF.setTextColor(Color.WHITE);
         mTVDF.setPadding(0, 0, 0, 6);
     }
 
     @Override
     protected void onStart() {
     	super.onStart();
-		Collect.getInstance().getActivityLogger().logOnStart(this); 
+		Collect.getInstance().getActivityLogger().logOnStart(this);
     }
-    
+
     @Override
     protected void onStop() {
-		Collect.getInstance().getActivityLogger().logOnStop(this); 
+		Collect.getInstance().getActivityLogger().logOnStop(this);
     	super.onStop();
     }
 
