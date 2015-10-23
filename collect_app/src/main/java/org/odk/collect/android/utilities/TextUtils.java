802e9245c19f52d1eb906113b6c7456c269cb93f
Yaw Anokwa <yanokwa@nafundi.com>
2015-10-23 12:59:11 +0700

    More strict MD support and spans are HTML compatible

--- collect_app/src/main/java/org/odk/collect/android/utilities/TextUtils.java
+++ collect_app/src/main/java/org/odk/collect/android/utilities/TextUtils.java
@@ -30,44 +30,75 @@ public class TextUtils {
 
     private static String markdownToHtml(String html) {
 
-        // Regular expressions should match https://github.com/enketo/enketo-transformer/blob/master/src/markdown.js
-        String strongRegex1 = "__(.*?)__";
-        String strongRegex2 = "\\*\\*(.*?)\\*\\*";
-        String strongReplacement = "<strong>$1</strong>";
+        // https://gist.github.com/jbroadway/2836900
+        // we try to be as strict as possible
+        html = html.replaceAll("(\\*\\*|__)(.*?)(\\*\\*|__)", "<strong>$2</strong>");
+        html = html.replaceAll("(\\*|_)(.*?)(\\*|_)", "<em>$2</em>");
+        html = html.replaceAll("\\[([^\\[]+)\\]\\(([^\\)]+)\\)", "<a href=\"$2\">$1</a>");
 
-        String emphasisRegex1 = "_([^\\s][^_\\n]*)_";
-        String emphasisRegex2 = "\\*([^\\s][^\\*\\n]*)\\*";
-        String emphasisReplacement = "<em>$1</em>";
-
-        String linkRegex = "\\[([^\\]]*)\\]\\(([^\\)]+)\\)";
-        String linkReplacement = "<a href=\"$2\">$1</a>";
-
-        html = html.replaceAll(strongRegex1, strongReplacement);
-        html = html.replaceAll(strongRegex2, strongReplacement);
-        html = html.replaceAll(emphasisRegex1, emphasisReplacement);
-        html = html.replaceAll(emphasisRegex2, emphasisReplacement);
-        html = html.replaceAll(linkRegex, linkReplacement);
-
-        String headerRegex = "(#+)([^\\n]*)\\n";
         StringBuffer headerOutput = new StringBuffer();
-        Pattern headerPattern = Pattern.compile(headerRegex);
-        Matcher headerMatcher = headerPattern.matcher(html);
+
+        Matcher headerMatcher = Pattern.compile("(?m)^(#+)(.*)").matcher(html);
         while (headerMatcher.find()) {
             headerMatcher.appendReplacement(headerOutput, createHeaderReplacement(headerMatcher));
         }
-        headerMatcher.appendTail(headerOutput);
-        html = headerOutput.toString();
+        html = headerMatcher.appendTail(headerOutput).toString();
+
+        StringBuffer paragraphOutput = new StringBuffer();
+        Matcher paragraphMatcher = Pattern.compile("\\n([^\\n]+)\\n").matcher(html);
+        while (paragraphMatcher.find()) {
+            paragraphMatcher.appendReplacement(paragraphOutput, createParagraphReplacement(paragraphMatcher));
+        }
+        html = paragraphMatcher.appendTail(paragraphOutput).toString();
+
+        StringBuffer spanOutput = new StringBuffer();
+        Matcher spanMatcher = Pattern.compile("((&lt;)|<)span(.*?)((&gt;)|>)(.*?)((&lt;)|<)/span((&gt;)|>)").matcher(html);
+        while (spanMatcher.find()) {
+            spanMatcher.appendReplacement(spanOutput, createSpanReplacement(spanMatcher));
+        }
+        html = spanMatcher.appendTail(spanOutput).toString();
 
         return html;
     }
 
     public static String createHeaderReplacement(Matcher matcher) {
+
         int level = matcher.group(1).length();
-        return "<h" + level + ">" + matcher.group(2).replaceAll("#+$", "").trim() + "</h" + level + ">\n";
+        return "<h" + level + ">" + matcher.group(2).trim() + "</h" + level + ">\n";
+    }
+
+    public static String createParagraphReplacement(Matcher matcher) {
+
+        String line = matcher.group(1);
+        String trimmed = line.trim();
+        if (trimmed.matches("^<\\/?(h|p)")) {
+            return "\n" + line + "\n";
+        }
+        return "\n<p>" + trimmed + "</p>\n";
+    }
+
+    public static String createSpanReplacement(Matcher matcher) {
+
+        String stylesText = matcher.group(3);
+        stylesText = stylesText.replaceAll("style=[\"'](.*?)[\"']", "$1");
+
+        String[] styles = stylesText.trim().split(";");
+        StringBuffer stylesOutput = new StringBuffer();
+
+        for (int i = 0; i < styles.length; i++) {
+            String[] stylesAttributes = styles[i].trim().split(":");
+            if (stylesAttributes[0].equals("color")) {
+                stylesOutput.append(" color=\"" + stylesAttributes[1] + "\"");
+            }
+            if (stylesAttributes[0].equals("font-family")) {
+                stylesOutput.append(" face=\"" + stylesAttributes[1] + "\"");
+            }
+        }
+
+        return "<font" + stylesOutput + ">" + matcher.group(6).trim() + "</font>";
     }
 
     public static CharSequence textToHtml(String text) {
-
         // There's some terrible bug that displays all the text as the
         // opening tag if a tag is the first thing in the string
         // so we hack around it so it begins with something else
