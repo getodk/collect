12890495833ace1950741749fa441e21270c7993
Jon Nordling <jnordling@blueraster.com>
2015-12-16 22:38:43 -0500

    Added the UI features for Geotrace widget

--- collect_app/src/main/java/org/odk/collect/android/activities/GeoTraceGoogleMapActivity.java
+++ collect_app/src/main/java/org/odk/collect/android/activities/GeoTraceGoogleMapActivity.java
@@ -14,18 +14,43 @@
 
 package org.odk.collect.android.activities;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 
 import org.odk.collect.android.R;
 import org.odk.collect.android.application.Collect;
+import org.osmdroid.DefaultResourceProxyImpl;
+
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
 
 /**
  * Version of the GeoPointMapActivity that uses the new Maps v2 API and Fragments to enable
@@ -35,11 +60,144 @@ import org.odk.collect.android.application.Collect;
  *
  */
 public class GeoTraceGoogleMapActivity extends FragmentActivity implements LocationListener, OnMarkerDragListener, OnMapLongClickListener {
+	private ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+	private ScheduledFuture schedulerHandler;
+	private ImageButton play_button;
+	private ImageButton save_button;
+	public ImageButton polygon_button;
+	public ImageButton layers_button;
+	public ImageButton clear_button;
+	private Button manual_button;
+	private ImageButton pause_button;
+	private ProgressDialog progress;
+	public AlertDialog.Builder builder;
+	private View traceSettingsView;
+	public LayoutInflater inflater;
+	private AlertDialog alert;
+	public AlertDialog.Builder p_builder;
+	private View polygonPolylineView;
+	private AlertDialog p_alert;
+	private Boolean beenPaused = false;
+	private Integer TRACE_MODE = 1; // 0 manual, 1 is automatic
+	private Boolean play_check = false;
+	private Spinner time_units;
+	private Spinner time_delay;
+	private SharedPreferences sharedPreferences;
+	public DefaultResourceProxyImpl resource_proxy;
+	private Boolean inital_location_found = false;
+
+	private GoogleMap mMap;
+	private UiSettings gmapSettings;
+	private LocationManager mLocationManager;
+	private Boolean mGPSOn = false;
+	private Boolean mNetworkOn =false;
+	private Location curLocation;
+	private LatLng curlatLng;
+	private Boolean initZoom = false;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.geotrace_google_layout);
+
+		mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.gmap)).getMap();
+		mMap.setMyLocationEnabled(true);
+		mMap.setOnMapLongClickListener(this);
+		mMap.setOnMarkerDragListener(this);
+		mMap.getUiSettings().setMyLocationButtonEnabled(false);
+		mMap.getUiSettings().setCompassEnabled(false);
+
+		List<String> providers = mLocationManager.getProviders(true);
+		for (String provider : providers) {
+			if (provider.equalsIgnoreCase(LocationManager.GPS_PROVIDER)) {
+				mGPSOn = true;
+				curLocation = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+			}
+			if (provider.equalsIgnoreCase(LocationManager.NETWORK_PROVIDER)) {
+				mNetworkOn = true;
+				curLocation = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+			}
+		}
+
+		// If there is a last know location go there
+		if(curLocation !=null){
+			curlatLng = new LatLng(curLocation.getLatitude(),curLocation.getLongitude());
+			mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(curlatLng, 16));
+			initZoom = true;
+		}
+
+
+		inflater = this.getLayoutInflater();
+		traceSettingsView = inflater.inflate(R.layout.geotrace_dialog, null);
+		polygonPolylineView = inflater.inflate(R.layout.polygon_polyline_dialog, null);
+		resource_proxy = new DefaultResourceProxyImpl(getApplicationContext());
+		time_delay = (Spinner) traceSettingsView.findViewById(R.id.trace_delay);
+		time_delay.setSelection(3);
+		time_units = (Spinner) traceSettingsView.findViewById(R.id.trace_scale);
+		pause_button =(ImageButton)findViewById(R.id.geotrace_pause_button);
+		layers_button = (ImageButton) findViewById(R.id.geoTrace_layers_button);
+		clear_button= (ImageButton) findViewById(R.id.geotrace_clear_button);
+		save_button= (ImageButton) findViewById(R.id.geotrace_save);
+		play_button = (ImageButton)findViewById(R.id.geotrace_play_button);
+		play_button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(final View v) {
+				//setGPSStatus();
+				//We are not in trace mode
+				if (!play_check){
+					if (curLocation == null){
+//						mMyLocationOverlay.runOnFirstFix(centerAroundFix);
+						progress.show();
+
+					}else{
+						if(!beenPaused){
+							alert.show();
+						}else{
+							RadioGroup rb = (RadioGroup) traceSettingsView.findViewById(R.id.radio_group);
+							int radioButtonID = rb.getCheckedRadioButtonId();
+							View radioButton = rb.findViewById(radioButtonID);
+							int idx = rb.indexOfChild(radioButton);
+							TRACE_MODE = idx;
+							if (TRACE_MODE ==0){
+								setupManualMode();
+							}else if (TRACE_MODE ==1){
+								setupAutomaticMode();
+							}else{
+								reset_trace_settings();
+							}
+						}
+						play_check=true;
+					}
+				}else{
+					play_check=false;
+					startGeoTrace();
+				}
+			}
+		});
+		progress = new ProgressDialog(this);
+		progress.setTitle(getString(R.string.getting_location));
+		progress.setMessage(getString(R.string.please_wait_long));
+		progress.setOnCancelListener(new DialogInterface.OnCancelListener() {
+			@Override
+			public void onCancel(DialogInterface dialog) {
+				play_button.setImageResource(R.drawable.ic_menu_mylocation);
+			}
+		});
+
+		manual_button = (Button)findViewById(R.id.manual_button);
+		manual_button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//addLocationMarker();
+
+			}
+		});
+
+		// Build ui of the dialog up front
+		buildDialogs();
+
+
 
 	}
 
@@ -80,9 +238,164 @@ public class GeoTraceGoogleMapActivity extends FragmentActivity implements Locat
 
 	}
 
+	private void buildDialogs(){
+
+		builder = new AlertDialog.Builder(this);
+
+		builder.setTitle(getString(R.string.geotrace_instruction));
+		builder.setMessage(getString(R.string.geotrace_instruction_message));
+		builder.setView(null);
+		builder.setView(traceSettingsView)
+				// Add action buttons
+				.setPositiveButton(getString(R.string.start), new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int id) {
+						startGeoTrace();
+						dialog.cancel();
+						alert.dismiss();
+					}
+				})
+				.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+						alert.dismiss();
+						reset_trace_settings();
+					}
+				})
+				.setOnCancelListener(new DialogInterface.OnCancelListener() {
+
+					@Override
+					public void onCancel(DialogInterface dialog) {
+						reset_trace_settings();
+					}
+				});
+
+
+		alert = builder.create();
+
+
+
+		p_builder = new AlertDialog.Builder(this);
+		p_builder.setTitle("Select Polygon/Polyline");
+//		p_builder.setMessage(getString(R.string.polygon_conection_message));
+		p_builder.setView(polygonPolylineView)
+				// Add action buttons
+				.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+
+					}
+				})
+				.setOnCancelListener(new DialogInterface.OnCancelListener() {
+
+					@Override
+					public void onCancel(DialogInterface dialog) {
+						dialog.cancel();
+						alert.dismiss();
+					}
+				});
+
+		p_alert = p_builder.create();
+
+
+
+	}
+	private void startGeoTrace(){
+		RadioGroup rb = (RadioGroup) traceSettingsView.findViewById(R.id.radio_group);
+		int radioButtonID = rb.getCheckedRadioButtonId();
+		View radioButton = rb.findViewById(radioButtonID);
+		int idx = rb.indexOfChild(radioButton);
+		beenPaused = true;
+		TRACE_MODE = idx;
+		if (TRACE_MODE ==0){
+			setupManualMode();
+		}else if (TRACE_MODE ==1){
+			setupAutomaticMode();
+		}else{
+			reset_trace_settings();
+		}
+		play_button.setVisibility(View.GONE);
+		save_button.setVisibility(View.GONE);
+		pause_button.setVisibility(View.VISIBLE);
+
+
+	}
+	private void reset_trace_settings(){
+		play_button.setImageResource(R.drawable.play_button);
+		play_check=false;
+	}
+	private void setupManualMode(){
+		manual_button.setVisibility(View.VISIBLE);
+
+	}
+	private void setupAutomaticMode(){
+		manual_button.setVisibility(View.VISIBLE);
+		String delay = time_delay.getSelectedItem().toString();
+		String units = time_units.getSelectedItem().toString();
+		Long time_delay;
+		TimeUnit time_units_value;
+		if (units =="Minutes"){
+			time_delay = Long.parseLong(delay) * (60*60);
+			time_units_value = TimeUnit.SECONDS;
+
+		}else{
+			//in Seconds
+			time_delay = Long.parseLong(delay);
+			time_units_value = TimeUnit.SECONDS;
+		}
+
+		setGeoTraceScheuler(time_delay, time_units_value);
+	}
+
+	public void setGeoTraceMode(View view){
+		boolean checked = ((RadioButton) view).isChecked();
+		switch(view.getId()) {
+			case R.id.trace_manual:
+				if (checked){
+					TRACE_MODE = 0;
+					time_units.setVisibility(View.GONE);
+					time_delay.setVisibility(View.GONE);
+					time_delay.invalidate();
+					time_units.invalidate();
+				}
+				break;
+			case R.id.trace_automatic:
+				if (checked){
+					TRACE_MODE = 1;
+					time_units.setVisibility(View.VISIBLE);
+					time_delay.setVisibility(View.VISIBLE);
+					time_delay.invalidate();
+					time_units.invalidate();
+				}
+				break;
+		}
+	}
+
+	/*
+		This functions handels the delay and the Runable for
+	*/
+
+	public void setGeoTraceScheuler(long delay, TimeUnit units){
+		schedulerHandler = scheduler.scheduleAtFixedRate(new Runnable() {
+			@Override
+			public void run() {
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						//addLocationMarker();
+					}
+				});
+			}
+		},delay, delay, units);
+
+	}
+
 
 	@Override
 	public void onLocationChanged(Location location) {
+		if (progress.isShowing()){
+			progress.dismiss();
+		}
 	}
 
 
