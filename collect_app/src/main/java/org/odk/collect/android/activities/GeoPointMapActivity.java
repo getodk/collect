990cc45bd707ba9f4971b916c199478cd7a13be9
Yaw Anokwa <yanokwa@nafundi.com>
2016-05-09 16:24:15 +0200

    Remove unused icons, replace fly to zoom, add copyright

--- collect_app/src/main/java/org/odk/collect/android/activities/GeoPointMapActivity.java
+++ collect_app/src/main/java/org/odk/collect/android/activities/GeoPointMapActivity.java
@@ -521,7 +521,7 @@ public class GeoPointMapActivity extends FragmentActivity implements LocationLis
 
 		if (zoomDialog == null) {
 			AlertDialog.Builder p_builder = new AlertDialog.Builder(this);
-			p_builder.setTitle(getString(R.string.fly_to_where));
+			p_builder.setTitle(getString(R.string.zoom_to_where));
 			p_builder.setView(zoomDialogView)
 					.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
 						public void onClick(DialogInterface dialog, int id) {
