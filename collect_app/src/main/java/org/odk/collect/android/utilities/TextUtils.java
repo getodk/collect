5e10d48f190adcc3e3f4f156584201b85444298a
Yaw Anokwa <yanokwa@nafundi.com>
2016-06-10 18:46:46 +0300

    Rewrite to match Enketo code as closely as possible.

--- collect_app/src/main/java/org/odk/collect/android/utilities/TextUtils.java
+++ collect_app/src/main/java/org/odk/collect/android/utilities/TextUtils.java
@@ -15,99 +15,75 @@
 package org.odk.collect.android.utilities;
 
 import android.text.Html;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
+import java.util.regex.MatchResult;
 
 public class TextUtils {
     private static final String t = "TextUtils";
 
-    private TextUtils() {
-        // static methods only
-    }
-
-    private static String markdownToHtml(String html) {
-
-        // https://gist.github.com/jbroadway/2836900
-        // we try to be as strict as possible
-        html = html.replaceAll("(\\*\\*|__)(.*?)(\\*\\*|__)", "<strong>$2</strong>");
-        html = html.replaceAll("(\\*|_)(.*?)(\\*|_)", "<em>$2</em>");
-        html = html.replaceAll("\\[([^\\[]+)\\]\\(([^\\)]+)\\)", "<a href=\"$2\">$1</a>");
-
-        StringBuffer headerOutput = new StringBuffer();
-        Matcher headerMatcher = Pattern.compile("(?m)^(#+)(.*)").matcher(html);
-        while (headerMatcher.find()) {
-            headerMatcher.appendReplacement(headerOutput, headerMatcher.quoteReplacement(createHeaderReplacement(headerMatcher)));
+    private static ReplaceCallback.Callback createHeader = new ReplaceCallback.Callback() {
+        public String matchFound(MatchResult match) {
+            int level = match.group(1).length();
+            return "<h" + level + ">" + match.group(2).replaceAll( "#+$", "" ).trim() + "</h" + level + ">";
         }
-        html = headerMatcher.appendTail(headerOutput).toString();
+    };
 
-        StringBuffer paragraphOutput = new StringBuffer();
-        Matcher paragraphMatcher = Pattern.compile("\\n([^\\n]+)\\n").matcher(html);
-        while (paragraphMatcher.find()) {
-            paragraphMatcher.appendReplacement(paragraphOutput, headerMatcher.quoteReplacement(createParagraphReplacement(paragraphMatcher)));
-        }
-        html = paragraphMatcher.appendTail(paragraphOutput).toString();
-
-        StringBuffer spanOutput = new StringBuffer();
-        Matcher spanMatcher = Pattern.compile("((&lt;)|<)span(.*?)((&gt;)|>)(.*?)((&lt;)|<)/span((&gt;)|>)").matcher(html);
-        while (spanMatcher.find()) {
-            spanMatcher.appendReplacement(spanOutput, headerMatcher.quoteReplacement(createSpanReplacement(spanMatcher)));
-        }
-        html = spanMatcher.appendTail(spanOutput).toString();
-
-        return html;
-    }
-
-    public static String createHeaderReplacement(Matcher matcher) {
-
-        int level = matcher.group(1).length();
-        return "<h" + level + ">" + matcher.group(2).trim() + "</h" + level + ">\n";
-    }
-
-    public static String createParagraphReplacement(Matcher matcher) {
-
-        String line = matcher.group(1);
-        String trimmed = line.trim();
-        if (trimmed.matches("^<\\/?(h|p)")) {
-            return "\n" + line + "\n";
-        }
-        return "\n<p>" + trimmed + "</p>\n";
-    }
-
-    public static String createSpanReplacement(Matcher matcher) {
-
-        String stylesText = matcher.group(3);
-        stylesText = stylesText.replaceAll("style=[\"'](.*?)[\"']", "$1");
-
-        String[] styles = stylesText.trim().split(";");
-        StringBuffer stylesOutput = new StringBuffer();
-
-        for (int i = 0; i < styles.length; i++) {
-            String[] stylesAttributes = styles[i].trim().split(":");
-            if (stylesAttributes[0].equals("color")) {
-                stylesOutput.append(" color=\"" + stylesAttributes[1] + "\"");
+    private static ReplaceCallback.Callback createParagraph = new ReplaceCallback.Callback() {
+        public String matchFound(MatchResult match) {
+            String trimmed = match.group(1).trim();
+            if (trimmed.matches("(?i)^<\\/?(h|p|bl)")) {
+                return match.group(1);
             }
-            if (stylesAttributes[0].equals("font-family")) {
-                stylesOutput.append(" face=\"" + stylesAttributes[1] + "\"");
+            return "<p>" + trimmed + "</p>";
+        }
+    };
+
+    private static ReplaceCallback.Callback createSpan = new ReplaceCallback.Callback() {
+        public String matchFound(MatchResult match) {
+            String attributes = sanitizeAttributes(match.group(1));
+            return "<font" + attributes + ">" + match.group(2).trim() + "</font>";
+        }
+
+        // throw away all styles except for color and font-family
+        private String sanitizeAttributes( String attributes ) {
+
+            String stylesText = attributes.replaceAll("style=[\"'](.*?)[\"']", "$1");
+            String[] styles = stylesText.trim().split(";");
+            StringBuffer stylesOutput = new StringBuffer();
+
+            for (int i = 0; i < styles.length; i++) {
+                String[] stylesAttributes = styles[i].trim().split(":");
+                if (stylesAttributes[0].equals("color")) {
+                    stylesOutput.append(" color=\"" + stylesAttributes[1] + "\"");
+                }
+                if (stylesAttributes[0].equals("font-family")) {
+                    stylesOutput.append(" face=\"" + stylesAttributes[1] + "\"");
+                }
             }
+
+            return stylesOutput.toString();
         }
+    };
 
-        return "<font" + stylesOutput + ">" + matcher.group(6).trim() + "</font>";
-    }
+    private static String markdownToHtml(String text) {
 
-    // http://stackoverflow.com/a/10187511/152938
-    public static CharSequence trimTrailingWhitespace(CharSequence source) {
+        // https://github.com/enketo/enketo-transformer/blob/master/src/markdown.js
 
-        if(source == null)
-            return "";
+        // span - replaced &lt; and &gt; with <>
+        text = ReplaceCallback.replace( "(?s)<\\s?span([^\\/\n]*)>((?:(?!<\\/).)+)<\\/\\s?span\\s?>", text, createSpan );
+        // strong
+        text = text.replaceAll( "(?s)__(.*?)__", "<strong>$1</strong>" );
+        text = text.replaceAll( "(?s)\\*\\*(.*?)\\*\\*", "<strong>$1</strong>" );
+        // emphasis
+        text = text.replaceAll( "(?s)_([^\\s][^_\n]*)_", "<em>$1</em>" );
+        text = text.replaceAll( "(?s)\\*([^\\s][^\\*\n]*)\\*", "<em>$1</em>" );
+        // links
+        text = text.replaceAll( "(?s)\\[([^\\]]*)\\]\\(([^\\)]+)\\)", "<a href=\"$2\" target=\"_blank\">$1</a>" );
+        // headers - requires ^ or breaks <font color="#f58a1f">color</font>
+        text = ReplaceCallback.replace( "(?s)^(#+)([^\n]*)$", text, createHeader );
+        // paragraphs
+        text = ReplaceCallback.replace( "(?s)([^\n]+)\n", text, createParagraph );
 
-        int i = source.length();
-
-        // loop back to the first non-whitespace character
-        while(--i >= 0 && Character.isWhitespace(source.charAt(i))) {
-        }
-
-        return source.subSequence(0, i+1);
+        return text;
     }
 
     public static CharSequence textToHtml(String text) {
@@ -116,7 +92,8 @@ public class TextUtils {
             return null;
         }
 
-        return trimTrailingWhitespace(Html.fromHtml(markdownToHtml(text)));
+        return Html.fromHtml(markdownToHtml(text));
+
     }
 
 } 
