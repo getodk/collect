3a207932a9a59c74c110f36c340f4c5cadc4b0e0
lindsay stevens <lindsay.stevens.au@gmail.com>
2016-09-23 12:43:33 +1000

    chg: robolectric to mock android instead

--- collect_app/src/test/java/org/odk/collect/android/utilities/TextUtilsTest.java
+++ collect_app/src/test/java/org/odk/collect/android/utilities/TextUtilsTest.java
@@ -1,35 +1,20 @@
 package org.odk.collect.android.utilities;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.robolectric.RobolectricTestRunner;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.mockito.Mockito.verify;
 
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(RobolectricTestRunner.class)
 public class TextUtilsTest {
-
-    private TextUtils textUtils;
-
-    @Mock
-    TextUtils.HtmlWrapper mHtml;
 
     @Captor
     private ArgumentCaptor<String> captor;
-
-    @Before
-    public void setUp(){
-        MockitoAnnotations.initMocks(this);
-        textUtils = new TextUtils(mHtml);
-    }
 
     /**
      * Should return null if provided with null and not throw a NPE.
@@ -37,19 +22,17 @@ public class TextUtilsTest {
     @Test
     public void textToHtml_BounceNullInput() {
         String input = null;
-        CharSequence observed = textUtils.textToHtml(input);
+        CharSequence observed = TextUtils.textToHtml(input);
         assertNull(observed);
     }
 
     /**
-     * Should call HTML processor if provided with non-null string.
+     * Should do something not-null-ish if provided with non-null string.
      */
     @Test
     public void textToHtml_CallsHTMLOnRealInput() {
         String input = "This *bold* markdown is \n- nice,\n- efficient.\n";
-        mHtml.toHtml(input);
-        verify(mHtml).toHtml(captor.capture());
-        String input_arg = captor.getValue();
-        assertEquals(input, input_arg);
+        CharSequence result = TextUtils.textToHtml(input);
+        assertNotNull(result);
     }
 }
