3d725777447b9fdc116bcaf1832568a7fd613f52
lindsay stevens <lindsay.stevens.au@gmail.com>
2016-10-12 12:44:08 +1100

    chg: elaborate test description, test impl as best possible

--- collect_app/src/test/java/org/odk/collect/android/utilities/TextUtilsTest.java
+++ collect_app/src/test/java/org/odk/collect/android/utilities/TextUtilsTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 
@@ -13,21 +13,37 @@ public class TextUtilsTest {
 
     /**
      * Should return null if provided with null and not throw a NPE.
+     *
+     * This is a silly test but it is here to guarantee this behaviour,
+     * since without it the method causes a crash when processing text for
+     * questions with no plain text label. See opendatakit/opendatakit#1247.
      */
     @Test
-    public void textToHtml_BounceNullInput() {
+    public void textToHtml_BouncesNullInput() {
         String input = null;
         CharSequence observed = TextUtils.textToHtml(input);
         assertNull(observed);
     }
 
     /**
-     * Should do something not-null-ish if provided with non-null string.
+     * Should convert Markdown to HTML.
+     *
+     * In the real app this call would produce the following string, but
+     * because the implementation uses android.Html.fromHtml, we end up with
+     * robolectric's implementation of fromHtml which isn't exactly the same.
+     * See opendatakit/collect#204.
+     *
+     * <p>This <em>bold</em> markdown is</p><p>- nice,</p><p>- efficient.</p>
+     *
+     * The main thing is that it's clear this interface processes Markdown to
+     * HTML, which is evident from the removal of the stars on *bold* and
+     * doubling of line returns (which is due to \n to <p></p> conversion).
      */
     @Test
-    public void textToHtml_CallsHTMLOnRealInput() {
+    public void textToHtml_ConvertsMarkdownToHtml() {
         String input = "This *bold* markdown is \n- nice,\n- efficient.\n";
-        CharSequence result = TextUtils.textToHtml(input);
-        assertNotNull(result);
+        String expected = "This bold markdown is\n\n- nice,\n\n- efficient.\n\n";
+        CharSequence actual = TextUtils.textToHtml(input);
+        assertEquals(expected, actual.toString());
     }
 }
