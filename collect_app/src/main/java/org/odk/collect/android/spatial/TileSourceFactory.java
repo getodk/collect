88d4ef7e4bffbcdaa9dbff0e315f2b3ea8e61b73
Maxim Soloviev <max@max2.me>
2016-11-22 17:44:18 -0800

    Incorporating feedback from code review.

--- collect_app/src/main/java/org/odk/collect/android/spatial/TileSourceFactory.java
+++ collect_app/src/main/java/org/odk/collect/android/spatial/TileSourceFactory.java
@@ -1,5 +1,8 @@
 package org.odk.collect.android.spatial;
 
+import android.content.Context;
+
+import org.odk.collect.android.R;
 import org.osmdroid.ResourceProxy;
 import org.osmdroid.tileprovider.MapTile;
 import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase;
@@ -10,29 +13,47 @@ import org.osmdroid.tileprovider.tilesource.XYTileSource;
   * are available in new version of osmdroid but not in currently used 4.2
  */
 public class TileSourceFactory {
-    public static final OnlineTileSourceBase USGS_TOPO = new OnlineTileSourceBase(
-            "USGS National Map Topo",
-            ResourceProxy.string.unknown,
-            0, 18, 256, "",
-            new String[] { "http://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/" }) {
-        @Override
-        public String getTileURLString(MapTile aTile) {
-            return getBaseUrl() + aTile.getZoomLevel() + "/" + aTile.getY() + "/" + aTile.getX();
-        }
-    };
+    public final OnlineTileSourceBase mUSGSTopo;
+    public final OnlineTileSourceBase mUsgsSat;
+    public final OnlineTileSourceBase mStamenTerrain;
 
-    public static final OnlineTileSourceBase USGS_SAT = new OnlineTileSourceBase(
-            "USGS National Map Sat",
-            ResourceProxy.string.unknown,
-            0, 18, 256, "",
-            new String[]{"http://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/"}) {
-        @Override
-        public String getTileURLString(MapTile aTile) {
-            return getBaseUrl() + aTile.getZoomLevel() + "/" + aTile.getY() + "/" + aTile.getX();
-        }
-    };
+    public TileSourceFactory(Context context) {
+        mUSGSTopo = new OnlineTileSourceBase(
+                context.getString(R.string.openmap_usgs_topo),
+                ResourceProxy.string.unknown,
+                0, 18, 256, "",
+                new String[] { "http://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/" }) {
+            @Override
+            public String getTileURLString(MapTile aTile) {
+                return getBaseUrl() + aTile.getZoomLevel() + "/" + aTile.getY() + "/" + aTile.getX();
+            }
+        };
 
-    public static final OnlineTileSourceBase STAMEN_TERRAIN = new XYTileSource("Stamen Terrain",
-            ResourceProxy.string.unknown, 0, 18, 256, ".jpg", new String[] {
-            "http://tile.stamen.com/terrain/" });
+        mUsgsSat = new OnlineTileSourceBase(
+                context.getString(R.string.openmap_usgs_sat),
+                ResourceProxy.string.unknown,
+                0, 18, 256, "",
+                new String[]{"http://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/"}) {
+            @Override
+            public String getTileURLString(MapTile aTile) {
+                return getBaseUrl() + aTile.getZoomLevel() + "/" + aTile.getY() + "/" + aTile.getX();
+            }
+        };
+
+        mStamenTerrain = new XYTileSource(context.getString(R.string.openmap_stamen_terrain),
+                ResourceProxy.string.unknown, 0, 18, 256, ".jpg", new String[] {
+                "http://tile.stamen.com/terrain/" });
+    }
+
+    public OnlineTileSourceBase getUSGSTopo() {
+        return mUSGSTopo;
+    }
+
+    public OnlineTileSourceBase getUsgsSat() {
+        return mUsgsSat;
+    }
+
+    public OnlineTileSourceBase getStamenTerrain() {
+        return mStamenTerrain;
+    }
 }
