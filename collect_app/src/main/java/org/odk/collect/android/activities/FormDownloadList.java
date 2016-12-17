e3779885b8589c76cb65f28e87126cada81030b3
Jeff Wishnie <jeff@wishnie.org>
2016-12-16 17:42:22 -0800

    Addresses issue #248 https://github.com/opendatakit/collect/issues/248
    Adds new utility class 'ListViewUtils' with static helpers for checking and unchecking ListView items.
    
    NOTE: Behavior of 'toggle' button is not actually a toggle. It does not flip the state of individual items.
    Instead what is implemented is really two binary functions "Check All" and "Uncheck All".
    
    A clean reimplementation would replace the label of the 'Toggle All' button with alternating 'Check All' and 'Uncheck All'
    labels, dependent on the current state of checked items in the list.
    
    This will be filed as a new bug.
    
    Changes to be committed:
    	modified:   collect_app/src/main/java/org/odk/collect/android/activities/FormDownloadList.java
    	new file:   collect_app/src/main/java/org/odk/collect/android/utilities/ListViewUtils.java

--- collect_app/src/main/java/org/odk/collect/android/activities/FormDownloadList.java
+++ collect_app/src/main/java/org/odk/collect/android/activities/FormDownloadList.java
@@ -53,6 +53,7 @@ import org.odk.collect.android.provider.FormsProviderAPI.FormsColumns;
 import org.odk.collect.android.tasks.DownloadFormListTask;
 import org.odk.collect.android.tasks.DownloadFormsTask;
 import org.odk.collect.android.utilities.CompatibilityUtils;
+import org.odk.collect.android.utilities.ListViewUtils;
 import org.odk.collect.android.utilities.WebUtils;
 
 import java.util.ArrayList;
@@ -116,7 +117,7 @@ public class FormDownloadList extends ListActivity implements FormListDownloader
     private SimpleAdapter mFormListAdapter;
     private ArrayList<HashMap<String, String>> mFormList;
 
-    private boolean mToggled = false;
+    // private boolean mToggled = false;
     private int mSelectedCount = 0;
 
     private static final boolean EXIT = true;
@@ -137,7 +138,7 @@ public class FormDownloadList extends ListActivity implements FormListDownloader
         getListView().setBackgroundColor(Color.WHITE);
 
         mDownloadButton = (Button) findViewById(R.id.add_button);
-        mDownloadButton.setEnabled(selectedItemCount() > 0);
+        mDownloadButton.setEnabled(ListViewUtils.selectedItemCount(getListView()) > 0);
         mDownloadButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -145,7 +146,7 @@ public class FormDownloadList extends ListActivity implements FormListDownloader
                 //    Collect.getInstance().getActivityLogger().logAction(this,
                 // "downloadSelectedFiles", ...);
                 downloadSelectedFiles();
-                mToggled = false;
+                // mToggled = false;
                 clearChoices();
             }
         });
@@ -154,18 +155,7 @@ public class FormDownloadList extends ListActivity implements FormListDownloader
         mToggleButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                // toggle selections of items to all or none
-                ListView ls = getListView();
-                mToggled = !mToggled;
-
-                Collect.getInstance().getActivityLogger().logAction(this, "toggleFormCheckbox",
-                        Boolean.toString(mToggled));
-
-                for (int pos = 0; pos < ls.getCount(); pos++) {
-                    ls.setItemChecked(pos, mToggled);
-                }
-
-                mDownloadButton.setEnabled(!(selectedItemCount() == 0));
+                mDownloadButton.setEnabled(ListViewUtils.toggleChecked(getListView()));
             }
         });
 
@@ -175,7 +165,7 @@ public class FormDownloadList extends ListActivity implements FormListDownloader
             public void onClick(View v) {
                 Collect.getInstance().getActivityLogger().logAction(this, "refreshForms", "");
 
-                mToggled = false;
+                // mToggled = false;
                 downloadFormList();
                 FormDownloadList.this.getListView().clearChoices();
                 clearChoices();
@@ -192,7 +182,7 @@ public class FormDownloadList extends ListActivity implements FormListDownloader
 
             // indicating whether or not select-all is on or off.
             if (savedInstanceState.containsKey(BUNDLE_TOGGLED_KEY)) {
-                mToggled = savedInstanceState.getBoolean(BUNDLE_TOGGLED_KEY);
+                // mToggled = savedInstanceState.getBoolean(BUNDLE_TOGGLED_KEY);
             }
 
             // how many items we've selected
@@ -286,7 +276,7 @@ public class FormDownloadList extends ListActivity implements FormListDownloader
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
-        mDownloadButton.setEnabled(!(selectedItemCount() == 0));
+        mDownloadButton.setEnabled(!(ListViewUtils.selectedItemCount(getListView()) == 0));
 
         Object o = getListAdapter().getItem(position);
         @SuppressWarnings("unchecked")
@@ -341,8 +331,9 @@ public class FormDownloadList extends ListActivity implements FormListDownloader
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putBoolean(BUNDLE_TOGGLED_KEY, mToggled);
-        outState.putInt(BUNDLE_SELECTED_COUNT, selectedItemCount());
+        // TODO: Is it safe to remove BUNDLE_TOGGLED_KEY ?
+        outState.putBoolean(BUNDLE_TOGGLED_KEY, false);
+        outState.putInt(BUNDLE_SELECTED_COUNT, ListViewUtils.selectedItemCount(getListView()));
         outState.putSerializable(BUNDLE_FORM_MAP, mFormNamesAndURLs);
         outState.putString(DIALOG_TITLE, mAlertTitle);
         outState.putString(DIALOG_MSG, mAlertMsg);
@@ -350,23 +341,6 @@ public class FormDownloadList extends ListActivity implements FormListDownloader
         outState.putBoolean(SHOULD_EXIT, mShouldExit);
         outState.putSerializable(FORMLIST, mFormList);
     }
-
-
-    /**
-     * returns the number of items currently selected in the list.
-     */
-    private int selectedItemCount() {
-        ListView lv = getListView();
-        int count = 0;
-        SparseBooleanArray sba = lv.getCheckedItemPositions();
-        for (int i = 0; i < lv.getCount(); i++) {
-            if (sba.get(i, false)) {
-                count++;
-            }
-        }
-        return count;
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -713,7 +687,7 @@ public class FormDownloadList extends ListActivity implements FormListDownloader
             }
             selectSupersededForms();
             mFormListAdapter.notifyDataSetChanged();
-            mDownloadButton.setEnabled(!(selectedItemCount() == 0));
+            mDownloadButton.setEnabled(!(ListViewUtils.selectedItemCount(getListView()) == 0));
         }
     }
 
