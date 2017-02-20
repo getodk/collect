784e9bff7f1cd269c55639f5db967b0b80651db9
Yaw Anokwa <yanokwa@users.noreply.github.com>
2017-02-20 13:25:58 +0800

    Add option to hide/show analytics preference (#433)

--- collect_app/src/main/java/org/odk/collect/android/preferences/PreferencesActivity.java
+++ collect_app/src/main/java/org/odk/collect/android/preferences/PreferencesActivity.java
@@ -66,7 +66,7 @@ public class PreferencesActivity extends PreferenceActivity implements OnPrefere
     public static final String KEY_SPLASH_PATH = "splashPath";
     public static final String KEY_FONT_SIZE = "font_size";
     public static final String KEY_DELETE_AFTER_SEND = "delete_send";
-    public static final String KEY_ENABLE_ANALYTICS = "enable_analytics";
+    public static final String KEY_ANALYTICS = "analytics";
 
     public static final String KEY_PROTOCOL = "protocol";
     public static final String KEY_OPEN_SOURCE_LICENSES = "open_source_licenses";
@@ -132,6 +132,8 @@ public class PreferencesActivity extends PreferenceActivity implements OnPrefere
 
     protected ListPreference mMapSdk;
     protected ListPreference mMapBasemap;
+
+    private CheckBoxPreference mAnalyticsPreference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -407,12 +409,22 @@ public class PreferencesActivity extends PreferenceActivity implements OnPrefere
             clientCategory.removePreference(highResolution);
         }
 
-        final CheckBoxPreference enableAnalyticsPreference = (CheckBoxPreference) findPreference(KEY_ENABLE_ANALYTICS);
-        enableAnalyticsPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+        PreferenceCategory analyticsCategory = (PreferenceCategory) findPreference(
+                getString(R.string.analytics_preferences));
+        mAnalyticsPreference = (CheckBoxPreference) findPreference(KEY_ANALYTICS);
+
+        boolean analyticsAvailable = adminPreferences.getBoolean(
+                AdminPreferencesActivity.KEY_ANALYTICS, true);
+        if (!(analyticsAvailable || adminMode)) {
+            analyticsCategory.removePreference(mAnalyticsPreference);
+            getPreferenceScreen().removePreference(analyticsCategory);
+        }
+
+        mAnalyticsPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
                 GoogleAnalytics googleAnalytics = GoogleAnalytics.getInstance(getApplicationContext());
-                googleAnalytics.setAppOptOut(!enableAnalyticsPreference.isChecked());
+                googleAnalytics.setAppOptOut(!mAnalyticsPreference.isChecked());
                 return true;
             }
         });
