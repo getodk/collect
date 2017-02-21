59330ccd3f1ea231a6bc30a72d4af74048a202fc
Grzegorz Orczykowski <gorczykowski@soldevelo.com>
2017-02-21 13:59:39 +0100

    DateWidget 3

--- collect_app/src/main/java/org/odk/collect/android/widgets/DateWidget.java
+++ collect_app/src/main/java/org/odk/collect/android/widgets/DateWidget.java
@@ -143,10 +143,8 @@ public class DateWidget extends QuestionWidget {
             this.mDatePicker.setSpinnersShown(true);
         } else if ("year".equals(appearance)) {
             hideMonth = true;
-            if (Build.VERSION.SDK_INT >= 11) {
-                this.mDatePicker.setCalendarViewShown(false);
-                this.mDatePicker.setSpinnersShown(true);
-            }
+            this.mDatePicker.setCalendarViewShown(false);
+            this.mDatePicker.setSpinnersShown(true);
         } else if ("no-calendar".equals(appearance)) {
             if (Build.VERSION.SDK_INT >= 11) {
                 this.mDatePicker.setCalendarViewShown(false);
