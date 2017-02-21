8669ad582f29769dbd571759700613764bd3940c
Grzegorz Orczykowski <gorczykowski@soldevelo.com>
2017-02-21 13:54:48 +0100

    DateTimeWidget 1

--- collect_app/src/main/java/org/odk/collect/android/widgets/DateTimeWidget.java
+++ collect_app/src/main/java/org/odk/collect/android/widgets/DateTimeWidget.java
@@ -155,19 +155,15 @@ public class DateTimeWidget extends QuestionWidget {
     private void hideDayFieldIfNotInFormat(FormEntryPrompt prompt) {
         String appearance = prompt.getQuestion().getAppearanceAttr();
         if (appearance == null) {
-            if (Build.VERSION.SDK_INT >= 11) {
-                showCalendar = true;
-                this.mDatePicker.setCalendarViewShown(true);
-                if (Build.VERSION.SDK_INT >= 12) {
-                    CalendarView cv = this.mDatePicker.getCalendarView();
-                    cv.setShowWeekNumber(false);
-                }
-                this.mDatePicker.setSpinnersShown(true);
-                hideDay = true;
-                hideMonth = false;
-            } else {
-                return;
+            showCalendar = true;
+            this.mDatePicker.setCalendarViewShown(true);
+            if (Build.VERSION.SDK_INT >= 12) {
+                CalendarView cv = this.mDatePicker.getCalendarView();
+                cv.setShowWeekNumber(false);
             }
+            this.mDatePicker.setSpinnersShown(true);
+            hideDay = true;
+            hideMonth = false;
         } else if ("month-year".equals(appearance)) {
             hideDay = true;
             if (Build.VERSION.SDK_INT >= 11) {
