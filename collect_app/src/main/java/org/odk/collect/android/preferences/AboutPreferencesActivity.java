ddea4bb9145245cbb49d7922c37c8a0664d689ce
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2017-02-26 23:33:41 +0100

    about screen messages and copyright reviewed. #304

--- collect_app/src/main/java/org/odk/collect/android/preferences/AboutPreferencesActivity.java
+++ collect_app/src/main/java/org/odk/collect/android/preferences/AboutPreferencesActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Nafundi
+ * Copyright 2017 Marcos Lopez Gonzalez (asturcon1234@gmail.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
