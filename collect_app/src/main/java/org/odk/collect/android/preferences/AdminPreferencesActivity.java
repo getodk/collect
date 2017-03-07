7351502e60d66ea5706ef6528befbd1827d1f6e2
Shobhit Agarwal <shobhitagarwal1612@gmail.com>
2017-03-07 20:34:11 +0530

    Use Fragments for Preference Classes (#510)

--- collect_app/src/main/java/org/odk/collect/android/preferences/AdminPreferencesActivity.java
+++ collect_app/src/main/java/org/odk/collect/android/preferences/AdminPreferencesActivity.java
@@ -14,33 +14,26 @@
 
 package org.odk.collect.android.preferences;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.ListPreference;
-import android.preference.Preference;
+import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import org.javarosa.core.model.FormDef;
 import org.odk.collect.android.R;
 import org.odk.collect.android.application.Collect;
-import static org.odk.collect.android.preferences.AdminKeys.*;
+import org.odk.collect.android.utilities.ToastUtils;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+
+import static org.odk.collect.android.preferences.AdminKeys.KEY_FORM_PROCESSING_LOGIC;
 
 /**
  * Handles admin preferences, which are password-protectable and govern which app features and
@@ -49,150 +42,9 @@ import java.io.ObjectOutputStream;
  * @author Thomas Smyth, Sassafras Tech Collective (tom@sassafrastech.com; constraint behavior
  *         option)
  */
-public class AdminPreferencesActivity extends AppPreferenceActivity {
-
-    public static String ADMIN_PREFERENCES = "admin_prefs";
-
+public class AdminPreferencesActivity extends PreferenceActivity {
     private static final int SAVE_PREFS_MENU = Menu.FIRST;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setTitle(getString(R.string.admin_preferences));
-
-        PreferenceManager prefMgr = getPreferenceManager();
-        prefMgr.setSharedPreferencesName(ADMIN_PREFERENCES);
-        prefMgr.setSharedPreferencesMode(MODE_WORLD_READABLE);
-
-        addPreferencesFromResource(R.xml.admin_preferences);
-
-        ListPreference mFormProcessingLogicPreference = listPref(KEY_FORM_PROCESSING_LOGIC);
-        mFormProcessingLogicPreference.setSummary(mFormProcessingLogicPreference.getEntry());
-        mFormProcessingLogicPreference.setOnPreferenceChangeListener(
-                new Preference.OnPreferenceChangeListener() {
-
-                    @Override
-                    public boolean onPreferenceChange(Preference preference, Object newValue) {
-                        int index = ((ListPreference) preference).findIndexOfValue(
-                                newValue.toString());
-                        String entry = (String) ((ListPreference) preference).getEntries()[index];
-                        preference.setSummary(entry);
-                        return true;
-                    }
-                });
-
-        Preference mChangeAdminPwPreference = pref(KEY_CHANGE_ADMIN_PASSWORD);
-        mChangeAdminPwPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                AlertDialog.Builder b = new AlertDialog.Builder(AdminPreferencesActivity.this);
-
-                LayoutInflater factory = LayoutInflater.from(AdminPreferencesActivity.this);
-                final View dialogView = factory.inflate(R.layout.password_dialog_layout, null);
-
-                final EditText passwordEditText = (EditText) dialogView.findViewById(R.id.pwd_field);
-                final EditText verifyEditText = (EditText) dialogView.findViewById(R.id.verify_field);
-
-                b.setTitle(R.string.change_admin_password);
-                b.setView(dialogView);
-                b.setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        String pw = passwordEditText.getText().toString();
-                        String ver = verifyEditText.getText().toString();
-
-                        if (!pw.equalsIgnoreCase("") && !ver.equalsIgnoreCase("") && pw.equals(ver)) {
-                            // passwords are the same
-                            SharedPreferences.Editor editor = getSharedPreferences(ADMIN_PREFERENCES, MODE_PRIVATE).edit();
-                            editor.putString(KEY_ADMIN_PW, pw);
-                            Toast.makeText(AdminPreferencesActivity.this,
-                                    R.string.admin_password_changed, Toast.LENGTH_SHORT).show();
-                            editor.commit();
-                            dialog.dismiss();
-                            Collect.getInstance().getActivityLogger()
-                                    .logAction(this, "AdminPasswordDialog", "CHANGED");
-                        } else if (pw.equalsIgnoreCase("") && ver.equalsIgnoreCase("")) {
-                            SharedPreferences.Editor editor = getSharedPreferences(ADMIN_PREFERENCES, MODE_PRIVATE).edit();
-                            editor.putString(KEY_ADMIN_PW, "");
-                            editor.commit();
-                            Toast.makeText(AdminPreferencesActivity.this,
-                                    R.string.admin_password_disabled, Toast.LENGTH_SHORT).show();
-                            dialog.dismiss();
-                            Collect.getInstance().getActivityLogger()
-                                    .logAction(this, "AdminPasswordDialog", "DISABLED");
-                        } else {
-                            Toast.makeText(AdminPreferencesActivity.this,
-                                    R.string.admin_password_mismatch, Toast.LENGTH_SHORT).show();
-                            Collect.getInstance().getActivityLogger()
-                                    .logAction(this, "AdminPasswordDialog", "MISMATCH");
-                        }
-                    }
-                });
-                b.setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        Collect.getInstance().getActivityLogger().logAction(this, "AdminPasswordDialog", "CANCELLED");
-                    }
-                });
-
-                b.setCancelable(false);
-                AlertDialog dialog = b.create();
-                dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-                dialog.show();
-                return true;
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        Collect.getInstance().getActivityLogger()
-                .logAction(this, "onCreateOptionsMenu", "show");
-        super.onCreateOptionsMenu(menu);
-
-        menu
-                .add(0, SAVE_PREFS_MENU, 0, R.string.save_preferences)
-                .setIcon(R.drawable.ic_menu_save)
-                .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case SAVE_PREFS_MENU:
-                File writeDir = new File(Collect.SETTINGS);
-                if (!writeDir.exists()) {
-                    if (!writeDir.mkdirs()) {
-                        Toast.makeText(
-                                this,
-                                "Error creating directory "
-                                        + writeDir.getAbsolutePath(),
-                                Toast.LENGTH_SHORT).show();
-                        return false;
-                    }
-                }
-
-                File dst = new File(writeDir.getAbsolutePath()
-                        + "/collect.settings");
-                boolean success = AdminPreferencesActivity.saveSharedPreferencesToFile(dst, this);
-                if (success) {
-                    Toast.makeText(
-                            this,
-                            "Settings successfully written to "
-                                    + dst.getAbsolutePath(), Toast.LENGTH_LONG)
-                            .show();
-                } else {
-                    Toast.makeText(this,
-                            "Error writing settings to " + dst.getAbsolutePath(),
-                            Toast.LENGTH_LONG).show();
-                }
-                return true;
-
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    public static String ADMIN_PREFERENCES = "admin_prefs";
 
     public static boolean saveSharedPreferencesToFile(File dst, Context context) {
         // this should be in a thread if it gets big, but for now it's tiny
@@ -209,8 +61,6 @@ public class AdminPreferencesActivity extends AppPreferenceActivity {
             output.writeObject(adminPreferences.getAll());
 
             res = true;
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -268,5 +118,53 @@ public class AdminPreferencesActivity extends AppPreferenceActivity {
         }
 
         return mode;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new AdminPreferencesFragment()).commit();
+        setTitle(getString(R.string.admin_preferences));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Collect.getInstance().getActivityLogger()
+                .logAction(this, "onCreateOptionsMenu", "show");
+        super.onCreateOptionsMenu(menu);
+
+        menu
+                .add(0, SAVE_PREFS_MENU, 0, R.string.save_preferences)
+                .setIcon(R.drawable.ic_menu_save)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case SAVE_PREFS_MENU:
+                File writeDir = new File(Collect.SETTINGS);
+                if (!writeDir.exists()) {
+                    if (!writeDir.mkdirs()) {
+                        ToastUtils.showShortToast("Error creating directory "
+                                        + writeDir.getAbsolutePath());
+                        return false;
+                    }
+                }
+
+                File dst = new File(writeDir.getAbsolutePath()
+                        + "/collect.settings");
+                boolean success = AdminPreferencesActivity.saveSharedPreferencesToFile(dst, this);
+                if (success) {
+                    ToastUtils.showLongToast("Settings successfully written to "
+                            + dst.getAbsolutePath());
+                } else {
+                    ToastUtils.showLongToast("Error writing settings to " + dst.getAbsolutePath());
+                }
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ collect_app/src/main/java/org/odk/collect/android/preferences/PreferencesFragment.java
@@ -0,0 +1,385 @@
+package org.odk.collect.android.preferences;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.util.Log;
+
+import com.google.android.gms.analytics.GoogleAnalytics;
+
+import org.odk.collect.android.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.odk.collect.android.preferences.PreferenceKeys.*;
+
+
+public class PreferencesFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+    public static final String INTENT_KEY_ADMIN_MODE = "adminMode";
+    private static final String TAG = "PreferencesFragment";
+
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+
+        final boolean adminMode = getActivity().getIntent().getBooleanExtra(INTENT_KEY_ADMIN_MODE, false);
+
+        removeAllDisabledPrefs();
+
+        // ToDo: order these logically
+        initProtocolPrefs(adminMode);
+        initGoogleAccountPref();
+        initUserAndPasswordPrefs();
+        initNavigationPrefs();
+        initConstraintBehaviorPref();
+        initFontSizePref();
+        initAnalyticsPref();
+        initSplashPrefs();
+        initMapPrefs();
+
+    }
+
+
+    @Override
+    public void onResume() {
+        Log.d(TAG, "onResume");
+        super.onResume();
+
+        // has to go in onResume because it may get updated by
+        // a sub-preference screen
+        // this just keeps the widgets in sync
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+        ListPreference googleAccountPreference = (ListPreference) findPreference(KEY_SELECTED_GOOGLE_ACCOUNT);
+        if (googleAccountPreference != null) {
+            String account = sp.getString(KEY_SELECTED_GOOGLE_ACCOUNT, "");
+            googleAccountPreference.setSummary(account);
+            googleAccountPreference.setValue(account);
+        }
+
+        final EditTextPreference usernamePreference = (EditTextPreference) findPreference(KEY_USERNAME);
+        if (usernamePreference != null) {
+            String user = sp.getString(KEY_USERNAME, "");
+            usernamePreference.setSummary(user);
+            usernamePreference.setText(user);
+        }
+
+        final EditTextPreference passwordPreference = (EditTextPreference) findPreference(KEY_PASSWORD);
+        if (passwordPreference != null) {
+            String pw = sp.getString(KEY_PASSWORD, "");
+            if (pw.length() > 0) {
+                passwordPreference.setSummary("********");
+                passwordPreference.setText(pw);
+            }
+        }
+    }
+
+
+    private void removeAllDisabledPrefs() {
+        DisabledPreferencesRemover preferencesRemover = new DisabledPreferencesRemover((PreferencesActivity) getActivity(), this);
+        preferencesRemover.remove(AdminKeys.adminToGeneral);
+        preferencesRemover.removeEmptyCategories();
+    }
+
+    private void initAnalyticsPref() {
+        final CheckBoxPreference analyticsPreference = (CheckBoxPreference) findPreference(KEY_ANALYTICS);
+
+        if (analyticsPreference != null) {
+            analyticsPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    GoogleAnalytics googleAnalytics = GoogleAnalytics.getInstance(getActivity().getApplicationContext());
+                    googleAnalytics.setAppOptOut(!analyticsPreference.isChecked());
+                    return true;
+                }
+            });
+        }
+    }
+
+    private void initSplashPrefs() {
+        final PreferenceScreen pref = (PreferenceScreen) findPreference(KEY_SPLASH_PATH);
+
+        if (pref != null) {
+            pref.setOnPreferenceClickListener(new SplashClickListener((PreferencesActivity) getActivity(), pref));
+            pref.setSummary(pref.getSharedPreferences().getString(
+                    KEY_SPLASH_PATH, getString(R.string.default_splash_path)));
+        }
+    }
+
+    private void initFontSizePref() {
+        final ListPreference pref = (ListPreference) findPreference(KEY_FONT_SIZE);
+
+        if (pref != null) {
+            pref.setSummary(pref.getEntry());
+            pref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    int index = ((ListPreference) preference).findIndexOfValue(newValue.toString());
+                    CharSequence entry = ((ListPreference) preference).getEntries()[index];
+                    preference.setSummary(entry);
+                    return true;
+                }
+            });
+        }
+    }
+
+    private void initConstraintBehaviorPref() {
+        final ListPreference pref = (ListPreference) findPreference(KEY_CONSTRAINT_BEHAVIOR);
+
+        if (pref != null) {
+            pref.setSummary(pref.getEntry());
+            pref.setOnPreferenceChangeListener(
+                    new Preference.OnPreferenceChangeListener() {
+
+                        @Override
+                        public boolean onPreferenceChange(Preference preference, Object newValue) {
+                            int index = ((ListPreference) preference).findIndexOfValue(
+                                    newValue.toString());
+                            CharSequence entry = ((ListPreference) preference).getEntries()[index];
+                            preference.setSummary(entry);
+                            return true;
+                        }
+                    });
+        }
+    }
+
+    private void initMapPrefs() {
+        final ListPreference mapSdk = (ListPreference) findPreference(KEY_MAP_SDK);
+        final ListPreference mapBasemap = (ListPreference) findPreference(KEY_MAP_BASEMAP);
+
+        if (mapSdk == null || mapBasemap == null)
+            return;
+
+        mapSdk.setSummary(mapSdk.getEntry());
+        mapSdk.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+
+                int index = ((ListPreference) preference).findIndexOfValue(newValue.toString());
+                if (index == ARRAY_INDEX_GOOGLE_MAPS) {
+                    mapBasemap.setEntryValues(R.array.map_google_basemap_selector_entry_values);
+                    mapBasemap.setEntries(R.array.map_google_basemap_selector_entries);
+                    mapBasemap.setValue(GOOGLE_MAPS_BASEMAP_DEFAULT);
+                    mapBasemap.setSummary(mapBasemap.getEntry());
+                } else {
+                    // Else its OSM Maps
+                    mapBasemap.setEntryValues(R.array.map_osm_basemap_selector_entry_values);
+                    mapBasemap.setEntries(R.array.map_osm_basemap_selector_entries);
+                    mapBasemap.setValue(OSM_MAPS_BASEMAP_DEFAULT);
+                    mapBasemap.setSummary(mapBasemap.getEntry());
+                }
+
+                String entry = (String) ((ListPreference) preference).getEntries()[index];
+                preference.setSummary(entry);
+                return true;
+            }
+        });
+
+        if (mapSdk.getValue().equals(OSM_BASEMAP_KEY)) {
+            mapBasemap.setEntryValues(R.array.map_osm_basemap_selector_entry_values);
+            mapBasemap.setEntries(R.array.map_osm_basemap_selector_entries);
+        } else {
+            mapBasemap.setEntryValues(R.array.map_google_basemap_selector_entry_values);
+            mapBasemap.setEntries(R.array.map_google_basemap_selector_entries);
+        }
+        mapBasemap.setSummary(mapBasemap.getEntry());
+        mapBasemap.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                int index = ((ListPreference) preference).findIndexOfValue(newValue.toString());
+                String entry = (String) ((ListPreference) preference).getEntries()[index];
+                preference.setSummary(entry);
+                return true;
+            }
+        });
+    }
+
+    private void initNavigationPrefs() {
+        final ListPreference pref = (ListPreference) findPreference(KEY_NAVIGATION);
+
+        if (pref != null) {
+            pref.setSummary(pref.getEntry());
+            pref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    int index = ((ListPreference) preference).findIndexOfValue(newValue.toString());
+                    String entry = (String) ((ListPreference) preference).getEntries()[index];
+                    preference.setSummary(entry);
+                    return true;
+                }
+            });
+        }
+    }
+
+    private void initUserAndPasswordPrefs() {
+        final EditTextPreference userPref = (EditTextPreference) findPreference(KEY_USERNAME);
+
+        if (userPref != null) {
+            userPref.setOnPreferenceChangeListener(this);
+            userPref.setSummary(userPref.getText());
+            userPref.getEditText().setFilters(new InputFilter[]{getReturnFilter()});
+        }
+
+        final EditTextPreference passwordPref = (EditTextPreference) findPreference(KEY_PASSWORD);
+
+        if (passwordPref != null) {
+            passwordPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    String pw = newValue.toString();
+
+                    if (pw.length() > 0) {
+                        passwordPref.setSummary("********");
+                    } else {
+                        passwordPref.setSummary("");
+                    }
+                    return true;
+                }
+            });
+            if (passwordPref.getText() != null && passwordPref.getText().length() > 0) {
+                passwordPref.setSummary("********");
+            }
+            passwordPref.getEditText().setFilters(new InputFilter[]{getReturnFilter()});
+        }
+    }
+
+    private void initGoogleAccountPref() {
+        final ListPreference pref = (ListPreference) findPreference(KEY_SELECTED_GOOGLE_ACCOUNT);
+
+        if (pref == null)
+            return;
+
+        // get list of google accounts
+        // ToDo: this code is duplicated somewhere. Fix the duplication.
+        final List<String> accountEntries = new ArrayList<>();
+        final List<String> accountValues = new ArrayList<>();
+        final Account[] accounts = AccountManager.get(getActivity().getApplicationContext()).getAccountsByType(
+                "com.google");
+
+        for (Account account : accounts) {
+            accountEntries.add(account.name);
+            accountValues.add(account.name);
+        }
+        accountEntries.add(getString(R.string.no_account));
+        accountValues.add("");
+
+        pref.setEntries(accountEntries.toArray(new String[accountEntries.size()]));
+        pref.setEntryValues(accountValues.toArray(new String[accountValues.size()]));
+        pref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                int index = ((ListPreference) preference).findIndexOfValue(
+                        newValue.toString());
+                String value =
+                        (String) ((ListPreference) preference).getEntryValues()[index];
+                preference.setSummary(value);
+                return true;
+            }
+        });
+        pref.setSummary(pref.getValue());
+    }
+
+    private void initProtocolPrefs(final boolean adminMode) {
+        final ListPreference protocolPref = (ListPreference) findPreference(KEY_PROTOCOL);
+        final Preference settingsPref = findPreference(KEY_PROTOCOL_SETTINGS);
+
+        if (protocolPref == null || settingsPref == null)
+            return;
+
+        protocolPref.setSummary(protocolPref.getEntry());
+        setProtocolIntent(adminMode, protocolPref.getValue(), settingsPref);
+
+        protocolPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                ListPreference lpref = (ListPreference) preference;
+                String oldValue = lpref.getValue();
+                int index = lpref.findIndexOfValue(newValue.toString());
+                preference.setSummary(lpref.getEntries()[index]);
+
+                Intent prefIntent = setProtocolIntent(adminMode,
+                        lpref.getEntryValues()[index], settingsPref);
+
+                if (!newValue.equals(oldValue)) {
+                    startActivity(prefIntent);
+                }
+
+                return true;
+            }
+        });
+    }
+
+    private Intent setProtocolIntent(boolean adminMode, CharSequence value,
+                                     Preference protocolSettings) {
+        final Intent prefIntent;
+
+        if (value.equals(getString(R.string.protocol_odk_default))) {
+            setDefaultAggregatePaths();
+            prefIntent = new Intent(getActivity(), AggregatePreferencesActivity.class);
+        } else if (value.equals(
+                getString(R.string.protocol_google_sheets))) {
+            prefIntent = new Intent(getActivity(), GooglePreferencesActivity.class);
+        } else {
+            // other
+            prefIntent = new Intent(getActivity(), OtherPreferencesActivity.class);
+        }
+        prefIntent.putExtra(INTENT_KEY_ADMIN_MODE, adminMode);
+        protocolSettings.setIntent(prefIntent);
+        return prefIntent;
+    }
+
+    private void setDefaultAggregatePaths() {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString(KEY_FORMLIST_URL, getString(R.string.default_odk_formlist));
+        editor.putString(KEY_SUBMISSION_URL, getString(R.string.default_odk_submission));
+        editor.commit();
+    }
+
+
+    /**
+     * Disallows carriage returns from user entry
+     */
+    protected InputFilter getReturnFilter() {
+        return new InputFilter() {
+            public CharSequence filter(CharSequence source, int start, int end, Spanned dest,
+                                       int dstart, int dend) {
+                for (int i = start; i < end; i++) {
+                    if (Character.getType((source.charAt(i))) == Character.CONTROL) {
+                        return "";
+                    }
+                }
+                return null;
+            }
+        };
+    }
+
+
+    /**
+     * Generic listener that sets the summary to the newly selected/entered value
+     */
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        preference.setSummary((CharSequence) newValue);
+        return true;
+    }
+
+}
