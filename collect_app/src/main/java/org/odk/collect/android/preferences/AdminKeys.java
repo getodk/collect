7be1bd9ef435769edd24771b3fb81534d2d0ebb5
Rahul Yadav <rahuly247@gmail.com>
2017-03-23 01:20:18 +0530

    Add AdminPreferencesActivityTest (#617)

--- collect_app/src/main/java/org/odk/collect/android/preferences/AdminKeys.java
+++ collect_app/src/main/java/org/odk/collect/android/preferences/AdminKeys.java
@@ -24,33 +24,33 @@ public final class AdminKeys {
     public  static final String KEY_MARK_AS_FINALIZED         = "mark_as_finalized";
 
     // server
-            static final String KEY_CHANGE_ADMIN_PASSWORD     = "admin_password";
-            static final String KEY_CHANGE_GOOGLE_ACCOUNT     = "change_google_account";
-    private static final String KEY_CHANGE_SERVER             = "change_server";
-    private static final String KEY_CHANGE_USERNAME           = "change_username";
-    private static final String KEY_CHANGE_PASSWORD           = "change_password";
-    private static final String KEY_CHANGE_PROTOCOL_SETTINGS  = "change_protocol_settings";
+    static final String KEY_CHANGE_ADMIN_PASSWORD     = "admin_password";
+    static final String KEY_CHANGE_GOOGLE_ACCOUNT     = "change_google_account";
+    static final String KEY_CHANGE_SERVER             = "change_server";
+    static final String KEY_CHANGE_USERNAME           = "change_username";
+    static final String KEY_CHANGE_PASSWORD           = "change_password";
+    static final String KEY_CHANGE_PROTOCOL_SETTINGS  = "change_protocol_settings";
 
     // client
-            static final String KEY_FORM_PROCESSING_LOGIC     = "form_processing_logic";
+    static final String KEY_FORM_PROCESSING_LOGIC     = "form_processing_logic";
 
-    private static final String KEY_CHANGE_FONT_SIZE          = "change_font_size";
-    private static final String KEY_DEFAULT_TO_FINALIZED      = "default_to_finalized";
-    private static final String KEY_HIGH_RESOLUTION           = "high_resolution";
-    private static final String KEY_SHOW_SPLASH_SCREEN        = "show_splash_screen";
-    private static final String KEY_DELETE_AFTER_SEND         = "delete_after_send";
-    private static final String KEY_INSTANCE_FORM_SYNC        = "instance_form_sync";
+    static final String KEY_CHANGE_FONT_SIZE          = "change_font_size";
+    static final String KEY_DEFAULT_TO_FINALIZED      = "default_to_finalized";
+    static final String KEY_HIGH_RESOLUTION           = "high_resolution";
+    static final String KEY_SHOW_SPLASH_SCREEN        = "show_splash_screen";
+    static final String KEY_DELETE_AFTER_SEND         = "delete_after_send";
+    static final String KEY_INSTANCE_FORM_SYNC        = "instance_form_sync";
 
-    private static final String KEY_AUTOSEND_WIFI             = "autosend_wifi";
-    private static final String KEY_AUTOSEND_NETWORK          = "autosend_network";
+    static final String KEY_AUTOSEND_WIFI             = "autosend_wifi";
+    static final String KEY_AUTOSEND_NETWORK          = "autosend_network";
 
-    private static final String KEY_NAVIGATION                = "navigation";
-    private static final String KEY_CONSTRAINT_BEHAVIOR       = "constraint_behavior";
+    static final String KEY_NAVIGATION                = "navigation";
+    static final String KEY_CONSTRAINT_BEHAVIOR       = "constraint_behavior";
 
-    private static final String KEY_SHOW_MAP_SDK              = "show_map_sdk";
-    private static final String KEY_SHOW_MAP_BASEMAP          = "show_map_basemap";
+    static final String KEY_SHOW_MAP_SDK              = "show_map_sdk";
+    static final String KEY_SHOW_MAP_BASEMAP          = "show_map_basemap";
 
-    private static final String KEY_ANALYTICS                 = "analytics";
+    static final String KEY_ANALYTICS                 = "analytics";
 
     /**
      * The admin preferences allow removing general preferences. This array contains
---
+++ collect_app/src/test/java/org/odk/collect/android/preferences/AdminPreferencesActivityTest.java
@@ -0,0 +1,105 @@
+package org.odk.collect.android.preferences;
+
+import android.content.SharedPreferences;
+import android.preference.CheckBoxPreference;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.odk.collect.android.BuildConfig;
+import org.robolectric.ParameterizedRobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+import static org.robolectric.ParameterizedRobolectricTestRunner.Parameters;
+import static org.robolectric.util.FragmentTestUtil.startFragment;
+
+/**
+ * Unit test for checking {@link SharedPreferences}'s behaviour
+ */
+@Config(constants = BuildConfig.class)
+@RunWith(ParameterizedRobolectricTestRunner.class)
+public class AdminPreferencesActivityTest {
+
+    private String id;
+    private String adminKey;
+    private AdminPreferencesFragment adminPreferencesFragment;
+    private SharedPreferences sharedPreferences;
+
+    public AdminPreferencesActivityTest(String id, String adminKey) {
+        this.id = id;
+        this.adminKey = adminKey;
+    }
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                //Main menu preferences
+                {"edit_saved", AdminKeys.KEY_EDIT_SAVED},
+                {"send_finalized", AdminKeys.KEY_SEND_FINALIZED},
+                {"view_sent", AdminKeys.KEY_VIEW_SENT},
+                {"get_blank", AdminKeys.KEY_GET_BLANK},
+                {"delete_saved", AdminKeys.KEY_DELETE_SAVED},
+
+                //Change setting preferences
+                {"change_server", AdminKeys.KEY_CHANGE_SERVER},
+                {"change_protocol_settings", AdminKeys.KEY_CHANGE_PROTOCOL_SETTINGS},
+                {"change_username", AdminKeys.KEY_CHANGE_USERNAME},
+                {"change_password", AdminKeys.KEY_CHANGE_PASSWORD},
+                {"change_google_account", AdminKeys.KEY_CHANGE_GOOGLE_ACCOUNT},
+                {"autosend_wifi", AdminKeys.KEY_AUTOSEND_WIFI},
+                {"autosend_network", AdminKeys.KEY_AUTOSEND_NETWORK},
+                {"navigation", AdminKeys.KEY_NAVIGATION},
+                {"constraint_behavior", AdminKeys.KEY_CONSTRAINT_BEHAVIOR},
+                {"change_font_size", AdminKeys.KEY_CHANGE_FONT_SIZE},
+                {"instance_form_sync", AdminKeys.KEY_INSTANCE_FORM_SYNC},
+                {"default_to_finalized", AdminKeys.KEY_DEFAULT_TO_FINALIZED},
+                {"delete_after_send", AdminKeys.KEY_DELETE_AFTER_SEND},
+                {"high_resolution", AdminKeys.KEY_HIGH_RESOLUTION},
+                {"show_splash_screen", AdminKeys.KEY_SHOW_SPLASH_SCREEN},
+                {"show_map_sdk", AdminKeys.KEY_SHOW_MAP_SDK},
+                {"show_map_basemap", AdminKeys.KEY_SHOW_MAP_BASEMAP},
+                {"analytics", AdminKeys.KEY_ANALYTICS},
+
+                //Form entry preferences
+                {"access_settings", AdminKeys.KEY_ACCESS_SETTINGS},
+                {"change_language", AdminKeys.KEY_CHANGE_LANGUAGE},
+                {"jump_to", AdminKeys.KEY_JUMP_TO},
+                {"save_mid", AdminKeys.KEY_SAVE_MID},
+                {"save_as", AdminKeys.KEY_SAVE_AS},
+                {"mark_as_finalized", AdminKeys.KEY_MARK_AS_FINALIZED}
+        });
+    }
+
+    /**
+     * Runs {@link Before} each test.
+     */
+    @Before
+    public void setUp() throws Exception {
+        adminPreferencesFragment = new AdminPreferencesFragment();
+        startFragment(adminPreferencesFragment);
+
+        sharedPreferences = adminPreferencesFragment.getActivity().
+                getSharedPreferences(AdminPreferencesActivity.ADMIN_PREFERENCES, 0);
+    }
+
+    /**
+     * {@link Test} to assert admin setting shows and hides the associated
+     * {@link android.widget.CheckBox} in general settings
+     */
+    @Test
+    public void shouldUpdateAdminSharedPreferences() throws NullPointerException {
+        CheckBoxPreference checkBoxPreference = (CheckBoxPreference)
+                adminPreferencesFragment.findPreference(id);
+        checkBoxPreference.setChecked(true);
+        boolean actual = sharedPreferences.getBoolean(adminKey, false);
+        assertEquals("Error in preference " + adminKey, actual, true);
+
+        checkBoxPreference.setChecked(false);
+        actual = sharedPreferences.getBoolean(adminKey, true);
+        assertEquals("Error in preference " + adminKey, actual, false);
+    }
+}
