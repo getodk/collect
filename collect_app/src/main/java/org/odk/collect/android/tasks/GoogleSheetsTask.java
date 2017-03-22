68474546380919d4d7b64f31208005c2a9365b08
Shobhit Agarwal <shobhitagarwal1612@gmail.com>
2017-03-22 09:40:18 +0530

    Removes redundant SCOPE

--- collect_app/src/main/java/org/odk/collect/android/tasks/GoogleSheetsTask.java
+++ collect_app/src/main/java/org/odk/collect/android/tasks/GoogleSheetsTask.java
@@ -37,7 +37,7 @@ public abstract class GoogleSheetsTask<Params, Progress, Result> extends
     public static final int REQUEST_AUTHORIZATION = 1001;
     public static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
     public static final int REQUEST_PERMISSION_GET_ACCOUNTS = 1003;
-    public static final String[] SCOPES = {SheetsScopes.SPREADSHEETS, DriveScopes.DRIVE};
+    public static final String[] SCOPES = {DriveScopes.DRIVE};
     private final static String tag = "GoogleSheetsTask";
     protected String mGoogleUserName = null;
     protected com.google.api.services.sheets.v4.Sheets mSheetsService = null;
