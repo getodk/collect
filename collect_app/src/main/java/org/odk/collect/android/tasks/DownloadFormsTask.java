bd9a7dafc771f0ca8d0b64c8bd177ff41764f5d3
Shobhit Agarwal <shobhitagarwal1612@gmail.com>
2017-04-08 02:54:53 +0530

    Replaces if with switch

--- collect_app/src/main/java/org/odk/collect/android/tasks/DownloadFormsTask.java
+++ collect_app/src/main/java/org/odk/collect/android/tasks/DownloadFormsTask.java
@@ -625,21 +625,25 @@ public class DownloadFormsTask extends
                         continue;
                     }
                     String tag = child.getName();
-                    if (tag.equals("filename")) {
-                        filename = XFormParser.getXMLText(child, true);
-                        if (filename != null && filename.length() == 0) {
-                            filename = null;
-                        }
-                    } else if (tag.equals("hash")) {
-                        hash = XFormParser.getXMLText(child, true);
-                        if (hash != null && hash.length() == 0) {
-                            hash = null;
-                        }
-                    } else if (tag.equals("downloadUrl")) {
-                        downloadUrl = XFormParser.getXMLText(child, true);
-                        if (downloadUrl != null && downloadUrl.length() == 0) {
-                            downloadUrl = null;
-                        }
+                    switch (tag) {
+                        case "filename":
+                            filename = XFormParser.getXMLText(child, true);
+                            if (filename != null && filename.length() == 0) {
+                                filename = null;
+                            }
+                            break;
+                        case "hash":
+                            hash = XFormParser.getXMLText(child, true);
+                            if (hash != null && hash.length() == 0) {
+                                hash = null;
+                            }
+                            break;
+                        case "downloadUrl":
+                            downloadUrl = XFormParser.getXMLText(child, true);
+                            if (downloadUrl != null && downloadUrl.length() == 0) {
+                                downloadUrl = null;
+                            }
+                            break;
                     }
                 }
                 if (filename == null || downloadUrl == null || hash == null) {
