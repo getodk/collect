c776c43919032fde03b3c901c7bf9a6b3cb13d89
Sergei Novikov <sanovikov71@gmail.com>
2017-04-08 04:06:36 +0400

    A custom ActivityTestRule is introduced. Fix #796. (#848)
    Change-Id: I3da82cbc79163d11a7d4a70682bad074e586a90a

--- collect_app/src/androidTest/java/org/odk/collect/android/externalintents/InstanceChooserListTest.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/externalintents/InstanceChooserListTest.java
@@ -1,10 +1,8 @@
 package org.odk.collect.android.externalintents;
 
-import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,25 +10,14 @@ import org.odk.collect.android.activities.InstanceChooserList;
 
 import java.io.IOException;
 
-import static org.odk.collect.android.externalintents.ImplicitActivitiesUtils.clearDirectories;
-import static org.odk.collect.android.externalintents.ImplicitActivitiesUtils.testDirectories;
+import static org.odk.collect.android.externalintents.ExportedActivitiesUtils.testDirectories;
 
 @RunWith(AndroidJUnit4.class)
 public class InstanceChooserListTest {
 
     @Rule
     public ActivityTestRule<InstanceChooserList> mInstanceChooserListRule =
-            new ActivityTestRule<>(InstanceChooserList.class, false, false);
-
-    @Before
-    public void setUp() {
-
-        clearDirectories();
-
-        Intent intent = new Intent();
-        mInstanceChooserListRule.launchActivity(intent);
-
-    }
+            new ExportedActivityTestRule<>(InstanceChooserList.class);
 
     @Test
     public void instanceChooserListMakesDirsTest() throws IOException {
