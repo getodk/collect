11635bcd48b632c59160d75c38f978156751077f
lognaturel <lognaturel@gmail.com>
2017-04-10 12:36:05 -0700

    Don't log user recoverable error.

--- collect_app/src/main/java/org/odk/collect/android/activities/GoogleSheetsUploaderActivity.java
+++ collect_app/src/main/java/org/odk/collect/android/activities/GoogleSheetsUploaderActivity.java
@@ -567,7 +567,6 @@ public class GoogleSheetsUploaderActivity extends Activity implements InstanceUp
                 getIDOfFolderWithName(GOOGLE_DRIVE_ROOT_FOLDER, null);
                 uploadInstances(selection, selectionArgs, token);
             } catch (UserRecoverableAuthException e) {
-                Timber.e(e, "Authorization requested.. The reason uploading instances failed is due to: %s ", e.getMessage());
                 mResults = null;
                 startActivityForResult(e.getIntent(), REQUEST_AUTHORIZATION);
             } catch (IOException | GoogleAuthException e) {
