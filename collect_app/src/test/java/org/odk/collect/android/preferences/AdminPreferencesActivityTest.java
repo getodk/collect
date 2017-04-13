9ba1c9e4be4dc3200a9ca48f9c20bc2f876cfa98
Neil Penman <neilpenman@gmail.com>
2017-04-13 17:14:21 +1000

    undo bad merge

--- collect_app/src/test/java/org/odk/collect/android/preferences/AdminPreferencesActivityTest.java
+++ collect_app/src/test/java/org/odk/collect/android/preferences/AdminPreferencesActivityTest.java
@@ -24,56 +24,6 @@ public class AdminPreferencesActivityTest {
     private AdminPreferencesFragment adminPreferencesFragment;
     private SharedPreferences sharedPreferences;
 
-    public AdminPreferencesActivityTest(String id, String adminKey) {
-        this.id = id;
-        this.adminKey = adminKey;
-    }
-
-    @Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                //Main menu preferences
-                {"edit_saved", AdminKeys.KEY_EDIT_SAVED},
-                {"send_finalized", AdminKeys.KEY_SEND_FINALIZED},
-                {"view_sent", AdminKeys.KEY_VIEW_SENT},
-                {"get_blank", AdminKeys.KEY_GET_BLANK},
-                {"delete_saved", AdminKeys.KEY_DELETE_SAVED},
-
-                //Change setting preferences
-                {"change_server", AdminKeys.KEY_CHANGE_SERVER},
-                {"change_protocol_settings", AdminKeys.KEY_CHANGE_PROTOCOL_SETTINGS},
-                {"change_username", AdminKeys.KEY_CHANGE_USERNAME},
-                {"change_password", AdminKeys.KEY_CHANGE_PASSWORD},
-                {"change_google_account", AdminKeys.KEY_CHANGE_GOOGLE_ACCOUNT},
-                {"autosend_wifi", AdminKeys.KEY_AUTOSEND_WIFI},
-                {"autosend_network", AdminKeys.KEY_AUTOSEND_NETWORK},
-                {"navigation", AdminKeys.KEY_NAVIGATION},
-                {"constraint_behavior", AdminKeys.KEY_CONSTRAINT_BEHAVIOR},
-                {"change_font_size", AdminKeys.KEY_CHANGE_FONT_SIZE},
-                {"instance_form_sync", AdminKeys.KEY_INSTANCE_FORM_SYNC},
-                {"default_to_finalized", AdminKeys.KEY_DEFAULT_TO_FINALIZED},
-                {"delete_after_send", AdminKeys.KEY_DELETE_AFTER_SEND},
-                {"high_resolution", AdminKeys.KEY_HIGH_RESOLUTION},
-                {"show_splash_screen", AdminKeys.KEY_SHOW_SPLASH_SCREEN},
-                {"show_map_sdk", AdminKeys.KEY_SHOW_MAP_SDK},
-                {"show_map_basemap", AdminKeys.KEY_SHOW_MAP_BASEMAP},
-                {"analytics", AdminKeys.KEY_ANALYTICS},
-
-                //Form entry preferences
-                {"access_settings", AdminKeys.KEY_ACCESS_SETTINGS},
-                {"change_language", AdminKeys.KEY_CHANGE_LANGUAGE},
-                {"jump_to", AdminKeys.KEY_JUMP_TO},
-                {"save_mid", AdminKeys.KEY_SAVE_MID},
-                {"save_as", AdminKeys.KEY_SAVE_AS},
-                {"mark_as_finalized", AdminKeys.KEY_MARK_AS_FINALIZED},
-
-                {"timer_log", AdminKeys.KEY_TIMER_LOG_ENABLED}
-        });
-    }
-
-    /**
-     * Runs {@link Before} each test.
-     */
     @Before
     public void setUp() throws Exception {
         adminPreferencesFragment = new AdminPreferencesFragment();
