fb45d602196b487de2aa86a4e8114ef128428544
lognaturel <lognaturel@gmail.com>
2017-04-21 16:22:11 -0700

    Suppress failing tests.

--- collect_app/src/androidTest/java/org/odk/collect/android/externalintents/InstanceChooserListTest.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/externalintents/InstanceChooserListTest.java
@@ -1,5 +1,6 @@
 package org.odk.collect.android.externalintents;
 
+import android.support.test.filters.Suppress;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -12,6 +13,8 @@ import java.io.IOException;
 
 import static org.odk.collect.android.externalintents.ExportedActivitiesUtils.testDirectories;
 
+@Suppress
+// Frequent failures: https://github.com/opendatakit/collect/issues/796
 @RunWith(AndroidJUnit4.class)
 public class InstanceChooserListTest {
 
