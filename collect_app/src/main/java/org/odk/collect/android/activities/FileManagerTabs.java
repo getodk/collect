8c8ca00b9dfc23dfb3ea9a70205bd578bda87716
Shobhit Agarwal <shobhitagarwal1612@gmail.com>
2017-04-24 17:33:00 +0530

    Removes unused imports

--- collect_app/src/main/java/org/odk/collect/android/activities/FileManagerTabs.java
+++ collect_app/src/main/java/org/odk/collect/android/activities/FileManagerTabs.java
@@ -17,7 +17,6 @@ package org.odk.collect.android.activities;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
--- collect_app/src/main/java/org/odk/collect/android/activities/InstanceChooserList.java
+++ collect_app/src/main/java/org/odk/collect/android/activities/InstanceChooserList.java
@@ -24,7 +24,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
