783c6dc1e70827de7fca0d8ed7ab8a88f8d02017
Alex Anderson <anderson@medicmobile.org>
2017-04-24 22:22:22 +0200

    Unsuppress InstanceUploaderTaskTest

--- collect_app/src/androidTest/java/org/odk/collect/android/tasks/InstanceUploaderTaskTest.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/tasks/InstanceUploaderTaskTest.java
@@ -1,7 +1,6 @@
 package org.odk.collect.android.tasks;
 
 import android.net.Uri;
-import android.support.test.filters.Suppress;
 
 import java.io.File;
 
@@ -25,8 +24,6 @@ import static org.odk.collect.android.test.MockedServerTestUtils.willRespond;
 import static org.odk.collect.android.test.TestUtils.assertMatches;
 import static org.odk.collect.android.test.TestUtils.createTempFile;
 
-@Suppress
-// Pending fix https://github.com/opendatakit/collect/pull/930
 public class InstanceUploaderTaskTest {
     private InstancesDao dao;
     private MockWebServer server;
