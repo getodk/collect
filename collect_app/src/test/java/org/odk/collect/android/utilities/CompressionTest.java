1382c10831bcc5af89058e1fa3046d77316755b4
Shobhit Agarwal <shobhitagarwal1612@gmail.com>
2017-04-25 23:29:57 +0530

    Fixes checkstyle

--- collect_app/src/test/java/org/odk/collect/android/utilities/CompressionTest.java
+++ collect_app/src/test/java/org/odk/collect/android/utilities/CompressionTest.java
@@ -21,12 +21,12 @@ public class CompressionTest {
 
     @Before
     public void setUp() {
-        text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor " +
-                "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis " +
-                "nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
-                "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore " +
-                "eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt " +
-                "in culpa qui officia deserunt mollit anim id est laborum.";
+        text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor "
+                + "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis "
+                + "nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+                + "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore "
+                + "eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt "
+                + "in culpa qui officia deserunt mollit anim id est laborum.";
     }
 
     @After
