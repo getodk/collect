467a580a448e2695bf36495475b26eb11f410ed4
Shobhit Agarwal <shobhitagarwal1612@gmail.com>
2017-05-25 09:35:13 +0530

    Refactor shared preferences (#1036)
    * Refactors shared preferences
    
    * Removes unnecessary loop
    
    * Copy keySets so they can be modified.
    
    * Add missing general preferences to QR code.

--- collect_app/src/main/java/org/odk/collect/android/preferences/PreferenceKeys.java
+++ collect_app/src/main/java/org/odk/collect/android/preferences/PreferenceKeys.java
@@ -1,7 +1,9 @@
 package org.odk.collect.android.preferences;
 
-import java.util.Arrays;
-import java.util.Collection;
+import org.odk.collect.android.R;
+import org.odk.collect.android.application.Collect;
+
+import java.util.HashMap;
 
 public final class PreferenceKeys {
     public static final String KEY_LAST_VERSION             = "lastVersion";
@@ -67,36 +69,35 @@ public final class PreferenceKeys {
            static final String GOOGLE_MAPS_BASEMAP_DEFAULT  = "streets";
            static final String OSM_MAPS_BASEMAP_DEFAULT     = "mapquest_streets";
 
-    /**
-     * General keys
-     */
-    public static final Collection<String> ALL_GENERAL_KEYS = Arrays.asList(
-            KEY_SHOW_SPLASH,
-            KEY_SPLASH_PATH,
-            KEY_FONT_SIZE,
-            KEY_DELETE_AFTER_SEND,
-            KEY_INSTANCE_SYNC,
-            KEY_PROTOCOL,
-            KEY_PROTOCOL_SETTINGS,
-            KEY_SELECTED_GOOGLE_ACCOUNT,
-            KEY_USERNAME,
-            KEY_FORM_METADATA,
-            KEY_METADATA_USERNAME,
-            KEY_METADATA_PHONENUMBER,
-            KEY_METADATA_EMAIL,
-            KEY_SERVER_URL,
-            KEY_GOOGLE_SHEETS_URL,
-            KEY_FORMLIST_URL,
-            KEY_SUBMISSION_URL,
-            KEY_NAVIGATION,
-            KEY_CONSTRAINT_BEHAVIOR,
-            KEY_COMPLETED_DEFAULT,
-            KEY_MAP_SDK,
-            KEY_MAP_BASEMAP,
-            OSM_BASEMAP_KEY,
-            GOOGLE_MAPS_BASEMAP_DEFAULT,
-            OSM_MAPS_BASEMAP_DEFAULT,
-            KEY_AUTOSEND_WIFI,
-            KEY_AUTOSEND_NETWORK
-    );
+    private static HashMap<String, Object> getHashMap() {
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put(KEY_SHOW_SPLASH, false);
+        hashMap.put(KEY_SPLASH_PATH, Collect.getInstance().getString(R.string.default_splash_path));
+        hashMap.put(KEY_FONT_SIZE, "21");
+        hashMap.put(KEY_DELETE_AFTER_SEND, false);
+        hashMap.put(KEY_ANALYTICS, true);
+        hashMap.put(KEY_INSTANCE_SYNC, true);
+        hashMap.put(KEY_APP_LANGUAGE, "");
+        hashMap.put(KEY_PROTOCOL, Collect.getInstance().getString(R.string.protocol_odk_default));
+        hashMap.put(KEY_SELECTED_GOOGLE_ACCOUNT, "");
+        hashMap.put(KEY_USERNAME, "");
+        hashMap.put(KEY_METADATA_USERNAME, "");
+        hashMap.put(KEY_METADATA_PHONENUMBER, "");
+        hashMap.put(KEY_METADATA_EMAIL, "");
+        hashMap.put(KEY_SERVER_URL, Collect.getInstance().getString(R.string.default_server_url));
+        hashMap.put(KEY_GOOGLE_SHEETS_URL, "");
+        hashMap.put(KEY_FORMLIST_URL, Collect.getInstance().getString(R.string.default_odk_formlist));
+        hashMap.put(KEY_SUBMISSION_URL, Collect.getInstance().getString(R.string.default_odk_submission));
+        hashMap.put(KEY_NAVIGATION, "swipe");
+        hashMap.put(KEY_CONSTRAINT_BEHAVIOR, "on_swipe");
+        hashMap.put(KEY_COMPLETED_DEFAULT, true);
+        hashMap.put(KEY_MAP_SDK, "google_maps");
+        hashMap.put(KEY_MAP_BASEMAP, "streets");
+        hashMap.put(KEY_AUTOSEND_WIFI, false);
+        hashMap.put(KEY_AUTOSEND_NETWORK, false);
+        return hashMap;
+    }
+
+    public static final HashMap<String, Object> GENERAL_KEYS = getHashMap();
+
 }
