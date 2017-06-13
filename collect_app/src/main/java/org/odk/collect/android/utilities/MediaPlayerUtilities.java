33f89bce64fddab941c7d16337a0d40724415774
Shobhit Agarwal <shobhitagarwal1612@gmail.com>
2017-06-14 01:09:24 +0530

    Audio widget (#1145)
    * Adds xml layout for audio widget
    
    * Adds new icons
    
    * Adds media player and layout
    
    * Adds utility class
    
    * Bugs fixed
    
    * Adds fast forward and fast rewind buttons
    Removes media name label
    
    * Bugs fixed
    
    * Adds media controller class and refactors code
    
    * Improves media player layout
    
    * Bugs fixed
    Removes unused methods
    
    * Removes unused string resources
    
    * Replaces add with paperclip icon
    
    * disables swiping when handling the seekbar
    
    * Updates time elapsed and pauses audio on scrubbing
    
    * Updates timer and seekBar on fast forwarding and fast rewinding in pause state
    
    * Renames controller class
    
    * Bugs fixed

---
+++ collect_app/src/main/java/org/odk/collect/android/utilities/MediaPlayerUtilities.java
@@ -0,0 +1,35 @@
+package org.odk.collect.android.utilities;
+
+public class MediaPlayerUtilities {
+
+    /**
+     * Function to convert milliseconds time to
+     * Timer Format
+     * Hours:Minutes:Seconds
+     */
+    public static String milliSecondsToTimer(long milliseconds) {
+        String finalTimerString = "";
+        String secondsString = "";
+
+        // Convert total duration into time
+        int hours = (int) (milliseconds / (1000 * 60 * 60));
+        int minutes = (int) (milliseconds % (1000 * 60 * 60)) / (1000 * 60);
+        int seconds = (int) ((milliseconds % (1000 * 60 * 60)) % (1000 * 60) / 1000);
+        // Add hours if there
+        if (hours > 0) {
+            finalTimerString = hours + ":";
+        }
+
+        // Prepending 0 to seconds if it is one digit
+        if (seconds < 10) {
+            secondsString = "0" + seconds;
+        } else {
+            secondsString = "" + seconds;
+        }
+
+        finalTimerString = finalTimerString + minutes + ":" + secondsString;
+
+        // return timer string
+        return finalTimerString;
+    }
+}
