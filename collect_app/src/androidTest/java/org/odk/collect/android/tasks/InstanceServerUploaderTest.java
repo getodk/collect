22e942f1155a87b93e55d8307b43d0dd61355b63
Hélène Martin <lognaturel@gmail.com>
2017-07-18 15:22:10 -0700

    Import Outcome from InstanceUploader base class. (#1261)

--- collect_app/src/androidTest/java/org/odk/collect/android/tasks/InstanceServerUploaderTest.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/tasks/InstanceServerUploaderTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 import org.odk.collect.android.dao.InstancesDao;
 import org.odk.collect.android.dto.Instance;
 import org.odk.collect.android.provider.InstanceProviderAPI;
-import org.odk.collect.android.tasks.InstanceServerUploader.Outcome;
+import org.odk.collect.android.tasks.InstanceUploader.Outcome;
 import org.odk.collect.android.test.MockedServerTest;
 
 import okhttp3.mockwebserver.RecordedRequest;
