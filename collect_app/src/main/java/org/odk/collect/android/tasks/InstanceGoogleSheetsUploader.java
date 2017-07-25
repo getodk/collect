009b67dcb77247e89fe9c030f72e1e815da86f34
Grzegorz Orczykowski <gorczykowski@soldevelo.com>
2017-07-26 01:14:28 +0200

    Log known non-fatal exceptions at info level (#1250)

--- collect_app/src/main/java/org/odk/collect/android/tasks/InstanceGoogleSheetsUploader.java
+++ collect_app/src/main/java/org/odk/collect/android/tasks/InstanceGoogleSheetsUploader.java
@@ -187,7 +187,7 @@ public class InstanceGoogleSheetsUploader extends InstanceUploader {
             try {
                 spreadsheetId = UrlUtils.getSpreadsheetID(id);
             } catch (BadUrlException e) {
-                Timber.e(e);
+                Timber.i(e);
                 outcome.results.put(id, e.getMessage());
                 return false;
             }
