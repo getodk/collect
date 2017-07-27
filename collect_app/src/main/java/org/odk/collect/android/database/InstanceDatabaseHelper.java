463ba9acb2a213c604b8606b697e633bad6fb925
grzegorz <gorczykowski@soldevelo.com>
2017-07-27 13:33:17 +0200

    Refactored InstanceDatabaseHelper

--- collect_app/src/main/java/org/odk/collect/android/database/InstanceDatabaseHelper.java
+++ collect_app/src/main/java/org/odk/collect/android/database/InstanceDatabaseHelper.java
@@ -18,6 +18,7 @@ package org.odk.collect.android.database;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 
 import org.odk.collect.android.application.Collect;
 import org.odk.collect.android.provider.InstanceProviderAPI;
@@ -32,14 +33,93 @@ import static org.odk.collect.android.provider.InstanceProvider.INSTANCES_TABLE_
 public class InstanceDatabaseHelper extends ODKSQLiteOpenHelper {
     private static final int DATABASE_VERSION = 4;
 
-
     public InstanceDatabaseHelper(String databaseName) {
         super(Collect.METADATA_PATH, databaseName, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + INSTANCES_TABLE_NAME + " ("
+        createTablesForVersion4(db, INSTANCES_TABLE_NAME);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Timber.i("Upgrading database from version %d to %d" + ", which will destroy all old data", oldVersion, newVersion);
+
+        boolean success = true;
+        switch (oldVersion) {
+            case 1:
+                success = upgradeToVersion2(db);
+            case 2:
+                success &= upgradeToVersion3(db);
+            case 3:
+                success &= upgradeToVersion4(db);
+                break;
+            default:
+                Timber.i("Unknown version " + newVersion);
+        }
+
+        if (success) {
+            Timber.i("Upgrading database from version " + oldVersion + " to " + newVersion + " completed with success.");
+        } else {
+            Timber.i("Upgrading database from version " + oldVersion + " to " + newVersion + " failed.");
+        }
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+
+    private boolean upgradeToVersion2(SQLiteDatabase db) {
+        boolean success = true;
+        try {
+            db.execSQL("ALTER TABLE " + INSTANCES_TABLE_NAME + " ADD COLUMN "
+                    + InstanceProviderAPI.InstanceColumns.CAN_EDIT_WHEN_COMPLETE + " text;");
+            db.execSQL("UPDATE " + INSTANCES_TABLE_NAME + " SET "
+                    + InstanceProviderAPI.InstanceColumns.CAN_EDIT_WHEN_COMPLETE + " = '" + Boolean.toString(true)
+                    + "' WHERE " + InstanceProviderAPI.InstanceColumns.STATUS + " IS NOT NULL AND "
+                    + InstanceProviderAPI.InstanceColumns.STATUS + " != '" + InstanceProviderAPI.STATUS_INCOMPLETE
+                    + "'");
+        } catch (SQLiteException e) {
+            Timber.i(e);
+            success = false;
+        }
+        return success;
+    }
+
+    private boolean upgradeToVersion3(SQLiteDatabase db) {
+        boolean success = true;
+        try {
+            db.execSQL("ALTER TABLE " + INSTANCES_TABLE_NAME + " ADD COLUMN "
+                    + InstanceProviderAPI.InstanceColumns.JR_VERSION + " text;");
+        } catch (SQLiteException e) {
+            Timber.i(e);
+            success = false;
+        }
+        return success;
+    }
+
+    private boolean upgradeToVersion4(SQLiteDatabase db) {
+        boolean success = true;
+        try {
+            Cursor cursor = db.rawQuery("SELECT * FROM " + INSTANCES_TABLE_NAME + " LIMIT 0", null);
+            int columnIndex = cursor.getColumnIndex(InstanceProviderAPI.InstanceColumns.DELETED_DATE);
+            cursor.close();
+
+            // Only add the column if it doesn't already exist
+            if (columnIndex == -1) {
+                db.execSQL("ALTER TABLE " + INSTANCES_TABLE_NAME + " ADD COLUMN "
+                        + InstanceProviderAPI.InstanceColumns.DELETED_DATE + " date;");
+            }
+        } catch (SQLiteException e) {
+            Timber.i(e);
+            success = false;
+        }
+        return success;
+    }
+
+    private void createTablesForVersion4(SQLiteDatabase db, String tableName) {
+        db.execSQL("CREATE TABLE " + tableName + " ("
                 + InstanceProviderAPI.InstanceColumns._ID + " integer primary key, "
                 + InstanceProviderAPI.InstanceColumns.DISPLAY_NAME + " text not null, "
                 + InstanceProviderAPI.InstanceColumns.SUBMISSION_URI + " text, "
@@ -51,41 +131,5 @@ public class InstanceDatabaseHelper extends ODKSQLiteOpenHelper {
                 + InstanceProviderAPI.InstanceColumns.LAST_STATUS_CHANGE_DATE + " date not null, "
                 + InstanceProviderAPI.InstanceColumns.DISPLAY_SUBTEXT + " text not null,"
                 + InstanceProviderAPI.InstanceColumns.DELETED_DATE + " date );");
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        final int initialVersion = oldVersion;
-        if (oldVersion == 1) {
-            db.execSQL("ALTER TABLE " + INSTANCES_TABLE_NAME + " ADD COLUMN "
-                    + InstanceProviderAPI.InstanceColumns.CAN_EDIT_WHEN_COMPLETE + " text;");
-            db.execSQL("UPDATE " + INSTANCES_TABLE_NAME + " SET "
-                    + InstanceProviderAPI.InstanceColumns.CAN_EDIT_WHEN_COMPLETE + " = '" + Boolean.toString(true)
-                    + "' WHERE " + InstanceProviderAPI.InstanceColumns.STATUS + " IS NOT NULL AND "
-                    + InstanceProviderAPI.InstanceColumns.STATUS + " != '" + InstanceProviderAPI.STATUS_INCOMPLETE
-                    + "'");
-            oldVersion = 2;
-        }
-        if (oldVersion == 2) {
-            db.execSQL("ALTER TABLE " + INSTANCES_TABLE_NAME + " ADD COLUMN "
-                    + InstanceProviderAPI.InstanceColumns.JR_VERSION + " text;");
-        }
-        if (oldVersion == 3) {
-            Cursor cursor = db.rawQuery("SELECT * FROM " + INSTANCES_TABLE_NAME + " LIMIT 0", null);
-            int columnIndex = cursor.getColumnIndex(InstanceProviderAPI.InstanceColumns.DELETED_DATE);
-            cursor.close();
-
-            // Only add the column if it doesn't already exist
-            if (columnIndex == -1) {
-                db.execSQL("ALTER TABLE " + INSTANCES_TABLE_NAME + " ADD COLUMN "
-                        + InstanceProviderAPI.InstanceColumns.DELETED_DATE + " date;");
-            }
-        }
-        Timber.w("Successfully upgraded database from version %d to %d, without destroying all the old data",
-                initialVersion, newVersion);
-    }
-
-    @Override
-    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
     }
 }
