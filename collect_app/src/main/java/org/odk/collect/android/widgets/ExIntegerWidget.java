caf47d6629192a0a2fcf03cff2f7a80a8fccb503
Grzegorz Orczykowski <gorczykowski@soldevelo.com>
2017-07-31 22:59:44 +0200

    Fix external integer widget value formatting error (#1283)

--- collect_app/src/main/java/org/odk/collect/android/widgets/ExIntegerWidget.java
+++ collect_app/src/main/java/org/odk/collect/android/widgets/ExIntegerWidget.java
@@ -31,7 +31,6 @@ import org.odk.collect.android.external.ExternalAppsUtils;
 
 import java.util.Locale;
 
-
 /**
  * Launch an external app to supply an integer value. If the app
  * does not launch, enable the text area for regular data entry.
@@ -74,7 +73,7 @@ public class ExIntegerWidget extends ExStringWidget {
         Integer i = getIntegerAnswerValue();
 
         if (i != null) {
-            answer.setText(String.format(Locale.getDefault(), "%d", i.toString()));
+            answer.setText(String.format(Locale.US, "%d", i));
         }
     }
 
