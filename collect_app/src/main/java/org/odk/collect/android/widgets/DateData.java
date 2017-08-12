954c8a792c42afd39b6e66ae2f571ff3c7d0e02b
deepak-thakur <mr.thakurdeepak@rediffmail.com>
2017-08-12 23:46:43 +0530

    solved 16 errors in the Date Widget Class.

---
+++ collect_app/src/main/java/org/odk/collect/android/widgets/DateData.java
@@ -0,0 +1,11 @@
+package org.odk.collect.android.widgets;
+
+/**
+ * Created by GhOst -ProtocoL on 12/08/2017.
+ */
+
+class DateData extends IAnswerData {
+    public DateData(Object toDate) {
+        super();
+    }
+}
---
+++ collect_app/src/main/java/org/odk/collect/android/widgets/LocalDateTime.java
@@ -0,0 +1,13 @@
+package org.odk.collect.android.widgets;
+
+/**
+ * Created by GhOst -ProtocoL on 12/08/2017.
+ */
+
+class LocalDateTime {
+    public void withYear(int year) {
+    }
+
+    public Object toDate() {
+    }
+}
