3a6061e5bb99d93a456619cb12830070e458aaa2
James Knight <james@jknight.co>
2017-09-27 18:41:25 +0200

    Checkstyle fixes.

--- collect_app/src/main/java/org/odk/collect/android/utilities/ViewUtil.java
+++ collect_app/src/main/java/org/odk/collect/android/utilities/ViewUtil.java
@@ -23,7 +23,10 @@ public class ViewUtil {
             final int result = sNextGeneratedId.get();
             // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
             int newValue = result + 1;
-            if (newValue > 0x00FFFFFF) newValue = 1; // Roll over to 1, not 0.
+            if (newValue > 0x00FFFFFF) {
+                newValue = 1; // Roll over to 1, not 0.
+            }
+
             if (sNextGeneratedId.compareAndSet(result, newValue)) {
                 return result;
             }
