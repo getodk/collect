12ba5011b7ce2d27cfe10b1aeb368f09711c2c9b
James Knight <james@jknight.co>
2017-10-17 17:11:25 +0200

    Loading forms into test, now just need to figure out how to best test these ID-less Views.

--- collect_app/src/androidTest/java/org/odk/collect/android/AllFormsWidgetTest.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/AllFormsWidgetTest.java
@@ -2,37 +2,60 @@ package org.odk.collect.android;
 
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
+import android.content.res.AssetManager;
+import android.os.Environment;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.view.View;
-import android.widget.FrameLayout;
 
-import org.hamcrest.Description;
-import org.junit.Before;
+import org.apache.commons.io.IOUtils;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.odk.collect.android.activities.FormEntryActivity;
+import org.odk.collect.android.views.ODKView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static org.hamcrest.Matchers.is;
+import static org.odk.collect.android.activities.FormEntryActivity.EXTRA_TESTING_PATH;
 
 @RunWith(AndroidJUnit4.class)
 public class AllFormsWidgetTest {
+
     @Rule
     public FormEntryActivityTestRule activityTestRule = new FormEntryActivityTestRule();
 
-    private FormEntryActivity activity;
+    @BeforeClass
+    public static void copyFormToSdCard() throws IOException {
+        Context instrumentationContext = InstrumentationRegistry.getContext();
 
-    @Before
-    public void setupActivity() {
-        activity = activityTestRule.getActivity();
+        AssetManager assetManager = instrumentationContext.getAssets();
+        InputStream inputStream = assetManager.open("all_widgets.xml");
+
+        String path = Environment.getExternalStorageDirectory().getPath() + "/odk/forms/all_widgets.xml";
+        File outFile = new File(path);
+
+        OutputStream outputStream = new FileOutputStream(outFile);
+        IOUtils.copy(inputStream, outputStream);
     }
 
     @Test
     public void testActivityOpen()  {
+        onView(withClassName(is(ODKView.class.toString()))).perform(swipeLeft());
+        onView(withClassName(is(ODKView.class.toString()))).perform(swipeLeft());
+        onView(withClassName(is(ODKView.class.toString()))).perform(swipeLeft());
+        onView(withClassName(is(ODKView.class.toString()))).perform(swipeLeft());
+        onView(withClassName(is(ODKView.class.toString()))).perform(swipeLeft());
+        onView(withClassName(is(ODKView.class.toString()))).perform(swipeLeft());
     }
 
     private class FormEntryActivityTestRule extends ActivityTestRule<FormEntryActivity> {
@@ -46,10 +69,15 @@ public class AllFormsWidgetTest {
             Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
             Intent intent = new Intent(context, FormEntryActivity.class);
 
-            Uri formUri = Uri.parse("file:///android_asset/all_widgets.xml");
-            intent.setData(formUri);
+            String path = Environment.getExternalStorageDirectory().getPath() + "/odk/forms/all_widgets.xml";
+            intent.putExtra(EXTRA_TESTING_PATH, path);
 
             return intent;
+        }
+
+        @Override
+        protected void afterActivityLaunched() {
+            super.afterActivityLaunched();
         }
     }
 
