1fe271dd9633094b589a9925f7dddf454a040c80
grzegorz <gorczykowski@soldevelo.com>
2017-12-20 19:08:34 +0100

    Read auto submit and auto delete from forms

--- collect_app/src/main/java/org/odk/collect/android/utilities/FileUtils.java
+++ collect_app/src/main/java/org/odk/collect/android/utilities/FileUtils.java
@@ -56,6 +56,8 @@ public class FileUtils {
     public static final String TITLE = "title";
     public static final String SUBMISSIONURI = "submission";
     public static final String BASE64_RSA_PUBLIC_KEY = "base64RsaPublicKey";
+    public static final String AUTO_DELETE = "autoDelete";
+    public static final String AUTO_SUBMIT = "autoSubmit";
 
     public static String getMimeType(String fileUrl) throws IOException {
         FileNameMap fileNameMap = URLConnection.getFileNameMap();
@@ -362,12 +364,15 @@ public class FileUtils {
         }
         try {
             final Element submission = model.getElement(xforms, "submission");
+            final String base64RsaPublicKey = submission.getAttributeValue(null, "base64RsaPublicKey");
+            String autoDelete = submission.getAttributeValue(null, "auto-delete");
+            String autoSubmit = submission.getAttributeValue(null, "auto-submit");
             fields.put(SUBMISSIONURI, submission.getAttributeValue(null, "action"));
-            final String base64RsaPublicKey = submission.getAttributeValue(null,
-                    "base64RsaPublicKey");
             fields.put(BASE64_RSA_PUBLIC_KEY,
                     (base64RsaPublicKey == null || base64RsaPublicKey.trim().length() == 0)
                             ? null : base64RsaPublicKey.trim());
+            fields.put(AUTO_DELETE, autoDelete);
+            fields.put(AUTO_SUBMIT, autoSubmit);
         } catch (Exception e) {
             Timber.i("XML file %s does not have a submission element", xmlFile.getAbsolutePath());
             // and that's totally fine.
