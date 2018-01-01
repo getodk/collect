8cedf399ffeb5376b6809d9094300b91d39b2b86
lognaturel <lognaturel@gmail.com>
2018-01-01 15:45:09 -0500

    Fix junit validation error
    org.junit.internal.runners.rules.ValidationError: The @Rule 'activityTestRule' must be public.
        at org.junit.internal.runners.rules.RuleMemberValidator.validate(RuleMemberValidator.java:222)
        at org.junit.internal.runners.rules.RuleMemberValidator.validateMember(RuleMemberValidator.java:99)

--- collect_app/src/androidTest/java/org/odk/collect/android/location/GeoActivityTest.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/location/GeoActivityTest.java
@@ -13,7 +13,7 @@ import static junit.framework.Assert.assertTrue;
 public class GeoActivityTest {
 
     @Rule
-    ActivityTestRule<GeoActivity> activityTestRule;
+    public ActivityTestRule<GeoActivity> activityTestRule;
 
     @Test
     public void shouldDoSomething() {
