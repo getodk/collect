9b0ebb7c4455dfc8fc6683c18d47778f96b2f27c
lakshyagupta21 <lakshyagupta21@gmail.com>
2018-02-15 05:39:34 +0530

    license header added

--- collect_app/src/main/java/org/odk/collect/android/activities/WebViewActivity.java
+++ collect_app/src/main/java/org/odk/collect/android/activities/WebViewActivity.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright (C) 2018 Lakshya
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package org.odk.collect.android.activities;
 
 import android.graphics.Bitmap;
@@ -13,10 +27,6 @@ import android.widget.ProgressBar;
 
 import org.odk.collect.android.R;
 import org.odk.collect.android.utilities.CustomTabHelper;
-
-/**
- * Created by dexter on 29/1/18.
- */
 
 public class WebViewActivity extends AppCompatActivity {
 
