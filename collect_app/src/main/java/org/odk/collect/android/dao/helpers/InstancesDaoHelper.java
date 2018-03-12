27f703aba237538ea8f946e5a92b991953d4a624
grzegorz <gorczykowski@soldevelo.com>
2018-03-12 13:19:00 +0100

    Fixed NullPointerException when formController.getInstancePath() is null

--- collect_app/src/main/java/org/odk/collect/android/dao/helpers/InstancesDaoHelper.java
+++ collect_app/src/main/java/org/odk/collect/android/dao/helpers/InstancesDaoHelper.java
@@ -44,7 +44,7 @@ public final class InstancesDaoHelper {
         boolean complete = false;
 
         FormController formController = Collect.getInstance().getFormController();
-        if (formController != null) {
+        if (formController != null && formController.getInstancePath() != null) {
             // First check if we're at the end of the form, then check the preferences
             complete = end && (boolean) GeneralSharedPreferences.getInstance()
                     .get(PreferenceKeys.KEY_COMPLETED_DEFAULT);
