2dfd70b441e68d749b62c3ea890256258535edcc
akshay-ap <akshay.ap95@gmail.com>
2018-03-12 10:52:16 +0530

    Add test for null Object in Long array

--- collect_app/src/test/java/org/odk/collect/android/utilities/ArrayUtilsTest.java
+++ collect_app/src/test/java/org/odk/collect/android/utilities/ArrayUtilsTest.java
@@ -15,6 +15,19 @@ public class ArrayUtilsTest {
         assertArrayEquals("toPrimitiveNullTest", emptyLongPrimitive, ArrayUtils.toPrimitive(null));
     }
 
+    @Test(expected = NullPointerException.class)
+    public void nullToPrimitive() {
+        Long[] object = new Long[] {1L, null, 3L, 4L, 5L};
+        ArrayUtils.toPrimitive(object);
+
+        Long[] objectStartingWithnull = new Long[] {null, 2L, 3L};
+        ArrayUtils.toPrimitive(objectStartingWithnull);
+
+        Long[] objectEndingWithnull = new Long[] {1L, 2L, null};
+        ArrayUtils.toPrimitive(objectEndingWithnull);
+
+    }
+
     @Test
     public void toObject() throws Exception {
 
