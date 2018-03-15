4d64aa3bf8365e923064695c5a6b1587faa8f88b
Ugo Amanoh <ugoamanoh@gmail.com>
2018-03-15 13:55:22 +0100

    Fixed checkstyle

--- collect_app/src/main/java/org/odk/collect/android/widgets/SelectMultiWidget.java
+++ collect_app/src/main/java/org/odk/collect/android/widgets/SelectMultiWidget.java
@@ -142,7 +142,7 @@ public class SelectMultiWidget extends SelectTextWidget implements MultiChoiceWi
                 // show warning when selected choice value has spaces
                 int index = (int) checkBox.getTag();
                 String value = items.get(index).getValue();
-                if (isChecked && value != null && value.contains(" ")){
+                if (isChecked && value != null && value.contains(" ")) {
                     String errorMsg = context.getString(R.string.invalid_space_in_answer, value);
                     Timber.e(errorMsg);
                     ToastUtils.showLongToast(errorMsg);
