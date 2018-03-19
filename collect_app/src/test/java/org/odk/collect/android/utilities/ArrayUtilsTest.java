62ab26974320aa3e42d3bd1ac3b60d3b10f100ca
akshay-ap <akshay.ap95@gmail.com>
2018-03-19 11:31:42 +0530

    Simplify ArrayUtilsTest class

--- collect_app/src/test/java/org/odk/collect/android/utilities/ArrayUtilsTest.java
+++ collect_app/src/test/java/org/odk/collect/android/utilities/ArrayUtilsTest.java
@@ -1,57 +1,45 @@
 package org.odk.collect.android.utilities;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+import static org.odk.collect.android.utilities.ArrayUtils.toPrimitive;
+import static org.odk.collect.android.utilities.ArrayUtils.toObject;
 
 public class ArrayUtilsTest {
 
     @Test
-    public void toPrimitive() throws Exception {
-        Long[] object = new Long[] {1L, 2L, 3L};
-        long[] primitive = new long[] {1, 2, 3};
-        assertArrayEquals(primitive, ArrayUtils.toPrimitive(object));
-
-        long[] emptyLongPrimitive = new long[0];
-        assertArrayEquals("toPrimitive(null) did not produce an empty long array", emptyLongPrimitive, ArrayUtils.toPrimitive(null));
+    public void toPrimitiveCreatesPrimitiveLongArray() throws Exception {
+        assertArrayEquals(new long[] {1, 2, 3, 4, 5}, toPrimitive(new Long[] {1L, 2L, 3L, 4L, 5L}));
     }
 
     @Test
-    public void nullToPrimitive() {
-        try {
-            Long[] object = new Long[] {1L, null, 3L, 4L, 5L};
-            ArrayUtils.toPrimitive(object);
-            Assert.fail("NullPointer Exception was expected!");
-        } catch (Exception e) {
-            assertEquals(true, e instanceof NullPointerException);
-        }
+    public void nullToPrimitiveCreatesEmptyPrimitiveLongArray() throws Exception {
+        assertArrayEquals(new long[0], toPrimitive(null));
+    }
 
-        try {
-            Long[] objectStartingWithnull = new Long[] {null, 2L, 3L};
-            ArrayUtils.toPrimitive(objectStartingWithnull);
-            Assert.fail("NullPointer Exception was expected!");
-        } catch (Exception e) {
-            assertEquals(true, e instanceof NullPointerException);
-        }
+    @Test(expected = NullPointerException.class)
+    public void arrayContainingNullCausesNpe() {
+        toPrimitive(new Long[] {1L, null, 3L, 4L, 5L});
+    }
 
-        try {
-            Long[] objectEndingWithnull = new Long[] {1L, 2L, null};
-            ArrayUtils.toPrimitive(objectEndingWithnull);
-            Assert.fail("NullPointer Exception was expected!");
-        } catch (Exception e) {
-            assertEquals(true, e instanceof NullPointerException);
-        }
+    @Test(expected = NullPointerException.class)
+    public void arrayStartingWithNullCausesNpe() {
+        toPrimitive(new Long[] {null, 3L, 4L, 5L});
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void arrayEndingWithNullCausesNpe() {
+        toPrimitive(new Long[] {1L, 3L, 4L, null});
     }
 
     @Test
-    public void toObject() throws Exception {
-        Long[] object = new Long[] {1L, 2L, 3L};
-        long[] primitive = new long[] {1, 2, 3};
-        assertArrayEquals(object, ArrayUtils.toObject(primitive));
+    public void toObjectCreatesLongArray() throws Exception {
+        assertArrayEquals(new Long[] {1L, 2L, 3L, 4L, 5L}, toObject(new long[] {1, 2, 3, 4, 5}));
+    }
 
-        Long[] emptyLongObject = new Long[0];
-        assertArrayEquals(emptyLongObject, ArrayUtils.toObject(null));
+    @Test
+    public void nullBecomesEmptyLongArray() throws Exception {
+        assertArrayEquals(new Long[0], toObject(null));
     }
 }
