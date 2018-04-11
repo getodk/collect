7b999e2c1acca08d1a67f30904a989f1a6e1ceb4
Bruno Castro <brunoguic@gmail.com>
2018-04-11 18:39:55 -0300

    Remove duplicated test, fix #2086

--- collect_app/src/androidTest/java/org/odk/collect/android/SharedPreferencesTest.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/SharedPreferencesTest.java
@@ -67,37 +67,6 @@ public class SharedPreferencesTest {
     }
 
     @Test
-    public void checkTypesOnAJsonObjectTest() throws JSONException {
-        //Create KEYs with desired type
-        String BOOLEAN_KEY = "BOOL";
-        String STRING_KEY = "STRING";
-
-        //Init some variables
-        JSONObject jsonObject = new JSONObject();
-        HashMap<String, Class> expectedTypes = new HashMap<>();
-        expectedTypes.put(BOOLEAN_KEY, Boolean.class);
-        expectedTypes.put(STRING_KEY, String.class);
-
-        //Expected boolean in value
-        jsonObject.put(BOOLEAN_KEY, true);
-        SharedPreferencesUtils.checkTypesOnJson(jsonObject, expectedTypes);
-
-        //Expected String in value
-        jsonObject.put(STRING_KEY, "value");
-        SharedPreferencesUtils.checkTypesOnJson(jsonObject, expectedTypes);
-
-        //Expected boolean in value and has a String
-        jsonObject.put(BOOLEAN_KEY, "true");
-        boolean hasThrown = false;
-        try{
-            SharedPreferencesUtils.checkTypesOnJson(jsonObject, expectedTypes);
-        }catch (JSONException e){
-            hasThrown = true;
-        }
-        assertTrue(hasThrown);
-    }
-
-    @Test
     public void adminDefaultSharedPreferencesTest() {
         AdminSharedPreferences.getInstance().loadDefaultPreferences();
 
