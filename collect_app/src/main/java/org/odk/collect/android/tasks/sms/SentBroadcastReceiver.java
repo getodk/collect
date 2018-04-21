729013877db28ea2f05f064e81c74e45e998c24d
Joel Dean <jdeanjj1000@gmail.com>
2018-04-20 21:08:07 -0500

    added base package with classes for constructs required for sending forms as sms.

---
+++ collect_app/src/main/java/org/odk/collect/android/tasks/sms/SentBroadcastReceiver.java
@@ -0,0 +1,15 @@
+package org.odk.collect.android.tasks.sms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class SentBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
---
+++ collect_app/src/main/java/org/odk/collect/android/tasks/sms/SmsSubmissionModel.java
@@ -0,0 +1,4 @@
+package org.odk.collect.android.tasks.sms;
+
+public class SmsSubmissionModel {
+}
