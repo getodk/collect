32d00a6f07c191174868539e137bbac9415f6445
Joel Dean <jdeanjj1000@gmail.com>
2018-04-30 22:34:48 -0500

    integrated guidance hint with dark/light theme functionality

--- collect_app/src/main/java/org/odk/collect/android/widgets/QuestionWidget.java
+++ collect_app/src/main/java/org/odk/collect/android/widgets/QuestionWidget.java
@@ -27,7 +27,6 @@ import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
 import android.text.method.LinkMovementMethod;
 import android.util.TypedValue;
 import android.view.Gravity;
@@ -197,7 +196,7 @@ public abstract class QuestionWidget
 
         guidanceTextView.setText(TextUtils.textToHtml(guidance));
 
-        guidanceTextView.setTextColor(ContextCompat.getColor(getContext(), R.color.primaryTextColor));
+        guidanceTextView.setTextColor(themeUtils.getAttributeValue(R.attr.primaryTextColor));
         guidanceTextView.setMovementMethod(LinkMovementMethod.getInstance());
         return guidanceTextView;
     }
