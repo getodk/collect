8c68b440a186cd120e06d4b9508d5e22afd8c3b2
Joel Dean <jdeanjj1000@gmail.com>
2018-05-16 22:56:26 -0500

    Draw ProgressBar after view has loaded and made animations optional.
    Even though the ProgressBar gets instantiated there are times when not all child views are drawn so I utilized the post method of the main layout to trigger the drawing of the progress bar that way the bar is only drawn when all views are ready. Animations are now optional for cases like initial displaying of the current progress.

--- collect_app/src/main/java/org/odk/collect/android/views/ProgressBar.java
+++ collect_app/src/main/java/org/odk/collect/android/views/ProgressBar.java
@@ -21,6 +21,8 @@ public class ProgressBar extends FrameLayout {
     View progressBar;
     @BindView(R.id.divider)
     View divider;
+    @BindView(R.id.main_layout)
+    FrameLayout mainLayout;
 
     public ProgressBar(@NonNull Context context) {
         this(context, null);
@@ -34,26 +36,34 @@ public class ProgressBar extends FrameLayout {
         super(context, attrs, defStyleAttr);
 
         if (!isInEditMode()) {
-            init(context, attrs, defStyleAttr);
+            init();
         }
     }
 
-    private void init(Context context, AttributeSet attrs, int defStyleAttr) {
+    private void init() {
         View view = LayoutInflater.from(getContext()).inflate(R.layout.progress_bar_view, this, true);
-        ButterKnife.apply(view);
+        ButterKnife.bind(this, view);
     }
 
-    public void setProgressPercent(int progress) {
-        int endWith = (progress / 100) * divider.getMeasuredWidth();
+    public void setProgressPercent(int progress, boolean animate) {
+        mainLayout.post(() -> {
+            int progressWidth = (progress / 100) * divider.getMeasuredWidth();
 
-        ValueAnimator anim = ValueAnimator.ofInt(progressBar.getMeasuredWidth(), endWith);
-        anim.addUpdateListener(valueAnimator -> {
-            int val = (Integer) valueAnimator.getAnimatedValue();
-            ViewGroup.LayoutParams layoutParams = progressBar.getLayoutParams();
-            layoutParams.width = val;
-            progressBar.setLayoutParams(layoutParams);
+            if (animate) {
+                ValueAnimator anim = ValueAnimator.ofInt(progressBar.getMeasuredWidth(), progressWidth);
+                anim.addUpdateListener(valueAnimator -> {
+                    int val = (Integer) valueAnimator.getAnimatedValue();
+                    ViewGroup.LayoutParams layoutParams = progressBar.getLayoutParams();
+                    layoutParams.width = val;
+                    progressBar.setLayoutParams(layoutParams);
+                });
+                anim.setDuration(DURATION_MILLIS);
+                anim.start();
+            } else {
+                ViewGroup.LayoutParams layoutParams = progressBar.getLayoutParams();
+                layoutParams.width = progressWidth;
+                progressBar.setLayoutParams(layoutParams);
+            }
         });
-        anim.setDuration(DURATION_MILLIS);
-        anim.start();
     }
 }
