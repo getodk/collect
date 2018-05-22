973d56da7a592add6620a9f895acc98ec745e72f
Joel Dean <jdeanjj1000@gmail.com>
2018-05-22 07:34:22 -0500

    Updated the InstanceUploaderAdapter to display all states and interact with all the functionality of the SmsService and existing system.
    The UI state of this list can now show progress and states of normal uploads along with sms submissions. There are also constructs in place to show more detail for sms submissions since they are sending updates from the background.
    UI States were for MessageStatus so that when the SmsService receives a change and it's being propagated back to the UI via the event bus it can be rendered well. Each state has an associated icon and subtext that's generated. Along with that, the progress of the submission was binded to the UI as well.

--- collect_app/src/main/java/org/odk/collect/android/adapters/InstanceUploaderAdapter.java
+++ collect_app/src/main/java/org/odk/collect/android/adapters/InstanceUploaderAdapter.java
@@ -7,25 +7,52 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.odk.collect.android.R;
+import org.odk.collect.android.application.Collect;
 import org.odk.collect.android.events.RxEventBus;
-import org.odk.collect.android.events.SmsProgressEvent;
+import org.odk.collect.android.events.SmsEvent;
 import org.odk.collect.android.provider.InstanceProviderAPI;
+import org.odk.collect.android.tasks.sms.SmsService;
+import org.odk.collect.android.tasks.sms.contracts.SmsSubmissionManagerContract;
+import org.odk.collect.android.tasks.sms.models.MessageStatus;
+import org.odk.collect.android.tasks.sms.models.SmsSubmission;
+import org.odk.collect.android.tasks.sms.models.SmsProgress;
 import org.odk.collect.android.views.ProgressBar;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
+
+import static org.odk.collect.android.provider.InstanceProviderAPI.STATUS_SUBMISSION_FAILED;
+import static org.odk.collect.android.provider.InstanceProviderAPI.STATUS_SUBMITTED;
 
 public class InstanceUploaderAdapter extends CursorAdapter {
-    private RxEventBus eventBus;
+
+    @Inject
+    RxEventBus eventBus;
+    @Inject
+    SmsSubmissionManagerContract submissionManager;
+    @Inject
+    SmsService smsService;
+
+    private Context context;
     private CompositeDisposable compositeDisposable;
 
-    public InstanceUploaderAdapter(Context context, Cursor cursor, RxEventBus eventBus) {
+    public InstanceUploaderAdapter(Context context, Cursor cursor) {
         super(context, cursor);
-        this.eventBus = eventBus;
+        this.context = context;
+        Collect.getInstance().getComponent().inject(this);
         compositeDisposable = new CompositeDisposable();
     }
 
@@ -51,12 +78,146 @@ public class InstanceUploaderAdapter extends CursorAdapter {
 
         long instanceId = cursor.getLong(cursor.getColumnIndex(InstanceProviderAPI.InstanceColumns._ID));
 
-        viewHolder.progressBar.setProgressPercent(100, false);
-        compositeDisposable.add(eventBus.register(SmsProgressEvent.class)
+        SmsSubmission model = submissionManager.getSubmissionModel(String.valueOf(instanceId));
+
+        boolean isSmsSubmission = model != null;
+
+        String status = cursor.getString(cursor.getColumnIndex(InstanceProviderAPI.InstanceColumns.STATUS));
+
+        switch (status) {
+
+            case STATUS_SUBMISSION_FAILED:
+                if (isSmsSubmission) {
+                    viewHolder.statusIcon.setImageResource(R.drawable.message_alert);
+                } else {
+                    viewHolder.statusIcon.setImageResource(R.drawable.exclamation);
+                }
+                break;
+
+            case STATUS_SUBMITTED:
+                viewHolder.statusIcon.setImageResource(R.drawable.check);
+                break;
+
+            default:
+                viewHolder.statusIcon.setImageResource(R.drawable.pencil);
+        }
+
+        if (isSmsSubmission) {
+            viewHolder.progressBar.setProgressPercent((int) model.getCompletion().getPercentage(), false);
+        } else {
+            if (status.equals(STATUS_SUBMITTED)) {
+                viewHolder.progressBar.setProgressPercent(100, false);
+            } else if (status.equals(STATUS_SUBMISSION_FAILED)) {
+                viewHolder.progressBar.setProgressPercent(50, false);
+            }
+        }
+
+        if (isSmsSubmission) {
+            MessageStatus messageStatus = submissionManager.checkNextMessageStatus(String.valueOf(instanceId));
+
+            if (messageStatus != null) {
+                setSmsSubmissionStateIcons(messageStatus, viewHolder);
+            }
+
+            SmsEvent currentStatus = new SmsEvent();
+            currentStatus.setStatus(messageStatus);
+            currentStatus.setLastUpdated(model.getLastUpdated());
+            currentStatus.setProgress(model.getCompletion());
+
+            setDisplaySubTextView(currentStatus, viewHolder);
+
+            setupCloseButton(viewHolder, messageStatus);
+            viewHolder.closeButton.setOnClickListener(v -> smsService.cancelFormSubmission(String.valueOf(instanceId)));
+        }
+
+        compositeDisposable.add(eventBus.register(SmsEvent.class)
                 .filter(event -> event.getInstanceId().equals(String.valueOf(instanceId)))
-                .subscribe(smsProgressEvent -> {
-                    viewHolder.progressBar.setProgressPercent(smsProgressEvent.getProgress().getPercentage(), true);
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(event -> {
+                    viewHolder.progressBar.setProgressPercent((int) event.getProgress().getPercentage(), true);
+                    setSmsSubmissionStateIcons(event.getStatus(), viewHolder);
+                    setDisplaySubTextView(event, viewHolder);
+                    setupCloseButton(viewHolder, event.getStatus());
                 }));
+    }
+
+    private void setupCloseButton(ViewHolder viewHolder, MessageStatus status) {
+
+        if (status == null) {
+            return;
+        }
+
+        if (status.equals(MessageStatus.Sending)) {
+            viewHolder.closeButton.setVisibility(View.VISIBLE);
+            viewHolder.checkbox.setVisibility(View.GONE);
+        } else {
+            viewHolder.closeButton.setVisibility(View.GONE);
+            viewHolder.checkbox.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void setSmsSubmissionStateIcons(MessageStatus messageStatus, ViewHolder viewHolder) {
+
+        if (messageStatus == null) {
+            return;
+        }
+
+        switch (messageStatus) {
+            case Sent:
+                viewHolder.statusIcon.setImageResource(R.drawable.check);
+                break;
+            case Sending:
+                viewHolder.statusIcon.setImageResource(R.drawable.message_text_outline);
+                break;
+
+            case Ready:
+                viewHolder.statusIcon.setImageResource(R.drawable.pencil);
+                break;
+
+            default:
+                viewHolder.statusIcon.setImageResource(R.drawable.message_alert);
+                break;
+        }
+    }
+
+    private void setDisplaySubTextView(SmsEvent progress, ViewHolder viewHolder) {
+        String text = getDisplaySubtext(progress);
+        if (text != null) {
+            viewHolder.displaySubtext.setText(text);
+        }
+    }
+
+    private String getDisplaySubtext(SmsEvent event) {
+        Date date = event.getLastUpdated();
+        SmsProgress progress = event.getProgress();
+
+        if (event.getStatus() == null) {
+            return null;
+        }
+
+        switch (event.getStatus()) {
+            case NoReception:
+                return new SimpleDateFormat(context.getString(R.string.sms_no_reception), Locale.getDefault()).format(date);
+            case AirplaneMode:
+                return new SimpleDateFormat(context.getString(R.string.sms_airplane_mode), Locale.getDefault()).format(date);
+            case FatalError:
+                return new SimpleDateFormat(context.getString(R.string.sms_fatal_error), Locale.getDefault()).format(date);
+            case Sending:
+                return context.getResources().getQuantityString(R.plurals.sms_sending, (int) progress.getTotalCount(), progress.getCompletedCount(), progress.getTotalCount());
+            case Queued:
+                return context.getString(R.string.sms_submission_queued);
+            case Sent:
+                return new SimpleDateFormat(context.getString(R.string.sent_on_date_at_time),
+                        Locale.getDefault()).format(date);
+            case NoMessage:
+                return context.getString(R.string.sms_no_message);
+            case Canceled:
+                return new SimpleDateFormat(context.getString(R.string.sms_last_submission_on_date_at_time),
+                        Locale.getDefault()).format(date);
+        }
+
+        return "";
     }
 
     static class ViewHolder {
@@ -70,6 +231,10 @@ public class InstanceUploaderAdapter extends CursorAdapter {
         CheckBox checkbox;
         @BindView(R.id.progress_bar)
         ProgressBar progressBar;
+        @BindView(R.id.status_icon)
+        ImageView statusIcon;
+        @BindView(R.id.close_box)
+        ImageView closeButton;
 
         ViewHolder(View view) {
             ButterKnife.bind(this, view);
