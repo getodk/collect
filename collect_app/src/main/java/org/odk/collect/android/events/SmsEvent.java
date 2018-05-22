0b033e90c65e19d98faaf7caeb3082edfd9c1770
Joel Dean <jdeanjj1000@gmail.com>
2018-05-21 19:23:00 -0500

    Refactored SMSEvent
    Renamed this event so that it could be more concise and general purpose for the SMS layer. Also added lastUpdated property that's able to transfer the last time a submission was updated for the different states.

---
+++ collect_app/src/main/java/org/odk/collect/android/events/SmsEvent.java
@@ -0,0 +1,55 @@
+package org.odk.collect.android.events;
+
+import org.odk.collect.android.tasks.sms.models.MessageStatus;
+import org.odk.collect.android.tasks.sms.models.SmsProgress;
+
+import java.util.Date;
+
+public class SmsEvent {
+    private MessageStatus status;
+    private SmsProgress progress;
+    private String instanceId;
+    private Date lastUpdated;
+
+    public SmsEvent() {
+        progress = new SmsProgress();
+    }
+
+    public SmsEvent(String instanceId, MessageStatus status) {
+        this.status = status;
+        this.instanceId = instanceId;
+        progress = new SmsProgress();
+    }
+
+    public MessageStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(MessageStatus status) {
+        this.status = status;
+    }
+
+    public String getInstanceId() {
+        return instanceId;
+    }
+
+    public void setInstanceId(String instanceId) {
+        this.instanceId = instanceId;
+    }
+
+    public SmsProgress getProgress() {
+        return progress;
+    }
+
+    public void setProgress(SmsProgress progress) {
+        this.progress = progress;
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+}
