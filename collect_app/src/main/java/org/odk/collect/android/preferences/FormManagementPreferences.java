473307da9237f25f90396f8a6dee3e96aaf806b4
Grzegorz Orczykowski <gorczykowski@soldevelo.com>
2018-06-06 21:31:44 +0200

    Fixed NullPointerException in settings (#2275)

--- collect_app/src/main/java/org/odk/collect/android/preferences/FormManagementPreferences.java
+++ collect_app/src/main/java/org/odk/collect/android/preferences/FormManagementPreferences.java
@@ -72,7 +72,10 @@ public class FormManagementPreferences extends BasePreferenceFragment {
                 if (key.equals(KEY_PERIODIC_FORM_UPDATES_CHECK)) {
                     ServerPollingJob.schedulePeriodicJob((String) newValue);
                     if (newValue.equals(getString(R.string.never_value))) {
-                        findPreference(KEY_AUTOMATIC_UPDATE).setEnabled(false);
+                        Preference automaticUpdatePreference = findPreference(KEY_AUTOMATIC_UPDATE);
+                        if (automaticUpdatePreference != null) {
+                            automaticUpdatePreference.setEnabled(false);
+                        }
                     }
                     getActivity().recreate();
                 }
