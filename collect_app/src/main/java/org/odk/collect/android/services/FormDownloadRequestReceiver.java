f10558cb87b003e94aa43b69bc9b40b7edfacfe3
Kigamba <n.kigamba@students.ku.ac.ke>
2018-07-23 15:03:03 +0300

    Enable form download service accessible from all phones

---
+++ collect_app/src/main/java/org/odk/collect/android/services/FormDownloadRequestReceiver.java
@@ -0,0 +1,20 @@
+package org.odk.collect.android.services;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import org.odk.collect.android.utilities.ApplicationConstants;
+
+public class FormDownloadRequestReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent serviceIntent =  new Intent(context, FormDownloadService.class);
+        if (serviceIntent.hasExtra(ApplicationConstants.BundleKeys.FORM_ID)) {
+            serviceIntent.putExtra(ApplicationConstants.BundleKeys.FORM_ID, intent.getStringExtra(ApplicationConstants.BundleKeys.FORM_ID));
+
+            context.startService(serviceIntent);
+        }
+    }
+}
