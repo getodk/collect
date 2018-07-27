341ef84a3c47fe1cc1a8327499c66b6e16495944
Rushikesh Karad <karad.rushikesh5@gmail.com>
2018-07-28 01:51:21 +0530

    Update FormEntryActivity.java

--- collect_app/src/main/java/org/odk/collect/android/activities/FormEntryActivity.java
+++ collect_app/src/main/java/org/odk/collect/android/activities/FormEntryActivity.java
@@ -25,7 +25,6 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore.Images;
-import android.provider.OpenableColumns;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.FileProvider;
