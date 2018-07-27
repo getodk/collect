2c7b02f4f88aa05a70aca9be6b945f6e76e98837
Rushikesh Karad <karad.rushikesh5@gmail.com>
2018-07-28 00:39:29 +0530

    For getting file name from uri
    The previous code was not working for all devices above nougat including nougat. So this change is made. First I made a copy of the content and then used that file.

--- collect_app/src/main/java/org/odk/collect/android/utilities/MediaUtils.java
+++ collect_app/src/main/java/org/odk/collect/android/utilities/MediaUtils.java
@@ -631,28 +631,41 @@ public class MediaUtils {
      * @param selection     (Optional) Filter used in the query.
      * @param selectionArgs (Optional) Selection arguments used in the query.
      * @return The value of the _data column, which is typically a file path.
-     * @author paulburke
      */
-    public static String getDataColumn(Context context, Uri uri,
-                                       String selection, String[] selectionArgs) {
 
-        Cursor cursor = null;
-        final String column = "_data";
-        final String[] projection = {column};
-
-        try {
-            cursor = context.getContentResolver().query(uri, projection,
-                    selection, selectionArgs, null);
-            if (cursor != null && cursor.moveToFirst()) {
-
-                final int column_index = cursor.getColumnIndexOrThrow(column);
-                return cursor.getString(column_index);
-            }
-        } finally {
-            if (cursor != null) {
-                cursor.close();
-            }
+    public static String getDataColumn(Context context, Uri contentUri,String selection, String[] selectionArgs) {
+        //copy file and send new file path
+        File TEMP_DIR_PATH = context.getFilesDir();
+        String fileName = getFileName(contentUri);
+        if (!TextUtils.isEmpty(fileName)) {
+            File copyFile = new File(TEMP_DIR_PATH + File.separator + fileName);
+            copy(context, contentUri, copyFile);
+            return copyFile.getAbsolutePath();
         }
         return null;
+    }
+
+    public static String getFileName(Uri uri) {
+        if (uri == null) return null;
+        String fileName = null;
+        String path = uri.getPath();
+        int cut = path.lastIndexOf('/');
+        if (cut != -1) {
+            fileName = path.substring(cut + 1);
+        }
+        return fileName;
+    }
+
+    public static void copy(Context context, Uri srcUri, File dstFile) {
+        try {
+            InputStream inputStream = context.getContentResolver().openInputStream(srcUri);
+            if (inputStream == null) return;
+            OutputStream outputStream = new FileOutputStream(dstFile);
+            IOUtils.copy(inputStream, outputStream);
+            inputStream.close();
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
