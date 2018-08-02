a488bf98bc4df28908049b72d3f2161f4876603f
grzegorz <gorczykowski@soldevelo.com>
2018-08-02 23:41:53 +0200

    [CODE_REFACTORING]: moved isFrontCameraAvailable() method to an util class

--- collect_app/src/main/java/org/odk/collect/android/utilities/CameraUtils.java
+++ collect_app/src/main/java/org/odk/collect/android/utilities/CameraUtils.java
@@ -17,8 +17,15 @@ limitations under the License.
  */
 
 import android.app.Activity;
+import android.content.Context;
 import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.view.Surface;
+
+import org.odk.collect.android.application.Collect;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -102,5 +109,37 @@ public class CameraUtils {
         } catch (IOException e) {
             Timber.e(e);
         }
+    }
+
+    public static boolean isFrontCameraAvailable() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            try {
+                //https://developer.android.com/reference/android/hardware/camera2/CameraCharacteristics.html
+                CameraManager cameraManager = (CameraManager) Collect.getInstance()
+                        .getSystemService(Context.CAMERA_SERVICE);
+                if (cameraManager != null) {
+                    String[] cameraId = cameraManager.getCameraIdList();
+                    for (String id : cameraId) {
+                        CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(id);
+                        Integer facing = characteristics.get(CameraCharacteristics.LENS_FACING);
+                        if (facing != null && facing == CameraCharacteristics.LENS_FACING_FRONT) {
+                            return true;
+                        }
+                    }
+                }
+            } catch (CameraAccessException | NullPointerException e) {
+                Timber.e(e);
+            }
+        } else {
+            //https://developer.android.com/guide/topics/media/camera.html#check-camera-features
+            for (int camNo = 0; camNo < Camera.getNumberOfCameras(); camNo++) {
+                Camera.CameraInfo camInfo = new Camera.CameraInfo();
+                Camera.getCameraInfo(camNo, camInfo);
+                if (camInfo.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                    return true;
+                }
+            }
+        }
+        return false; // No front-facing camera found
     }
 }
