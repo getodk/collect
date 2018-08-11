409cc185c82a509cca40a3118d28b2b5fa7d24a4
Joel Dean <jdeanjj1000@gmail.com>
2018-08-11 16:24:09 -0500

    Added an onBackPressedListener
    This enables fragments to get the onBackPressed events so that they can react to the user navigating away from the screen.

---
+++ collect_app/src/main/java/org/odk/collect/android/listeners/OnBackPressedListener.java
@@ -0,0 +1,8 @@
+package org.odk.collect.android.listeners;
+
+/***
+ * Used by components that need to get the onBackPressed events
+ */
+public interface OnBackPressedListener {
+    void doBack();
+}
