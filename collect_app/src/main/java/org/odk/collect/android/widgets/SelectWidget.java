563e726d7116c14e090f6ccf43838c4f722f62c6
grzegorz <gorczykowski@soldevelo.com>
2018-09-19 20:53:28 +0200

    Improved the comment

--- collect_app/src/main/java/org/odk/collect/android/widgets/SelectWidget.java
+++ collect_app/src/main/java/org/odk/collect/android/widgets/SelectWidget.java
@@ -171,7 +171,7 @@ public abstract class SelectWidget extends QuestionWidget {
 
     void adjustRecyclerViewSize(AbstractSelectListAdapter adapter, RecyclerView recyclerView) {
         if (adapter.getItemCount() > MAX_ITEMS_WITHOUT_SCREEN_BOUND) {
-            // Only let the RecyclerView take up 80% of the screen height in order to speed up loading if there are many items
+            // Only let the RecyclerView take up 90% of the screen height in order to speed up loading if there are many items
             DisplayMetrics displayMetrics = new DisplayMetrics();
             ((FormEntryActivity) getContext()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
             recyclerView.getLayoutParams().height = (int) (displayMetrics.heightPixels * 0.9);
