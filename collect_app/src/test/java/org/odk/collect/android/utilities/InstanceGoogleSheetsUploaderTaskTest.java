c3b1dd12906c85f42355eea605076721bfce9874
lognaturel <lognaturel@gmail.com>
2018-10-20 20:21:16 -0700

    Make location validating method static for easier testing

--- collect_app/src/test/java/org/odk/collect/android/utilities/InstanceGoogleSheetsUploaderTaskTest.java
+++ collect_app/src/test/java/org/odk/collect/android/utilities/InstanceGoogleSheetsUploaderTaskTest.java
@@ -7,19 +7,17 @@ import org.junit.Test;
 import org.odk.collect.android.upload.InstanceGoogleSheetsUploader;
 
 public class InstanceGoogleSheetsUploaderTaskTest {
-    InstanceGoogleSheetsUploader uploader = new InstanceGoogleSheetsUploader(null);
-
     @Test
     public void gpsLocationRegexTests() {
-        assertFalse(uploader.isLocationValid("{}{"));
-        assertFalse(uploader.isLocationValid("28"));
-        assertFalse(uploader.isLocationValid("-@123"));
-        assertFalse(uploader.isLocationValid(";'[@123"));
-        assertFalse(uploader.isLocationValid("*&1w345"));
-        assertFalse(uploader.isLocationValid("41 24.2028, 2 10.4418"));
-        assertFalse(uploader.isLocationValid("41.40338"));
-        assertTrue(uploader.isLocationValid("-9.9 -9.9 -9.9 9.9"));
-        assertTrue(uploader.isLocationValid("-0.0 0.8 -9.7 9.9"));
-        assertTrue(uploader.isLocationValid("8.0 0.8 8.7 8.9"));
+        assertFalse(InstanceGoogleSheetsUploader.isLocationValid("{}{"));
+        assertFalse(InstanceGoogleSheetsUploader.isLocationValid("28"));
+        assertFalse(InstanceGoogleSheetsUploader.isLocationValid("-@123"));
+        assertFalse(InstanceGoogleSheetsUploader.isLocationValid(";'[@123"));
+        assertFalse(InstanceGoogleSheetsUploader.isLocationValid("*&1w345"));
+        assertFalse(InstanceGoogleSheetsUploader.isLocationValid("41 24.2028, 2 10.4418"));
+        assertFalse(InstanceGoogleSheetsUploader.isLocationValid("41.40338"));
+        assertTrue(InstanceGoogleSheetsUploader.isLocationValid("-9.9 -9.9 -9.9 9.9"));
+        assertTrue(InstanceGoogleSheetsUploader.isLocationValid("-0.0 0.8 -9.7 9.9"));
+        assertTrue(InstanceGoogleSheetsUploader.isLocationValid("8.0 0.8 8.7 8.9"));
     }
 }
