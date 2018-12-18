1677f2b09e9c515c330a0a1e27c68be53cd674aa
grzegorz <gorczykowski@soldevelo.com>
2018-12-18 15:28:53 +0100

    Fixed the deprecated builder

--- collect_app/src/main/java/org/odk/collect/android/utilities/NotificationUtils.java
+++ collect_app/src/main/java/org/odk/collect/android/utilities/NotificationUtils.java
@@ -54,14 +54,13 @@ public class NotificationUtils {
                                         String contentText) {
         Context context = Collect.getInstance();
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context).setContentIntent(contentIntent);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID).setContentIntent(contentIntent);
 
         builder
                 .setContentTitle(context.getString(title))
                 .setContentText(contentText)
                 .setSmallIcon(IconUtils.getNotificationAppIcon())
-                .setAutoCancel(true)
-                .setChannelId(CHANNEL_ID);
+                .setAutoCancel(true);
 
         NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         if (manager != null) {
