8b8ed4b0e7c668c5c0d79bd5581787693293ae6c
grzegorz <gorczykowski@soldevelo.com>
2019-01-16 17:02:42 +0100

    Fixed tests

--- collect_app/src/test/java/org/odk/collect/android/logic/EventTest.java
+++ collect_app/src/test/java/org/odk/collect/android/logic/EventTest.java
@@ -18,29 +18,28 @@ package org.odk.collect.android.logic;
 
 import org.junit.Test;
 
-import static org.javarosa.form.api.FormEntryController.EVENT_QUESTION;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.odk.collect.android.utilities.EventLogger.EventTypes.BACKGROUND_LOCATION_DISABLED;
-import static org.odk.collect.android.utilities.EventLogger.EventTypes.BACKGROUND_LOCATION_ENABLED;
-import static org.odk.collect.android.utilities.EventLogger.EventTypes.CONSTRAINT_ERROR;
-import static org.odk.collect.android.utilities.EventLogger.EventTypes.DELETE_REPEAT;
-import static org.odk.collect.android.utilities.EventLogger.EventTypes.FEC;
-import static org.odk.collect.android.utilities.EventLogger.EventTypes.FINALIZE_ERROR;
-import static org.odk.collect.android.utilities.EventLogger.EventTypes.FORM_EXIT;
-import static org.odk.collect.android.utilities.EventLogger.EventTypes.FORM_FINALIZE;
-import static org.odk.collect.android.utilities.EventLogger.EventTypes.FORM_RESUME;
-import static org.odk.collect.android.utilities.EventLogger.EventTypes.FORM_SAVE;
-import static org.odk.collect.android.utilities.EventLogger.EventTypes.FORM_START;
-import static org.odk.collect.android.utilities.EventLogger.EventTypes.GOOGLE_PLAY_SERVICES_NOT_AVAILABLE;
-import static org.odk.collect.android.utilities.EventLogger.EventTypes.HIERARCHY;
-import static org.odk.collect.android.utilities.EventLogger.EventTypes.LOCATION_PERMISSIONS_GRANTED;
-import static org.odk.collect.android.utilities.EventLogger.EventTypes.LOCATION_PERMISSIONS_NOT_GRANTED;
-import static org.odk.collect.android.utilities.EventLogger.EventTypes.LOCATION_PROVIDERS_DISABLED;
-import static org.odk.collect.android.utilities.EventLogger.EventTypes.LOCATION_PROVIDERS_ENABLED;
-import static org.odk.collect.android.utilities.EventLogger.EventTypes.SAVE_ERROR;
+import static org.odk.collect.android.logic.Event.EventTypes.BACKGROUND_LOCATION_DISABLED;
+import static org.odk.collect.android.logic.Event.EventTypes.BACKGROUND_LOCATION_ENABLED;
+import static org.odk.collect.android.logic.Event.EventTypes.CONSTRAINT_ERROR;
+import static org.odk.collect.android.logic.Event.EventTypes.DELETE_REPEAT;
+import static org.odk.collect.android.logic.Event.EventTypes.FINALIZE_ERROR;
+import static org.odk.collect.android.logic.Event.EventTypes.FORM_EXIT;
+import static org.odk.collect.android.logic.Event.EventTypes.FORM_FINALIZE;
+import static org.odk.collect.android.logic.Event.EventTypes.FORM_RESUME;
+import static org.odk.collect.android.logic.Event.EventTypes.FORM_SAVE;
+import static org.odk.collect.android.logic.Event.EventTypes.FORM_START;
+import static org.odk.collect.android.logic.Event.EventTypes.GOOGLE_PLAY_SERVICES_NOT_AVAILABLE;
+import static org.odk.collect.android.logic.Event.EventTypes.HIERARCHY;
+import static org.odk.collect.android.logic.Event.EventTypes.LOCATION_PERMISSIONS_GRANTED;
+import static org.odk.collect.android.logic.Event.EventTypes.LOCATION_PERMISSIONS_NOT_GRANTED;
+import static org.odk.collect.android.logic.Event.EventTypes.LOCATION_PROVIDERS_DISABLED;
+import static org.odk.collect.android.logic.Event.EventTypes.LOCATION_PROVIDERS_ENABLED;
+import static org.odk.collect.android.logic.Event.EventTypes.QUESTION;
+import static org.odk.collect.android.logic.Event.EventTypes.SAVE_ERROR;
 
 public class EventTest {
     private static final long START_TIME = 1545392727685L;
@@ -48,7 +47,7 @@ public class EventTest {
 
     @Test
     public void testToString() {
-        Event event = new Event(START_TIME, FEC, EVENT_QUESTION, "/data/text1");
+        Event event = new Event(START_TIME, QUESTION, "/data/text1");
         assertNotNull(event);
         assertTrue(event.isIntervalViewEvent());
         assertEquals("question,/data/text1,1545392727685,", event.toString());
@@ -58,7 +57,7 @@ public class EventTest {
 
     @Test
     public void testToStringWithLocationCoordinates() {
-        Event event = new Event(START_TIME, FEC, EVENT_QUESTION, "/data/text1");
+        Event event = new Event(START_TIME, QUESTION, "/data/text1");
         event.setLocationCoordinates("54.35202520000001", "18.64663840000003", "10");
         event.setEnd(END_TIME);
         assertNotNull(event);
@@ -68,102 +67,102 @@ public class EventTest {
 
     @Test
     public void testToStringNullValues() {
-        Event event = new Event(START_TIME, FEC, EVENT_QUESTION, null);
+        Event event = new Event(START_TIME, QUESTION, null);
         event.setLocationCoordinates(null, null, null);
         assertNotNull(event);
         assertTrue(event.isIntervalViewEvent());
         event.setEnd(END_TIME);
-        assertEquals("question,null,1545392727685,1545392728527,null,null,null", event.toString());
+        assertEquals("question,null,1545392727685,1545392728527", event.toString());
     }
 
     @Test
     public void testEventTypes() {
-        Event event = new Event(START_TIME, FEC, EVENT_QUESTION, "");
+        Event event = new Event(START_TIME, QUESTION, "");
         assertNotNull(event);
         assertTrue(event.isIntervalViewEvent());
         assertEquals("question,,1545392727685,", event.toString());
 
-        event = new Event(START_TIME, FORM_START, 0, "");
+        event = new Event(START_TIME, FORM_START, "");
         assertNotNull(event);
         assertFalse(event.isIntervalViewEvent());
         assertEquals("form start,,1545392727685,", event.toString());
 
-        event = new Event(START_TIME, FORM_EXIT, 0, "");
+        event = new Event(START_TIME, FORM_EXIT, "");
         assertNotNull(event);
         assertFalse(event.isIntervalViewEvent());
         assertEquals("form exit,,1545392727685,", event.toString());
 
-        event = new Event(START_TIME, FORM_RESUME, 0, "");
+        event = new Event(START_TIME, FORM_RESUME, "");
         assertNotNull(event);
         assertFalse(event.isIntervalViewEvent());
         assertEquals("form resume,,1545392727685,", event.toString());
 
-        event = new Event(START_TIME, FORM_SAVE, 0, "");
+        event = new Event(START_TIME, FORM_SAVE, "");
         assertNotNull(event);
         assertFalse(event.isIntervalViewEvent());
         assertEquals("form save,,1545392727685,", event.toString());
 
-        event = new Event(START_TIME, FORM_FINALIZE, 0, "");
+        event = new Event(START_TIME, FORM_FINALIZE, "");
         assertNotNull(event);
         assertFalse(event.isIntervalViewEvent());
         assertEquals("form finalize,,1545392727685,", event.toString());
 
-        event = new Event(START_TIME, HIERARCHY, 0, "");
+        event = new Event(START_TIME, HIERARCHY, "");
         assertNotNull(event);
         assertTrue(event.isIntervalViewEvent());
         assertEquals("jump,,1545392727685,", event.toString());
 
-        event = new Event(START_TIME, SAVE_ERROR, 0, "");
+        event = new Event(START_TIME, SAVE_ERROR, "");
         assertNotNull(event);
         assertFalse(event.isIntervalViewEvent());
         assertEquals("save error,,1545392727685,", event.toString());
 
-        event = new Event(START_TIME, FINALIZE_ERROR, 0, "");
+        event = new Event(START_TIME, FINALIZE_ERROR, "");
         assertNotNull(event);
         assertFalse(event.isIntervalViewEvent());
         assertEquals("finalize error,,1545392727685,", event.toString());
 
-        event = new Event(START_TIME, CONSTRAINT_ERROR, 0, "");
+        event = new Event(START_TIME, CONSTRAINT_ERROR, "");
         assertNotNull(event);
         assertFalse(event.isIntervalViewEvent());
         assertEquals("constraint error,,1545392727685,", event.toString());
 
-        event = new Event(START_TIME, DELETE_REPEAT, 0, "");
+        event = new Event(START_TIME, DELETE_REPEAT, "");
         assertNotNull(event);
         assertFalse(event.isIntervalViewEvent());
         assertEquals("delete repeat,,1545392727685,", event.toString());
 
-        event = new Event(START_TIME, GOOGLE_PLAY_SERVICES_NOT_AVAILABLE, 0, "");
+        event = new Event(START_TIME, GOOGLE_PLAY_SERVICES_NOT_AVAILABLE, "");
         assertNotNull(event);
         assertFalse(event.isIntervalViewEvent());
         assertEquals("google play services not available,,1545392727685,", event.toString());
 
-        event = new Event(START_TIME, LOCATION_PERMISSIONS_GRANTED, 0, "");
+        event = new Event(START_TIME, LOCATION_PERMISSIONS_GRANTED, "");
         assertNotNull(event);
         assertFalse(event.isIntervalViewEvent());
         assertEquals("location permissions granted,,1545392727685,", event.toString());
 
-        event = new Event(START_TIME, LOCATION_PERMISSIONS_NOT_GRANTED, 0, "");
+        event = new Event(START_TIME, LOCATION_PERMISSIONS_NOT_GRANTED,  "");
         assertNotNull(event);
         assertFalse(event.isIntervalViewEvent());
         assertEquals("location permissions not granted,,1545392727685,", event.toString());
 
-        event = new Event(START_TIME, BACKGROUND_LOCATION_ENABLED, 0, "");
+        event = new Event(START_TIME, BACKGROUND_LOCATION_ENABLED, "");
         assertNotNull(event);
         assertFalse(event.isIntervalViewEvent());
         assertEquals("background location enabled,,1545392727685,", event.toString());
 
-        event = new Event(START_TIME, BACKGROUND_LOCATION_DISABLED, 0, "");
+        event = new Event(START_TIME, BACKGROUND_LOCATION_DISABLED, "");
         assertNotNull(event);
         assertFalse(event.isIntervalViewEvent());
         assertEquals("background location disabled,,1545392727685,", event.toString());
 
-        event = new Event(START_TIME, LOCATION_PROVIDERS_ENABLED, 0, "");
+        event = new Event(START_TIME, LOCATION_PROVIDERS_ENABLED, "");
         assertNotNull(event);
         assertFalse(event.isIntervalViewEvent());
         assertEquals("location providers enabled,,1545392727685,", event.toString());
 
-        event = new Event(START_TIME, LOCATION_PROVIDERS_DISABLED, 0, "");
+        event = new Event(START_TIME, LOCATION_PROVIDERS_DISABLED, "");
         assertNotNull(event);
         assertFalse(event.isIntervalViewEvent());
         assertEquals("location providers disabled,,1545392727685,", event.toString());
