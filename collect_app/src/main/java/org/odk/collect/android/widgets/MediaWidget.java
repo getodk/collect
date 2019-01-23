5920f83effd713541bfe032251fac481828ff139
Shobhit Agarwal <shobhitagarwal1612@gmail.com>
2019-01-23 23:23:38 +0530

    Adds new abstract class, MediaWidget
    The purpose of this class would be to pull out all functionality related
    to media player

---
+++ collect_app/src/main/java/org/odk/collect/android/widgets/MediaWidget.java
@@ -0,0 +1,12 @@
+package org.odk.collect.android.widgets;
+
+import android.content.Context;
+
+import org.javarosa.form.api.FormEntryPrompt;
+
+public abstract class MediaWidget extends QuestionWidget {
+
+    public MediaWidget(Context context, FormEntryPrompt prompt) {
+        super(context, prompt);
+    }
+}
