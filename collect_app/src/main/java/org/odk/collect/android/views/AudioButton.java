7d3e62da203244c2f9d6187a30d9ce740c3e9562
Shobhit Agarwal <shobhitagarwal1612@gmail.com>
2019-02-11 20:18:26 +0530

    Remove audio handler inner class

--- collect_app/src/main/java/org/odk/collect/android/views/AudioButton.java
+++ collect_app/src/main/java/org/odk/collect/android/views/AudioButton.java
@@ -17,20 +17,10 @@ package org.odk.collect.android.views;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.MediaPlayer;
 import android.support.v7.widget.AppCompatImageButton;
 import android.util.AttributeSet;
 
-import org.javarosa.core.reference.InvalidReferenceException;
-import org.javarosa.core.reference.ReferenceManager;
-import org.odk.collect.android.R;
 import org.odk.collect.android.controller.MediaController;
-import org.odk.collect.android.utilities.ToastUtils;
-
-import java.io.File;
-import java.io.IOException;
-
-import timber.log.Timber;
 
 /**
  * @author ctsims
@@ -80,59 +70,6 @@ public class AudioButton extends AppCompatImageButton {
             resetBitmap();
         } else {
             playAudio();
-        }
-    }
-
-    /**
-     * Useful class for handling the playing and stopping of audio prompts.
-     * This is used here, and also in the GridMultiWidget and GridWidget
-     * to play prompts as items are selected.
-     *
-     * @author mitchellsundt@gmail.com
-     */
-    public static class AudioHandler {
-        private final String uri;
-        private final MediaPlayer mediaPlayer;
-
-        public AudioHandler(String uri, MediaPlayer mediaPlayer) {
-            this.uri = uri;
-            this.mediaPlayer = mediaPlayer;
-        }
-
-        public void playAudio(Context c) {
-            if (uri == null) {
-                // No audio file specified
-                Timber.e("No audio file was specified");
-                ToastUtils.showLongToast(R.string.audio_file_error);
-                return;
-            }
-
-            String audioFilename = "";
-            try {
-                audioFilename = ReferenceManager.instance().DeriveReference(uri).getLocalURI();
-            } catch (InvalidReferenceException e) {
-                Timber.e(e);
-            }
-
-            File audioFile = new File(audioFilename);
-            if (!audioFile.exists()) {
-                // We should have an audio clip, but the file doesn't exist.
-                String errorMsg = c.getString(R.string.file_missing, audioFile);
-                Timber.e(errorMsg);
-                ToastUtils.showLongToast(errorMsg);
-                return;
-            }
-
-            try {
-                mediaPlayer.reset();
-                mediaPlayer.setDataSource(audioFilename);
-                mediaPlayer.prepare();
-                mediaPlayer.start();
-            } catch (IOException e) {
-                String errorMsg = c.getString(R.string.audio_file_invalid, audioFilename);
-                Timber.e(errorMsg);
-                ToastUtils.showLongToast(errorMsg);
-            }
         }
     }
 }
