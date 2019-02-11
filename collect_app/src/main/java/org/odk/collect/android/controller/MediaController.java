6a6a99ef4004f542dff5e96e89580e537e81cb18
Shobhit Agarwal <shobhitagarwal1612@gmail.com>
2019-02-11 19:24:11 +0530

    Adds a method to play audio using uri

--- collect_app/src/main/java/org/odk/collect/android/controller/MediaController.java
+++ collect_app/src/main/java/org/odk/collect/android/controller/MediaController.java
@@ -15,19 +15,33 @@
  */
 package org.odk.collect.android.controller;
 
+import android.content.Context;
 import android.media.MediaPlayer;
+import android.support.annotation.NonNull;
 
+import org.javarosa.core.reference.InvalidReferenceException;
+import org.javarosa.core.reference.ReferenceManager;
+import org.odk.collect.android.R;
 import org.odk.collect.android.events.MediaEvent;
 import org.odk.collect.android.events.RxEventBus;
+import org.odk.collect.android.utilities.ToastUtils;
+
+import java.io.File;
+import java.io.IOException;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+
+import timber.log.Timber;
 
 @Singleton
 public final class MediaController implements MediaPlayer.OnCompletionListener, MediaPlayer.OnErrorListener {
 
     public static final int MEDIA_COMPLETED = 100;
     public static final int MEDIA_ERROR = 101;
+
+    @Inject
+    Context context;
 
     @Inject
     RxEventBus rxEventBus;
@@ -50,5 +64,32 @@ public final class MediaController implements MediaPlayer.OnCompletionListener, 
     public boolean onError(MediaPlayer mp, int what, int extra) {
         rxEventBus.post(new MediaEvent(MEDIA_ERROR));
         return false;
+    }
+
+    public void playAudio(@NonNull String uri) {
+        String audioFilename = "";
+        try {
+            audioFilename = ReferenceManager.instance().deriveReference(uri).getLocalURI();
+        } catch (InvalidReferenceException e) {
+            Timber.e(e);
+        }
+
+        File audioFile = new File(audioFilename);
+        if (audioFile.exists()) {
+            try {
+                player.reset();
+                player.setDataSource(audioFilename);
+                player.prepare();
+                player.start();
+            } catch (IOException e) {
+                String errorMsg = context.getString(R.string.audio_file_invalid, audioFilename);
+                Timber.e(errorMsg);
+                ToastUtils.showLongToast(errorMsg);
+            }
+        } else {
+            String errorMsg = context.getString(R.string.file_missing, audioFile);
+            Timber.e(errorMsg);
+            ToastUtils.showLongToast(errorMsg);
+        }
     }
 }
