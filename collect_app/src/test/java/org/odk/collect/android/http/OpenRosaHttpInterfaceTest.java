8f8c97475e0dd99cbc0d2f54133bab26f9ff1e4f
Callum Stott <callum@seadowg.com>
2019-02-26 11:19:39 +0000

    Add basic tests for OpenRosaHttpInterface.executeGetRequest

---
+++ collect_app/src/test/java/org/odk/collect/android/http/OpenRosaHttpInterfaceTest.java
@@ -0,0 +1,84 @@
+package org.odk.collect.android.http;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.odk.collect.android.utilities.FileUtils;
+
+import java.io.ByteArrayInputStream;
+import java.net.URI;
+import java.nio.charset.Charset;
+
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public abstract class OpenRosaHttpInterfaceTest {
+
+    protected abstract OpenRosaHttpInterface subject();
+
+    private MockWebServer mockWebServer = new MockWebServer();
+
+    @Before
+    public void setup() throws Exception {
+        mockWebServer.start();
+    }
+
+    @After
+    public void teardown() throws Exception {
+        mockWebServer.shutdown();
+    }
+
+    @Test
+    public void executeGetRequest_makesAGetRequestToUri() throws Exception {
+        mockWebServer.enqueue(new MockResponse());
+
+        URI uri = mockWebServer.url("/blah").uri();
+        subject().executeGetRequest(uri, null, null);
+
+        assertThat(mockWebServer.getRequestCount(), equalTo(1));
+
+        RecordedRequest request = mockWebServer.takeRequest();
+        assertThat(request.getMethod(), equalTo("GET"));
+        assertThat(request.getRequestUrl().uri(), equalTo(uri));
+    }
+
+    @Test
+    public void executeGetRequest_returnsBodyWithEmptyHash() throws Exception {
+        mockWebServer.enqueue(new MockResponse()
+                .setBody("I AM BODY"));
+
+        HttpGetResult result = subject().executeGetRequest(mockWebServer.url("").uri(), null, null);
+        assertThat(IOUtils.toString(result.getInputStream(), Charset.defaultCharset()), equalTo("I AM BODY"));
+        assertThat(result.getHash(), equalTo(""));
+    }
+
+    @Test
+    public void executeGetRequest_whenContentTypeIsXML_returnsBodyWithMD5Hash() throws Exception {
+        mockWebServer.enqueue(new MockResponse()
+                .addHeader("Content-Type", "text/xml")
+                .setBody("I AM BODY"));
+
+        HttpGetResult result = subject().executeGetRequest(mockWebServer.url("").uri(), "text/xml", null);
+        assertThat(IOUtils.toString(result.getInputStream(), Charset.defaultCharset()), equalTo("I AM BODY"));
+        assertThat(result.getHash(), equalTo(FileUtils.getMd5Hash(new ByteArrayInputStream("I AM BODY".getBytes()))));
+    }
+
+    @Test
+    public void executeGetRequest_returnsOpenRosaVersion() throws Exception {
+        mockWebServer.enqueue(new MockResponse()
+                .addHeader("X-OpenRosa-Version", "1.0"));
+
+        HttpGetResult result1 = subject().executeGetRequest(mockWebServer.url("").uri(), null, null);
+        assertThat(result1.isOpenRosaResponse(), equalTo(true));
+
+        mockWebServer.enqueue(new MockResponse());
+
+        HttpGetResult result2 = subject().executeGetRequest(mockWebServer.url("").uri(), null, null);
+        assertThat(result2.isOpenRosaResponse(), equalTo(false));
+    }
+}
