134b7051d3a5f72224ab10fa02af706b861fd074
Yaw Anokwa <yanokwa@gmail.com>
2019-03-26 19:29:47 -0700

    Enough data on selected splash image has been collected to decide on deprecation

--- collect_app/src/main/java/org/odk/collect/android/preferences/UserInterfacePreferences.java
+++ collect_app/src/main/java/org/odk/collect/android/preferences/UserInterfacePreferences.java
@@ -25,18 +25,14 @@ import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.view.View;
 
-import com.google.android.gms.analytics.HitBuilders;
 import com.google.common.collect.ObjectArrays;
 
 import org.odk.collect.android.R;
 import org.odk.collect.android.activities.MainMenuActivity;
-import org.odk.collect.android.application.Collect;
 import org.odk.collect.android.spatial.MapHelper;
-import org.odk.collect.android.utilities.FileUtils;
 import org.odk.collect.android.utilities.LocaleHelper;
 import org.odk.collect.android.utilities.MediaUtils;
 
-import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
 import java.util.TreeMap;
 
@@ -253,17 +249,9 @@ public class UserInterfacePreferences extends BasePreferenceFragment {
                 String sourceMediaPath = MediaUtils.getPathFromUri(getActivity(), selectedMedia,
                         MediaStore.Images.Media.DATA);
 
-                String sourceMediaPathHash = FileUtils.getMd5Hash(new ByteArrayInputStream(sourceMediaPath.getBytes()));
-
                 // setting image path
                 setSplashPath(sourceMediaPath);
 
-                Collect.getInstance().getDefaultTracker()
-                        .send(new HitBuilders.EventBuilder()
-                                .setCategory("PreferenceChange")
-                                .setAction("Selected splash image")
-                                .setLabel(sourceMediaPathHash)
-                                .build());
                 break;
         }
     }
