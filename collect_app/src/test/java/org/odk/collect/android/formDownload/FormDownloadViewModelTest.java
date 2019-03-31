45435e567ca0c86dd4a8a9ae700f66daaadcee09
Shobhit Agarwal <shobhitagarwal1612@gmail.com>
2019-03-31 15:37:12 +0530

    Improve test name

--- collect_app/src/test/java/org/odk/collect/android/formDownload/FormDownloadViewModelTest.java
+++ collect_app/src/test/java/org/odk/collect/android/formDownload/FormDownloadViewModelTest.java
@@ -29,7 +29,7 @@ public class FormDownloadViewModelTest {
     }
 
     @Test
-    public void alertDialogTest() {
+    public void doNotDisplayAlertDialogOnInitTest() {
         viewModel.getAlertDialog().subscribe(alertDialogTestSubscriber);
 
         alertDialogTestSubscriber.assertNoValues();
