ab555796b48140280a4e51a9ec1d2feac85d13f3
Shobhit Agarwal <shobhitagarwal1612@gmail.com>
2019-04-01 01:24:10 +0530

    Add FormDownloadRepository for ViewModel to trigger actions
    We need to shift control from view to viewModel to improve testability and reduce complexity due to state management. We need not check for task state as the ViewModel won't be destroyed when the activity is killed

---
+++ collect_app/src/main/java/org/odk/collect/android/ui/formdownload/FormDownloadRepository.java
@@ -0,0 +1,33 @@
+package org.odk.collect.android.ui.formdownload;
+
+import org.odk.collect.android.logic.FormDetails;
+import org.odk.collect.android.utilities.DownloadFormListUtils;
+
+import java.util.HashMap;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import io.reactivex.Observable;
+
+@Singleton
+public class FormDownloadRepository {
+
+    private final DownloadFormListUtils downloadFormListUtils;
+    private boolean isLoading;
+
+    @Inject
+    public FormDownloadRepository() {
+        downloadFormListUtils = new DownloadFormListUtils();
+    }
+
+    Observable<HashMap<String, FormDetails>> downloadForms(String url, String username, String password) {
+        return Observable.fromCallable(() -> downloadFormListUtils.downloadFormList(url, username, password, false))
+                .doOnSubscribe(__ -> isLoading = true)
+                .doOnNext(__ -> isLoading = false);
+    }
+
+    public boolean isLoading() {
+        return isLoading;
+    }
+}
