b474b9bad710817e7ca0e8cbbb9c2cf6274e1699
grzegorz <gorczykowski@soldevelo.com>
2019-05-31 00:33:26 +0200

    Factored out setUpAnswerText() method

--- collect_app/src/main/java/org/odk/collect/android/widgets/BaseStringWidget.java
+++ collect_app/src/main/java/org/odk/collect/android/widgets/BaseStringWidget.java
@@ -38,35 +38,8 @@ public abstract class BaseStringWidget extends QuestionWidget {
 
     public BaseStringWidget(Context context, FormEntryPrompt prompt) {
         super(context, prompt);
-        answerText = new EditText(context);
-        answerText.setId(ViewIds.generateViewId());
-        answerText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, getAnswerFontSize());
 
-        // needed to make long read only text scroll
-        answerText.setHorizontallyScrolling(false);
-        answerText.setSingleLine(false);
-
-        TableLayout.LayoutParams params = new TableLayout.LayoutParams();
-        params.setMargins(7, 5, 7, 5);
-        answerText.setLayoutParams(params);
-
-        answerText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                widgetValueChanged();
-            }
-        });
-
+        setUpAnswerText();
         setDisplayValueFromModel();
     }
 
@@ -111,5 +84,36 @@ public abstract class BaseStringWidget extends QuestionWidget {
             answerText.setText(currentAnswer);
             Selection.setSelection(answerText.getText(), answerText.getText().toString().length());
         }
+    }
+
+    private void setUpAnswerText() {
+        answerText = new EditText(getContext());
+        answerText.setId(ViewIds.generateViewId());
+        answerText.setTextSize(TypedValue.COMPLEX_UNIT_DIP, getAnswerFontSize());
+
+        // needed to make long read only text scroll
+        answerText.setHorizontallyScrolling(false);
+        answerText.setSingleLine(false);
+
+        TableLayout.LayoutParams params = new TableLayout.LayoutParams();
+        params.setMargins(7, 5, 7, 5);
+        answerText.setLayoutParams(params);
+
+        answerText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                widgetValueChanged();
+            }
+        });
     }
 }
