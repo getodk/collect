210a1ca6b9a82831425283ff9aef05b5d475bf19
Hélène Martin <lognaturel@gmail.com>
2019-07-03 22:23:03 -0700

    Reuse no-accuracy variant in accuracy variant

--- collect_app/src/test/java/org/odk/collect/android/location/LocationTestUtils.java
+++ collect_app/src/test/java/org/odk/collect/android/location/LocationTestUtils.java
@@ -7,20 +7,17 @@ public class LocationTestUtils {
 
     }
 
-    public static Location createLocation(String provider, double lat, double lon, double alt, float sd) {
-        Location location = new Location(provider);
-        location.setLatitude(lat);
-        location.setLongitude(lon);
-        location.setAltitude(alt);
-        location.setAccuracy(sd);
-        return location;
-    }
-
     public static Location createLocation(String provider, double lat, double lon, double alt) {
         Location location = new Location(provider);
         location.setLatitude(lat);
         location.setLongitude(lon);
         location.setAltitude(alt);
+        return location;
+    }
+
+    public static Location createLocation(String provider, double lat, double lon, double alt, float sd) {
+        Location location = createLocation(provider, lat, lon, alt);
+        location.setAccuracy(sd);
         return location;
     }
 }
