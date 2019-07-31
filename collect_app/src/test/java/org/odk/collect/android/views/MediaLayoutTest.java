20a5d24e3b1fd0e007edbe4e5f77299a6c526033
Callum Stott <callum@seadowg.com>
2019-07-31 17:47:18 +0100

    Use mock AudioButtonManager in MediaLayoutTest

--- collect_app/src/test/java/org/odk/collect/android/views/MediaLayoutTest.java
+++ collect_app/src/test/java/org/odk/collect/android/views/MediaLayoutTest.java
@@ -3,6 +3,7 @@ package org.odk.collect.android.views;
 import android.widget.TextView;
 
 import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.MutableLiveData;
 
 import org.javarosa.core.reference.InvalidReferenceException;
 import org.javarosa.core.reference.Reference;
@@ -13,16 +14,14 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
-import org.odk.collect.android.R;
 import org.odk.collect.android.audio.AudioButtonManager;
 import org.odk.collect.android.support.RobolectricHelpers;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.shadows.ShadowMediaPlayer;
-import org.robolectric.shadows.util.DataSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.not;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -35,11 +34,18 @@ public class MediaLayoutTest {
     @Mock
     public ReferenceManager referenceManager;
 
+    @Mock
+    public AudioButtonManager audioButtonManager;
+
     @Test
     public void withTextView_andAudio_playingAudio_highlightsText() throws Exception {
-        FragmentActivity activity = RobolectricHelpers.createThemedActivity();
         setupAudioFile("file://audio.mp3", referenceManager);
 
+        MutableLiveData<Boolean> isPlaying = new MutableLiveData<>();
+        isPlaying.setValue(false);
+        when(audioButtonManager.setAudio(any(), any(), any(), any(), any())).thenReturn(isPlaying);
+
+        FragmentActivity activity = RobolectricHelpers.createThemedActivity();
         MediaLayout mediaLayout = new MediaLayout(activity);
         mediaLayout.setAVT(
                 new TextView(activity),
@@ -48,21 +54,20 @@ public class MediaLayoutTest {
                 null,
                 null,
                 referenceManager,
-                new AudioButtonManager());
+                audioButtonManager);
 
         int originalTextColor = mediaLayout.getTextView().getCurrentTextColor();
 
-        mediaLayout.findViewById(R.id.audioButton).performClick();
+        isPlaying.setValue(true);
         int textColor = mediaLayout.getTextView().getCurrentTextColor();
         assertThat(textColor, not(equalTo(originalTextColor)));
 
-        mediaLayout.findViewById(R.id.audioButton).performClick();
+        isPlaying.setValue(false);
         textColor = mediaLayout.getTextView().getCurrentTextColor();
         assertThat(textColor, equalTo(originalTextColor));
     }
 
     private static void setupAudioFile(String audioURI, ReferenceManager referenceManager) throws InvalidReferenceException {
-        ShadowMediaPlayer.addMediaInfo(DataSource.toDataSource(audioURI), new ShadowMediaPlayer.MediaInfo());
         Reference reference = mock(Reference.class);
         when(reference.getLocalURI()).thenReturn(audioURI);
         when(referenceManager.deriveReference(audioURI)).thenReturn(reference);
