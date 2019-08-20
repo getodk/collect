45a198af610d55095d73e35ebb9dac865260f104
grzegorz <gorczykowski@soldevelo.com>
2019-08-20 16:08:29 +0200

    Added FormHierarchyTest1

---
+++ collect_app/src/androidTest/java/org/odk/collect/android/formentry/FormHierarchyTest1.java
@@ -0,0 +1,50 @@
+package org.odk.collect.android.formentry;
+
+import android.Manifest;
+
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.rule.GrantPermissionRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.odk.collect.android.R;
+import org.odk.collect.android.activities.FormEntryActivity;
+import org.odk.collect.android.espressoutils.FormEntry;
+import org.odk.collect.android.support.CopyFormRule;
+import org.odk.collect.android.support.ResetStateRule;
+import org.odk.collect.android.test.FormLoadingUtils;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.odk.collect.android.support.matchers.RecyclerViewMatcher.withRecyclerView;
+
+// https://github.com/opendatakit/collect/issues/2871
+public class FormHierarchyTest1 {
+
+    @Rule
+    public IntentsTestRule<FormEntryActivity> activityTestRule = FormLoadingUtils.getFormActivityTestRuleFor("formHierarchy1.xml");
+
+    @Rule
+    public RuleChain copyFormChain = RuleChain
+            .outerRule(GrantPermissionRule.grant(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            )
+            .around(new ResetStateRule())
+            .around(new CopyFormRule("formHierarchy1.xml", null));
+
+    @Test
+    public void questionsDisplayedInHierarchyTest() {
+        FormEntry.clickGoToIconInForm();
+
+        onView(withRecyclerView(R.id.list)
+                .atPositionOnView(0, R.id.primary_text))
+                .check(matches(withText("what is your name?")));
+
+        onView(withRecyclerView(R.id.list)
+                .atPositionOnView(1, R.id.primary_text))
+                .check(matches(withText("what is your age?")));
+    }
+}
