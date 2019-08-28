ec6429ffda7402a44f31173099195538dbc57118
Callum Stott <callum@seadowg.com>
2019-08-28 12:08:49 +0100

    Inline FormEntry helpers in FormEntryPage

--- collect_app/src/androidTest/java/org/odk/collect/android/espressoutils/pages/FormEntryPage.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/espressoutils/pages/FormEntryPage.java
@@ -1,20 +1,24 @@
 package org.odk.collect.android.espressoutils.pages;
 
+import androidx.test.espresso.Espresso;
 import androidx.test.rule.ActivityTestRule;
 
 import org.odk.collect.android.R;
-import org.odk.collect.android.espressoutils.FormEntry;
+import org.odk.collect.android.support.ActivityHelpers;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.swipeLeft;
 import static androidx.test.espresso.action.ViewActions.swipeRight;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.core.StringEndsWith.endsWith;
 
 public class FormEntryPage extends Page<FormEntryPage> {
 
@@ -32,17 +36,17 @@ public class FormEntryPage extends Page<FormEntryPage> {
     }
 
     public FormEntryPage clickOnGoToIconInForm() {
-        FormEntry.clickGoToIconInForm();
+        onView(withId(R.id.menu_goto)).perform(click());
         return this;
     }
 
     public FormEntryPage clickJumpEndButton() {
-        FormEntry.clickJumpEndButton();
+        onView(withId(R.id.jumpEndButton)).perform(click());
         return this;
     }
 
     public MainMenuPage clickSaveAndExit() {
-        FormEntry.clickSaveAndExit();
+        onView(withId(R.id.save_exit_button)).perform(click());
         return new MainMenuPage(rule).assertOnPage();
     }
 
@@ -55,9 +59,9 @@ public class FormEntryPage extends Page<FormEntryPage> {
         onView(withId(R.id.questionholder)).perform(swipeLeft());
         return new ErrorDialog(rule);
     }
-    
+
     public FormEntryPage clickOptionsIcon() {
-        FormEntry.clickOptionsIcon();
+        Espresso.openActionBarOverflowOrOptionsMenu(ActivityHelpers.getActivity());
         return this;
     }
 
@@ -67,12 +71,13 @@ public class FormEntryPage extends Page<FormEntryPage> {
     }
 
     public FormEntryPage checkAreNavigationButtonsDisplayed() {
-        FormEntry.checkAreNavigationButtonsDisplayed();
+        onView(withId(R.id.form_forward_button)).check(matches(isDisplayed()));
+        onView(withId(R.id.form_back_button)).check(matches(isDisplayed()));
         return this;
     }
 
     public FormEntryPage putText(String text) {
-        FormEntry.putText(text);
+        onView(withClassName(endsWith("EditText"))).perform(replaceText(text));
         return this;
     }
 
@@ -82,7 +87,7 @@ public class FormEntryPage extends Page<FormEntryPage> {
     }
 
     public FormEntryPage clickGoToIconInForm() {
-        FormEntry.clickGoToIconInForm();
+        onView(withId(R.id.menu_goto)).perform(click());
         return this;
     }
 }
