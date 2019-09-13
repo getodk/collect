9f55632583cf380c326365a180762bdf58b396da
grzegorz <gorczykowski@soldevelo.com>
2019-09-13 20:55:30 +0200

    Implemented adjustEditTextAnswerToIntegerWidget() method to share code used in integer widgets

--- collect_app/src/main/java/org/odk/collect/android/utilities/TextWidgetUtils.java
+++ collect_app/src/main/java/org/odk/collect/android/utilities/TextWidgetUtils.java
@@ -1,10 +1,18 @@
 package org.odk.collect.android.utilities;
 
+import android.text.InputFilter;
+import android.text.InputType;
+import android.text.Selection;
+import android.text.method.DigitsKeyListener;
+import android.widget.EditText;
+
 import org.javarosa.core.model.data.DecimalData;
 import org.javarosa.core.model.data.IAnswerData;
 import org.javarosa.core.model.data.IntegerData;
 import org.javarosa.core.model.data.StringData;
 import org.odk.collect.android.listeners.ThousandsSeparatorTextWatcher;
+
+import java.util.Locale;
 
 public class TextWidgetUtils {
 
@@ -84,6 +92,30 @@ public class TextWidgetUtils {
             } catch (Exception numberFormatException) {
                 return null;
             }
+        }
+    }
+
+    public static void adjustEditTextAnswerToIntegerWidget(EditText answerText, boolean useThousandSeparator, IAnswerData answerData) {
+        if (useThousandSeparator) {
+            answerText.addTextChangedListener(new ThousandsSeparatorTextWatcher(answerText));
+        }
+        answerText.setInputType(InputType.TYPE_NUMBER_FLAG_SIGNED);
+        // only allows numbers and no periods
+        answerText.setKeyListener(new DigitsKeyListener(true, false));
+        // ints can only hold 2,147,483,648. we allow 999,999,999
+        InputFilter[] fa = new InputFilter[1];
+        fa[0] = new InputFilter.LengthFilter(9);
+        if (useThousandSeparator) {
+            //11 since for a nine digit number , their will be 2 separators.
+            fa[0] = new InputFilter.LengthFilter(11);
+        }
+        answerText.setFilters(fa);
+
+        Integer i = TextWidgetUtils.getIntegerAnswerValueFromIAnswerData(answerData);
+
+        if (i != null) {
+            answerText.setText(String.format(Locale.US, "%d", i));
+            Selection.setSelection(answerText.getText(), answerText.getText().toString().length());
         }
     }
 }
--- collect_app/src/main/java/org/odk/collect/android/widgets/IntegerWidget.java
+++ collect_app/src/main/java/org/odk/collect/android/widgets/IntegerWidget.java
@@ -17,17 +17,11 @@ package org.odk.collect.android.widgets;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import androidx.annotation.NonNull;
-import android.text.InputFilter;
-import android.text.InputType;
-import android.text.Selection;
-import android.text.method.DigitsKeyListener;
 
 import org.javarosa.core.model.data.IAnswerData;
 import org.javarosa.form.api.FormEntryPrompt;
 import org.odk.collect.android.listeners.ThousandsSeparatorTextWatcher;
 import org.odk.collect.android.utilities.TextWidgetUtils;
-
-import java.util.Locale;
 
 /**
  * Widget that restricts values to integers.
@@ -41,32 +35,9 @@ public class IntegerWidget extends StringWidget {
 
     public IntegerWidget(Context context, FormEntryPrompt prompt, boolean readOnlyOverride, boolean useThousandSeparator) {
         super(context, prompt, readOnlyOverride);
-
-        answerText.setInputType(InputType.TYPE_NUMBER_FLAG_SIGNED);
-
         this.useThousandSeparator = useThousandSeparator;
-        if (useThousandSeparator) {
-            answerText.addTextChangedListener(new ThousandsSeparatorTextWatcher(answerText));
-        }
 
-        // only allows numbers and no periods
-        answerText.setKeyListener(new DigitsKeyListener(true, false));
-
-        // ints can only hold 2,147,483,648. we allow 999,999,999
-        InputFilter[] fa = new InputFilter[1];
-        fa[0] = new InputFilter.LengthFilter(9);
-        if (useThousandSeparator) {
-            //11 since for a nine digit number , their will be 2 separators.
-            fa[0] = new InputFilter.LengthFilter(11);
-        }
-        answerText.setFilters(fa);
-
-        Integer i = TextWidgetUtils.getIntegerAnswerValueFromIAnswerData(getFormEntryPrompt().getAnswerValue());
-
-        if (i != null) {
-            answerText.setText(String.format(Locale.US, "%d", i));
-            Selection.setSelection(answerText.getText(), answerText.getText().toString().length());
-        }
+        TextWidgetUtils.adjustEditTextAnswerToIntegerWidget(answerText, useThousandSeparator, getFormEntryPrompt().getAnswerValue());
     }
 
     @NonNull
