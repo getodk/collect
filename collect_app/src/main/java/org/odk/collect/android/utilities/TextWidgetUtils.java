a2fed8f3e1bb8ed699bf8e4170ca568affe0735d
grzegorz <gorczykowski@soldevelo.com>
2019-09-13 15:21:57 +0200

    Moved getDoubleAnswerValue() to helper class

--- collect_app/src/main/java/org/odk/collect/android/utilities/TextWidgetUtils.java
+++ collect_app/src/main/java/org/odk/collect/android/utilities/TextWidgetUtils.java
@@ -18,4 +18,19 @@ public class TextWidgetUtils {
         }
         return d;
     }
+
+    public static Double getDoubleAnswerValueFromIAnswerData(IAnswerData dataHolder) {
+        Double d = null;
+        if (dataHolder != null) {
+            Object dataValue = dataHolder.getValue();
+            if (dataValue != null) {
+                if (dataValue instanceof Integer) {
+                    d = (double) (Integer) dataValue;
+                } else {
+                    d = (Double) dataValue;
+                }
+            }
+        }
+        return d;
+    }
 }
