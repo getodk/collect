1d0322b91ac917d6328c7673e16a95d83b43d916
grzegorz <gorczykowski@soldevelo.com>
2019-09-16 21:53:58 +0200

    Fixed tests

--- collect_app/src/test/java/org/odk/collect/android/widgets/StringNumberWidgetTest.java
+++ collect_app/src/test/java/org/odk/collect/android/widgets/StringNumberWidgetTest.java
@@ -7,20 +7,15 @@ import net.bytebuddy.utility.RandomString;
 import org.javarosa.core.model.data.StringData;
 import org.odk.collect.android.widgets.base.GeneralStringWidgetTest;
 
-import java.util.Random;
-
 /**
  * @author James Knight
  */
-public class StringNumberWidgetTest
-        extends GeneralStringWidgetTest<StringNumberWidget, StringData> {
+public class StringNumberWidgetTest extends GeneralStringWidgetTest<StringNumberWidget, StringData> {
 
     @NonNull
     @Override
     public StringNumberWidget createWidget() {
-        Random random = new Random();
-        boolean useThousandSeparator = random.nextBoolean();
-        return new StringNumberWidget(activity, formEntryPrompt, false, useThousandSeparator);
+        return new StringNumberWidget(activity, formEntryPrompt, false);
     }
 
     @NonNull
