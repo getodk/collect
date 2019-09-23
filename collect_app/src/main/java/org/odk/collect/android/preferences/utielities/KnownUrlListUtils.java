d906593f33eb2d093dc8ec89145440b1784227ba
grzegorz <gorczykowski@soldevelo.com>
2019-09-23 14:01:08 +0200

    Store the server URL that was imported by QR code

---
+++ collect_app/src/main/java/org/odk/collect/android/preferences/utielities/KnownUrlListUtils.java
@@ -0,0 +1,44 @@
+package org.odk.collect.android.preferences.utielities;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.odk.collect.android.R;
+import org.odk.collect.android.application.Collect;
+import org.odk.collect.android.preferences.GeneralSharedPreferences;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class KnownUrlListUtils {
+    private static final String KNOWN_URL_LIST = "knownUrlList";
+
+    private KnownUrlListUtils() {
+    }
+
+    public static void addUrlToList(String url) {
+        List<String> urlList = getUrlList();
+
+        if (urlList.contains(url)) {
+            urlList.remove(url);
+        } else if (urlList.size() == 5) {
+            if (urlList.get(4).equals(Collect.getInstance().getString(R.string.default_server_url))) {
+                urlList.remove(3);
+            } else {
+                urlList.remove(4);
+            }
+        }
+
+        urlList.add(0, url);
+        GeneralSharedPreferences.getInstance().save(KNOWN_URL_LIST, new Gson().toJson(urlList));
+    }
+
+    public static List<String> getUrlList() {
+        String urlListString = (String) GeneralSharedPreferences.getInstance().get(KNOWN_URL_LIST);
+
+        return urlListString == null || urlListString.isEmpty()
+                ? new ArrayList<>(Collections.singletonList(Collect.getInstance().getString(R.string.default_server_url)))
+                : new Gson().fromJson(urlListString, new TypeToken<List<String>>() {}.getType());
+    }
+}
