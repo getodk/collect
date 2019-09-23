bfb50dfc8037291a1691138ef10bdda310650062
grzegorz <gorczykowski@soldevelo.com>
2019-09-24 00:37:14 +0200

    Code improvements

--- collect_app/src/main/java/org/odk/collect/android/widgets/QuestionWidget.java
+++ collect_app/src/main/java/org/odk/collect/android/widgets/QuestionWidget.java
@@ -44,6 +44,7 @@ import androidx.annotation.IdRes;
 import androidx.annotation.Nullable;
 
 import org.javarosa.core.model.FormIndex;
+import org.javarosa.core.model.QuestionDef;
 import org.javarosa.core.reference.ReferenceManager;
 import org.javarosa.form.api.FormEntryPrompt;
 import org.odk.collect.android.R;
@@ -626,25 +627,28 @@ public abstract class QuestionWidget
             }
         });
 
-        /*
-         * If a 'rows' attribute is on the input tag, set the minimum number of lines
-         * to display in the field to that value.
-         *
-         * I.e.,
-         * <input ref="foo" rows="5">
-         *   ...
-         * </input>
-         *
-         * will set the height of the EditText box to 5 rows high.
-         */
-        String height = prompt.getQuestion().getAdditionalAttribute(null, "rows");
-        if (height != null && height.length() != 0) {
-            try {
-                int rows = Integer.parseInt(height);
-                answerEditText.setMinLines(rows);
-                answerEditText.setGravity(Gravity.TOP); // to write test starting at the top of the edit area
-            } catch (Exception e) {
-                Timber.e("Unable to process the rows setting for the answerText field: %s", e.toString());
+        QuestionDef questionDef = prompt.getQuestion();
+        if (questionDef != null) {
+            /*
+             * If a 'rows' attribute is on the input tag, set the minimum number of lines
+             * to display in the field to that value.
+             *
+             * I.e.,
+             * <input ref="foo" rows="5">
+             *   ...
+             * </input>
+             *
+             * will set the height of the EditText box to 5 rows high.
+             */
+            String height = questionDef.getAdditionalAttribute(null, "rows");
+            if (height != null && height.length() != 0) {
+                try {
+                    int rows = Integer.parseInt(height);
+                    answerEditText.setMinLines(rows);
+                    answerEditText.setGravity(Gravity.TOP); // to write test starting at the top of the edit area
+                } catch (Exception e) {
+                    Timber.e("Unable to process the rows setting for the answerText field: %s", e.toString());
+                }
             }
         }
 
