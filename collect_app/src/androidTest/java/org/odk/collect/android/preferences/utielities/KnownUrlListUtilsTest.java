b413067ea8c730a444bb66513d821b3da6949dee
grzegorz <gorczykowski@soldevelo.com>
2019-09-25 15:12:26 +0200

    Added automated tests

---
+++ collect_app/src/androidTest/java/org/odk/collect/android/preferences/utielities/KnownUrlListUtilsTest.java
@@ -0,0 +1,72 @@
+package org.odk.collect.android.preferences.utielities;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.odk.collect.android.R;
+import org.odk.collect.android.preferences.GeneralSharedPreferences;
+
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.odk.collect.android.preferences.utielities.KnownUrlListUtils.KNOWN_URL_LIST;
+
+@RunWith(AndroidJUnit4.class)
+public class KnownUrlListUtilsTest {
+
+    @Before
+    public void setUp() {
+        GeneralSharedPreferences.getInstance().reset(KNOWN_URL_LIST);
+    }
+
+    @Test
+    public void urlListShouldContainOnlyDefaultUrlIfNothingHasBeenAdded() {
+        List<String> urlList = KnownUrlListUtils.getUrlList();
+        assertEquals(1, urlList.size());
+        assertEquals(InstrumentationRegistry.getInstrumentation().getTargetContext().getString(R.string.default_server_url), urlList.get(0));
+    }
+
+    @Test
+    public void theLastUrlShouldBeRemovedWhenTheListHasMoreThan5ElementsButNotTheDefaultUrl() {
+        KnownUrlListUtils.addUrlToList("Url1");
+        KnownUrlListUtils.addUrlToList("Url2");
+        KnownUrlListUtils.addUrlToList("Url3");
+        KnownUrlListUtils.addUrlToList("Url4");
+        KnownUrlListUtils.addUrlToList("Url5");
+        KnownUrlListUtils.addUrlToList("Url6");
+        KnownUrlListUtils.addUrlToList("Url7");
+
+        List<String> urlList = KnownUrlListUtils.getUrlList();
+        assertEquals(5, urlList.size());
+
+        assertEquals(InstrumentationRegistry.getInstrumentation().getTargetContext().getString(R.string.default_server_url), urlList.get(4));
+    }
+
+    @Test
+    public void urlListShouldContainUniqueValues() {
+        KnownUrlListUtils.addUrlToList("Url1");
+        KnownUrlListUtils.addUrlToList("Url1");
+        assertEquals(2, KnownUrlListUtils.getUrlList().size());
+    }
+
+    @Test
+    public void urlThatAlreadyExistsShouldBeMovedToTheTopOfTheList() {
+        KnownUrlListUtils.addUrlToList("Url1");
+        KnownUrlListUtils.addUrlToList("Url2");
+        List<String> urlList = KnownUrlListUtils.getUrlList();
+        assertEquals(3, urlList.size());
+        assertEquals("Url2", urlList.get(0));
+        assertEquals("Url1", urlList.get(1));
+        assertEquals(InstrumentationRegistry.getInstrumentation().getTargetContext().getString(R.string.default_server_url), urlList.get(2));
+
+        KnownUrlListUtils.addUrlToList("Url1");
+        urlList = KnownUrlListUtils.getUrlList();
+        assertEquals(3, urlList.size());
+        assertEquals("Url1", urlList.get(0));
+        assertEquals("Url2", urlList.get(1));
+        assertEquals(InstrumentationRegistry.getInstrumentation().getTargetContext().getString(R.string.default_server_url), urlList.get(2));
+    }
+}
