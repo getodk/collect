6a415981dd4704a316098483d435fa803eb52bcd
Abhishek <abhishekamrutelfs@gmail.com>
2019-10-21 14:45:38 +0530

    Added setting for captured location format #3093

--- collect_app/src/main/java/org/odk/collect/android/widgets/GeoPointWidget.java
+++ collect_app/src/main/java/org/odk/collect/android/widgets/GeoPointWidget.java
@@ -159,40 +159,6 @@ public class GeoPointWidget extends QuestionWidget implements BinaryWidget {
         return df.format(Double.valueOf(s));
     }
 
-    private String formatGps(double coordinates, String type) {
-        String location = Double.toString(coordinates);
-        String degreeSign = "°";
-        String degree = location.substring(0, location.indexOf('.'))
-                + degreeSign;
-        location = "0." + location.substring(location.indexOf('.') + 1);
-        double temp = Double.valueOf(location) * 60;
-        location = Double.toString(temp);
-        String mins = location.substring(0, location.indexOf('.')) + "'";
-
-        location = "0." + location.substring(location.indexOf('.') + 1);
-        temp = Double.valueOf(location) * 60;
-        location = Double.toString(temp);
-        String secs = location.substring(0, location.indexOf('.')) + '"';
-        if (type.equalsIgnoreCase("lon")) {
-            if (degree.startsWith("-")) {
-                degree = String.format(getContext()
-                        .getString(R.string.west), degree.replace("-", ""), mins, secs);
-            } else {
-                degree = String.format(getContext()
-                        .getString(R.string.east), degree.replace("-", ""), mins, secs);
-            }
-        } else {
-            if (degree.startsWith("-")) {
-                degree = String.format(getContext()
-                        .getString(R.string.south), degree.replace("-", ""), mins, secs);
-            } else {
-                degree = String.format(getContext()
-                        .getString(R.string.north), degree.replace("-", ""), mins, secs);
-            }
-        }
-        return degree;
-    }
-
     @Override
     public void setBinaryData(Object answer) {
         stringAnswer = (String) answer;
