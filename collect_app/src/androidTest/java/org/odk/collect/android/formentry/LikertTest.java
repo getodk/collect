acccd9adcb08c0b126dc8264c8fe683d01614190
estberg <estbergm@uw.edu>
2019-11-07 13:07:36 -0800

    still working on field update test...

--- collect_app/src/androidTest/java/org/odk/collect/android/formentry/LikertTest.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/formentry/LikertTest.java
@@ -20,8 +20,10 @@ import java.util.Collections;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isChecked;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isNotChecked;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -32,11 +34,12 @@ import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.anything;
 import static org.hamcrest.Matchers.endsWith;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.StringStartsWith.startsWith;
+import static org.odk.collect.android.test.CustomMatchers.withIndex;
 
 public class LikertTest {
     // having trouble getting correct labels, check the xslx and likert_test.xml file
     /* Test Cases:
-     * (2) Loading images
      * (3) FieldList Update
      */
     private static final String LIKERT_TEST_FORM = "likert_test.xml";
@@ -113,11 +116,53 @@ public class LikertTest {
         onView(allOf(withClassName(endsWith("RadioButton")), withTagValue(equalTo(1)))).check(matches(isNotChecked()));
     }
 
+    @Test
+    public void testImagesLoad() {
+        openWidgetList();
+        onView(withText("Likert Image Widget")).perform(click());
+
+        for (int i = 0; i < 5; i++) {
+            onView(allOf(withClassName(endsWith("ImageView")), withTagValue(equalTo(i)))).check(matches(isDisplayed()));
+        }
+    }
+
 //    @Test
-//    public void testImageLoad() {
+//    public void selectionChangeAtOneCascadeLevelWithMinimalAppearance_ShouldUpdateNextLevels() {
 //        openWidgetList();
-//        onView(withText("Likert Image Widget")).perform(click());
+//        onView(withText("Cascading likert")).perform(click());
+//        onView(withText(startsWith("Level1"))).perform(click());
 //
+//        // No choices should be shown for levels 2 and 3 when no selection is made for level 1
+//        onView(withText("A1")).check(doesNotExist());
+//        onView(withText("B1")).check(doesNotExist());
+//        onView(withText("C1")).check(doesNotExist());
+//        onView(withText("A1A")).check(doesNotExist());
+//
+//        // Selecting C for level 1 should only reveal options for C at level 2
+//        onView(withIndex(withClassName(endsWith("RadioButton")), 0)).perform(click());
+//        onView(withText("C")).perform(click());
+//        onView(withText("A1")).check(doesNotExist());
+//        onView(withText("B1")).check(doesNotExist());
+//        onView(withIndex(withClassName(endsWith("RadioButton")), 0)).perform(click());
+//        onView(withText("C1")).perform(click());
+//        onView(withText("A1A")).check(doesNotExist());
+//
+//        // Selecting A for level 1 should reveal options for A at level 2
+//        onView(withText("C")).perform(click());
+//        onView(withText("A")).perform(click());
+//        onView(withIndex(withClassName(endsWith("RadioButton")), 0)).perform(click());
+//        onView(withText("A1")).check(matches(isDisplayed()));
+//        onView(withText("A1A")).check(doesNotExist());
+//        onView(withText("B1")).check(doesNotExist());
+//        onView(withText("C1")).check(doesNotExist());
+//
+//        // Selecting A1 for level 2 should reveal options for A1 at level 3
+//        onView(withText("A1")).perform(click());
+//        onView(withIndex(withClassName(endsWith("RadioButton")), 0)).perform(click());
+//        onView(withText("A1A")).check(matches(isDisplayed()));
+//        onView(withText("B1A")).check(doesNotExist());
+//        onView(withText("B1")).check(doesNotExist());
+//        onView(withText("C1")).check(doesNotExist());
 //    }
 
     private void openWidgetList() {
