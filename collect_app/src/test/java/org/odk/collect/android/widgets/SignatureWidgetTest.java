c47adca03ed79bcfba05a6974e802e204cb287ef
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2019-12-16 21:26:20 +0100

    Added readOnly test for SignatureWidget

--- collect_app/src/test/java/org/odk/collect/android/widgets/SignatureWidgetTest.java
+++ collect_app/src/test/java/org/odk/collect/android/widgets/SignatureWidgetTest.java
@@ -1,6 +1,8 @@
 package org.odk.collect.android.widgets;
 
 import android.content.Intent;
+import android.view.View;
+
 import androidx.annotation.NonNull;
 
 import net.bytebuddy.utility.RandomString;
@@ -16,6 +18,7 @@ import org.odk.collect.android.widgets.base.FileWidgetTest;
 
 import java.io.File;
 
+import static junit.framework.TestCase.assertEquals;
 import static org.mockito.Mockito.when;
 
 /**
@@ -64,5 +67,14 @@ public class SignatureWidgetTest extends FileWidgetTest<SignatureWidget> {
         Intent intent = getIntentLaunchedByClick(R.id.simple_button);
         assertComponentEquals(activity, DrawActivity.class, intent);
         assertExtraEquals(DrawActivity.OPTION, DrawActivity.OPTION_SIGNATURE, intent);
+    }
+
+    @Test
+    public void readOnlyTest() {
+        when(formEntryPrompt.isReadOnly()).thenReturn(true);
+        when(formEntryPrompt.getAnswerText()).thenReturn("testPhoto.jpg");
+
+        assertEquals(View.GONE, getWidget().signButton.getVisibility());
+        assertEquals(View.VISIBLE, getWidget().imageView.getVisibility());
     }
 }
