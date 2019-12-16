21560e2c7c593bf43932ad895a6ed6e5d006ddaa
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2019-12-16 23:21:19 +0100

    Added readOnly test for ExStringlWidget

--- collect_app/src/test/java/org/odk/collect/android/widgets/ExStringWidgetTest.java
+++ collect_app/src/test/java/org/odk/collect/android/widgets/ExStringWidgetTest.java
@@ -1,13 +1,18 @@
 package org.odk.collect.android.widgets;
 
+import android.view.View;
+
 import androidx.annotation.NonNull;
 
 import net.bytebuddy.utility.RandomString;
 
 import org.javarosa.core.model.data.StringData;
+import org.junit.Test;
 import org.odk.collect.android.formentry.questions.QuestionDetails;
 import org.odk.collect.android.widgets.base.GeneralExStringWidgetTest;
 
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.mockito.Mockito.when;
 
 /**
@@ -32,5 +37,14 @@ public class ExStringWidgetTest extends GeneralExStringWidgetTest<ExStringWidget
     public void setUp() throws Exception {
         super.setUp();
         when(formEntryPrompt.getAppearanceHint()).thenReturn("");
+    }
+
+    @Test
+    public void readOnlyTest() {
+        when(formEntryPrompt.isReadOnly()).thenReturn(true);
+
+        assertEquals(View.GONE, getWidget().launchIntentButton.getVisibility());
+        assertEquals(View.VISIBLE, getWidget().answerText.getVisibility());
+        assertFalse(getWidget().answerText.isEnabled());
     }
 }
