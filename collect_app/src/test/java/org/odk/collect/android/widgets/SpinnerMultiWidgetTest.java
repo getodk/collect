d180c141043542f598dce25bb92163bc5d4ffe1d
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2019-12-16 23:54:08 +0100

    Added readOnly test for SpinnerMultiWidget

--- collect_app/src/test/java/org/odk/collect/android/widgets/SpinnerMultiWidgetTest.java
+++ collect_app/src/test/java/org/odk/collect/android/widgets/SpinnerMultiWidgetTest.java
@@ -1,11 +1,19 @@
 package org.odk.collect.android.widgets;
 
+import android.view.View;
+
 import androidx.annotation.NonNull;
 
+import junit.framework.TestCase;
+
 import org.javarosa.core.model.SelectChoice;
+import org.javarosa.core.model.data.SelectMultiData;
+import org.javarosa.core.model.data.helper.Selection;
+import org.junit.Test;
 import org.odk.collect.android.formentry.questions.QuestionDetails;
 import org.odk.collect.android.widgets.base.GeneralSelectMultiWidgetTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
@@ -26,9 +34,20 @@ public class SpinnerMultiWidgetTest extends GeneralSelectMultiWidgetTest<Spinner
         super.setUp();
 
         List<SelectChoice> selectChoices = getSelectChoices();
+        List<Selection> selections = new ArrayList<>();
         for (SelectChoice selectChoice : selectChoices) {
+            selections.add(selectChoice.selection());
             when(formEntryPrompt.getSelectChoiceText(selectChoice))
                     .thenReturn(selectChoice.getValue());
         }
+        when(formEntryPrompt.getAnswerValue()).thenReturn(new SelectMultiData(selections));
+    }
+
+    @Test
+    public void readOnlyTest() {
+        when(formEntryPrompt.isReadOnly()).thenReturn(true);
+
+        TestCase.assertEquals(View.GONE, getWidget().button.getVisibility());
+        TestCase.assertEquals(View.VISIBLE, getWidget().selectionText.getVisibility());
     }
 }
