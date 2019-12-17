af1d84f6fcc64c3a4a187b11fdeef0ae87ae2e35
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2019-12-17 21:11:42 +0100

    Added readOnly tests for ListWidget

--- collect_app/src/test/java/org/odk/collect/android/widgets/ListWidgetTest.java
+++ collect_app/src/test/java/org/odk/collect/android/widgets/ListWidgetTest.java
@@ -1,9 +1,17 @@
 package org.odk.collect.android.widgets;
 
+import android.view.View;
+import android.widget.RadioButton;
+
 import androidx.annotation.NonNull;
 
+import org.junit.Test;
 import org.odk.collect.android.formentry.questions.QuestionDetails;
 import org.odk.collect.android.widgets.base.GeneralSelectOneWidgetTest;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertFalse;
+import static org.mockito.Mockito.when;
 
 /**
  * @author James Knight
@@ -14,5 +22,15 @@ public class ListWidgetTest extends GeneralSelectOneWidgetTest<ListWidget> {
     @Override
     public ListWidget createWidget() {
         return new ListWidget(activity, new QuestionDetails(formEntryPrompt, "formAnalyticsID"), false, false);
+    }
+
+    @Test
+    public void readOnlyTest() {
+        when(formEntryPrompt.isReadOnly()).thenReturn(true);
+
+        for(RadioButton radioButton : getWidget().buttons) {
+            assertEquals(View.VISIBLE, radioButton.getVisibility());
+            assertFalse(radioButton.isEnabled());
+        }
     }
 }
