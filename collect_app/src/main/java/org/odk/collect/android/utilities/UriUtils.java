32cbd9cf467534429f1627299ffe41f8482591c3
Robert Morton <robert.morton@gmail.com>
2019-12-17 12:11:18 -0800

    Remove redundant comment that is also in FormsProvider.java

--- collect_app/src/main/java/org/odk/collect/android/utilities/UriUtils.java
+++ collect_app/src/main/java/org/odk/collect/android/utilities/UriUtils.java
@@ -6,8 +6,6 @@ public class UriUtils {
     // Prevent class from being constructed
     private UriUtils() {}
 
-    // Leading slashes are removed from paths to support minSdkVersion < 18:
-    // https://developer.android.com/reference/android/content/UriMatcher
     public  static String stripLeadingUriSlashes(@NonNull String path) {
         return path.replaceAll("^/+", "");
     }
