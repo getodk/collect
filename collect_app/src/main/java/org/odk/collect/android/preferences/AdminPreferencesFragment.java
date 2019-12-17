219478fcb7ea24681fabb19e0f5b315dc118f0b7
Callum Stott <callum@seadowg.com>
2019-12-17 11:46:39 +0000

    Fix borked rename

--- collect_app/src/main/java/org/odk/collect/android/preferences/AdminPreferencesFragment.java
+++ collect_app/src/main/java/org/odk/collect/android/preferences/AdminPreferencesFragment.java
@@ -144,7 +144,7 @@ public class AdminPreferencesFragment extends BasePreferenceFragment implements 
 
         if (fragment != null) {
             getActivity().getFragmentManager().beginTransaction()
-                    .replace(R.id.question_widget_container, fragment)
+                    .replace(R.id.preferences_fragment_container, fragment)
                     .addToBackStack(null)
                     .commit();
         }
@@ -221,7 +221,7 @@ public class AdminPreferencesFragment extends BasePreferenceFragment implements 
     }
 
     public void preventOtherWaysOfEditingForm() {
-        FormEntryAccessPreferences fragment = (FormEntryAccessPreferences) getFragmentManager().findFragmentById(R.id.question_widget_container);
+        FormEntryAccessPreferences fragment = (FormEntryAccessPreferences) getFragmentManager().findFragmentById(R.id.preferences_fragment_container);
         fragment.preventOtherWaysOfEditingForm();
     }
 }
