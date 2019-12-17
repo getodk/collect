b222bf5feeb99c4e41e3f3d7173d33b69c56111a
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2019-12-17 21:13:37 +0100

    Added readOnly tests for ListMultiWidget

--- collect_app/src/test/java/org/odk/collect/android/widgets/ListMultiWidgetTest.java
+++ collect_app/src/test/java/org/odk/collect/android/widgets/ListMultiWidgetTest.java
@@ -1,9 +1,17 @@
 package org.odk.collect.android.widgets;
 
+import android.view.View;
+import android.widget.CheckBox;
+
 import androidx.annotation.NonNull;
 
+import org.junit.Test;
 import org.odk.collect.android.formentry.questions.QuestionDetails;
 import org.odk.collect.android.widgets.base.GeneralSelectMultiWidgetTest;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertFalse;
+import static org.mockito.Mockito.when;
 
 /**
  * @author James Knight
@@ -14,5 +22,15 @@ public class ListMultiWidgetTest extends GeneralSelectMultiWidgetTest<ListMultiW
     @Override
     public ListMultiWidget createWidget() {
         return new ListMultiWidget(activity, new QuestionDetails(formEntryPrompt, "formAnalyticsID"), true);
+    }
+
+    @Test
+    public void readOnlyTest() {
+        when(formEntryPrompt.isReadOnly()).thenReturn(true);
+
+        for(CheckBox checkBox : getWidget().checkBoxes) {
+            assertEquals(View.VISIBLE, checkBox.getVisibility());
+            assertFalse(checkBox.isEnabled());
+        }
     }
 }
