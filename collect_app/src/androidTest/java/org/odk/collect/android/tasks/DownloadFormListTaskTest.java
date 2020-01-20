4d516905bd03d92658a3b809f41f1b4f9f1fdf7c
Callum Stott <callum@seadowg.com>
2020-01-20 15:57:49 +0000

    Update tests for new user agent

--- collect_app/src/androidTest/java/org/odk/collect/android/tasks/DownloadFormListTaskTest.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/tasks/DownloadFormListTaskTest.java
@@ -30,7 +30,7 @@ public class DownloadFormListTaskTest extends MockedServerTest {
         RecordedRequest r = nextRequest();
         assertEquals("GET", r.getMethod());
         assertEquals("/formList", r.getPath());
-        assertMatches("Dalvik/.* org.odk.collect.android/.*", r.getHeader("User-Agent"));
+        assertMatches("org.odk.collect.android/.* Dalvik/.*", r.getHeader("User-Agent"));
         assertEquals("1.0", r.getHeader("X-OpenRosa-Version"));
         assertEquals("gzip", r.getHeader("Accept-Encoding"));
 
--- collect_app/src/androidTest/java/org/odk/collect/android/tasks/InstanceServerUploaderTaskTest.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/tasks/InstanceServerUploaderTaskTest.java
@@ -64,7 +64,7 @@ public class InstanceServerUploaderTaskTest extends MockedServerTest {
             RecordedRequest r = nextRequest();
             assertEquals("HEAD", r.getMethod());
             assertMatches("/submission\\?deviceID=\\w+%3A\\w+", r.getPath());
-            assertMatches("Dalvik/.* org.odk.collect.android/.*", r.getHeader("User-Agent"));
+            assertMatches("org.odk.collect.android/.* Dalvik/.*", r.getHeader("User-Agent"));
             assertEquals("1.0", r.getHeader(OpenRosaConstants.VERSION_HEADER));
             assertTrue(r.getHeader("Accept-Encoding").contains("gzip"));
         }
@@ -74,7 +74,7 @@ public class InstanceServerUploaderTaskTest extends MockedServerTest {
             RecordedRequest r = nextRequest();
             assertEquals("POST", r.getMethod());
             assertMatches("/submission\\?deviceID=\\w+%3A\\w+", r.getPath());
-            assertMatches("Dalvik/.* org.odk.collect.android/.*", r.getHeader("User-Agent"));
+            assertMatches("org.odk.collect.android/.* Dalvik/.*", r.getHeader("User-Agent"));
             assertEquals("1.0", r.getHeader(OpenRosaConstants.VERSION_HEADER));
             assertTrue(r.getHeader("Accept-Encoding").contains("gzip"));
             assertMatches("multipart/form-data; boundary=.*", r.getHeader("Content-Type"));
