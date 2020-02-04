abe7f65a4c39aa7ba2ef59da653235197104623f
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2020-02-04 22:51:03 +0100

    Implemented isEnoughSpaceToPerformMigartion() method

--- collect_app/src/main/java/org/odk/collect/android/storage/StorageStateProvider.java
+++ collect_app/src/main/java/org/odk/collect/android/storage/StorageStateProvider.java
@@ -1,8 +1,14 @@
 package org.odk.collect.android.storage;
 
+import android.os.Build;
 import android.os.Environment;
+import android.os.StatFs;
 
 import org.odk.collect.android.preferences.GeneralSharedPreferences;
+
+import java.io.File;
+
+import timber.log.Timber;
 
 import static org.odk.collect.android.preferences.GeneralKeys.KEY_SCOPED_STORAGE_USED;
 
@@ -17,5 +23,51 @@ public class StorageStateProvider {
 
     boolean isStorageMounted() {
         return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+    }
+
+    public boolean isEnoughSpaceToPerformMigartion() {
+        try {
+            return getAvailableScopedStorageSize() > getOdkDirSize();
+        } catch (Exception | Error e) {
+            Timber.w(e);
+            return false;
+        }
+    }
+
+    private long getAvailableScopedStorageSize() {
+        String scopedStoragePath = new StoragePathProvider().getScopedExternalFilesDirPath();
+        if (scopedStoragePath.isEmpty()) {
+            return 0;
+        }
+
+        StatFs stat = new StatFs(scopedStoragePath);
+        long blockSize;
+        long availableBlocks;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            blockSize = stat.getBlockSizeLong();
+            availableBlocks = stat.getAvailableBlocksLong();
+        } else {
+            blockSize = stat.getBlockSize();
+            availableBlocks = stat.getAvailableBlocks();
+        }
+        return availableBlocks * blockSize;
+    }
+
+    private long getOdkDirSize() {
+        return getFolderSize(new File(new StoragePathProvider().getDirPath(StorageSubdirectory.ODK)));
+    }
+
+    private long getFolderSize(File directory) {
+        long length = 0;
+        if (directory != null && directory.exists()) {
+            for (File file : directory.listFiles()) {
+                if (file != null && file.exists()) {
+                    length += file.isFile()
+                            ? file.length()
+                            : getFolderSize(file);
+                }
+            }
+        }
+        return length;
     }
 }
