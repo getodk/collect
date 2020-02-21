b3a290e59ff821e94b96d3ae7bdfecfcbe11b6e5
Andrew Liu <andrewlliu@gmail.com>
2020-02-21 14:57:53 -0800

    testing for disabling qrcode menu iitem

--- collect_app/src/androidTest/java/org/odk/collect/android/settings/MainMenuSettingsTest.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/settings/MainMenuSettingsTest.java
@@ -5,45 +5,30 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.odk.collect.android.R;
 import org.odk.collect.android.activities.MainMenuActivity;
+import org.odk.collect.android.support.pages.AdminSettingsPage;
+import org.odk.collect.android.support.pages.MainMenuPage;
 
-import androidx.test.espresso.Espresso;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
-
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class MainMenuSettingsTest {
     @Rule
-    public ActivityTestRule<MainMenuActivity> activityRule =
-            new ActivityTestRule<>(MainMenuActivity.class);
+    public ActivityTestRule<MainMenuActivity> rule = new ActivityTestRule<>(MainMenuActivity.class);
 
     @Test
     public void configureWithQRCanBeDisabled() {
-        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
-
-        onView(withText(R.string.admin_preferences))
-                .perform(click());
-
-        onView(withText(R.string.main_menu_settings))
-                .perform(click());
-
-        onView(withText(R.string.qr_code))
-                .perform(click());
-
-        Espresso.pressBack();
-        Espresso.pressBack();
-
-        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
-
-        onView(withText(R.string.scan_qrcode_overflow_menu))
-                .check(doesNotExist());
+        new MainMenuPage(rule)
+                .clickOnMenu()
+                .clickAdminSettings()
+                .clickOnString(R.string.main_menu_settings)
+                .clickOnString(R.string.qr_code)
+                .pressBack(new AdminSettingsPage(rule))
+                .pressBack(new MainMenuPage(rule))
+                .clickOnMenu()
+                .checkIfTextDoesNotExist(R.string.scan_qrcode_overflow_menu);
     }
+
 }
