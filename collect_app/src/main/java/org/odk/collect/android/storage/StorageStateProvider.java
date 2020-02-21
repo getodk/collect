ea71d20a2c26a592559e7c10b91a90a008af9f0e
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2020-02-21 09:57:48 +0100

    Fixed build

--- collect_app/src/main/java/org/odk/collect/android/storage/StorageStateProvider.java
+++ collect_app/src/main/java/org/odk/collect/android/storage/StorageStateProvider.java
@@ -1,5 +1,6 @@
 package org.odk.collect.android.storage;
 
+import android.os.Build;
 import android.os.Environment;
 import android.os.StatFs;
 
@@ -45,7 +46,10 @@ public class StorageStateProvider {
         }
 
         StatFs stat = new StatFs(scopedStoragePath);
-        return stat.getAvailableBlocksLong() * stat.getBlockSizeLong();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            return stat.getAvailableBlocksLong() * stat.getBlockSizeLong();
+        }
+        return 0;
     }
 
     private long getOdkDirSize(StoragePathProvider storagePathProvider) {
