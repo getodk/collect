dba05b0af00ae247caeb648bd4f17a20cf58a5f8
stlp <stlp@lc-926.hsd1.wa.comcast.net>
2020-03-04 12:42:51 -0800

    added scan button in toolbar

--- collect_app/src/main/java/org/odk/collect/android/preferences/QRCodeTabs.java
+++ collect_app/src/main/java/org/odk/collect/android/preferences/QRCodeTabs.java
@@ -2,6 +2,8 @@ package org.odk.collect.android.preferences;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -19,6 +21,7 @@ import android.widget.ProgressBar;
 import org.odk.collect.android.BuildConfig;
 import org.odk.collect.android.R;
 import org.odk.collect.android.activities.CollectAbstractActivity;
+import org.odk.collect.android.activities.ScanQRCodeActivity;
 import org.odk.collect.android.adapters.TabAdapter;
 import org.odk.collect.android.application.Collect;
 import org.odk.collect.android.fragments.ShowQRCodeFragment;
@@ -52,8 +55,13 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.zxing.ChecksumException;
+import com.google.zxing.FormatException;
+import com.google.zxing.NotFoundException;
 
 public class QRCodeTabs extends CollectAbstractActivity {
+    private static final int SELECT_PHOTO = 111;
+
     private TabAdapter adapter;
     private TabLayout tabLayout;
     private ViewPager viewPager;
@@ -107,7 +115,55 @@ public class QRCodeTabs extends CollectAbstractActivity {
                     startActivity(Intent.createChooser(shareIntent, getString(R.string.share_qrcode)));
                 }
                 return true;
+            case R.id.menu_item_scan_sd_card:
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                if (photoPickerIntent.resolveActivity(this.getPackageManager()) != null) {
+                    startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+                } else {
+                    ToastUtils.showShortToast(getString(R.string.activity_not_found, getString(R.string.choose_image)));
+                    Timber.w(getString(R.string.activity_not_found, getString(R.string.choose_image)));
+                }
+                return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SELECT_PHOTO) {
+            if (resultCode == Activity.RESULT_OK) {
+                try {
+                    boolean qrCodeFound = false;
+                    final Uri imageUri = data.getData();
+                    if (imageUri != null) {
+                        final InputStream imageStream = this.getContentResolver()
+                                .openInputStream(imageUri);
+
+                        final Bitmap bitmap = BitmapFactory.decodeStream(imageStream);
+                        if (bitmap != null) {
+                            String response = QRCodeUtils.decodeFromBitmap(bitmap);
+                            if (response != null) {
+                                qrCodeFound = true;
+                                ScanQRCodeActivity.applySettings(this, response);
+                            }
+                        }
+                    }
+                    if (!qrCodeFound) {
+                        ToastUtils.showLongToast(R.string.qr_code_not_found);
+                    }
+                } catch (FormatException | NotFoundException | ChecksumException e) {
+                    Timber.i(e);
+                    ToastUtils.showLongToast(R.string.qr_code_not_found);
+                } catch (DataFormatException | IOException | OutOfMemoryError | IllegalArgumentException e) {
+                    Timber.e(e);
+                    ToastUtils.showShortToast(getString(R.string.invalid_qrcode));
+                }
+            } else {
+                Timber.i("Choosing QR code from sdcard cancelled");
+            }
+        }
     }
 }
