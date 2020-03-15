3c2cbfb5cde2759f9d751698d48c074b56ea7ab0
SaumiaSinghal <saumiasinghal@gmail.com>
2020-03-15 17:23:37 +0530

    fixed pmd

--- collect_app/src/main/java/org/odk/collect/android/preferences/CaptionedListPreference.java
+++ collect_app/src/main/java/org/odk/collect/android/preferences/CaptionedListPreference.java
@@ -4,7 +4,6 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Rect;
-import android.os.Parcelable;
 import android.preference.ListPreference;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
