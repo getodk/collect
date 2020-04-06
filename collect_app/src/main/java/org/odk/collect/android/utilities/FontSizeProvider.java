512fcc1dfe775e0510c51cbb052c933eeb84623d
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2020-04-06 13:01:13 +0200

    Improved getQuestionFontSize() method

--- collect_app/src/main/java/org/odk/collect/android/utilities/FontSizeProvider.java
+++ collect_app/src/main/java/org/odk/collect/android/utilities/FontSizeProvider.java
@@ -1,6 +1,5 @@
 package org.odk.collect.android.utilities;
 
-import org.odk.collect.android.application.Collect;
 import org.odk.collect.android.preferences.GeneralSharedPreferences;
 
 import static org.odk.collect.android.preferences.GeneralKeys.KEY_FONT_SIZE;
@@ -9,12 +8,10 @@ public class FontSizeProvider {
     public static final int DEFAULT_FONT_SIZE = 21;
 
     public static int getQuestionFontSize() {
-        // For testing:
-        Collect instance = Collect.getInstance();
-        if (instance == null) {
+        try {
+            return Integer.parseInt(String.valueOf(GeneralSharedPreferences.getInstance().get(KEY_FONT_SIZE)));
+        } catch (Exception | Error e) {
             return DEFAULT_FONT_SIZE;
         }
-
-        return Integer.parseInt(String.valueOf(GeneralSharedPreferences.getInstance().get(KEY_FONT_SIZE)));
     }
 }
