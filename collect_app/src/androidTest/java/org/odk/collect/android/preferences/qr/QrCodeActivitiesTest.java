29356d78cf88fb14dd2da397bc89b555c7b75b2c
Andrew Liu <andrewlliu@gmail.com>
2020-04-14 17:22:01 -0700

    fixed test

--- collect_app/src/androidTest/java/org/odk/collect/android/preferences/qr/QrCodeActivitiesTest.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/preferences/qr/QrCodeActivitiesTest.java
@@ -1,10 +1,14 @@
 package org.odk.collect.android.preferences.qr;
 
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentFactory;
 import androidx.fragment.app.testing.FragmentScenario;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.odk.collect.android.R;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -12,7 +16,10 @@ public class QrCodeActivitiesTest {
     @Test
     public void test() {
         FragmentScenario<QRScannerFragment> fs =
-                FragmentScenario.launchInContainer(QRScannerFragment.class);
+                FragmentScenario.launchInContainer(QRScannerFragment.class,
+                        new Bundle(),
+                        R.style.Theme_MaterialComponents,
+                        new FragmentFactory());
 
         fs.onFragment(new QrCodeFragmentAction());
 
