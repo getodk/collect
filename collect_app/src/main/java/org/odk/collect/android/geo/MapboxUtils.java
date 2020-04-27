71e8ba5dcc05f54052635460c444b5d0da22a429
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2020-04-27 23:41:42 +0200

    Fixed Mapbox to work offline

--- collect_app/src/main/java/org/odk/collect/android/geo/MapboxUtils.java
+++ collect_app/src/main/java/org/odk/collect/android/geo/MapboxUtils.java
@@ -23,6 +23,7 @@ public class MapboxUtils {
             // To use the Mapbox base maps, we have to initialize the Mapbox SDK with
             // an access token. Configure this token in collect_app/secrets.properties.
             mapbox = Mapbox.getInstance(Collect.getInstance(), BuildConfig.MAPBOX_ACCESS_TOKEN);
+            Mapbox.setConnected(true);
         } catch (Exception | Error e) {
             // To keep our APK from getting too big, we decided to include the
             // Mapbox native library only for the most common binary architectures.
