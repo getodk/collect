56a09f43747d7f39d2a48455acc9c734a16d6e08
SaumiaSinghal <saumiasinghal@gmail.com>
2020-05-13 20:24:50 +0530

    updated settings dialog fragment

--- collect_app/src/main/java/org/odk/collect/android/geo/SettingsDialogFragment.java
+++ collect_app/src/main/java/org/odk/collect/android/geo/SettingsDialogFragment.java
@@ -1,6 +1,32 @@
 package org.odk.collect.android.geo;
 
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
+import org.odk.collect.android.R;
+
 public class SettingsDialogFragment extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        super.onCreateDialog(savedInstanceState);
+
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(getString(R.string.input_method))
+                .setPositiveButton(getString(R.string.start), (dialog, id) -> {
+                    dialog.cancel();
+                    dismiss();
+                })
+                .setNegativeButton(R.string.cancel, (dialog, id) -> {
+                    dialog.cancel();
+                    dismiss();
+                })
+                .create();
+    }
 }
