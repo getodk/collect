502fa8bf8b7d6f7e9b0605ec1be8ff1f9c63198f
SaumiaSinghal <saumiasinghal@gmail.com>
2020-05-18 22:32:24 +0530

    updated deleteFormsTask

--- collect_app/src/main/java/org/odk/collect/android/listeners/DeleteFormsListener.java
+++ collect_app/src/main/java/org/odk/collect/android/listeners/DeleteFormsListener.java
@@ -22,4 +22,6 @@ package org.odk.collect.android.listeners;
  */
 public interface DeleteFormsListener {
     void deleteComplete(int deletedForms);
+
+    void progressUpdate(int progress, int total);
 }
