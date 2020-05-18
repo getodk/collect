e2d82ffea9471fab7a3cbb03b9cbaae8c7336ee5
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2020-05-18 12:51:13 +0200

    Factored out BARCODE_RESULT_KEY

--- collect_app/src/main/java/org/odk/collect/android/fragments/BarcodeWidgetScannerFragment.java
+++ collect_app/src/main/java/org/odk/collect/android/fragments/BarcodeWidgetScannerFragment.java
@@ -9,6 +9,8 @@ import com.journeyapps.barcodescanner.BarcodeResult;
 import java.util.Collection;
 
 public class BarcodeWidgetScannerFragment extends BaseCodeScannerFragment {
+    public static final String BARCODE_RESULT_KEY = "SCAN_RESULT";
+
     @Override
     protected Collection<String> getSupportedCodeFormats() {
         return IntentIntegrator.ALL_CODE_TYPES;
@@ -17,7 +19,7 @@ public class BarcodeWidgetScannerFragment extends BaseCodeScannerFragment {
     @Override
     protected void handleScanningResult(BarcodeResult result) {
         Intent returnIntent = new Intent();
-        returnIntent.putExtra("SCAN_RESULT", result.getText());
+        returnIntent.putExtra(BARCODE_RESULT_KEY, result.getText());
         getActivity().setResult(Activity.RESULT_OK, returnIntent);
         getActivity().finish();
     }
