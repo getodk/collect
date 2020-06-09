5aa3b4648d504c7ef54021ad59141750d9fd3c87
SaumiaSinghal <saumiasinghal@gmail.com>
2020-06-10 00:00:22 +0530

    make QuestionWidget as base class instead of BaseGeoWidget for GeoPointWidget

--- collect_app/src/main/java/org/odk/collect/android/widgets/GeoPointWidget.java
+++ collect_app/src/main/java/org/odk/collect/android/widgets/GeoPointWidget.java
@@ -18,15 +18,25 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import org.javarosa.core.model.QuestionDef;
 import org.javarosa.core.model.data.GeoPointData;
 import org.javarosa.core.model.data.IAnswerData;
+import org.javarosa.form.api.FormEntryPrompt;
 import org.odk.collect.android.R;
 import org.odk.collect.android.activities.GeoPointActivity;
 import org.odk.collect.android.activities.GeoPointMapActivity;
 import org.odk.collect.android.formentry.questions.QuestionDetails;
 import org.odk.collect.android.geo.MapConfigurator;
+import org.odk.collect.android.listeners.PermissionListener;
+import org.odk.collect.android.utilities.MultiClickGuard;
+import org.odk.collect.android.utilities.ThemeUtils;
 import org.odk.collect.android.widgets.utilities.GeoWidgetUtils;
 
 import static org.odk.collect.android.utilities.ApplicationConstants.RequestCodes;
@@ -42,26 +52,87 @@ import static org.odk.collect.android.utilities.WidgetAppearanceUtils.hasAppeara
  * @author Jon Nordling (jonnordling@gmail.com)
  */
 @SuppressLint("ViewConstructor")
-public class GeoPointWidget extends BaseGeoWidget {
+public class GeoPointWidget extends QuestionWidget {
     public static final String LOCATION = "gp";
     public static final String ACCURACY_THRESHOLD = "accuracyThreshold";
     public static final String READ_ONLY = "readOnly";
     public static final String DRAGGABLE_ONLY = "draggable";
-
     public static final double DEFAULT_LOCATION_ACCURACY = 5.0;
+
+    protected Button startGeoButton;
+    protected TextView answerDisplay;
+
     private final MapConfigurator mapConfigurator;
+    private boolean readOnly;
+    private boolean draggable = true;
     private boolean useMap;
     private double accuracyThreshold;
-    private boolean draggable = true;
     private String stringAnswer;
 
     public GeoPointWidget(Context context, QuestionDetails questionDetails, QuestionDef questionDef, MapConfigurator mapConfigurator) {
         super(context, questionDetails);
         this.mapConfigurator = mapConfigurator;
+        determineMapProperties(questionDef);
 
         stringAnswer = getFormEntryPrompt().getAnswerText();
-        determineMapProperties(questionDef);
-        updateButtonLabelsAndVisibility(stringAnswer != null);
+        boolean dataAvailable = false;
+        if (stringAnswer != null && !stringAnswer.isEmpty()) {
+            dataAvailable = true;
+            setBinaryData(stringAnswer);
+        }
+        updateButtonLabelsAndVisibility(dataAvailable);
+    }
+
+    @Override
+    protected View onCreateAnswerView(Context context, FormEntryPrompt prompt, int answerFontSize) {
+        ViewGroup answerView = (ViewGroup) LayoutInflater.from(context).inflate(R.layout.base_geo_widget_layout, null);
+
+        answerDisplay = answerView.findViewById(R.id.geo_answer_text);
+        answerDisplay.setTextColor(new ThemeUtils(context).getColorOnSurface());
+        answerDisplay.setTextSize(TypedValue.COMPLEX_UNIT_DIP, answerFontSize);
+
+        startGeoButton = answerView.findViewById(R.id.simple_button);
+
+        readOnly = getFormEntryPrompt().isReadOnly();
+        if (readOnly) {
+            startGeoButton.setVisibility(GONE);
+        } else {
+            startGeoButton.setText(getDefaultButtonLabel());
+            startGeoButton.setTextSize(TypedValue.COMPLEX_UNIT_DIP, answerFontSize);
+
+            startGeoButton.setOnClickListener(v -> onButtonClick());
+        }
+        return answerView;
+    }
+
+    @Override
+    public IAnswerData getAnswer() {
+        if (stringAnswer == null || stringAnswer.isEmpty()) {
+            return null;
+        } else {
+            return new GeoPointData(GeoWidgetUtils.getLocationParamsFromStringAnswer(stringAnswer));
+        }
+    }
+
+    @Override
+    public void clearAnswer() {
+        stringAnswer = null;
+        answerDisplay.setText(null);
+        updateButtonLabelsAndVisibility(false);
+        widgetValueChanged();
+    }
+
+    @Override
+    public void setOnLongClickListener(OnLongClickListener l) {
+        startGeoButton.setOnLongClickListener(l);
+        answerDisplay.setOnLongClickListener(l);
+    }
+
+    @Override
+    public void cancelLongPress() {
+        super.cancelLongPress();
+        startGeoButton.cancelLongPress();
+        answerDisplay.cancelLongPress();
     }
 
     private void determineMapProperties(QuestionDef questionDef) {
@@ -81,52 +152,19 @@ public class GeoPointWidget extends BaseGeoWidget {
         }
     }
 
-    public void updateButtonLabelsAndVisibility(boolean dataAvailable) {
-        if (useMap) {
-            if (getFormEntryPrompt().isReadOnly()) {
-                startGeoButton.setText(R.string.geopoint_view_read_only);
-            } else {
-                startGeoButton.setText(
-                        dataAvailable ? R.string.view_change_location : R.string.get_point);
-            }
-        } else {
-            if (!getFormEntryPrompt().isReadOnly()) {
-                startGeoButton.setText(
-                    dataAvailable ? R.string.change_location : R.string.get_point);
-            }
-        }
-    }
-
-    @Override
-    public IAnswerData getAnswer() {
-        if (stringAnswer == null || stringAnswer.isEmpty()) {
-            return null;
-        } else {
-            return new GeoPointData(GeoWidgetUtils.getLocationParamsFromStringAnswer(stringAnswer));
-        }
-    }
-
-    @Override
-    public void clearAnswer() {
-        stringAnswer = null;
-        super.clearAnswer();
-    }
-
-    @Override
-    public void setBinaryData(Object answer) {
+    private void setBinaryData(Object answer) {
         stringAnswer = (String) answer;
         answerDisplay.setText(getAnswerToDisplay(stringAnswer));
 
         if (answerDisplay.getText().toString().equals("")) {
-            stringAnswer = null;
+            stringAnswer = "";
         }
 
         updateButtonLabelsAndVisibility(stringAnswer != null);
         widgetValueChanged();
     }
 
-    @Override
-    public String getAnswerToDisplay(String answer) {
+    private String getAnswerToDisplay(String answer) {
         try {
             if (answer != null && !answer.isEmpty()) {
                 String[] parts = answer.split(" ");
@@ -144,12 +182,39 @@ public class GeoPointWidget extends BaseGeoWidget {
         return "";
     }
 
-    @Override
-    public String getDefaultButtonLabel() {
-        return getContext().getString(R.string.get_location);
+    private void updateButtonLabelsAndVisibility(boolean dataAvailable) {
+        if (useMap) {
+            if (readOnly) {
+                startGeoButton.setText(R.string.geopoint_view_read_only);
+            } else {
+                startGeoButton.setText(
+                        dataAvailable ? R.string.view_change_location : R.string.get_point);
+            }
+        } else {
+            if (!readOnly) {
+                startGeoButton.setText(
+                        dataAvailable ? R.string.change_location : R.string.get_point);
+            }
+        }
     }
 
-    public void startGeoActivity() {
+    private void onButtonClick() {
+        if (MultiClickGuard.allowClick(QuestionWidget.class.getName())) {
+            getPermissionUtils().requestLocationPermissions((Activity) getContext(), new PermissionListener() {
+                @Override
+                public void granted() {
+                    waitForData();
+                    startGeoActivity();
+                }
+
+                @Override
+                public void denied() {
+                }
+            });
+        }
+    }
+
+    private void startGeoActivity() {
         Context context = getContext();
         Intent intent = new Intent(
             context, useMap ? GeoPointMapActivity.class : GeoPointActivity.class);
@@ -162,5 +227,9 @@ public class GeoPointWidget extends BaseGeoWidget {
         intent.putExtra(ACCURACY_THRESHOLD, accuracyThreshold);
 
         ((Activity) context).startActivityForResult(intent, RequestCodes.LOCATION_CAPTURE);
+    }
+
+    private String getDefaultButtonLabel() {
+        return getContext().getString(R.string.get_location);
     }
 }
