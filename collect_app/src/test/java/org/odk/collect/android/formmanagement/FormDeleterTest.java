9afdcf7f06cf18169498c61ed7f752e87fe23df3
Callum Stott <callum@seadowg.com>
2020-07-28 17:00:33 +0100

    Make sure form soft delete is aware of form versions

--- collect_app/src/test/java/org/odk/collect/android/formmanagement/FormDeleterTest.java
+++ collect_app/src/test/java/org/odk/collect/android/formmanagement/FormDeleterTest.java
@@ -7,6 +7,8 @@ import org.odk.collect.android.provider.InstanceProviderAPI;
 import org.odk.collect.android.support.InMemFormsRepository;
 import org.odk.collect.android.support.InMemInstancesRepository;
 
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -31,5 +33,35 @@ public class FormDeleterTest {
 
         formDeleter.delete(1L);
         assertThat(formsRepository.getAll().isEmpty(), is(true));
+    }
+
+    @Test
+    public void whenOtherVersionOfFormHasInstances_deletesForm() {
+        InMemFormsRepository formsRepository = new InMemFormsRepository();
+        InMemInstancesRepository instancesRepository = new InMemInstancesRepository();
+        FormDeleter formDeleter = new FormDeleter(formsRepository, instancesRepository);
+
+        formsRepository.save(new Form.Builder()
+                .id(1L)
+                .jrFormId("1")
+                .jrVersion("old")
+                .build());
+
+        formsRepository.save(new Form.Builder()
+                .id(2L)
+                .jrFormId("1")
+                .jrVersion("new")
+                .build());
+
+        instancesRepository.addInstance(new Instance.Builder()
+                .jrFormId("1")
+                .jrVersion("old")
+                .status(InstanceProviderAPI.STATUS_COMPLETE)
+                .build());
+
+        formDeleter.delete(2L);
+        List<Form> forms = formsRepository.getAll();
+        assertThat(forms.size(), is(1));
+        assertThat(forms.get(0).getJrVersion(), is("old"));
     }
 }
