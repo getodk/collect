99f694114a16e4e59155c8bd7cd1650217c36a81
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2020-07-29 14:50:44 +0200

    Added tests to SelectOneMinimalWidgetTest

--- collect_app/src/test/java/org/odk/collect/android/widgets/SelectOneMinimalWidgetTest.java
+++ collect_app/src/test/java/org/odk/collect/android/widgets/SelectOneMinimalWidgetTest.java
@@ -4,9 +4,13 @@ import android.view.View;
 
 import androidx.annotation.NonNull;
 
+import org.javarosa.core.model.data.SelectOneData;
+import org.javarosa.core.model.data.helper.Selection;
 import org.junit.Test;
 import org.odk.collect.android.formentry.questions.QuestionDetails;
 import org.odk.collect.android.widgets.base.GeneralSelectOneWidgetTest;
+
+import java.util.Collections;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -22,8 +26,34 @@ public class SelectOneMinimalWidgetTest extends GeneralSelectOneWidgetTest<Selec
     @Test
     public void usingReadOnlyOptionShouldMakeAllClickableElementsDisabled() {
         when(formEntryPrompt.isReadOnly()).thenReturn(true);
-
         assertThat(getSpyWidget().binding.choicesSearchBox.getVisibility(), is(View.VISIBLE));
         assertThat(getSpyWidget().binding.choicesSearchBox.isEnabled(), is(Boolean.FALSE));
+    }
+
+    @Test
+    public void whenThereIsNoAnswer_shouldDefaultTextBeDisplayed() {
+        assertThat(getSpyWidget().binding.choicesSearchBox.getText().toString(), is("Select Answer"));
+    }
+
+    @Test
+    public void whenThereIsAnswer_shouldSelectedChoicesBeDisplayed() {
+        SelectOneData answer = getInitialAnswer();
+        Selection selectedChoice = (Selection) answer.getValue();
+        when(formEntryPrompt.getAnswerValue()).thenReturn(answer);
+        when(formEntryPrompt.getSelectItemText(selectedChoice)).thenReturn(selectedChoice.getValue());
+
+        assertThat(getSpyWidget().binding.choicesSearchBox.getText().toString(), is(selectedChoice.getValue()));
+    }
+
+    @Test
+    public void whenAnswerChanges_shouldAnswerLabelBeUpdated() {
+        assertThat(getSpyWidget().binding.choicesSearchBox.getText().toString(), is("Select Answer"));
+
+        SelectOneData answer = getInitialAnswer();
+        Selection selectedChoice = (Selection) answer.getValue();
+        when(formEntryPrompt.getSelectItemText(selectedChoice)).thenReturn(selectedChoice.getValue());
+        getSpyWidget().setBinaryData(Collections.singletonList(selectedChoice));
+
+        assertThat(getSpyWidget().binding.choicesSearchBox.getText().toString(), is(selectedChoice.getValue()));
     }
 }
