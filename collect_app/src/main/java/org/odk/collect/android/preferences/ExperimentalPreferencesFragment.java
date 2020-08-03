5b7f68355a11ab48191aca08978ed194e31844a9
Callum Stott <callum@seadowg.com>
2020-08-03 17:44:52 +0100

    Move Magenta theme to experimental preferences

--- collect_app/src/main/java/org/odk/collect/android/preferences/ExperimentalPreferencesFragment.java
+++ collect_app/src/main/java/org/odk/collect/android/preferences/ExperimentalPreferencesFragment.java
@@ -6,17 +6,26 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import org.odk.collect.android.R;
 import org.odk.collect.android.activities.CollectAbstractActivity;
+import org.odk.collect.android.activities.MainMenuActivity;
 import org.odk.collect.android.injection.DaggerUtils;
+
+import static org.odk.collect.android.activities.ActivityUtils.startActivityAndCloseAllOthers;
 
 public class ExperimentalPreferencesFragment extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.experimental_preferences, rootKey);
+
+        findPreference(GeneralKeys.KEY_MAGENTA_THEME).setOnPreferenceChangeListener((preference, newValue) -> {
+            startActivityAndCloseAllOthers(requireActivity(), MainMenuActivity.class);
+            return true;
+        });
     }
 
     @Override
