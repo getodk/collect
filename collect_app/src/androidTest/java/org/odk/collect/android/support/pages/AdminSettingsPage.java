4943ce89a2382bcae96ade6d03ebaf248a28c61f
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2020-08-12 01:01:46 +0200

    Removed duplicated tests

--- collect_app/src/androidTest/java/org/odk/collect/android/support/pages/AdminSettingsPage.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/support/pages/AdminSettingsPage.java
@@ -27,39 +27,11 @@ public class AdminSettingsPage extends Page<AdminSettingsPage> {
         return this;
     }
 
-    public AdminSettingsPage uncheckAllUserSettings() {
-        clickOnString(R.string.server);
-        clickOnString(R.string.app_theme);
-        clickOnString(R.string.language);
-        clickOnString(R.string.font_size);
-        clickOnString(R.string.navigation);
-        clickOnString(R.string.show_splash_title);
-        clickOnString(R.string.maps);
-        clickOnString(R.string.periodic_form_updates_check_title);
-        clickOnString(R.string.automatic_download);
-        scrollToViewAndClickText(getTranslatedString(R.string.hide_old_form_versions_setting_title));
-        scrollToViewAndClickText(getTranslatedString(R.string.autosend));
-        scrollToViewAndClickText(getTranslatedString(R.string.delete_after_send));
-        scrollToViewAndClickText(getTranslatedString(R.string.default_completed));
-        scrollToViewAndClickText(getTranslatedString(R.string.constraint_behavior_title));
-        scrollToViewAndClickText(getTranslatedString(R.string.high_resolution_title));
-        scrollToViewAndClickText(getTranslatedString(R.string.image_size_title));
-        scrollToViewAndClickText(getTranslatedString(R.string.guidance_hint_title));
-        scrollToViewAndClickText(getTranslatedString(R.string.instance_sync));
-        scrollToViewAndClickText(getTranslatedString(R.string.form_metadata));
-        scrollToViewAndClickText(getTranslatedString(R.string.analytics));
-        return this;
-    }
-
     public GeneralSettingsPage clickGeneralSettings() {
         scrollToViewAndClickText(getTranslatedString(R.string.general_preferences));
         return new GeneralSettingsPage(rule).assertOnPage();
     }
 
-    public AdminSettingsPage uncheckUserSettings(int id) {
-        scrollToViewAndClickText(getTranslatedString(id));
-        return this;
-    }
 
     public AdminSettingsPage clickFormEntrySettings() {
         scrollToViewAndClickText(getTranslatedString(R.string.form_entry_setting));
@@ -85,5 +57,4 @@ public class AdminSettingsPage extends Page<AdminSettingsPage> {
         clickOnString(R.string.server);
         return this;
     }
-
 }
