4e4a70d4b5b0b8ef45c89aa30ab3a0b9829d9ea1
Callum Stott <callum@seadowg.com>
2020-09-02 15:58:32 +0100

    Add style checks to submodules

--- material/src/test/java/org/odk/collect/material/MaterialBannerTest.java
+++ material/src/test/java/org/odk/collect/material/MaterialBannerTest.java
@@ -13,7 +13,8 @@ import org.robolectric.RobolectricTestRunner;
 import static android.view.View.inflate;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.robolectric.shadows.ShadowView.innerText;
 
 @RunWith(RobolectricTestRunner.class)
