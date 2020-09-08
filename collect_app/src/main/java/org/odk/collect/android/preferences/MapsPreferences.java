04092ab5d9361075a61f014171007794b7b5feb4
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2020-09-08 14:42:31 +0200

    Avoid opening multiple layer dialogs

--- collect_app/src/main/java/org/odk/collect/android/preferences/MapsPreferences.java
+++ collect_app/src/main/java/org/odk/collect/android/preferences/MapsPreferences.java
@@ -32,6 +32,7 @@ import org.odk.collect.android.storage.StoragePathProvider;
 import org.odk.collect.android.storage.StorageStateProvider;
 import org.odk.collect.android.storage.StorageSubdirectory;
 import org.odk.collect.android.utilities.FileUtils;
+import org.odk.collect.android.utilities.MultiClickGuard;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -50,15 +51,17 @@ public class MapsPreferences extends BasePreferenceFragment {
 
     @Override
     public void onDisplayPreferenceDialog(Preference preference) {
-        DialogFragment dialogFragment = null;
-        if (preference instanceof CaptionedListPreference) {
-            dialogFragment = ReferenceLayerPreferenceDialog.newInstance(preference.getKey());
-        } else {
-            super.onDisplayPreferenceDialog(preference);
-        }
-        if (dialogFragment != null) {
-            dialogFragment.setTargetFragment(this, 0);
-            dialogFragment.show(getParentFragmentManager(), ReferenceLayerPreferenceDialog.class.getName());
+        if (MultiClickGuard.allowClick(getClass().getName())) {
+            DialogFragment dialogFragment = null;
+            if (preference instanceof CaptionedListPreference) {
+                dialogFragment = ReferenceLayerPreferenceDialog.newInstance(preference.getKey());
+            } else {
+                super.onDisplayPreferenceDialog(preference);
+            }
+            if (dialogFragment != null) {
+                dialogFragment.setTargetFragment(this, 0);
+                dialogFragment.show(getParentFragmentManager(), ReferenceLayerPreferenceDialog.class.getName());
+            }
         }
     }
 
