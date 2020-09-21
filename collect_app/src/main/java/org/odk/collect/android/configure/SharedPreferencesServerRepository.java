f6fdb24ba0f429cb3956cb0a3ea2f3cb53da9e3d
Callum Stott <callum@seadowg.com>
2020-09-21 11:05:06 +0100

    Remove ChangingServerUrlUtils

--- collect_app/src/main/java/org/odk/collect/android/configure/SharedPreferencesServerRepository.java
+++ collect_app/src/main/java/org/odk/collect/android/configure/SharedPreferencesServerRepository.java
@@ -2,22 +2,40 @@ package org.odk.collect.android.configure;
 
 import android.content.SharedPreferences;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.odk.collect.android.R;
 import org.odk.collect.android.application.Collect;
 import org.odk.collect.android.preferences.MetaKeys;
-import org.odk.collect.android.preferences.utilities.ChangingServerUrlUtils;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class SharedPreferencesServerRepository implements ServerRepository {
 
     @Override
     public void save(String url) {
-        ChangingServerUrlUtils.addUrlToList(url);
+        List<String> urlList = getServers();
+
+        if (urlList.contains(url)) {
+            urlList.remove(url);
+        } else if (urlList.size() == 5) {
+            urlList.remove(4);
+        }
+
+        urlList.add(0, url);
+        getSharedPreferences().edit().putString(MetaKeys.SERVER_LIST, new Gson().toJson(urlList)).apply();
     }
 
     @Override
     public List<String> getServers() {
-        return ChangingServerUrlUtils.getUrlList();
+        String urlListString = getSharedPreferences().getString(MetaKeys.SERVER_LIST, null);
+
+        return urlListString == null || urlListString.isEmpty()
+                ? new ArrayList<>(Collections.singletonList(Collect.getInstance().getString(R.string.default_server_url)))
+                : new Gson().fromJson(urlListString, new TypeToken<List<String>>() {}.getType());
     }
 
     @Override
