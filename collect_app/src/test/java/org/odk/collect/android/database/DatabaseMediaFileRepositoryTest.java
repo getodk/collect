ab848d89f2b6b5947c4fed60ea5b2f18dbb7d3f4
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2020-09-22 08:53:53 +0200

    Added tests

--- collect_app/src/test/java/org/odk/collect/android/database/DatabaseMediaFileRepositoryTest.java
+++ collect_app/src/test/java/org/odk/collect/android/database/DatabaseMediaFileRepositoryTest.java
@@ -2,6 +2,9 @@ package org.odk.collect.android.database;
 
 import org.junit.Test;
 import org.odk.collect.android.dao.FormsDao;
+import org.odk.collect.android.utilities.FileUtil;
+
+import java.io.File;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -15,6 +18,32 @@ public class DatabaseMediaFileRepositoryTest {
         FormsDao formsDao = mock(FormsDao.class);
         when(formsDao.getFormMediaPath("1", "1")).thenReturn(null);
 
-        assertThat(new DatabaseMediaFileRepository(formsDao).getAll("1", "1").size(), is(0));
+        assertThat(new DatabaseMediaFileRepository(formsDao, new FileUtil()).getAll("1", "1").size(), is(0));
+    }
+
+    @Test
+    public void whenMediaFolderFileDoesNotExist_shouldGetAllMethodReturnEmptyArray() {
+        FormsDao formsDao = mock(FormsDao.class);
+        FileUtil fileUtil = mock(FileUtil.class);
+        File file = mock(File.class);
+        String formMediaPath = "/samplePath/";
+
+        when(formsDao.getFormMediaPath("1", "1")).thenReturn(formMediaPath);
+        when(fileUtil.getFileAtPath(formMediaPath)).thenReturn(file);
+        when(file.exists()).thenReturn(false);
+
+        assertThat(new DatabaseMediaFileRepository(formsDao, new FileUtil()).getAll("1", "1").size(), is(0));
+    }
+
+    @Test
+    public void whenMediaFolderFileIsNull_shouldGetAllMethodReturnEmptyArray() {
+        FormsDao formsDao = mock(FormsDao.class);
+        FileUtil fileUtil = mock(FileUtil.class);
+        String formMediaPath = "/samplePath/";
+
+        when(formsDao.getFormMediaPath("1", "1")).thenReturn(formMediaPath);
+        when(fileUtil.getFileAtPath(formMediaPath)).thenReturn(null);
+
+        assertThat(new DatabaseMediaFileRepository(formsDao, new FileUtil()).getAll("1", "1").size(), is(0));
     }
 }
