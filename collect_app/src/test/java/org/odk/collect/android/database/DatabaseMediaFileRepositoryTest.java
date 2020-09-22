12477a9466d996662bf20bf10f3ac8264109a9fe
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2020-09-22 15:38:49 +0200

    Improved tests

--- collect_app/src/test/java/org/odk/collect/android/database/DatabaseMediaFileRepositoryTest.java
+++ collect_app/src/test/java/org/odk/collect/android/database/DatabaseMediaFileRepositoryTest.java
@@ -4,8 +4,6 @@ import org.junit.Test;
 import org.odk.collect.android.dao.FormsDao;
 import org.odk.collect.android.utilities.FileUtil;
 
-import java.io.File;
-
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
@@ -24,25 +22,7 @@ public class DatabaseMediaFileRepositoryTest {
     @Test
     public void whenMediaFolderFileDoesNotExist_shouldGetAllMethodReturnEmptyArray() {
         FormsDao formsDao = mock(FormsDao.class);
-        FileUtil fileUtil = mock(FileUtil.class);
-        File file = mock(File.class);
-        String formMediaPath = "/samplePath/";
-
-        when(formsDao.getFormMediaPath("1", "1")).thenReturn(formMediaPath);
-        when(fileUtil.getFileAtPath(formMediaPath)).thenReturn(file);
-        when(file.exists()).thenReturn(false);
-
-        assertThat(new DatabaseMediaFileRepository(formsDao, new FileUtil()).getAll("1", "1").size(), is(0));
-    }
-
-    @Test
-    public void whenMediaFolderFileIsNull_shouldGetAllMethodReturnEmptyArray() {
-        FormsDao formsDao = mock(FormsDao.class);
-        FileUtil fileUtil = mock(FileUtil.class);
-        String formMediaPath = "/samplePath/";
-
-        when(formsDao.getFormMediaPath("1", "1")).thenReturn(formMediaPath);
-        when(fileUtil.getFileAtPath(formMediaPath)).thenReturn(null);
+        when(formsDao.getFormMediaPath("1", "1")).thenReturn("/samplePath/");
 
         assertThat(new DatabaseMediaFileRepository(formsDao, new FileUtil()).getAll("1", "1").size(), is(0));
     }
