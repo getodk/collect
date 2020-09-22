7784766fd8c608e6ce2a324cb519e9515fd4f372
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2020-09-22 08:53:43 +0200

    Handle non existing and null files

--- collect_app/src/main/java/org/odk/collect/android/database/DatabaseMediaFileRepository.java
+++ collect_app/src/main/java/org/odk/collect/android/database/DatabaseMediaFileRepository.java
@@ -2,18 +2,19 @@ package org.odk.collect.android.database;
 
 import org.odk.collect.android.dao.FormsDao;
 import org.odk.collect.android.forms.MediaFileRepository;
+import org.odk.collect.android.utilities.FileUtil;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.util.Arrays.asList;
-
 public class DatabaseMediaFileRepository implements MediaFileRepository {
     private final FormsDao formsDao;
+    private final FileUtil fileUtil;
 
-    public DatabaseMediaFileRepository(FormsDao formsDao) {
+    public DatabaseMediaFileRepository(FormsDao formsDao, FileUtil fileUtil) {
         this.formsDao = formsDao;
+        this.fileUtil = fileUtil;
     }
 
     @Override
@@ -21,6 +22,6 @@ public class DatabaseMediaFileRepository implements MediaFileRepository {
         String formMediaPath = formsDao.getFormMediaPath(jrFormID, formVersion);
         return formMediaPath == null
                 ? new ArrayList<>()
-                : asList(new File(formMediaPath).listFiles());
+                : fileUtil.listFiles(fileUtil.getFileAtPath(formMediaPath));
     }
 }
