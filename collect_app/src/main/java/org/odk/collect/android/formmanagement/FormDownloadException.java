26d649495d084b24b11d23c3c41979b0b00c6218
Callum Stott <callum@seadowg.com>
2020-09-30 13:33:01 +0100

    Remove unused constructor

--- collect_app/src/main/java/org/odk/collect/android/formmanagement/FormDownloadException.java
+++ collect_app/src/main/java/org/odk/collect/android/formmanagement/FormDownloadException.java
@@ -2,15 +2,4 @@ package org.odk.collect.android.formmanagement;
 
 public class FormDownloadException extends Exception {
 
-    /**
-     * @deprecated Exception shouldn't pass strings back to UI
-     */
-    @Deprecated
-    public FormDownloadException(String resultMessage) {
-        super(resultMessage);
-    }
-
-    public FormDownloadException() {
-        super();
-    }
 }
