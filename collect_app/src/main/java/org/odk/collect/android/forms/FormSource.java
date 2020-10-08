4d84aa3bbfd5063f4a6f6319c88be6c8638932b1
Callum Stott <callum@seadowg.com>
2020-10-08 17:08:28 +0100

    Add some JavaDoc to FormSource

--- collect_app/src/main/java/org/odk/collect/android/forms/FormSource.java
+++ collect_app/src/main/java/org/odk/collect/android/forms/FormSource.java
@@ -5,6 +5,10 @@ import org.odk.collect.android.utilities.WebCredentialsUtils;
 import java.io.InputStream;
 import java.util.List;
 
+/**
+ * A place where forms live (outside the app's storage). Ideally in future this would be
+ * a common interface for getting forms from a server, Google Drive or even the disk.
+ */
 public interface FormSource {
 
     List<FormListItem> fetchFormList() throws FormSourceException;
@@ -15,7 +19,17 @@ public interface FormSource {
 
     InputStream fetchMediaFile(String mediaFileURL) throws FormSourceException;
 
+    /**
+     * @deprecated This is specific to the Open Rosa/HTTP implementation so should really move
+     * down to that level
+     */
+    @Deprecated
     void updateUrl(String url);
 
+    /**
+     * @deprecated This is specific to the Open Rosa/HTTP implementation so should really move
+     * down to that level
+     */
+    @Deprecated
     void updateWebCredentialsUtils(WebCredentialsUtils webCredentialsUtils);
 }
