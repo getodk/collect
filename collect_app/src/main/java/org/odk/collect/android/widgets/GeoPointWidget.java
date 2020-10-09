49b0cc6a650a3db44910751520c272ab96dc1a9a
SaumiaSinghal <saumiasinghal@gmail.com>
2020-10-09 13:51:14 +0530

    code refactor

--- collect_app/src/main/java/org/odk/collect/android/widgets/GeoPointWidget.java
+++ collect_app/src/main/java/org/odk/collect/android/widgets/GeoPointWidget.java
@@ -24,24 +24,23 @@ import android.view.View;
 import org.javarosa.core.model.QuestionDef;
 import org.javarosa.core.model.data.GeoPointData;
 import org.javarosa.core.model.data.IAnswerData;
+import org.javarosa.form.api.FormEntryPrompt;
 import org.odk.collect.android.R;
 import org.odk.collect.android.activities.GeoPointActivity;
 import org.odk.collect.android.databinding.GeoWidgetAnswerBinding;
 import org.odk.collect.android.formentry.questions.QuestionDetails;
-<<<<<<< HEAD
 import org.odk.collect.android.listeners.PermissionListener;
 import org.odk.collect.android.utilities.MultiClickGuard;
 import org.odk.collect.android.widgets.interfaces.WidgetDataReceiver;
-=======
-import org.odk.collect.android.geo.MapConfigurator;
->>>>>>> 911881237... add unit tests
 import org.odk.collect.android.widgets.utilities.GeoWidgetUtils;
 import org.odk.collect.android.widgets.utilities.WaitingForDataRegistry;
 
 import static org.odk.collect.android.utilities.ApplicationConstants.RequestCodes;
+import static org.odk.collect.android.widgets.GeoPointMapWidget.ACCURACY_THRESHOLD;
+import static org.odk.collect.android.widgets.GeoPointMapWidget.DEFAULT_LOCATION_ACCURACY;
+import static org.odk.collect.android.widgets.GeoPointMapWidget.LOCATION;
 
 @SuppressLint("ViewConstructor")
-<<<<<<< HEAD
 public class GeoPointWidget extends QuestionWidget implements WidgetDataReceiver {
 
     private final WaitingForDataRegistry waitingForDataRegistry;
@@ -56,54 +55,16 @@ public class GeoPointWidget extends QuestionWidget implements WidgetDataReceiver
         super(context, questionDetails);
         this.waitingForDataRegistry = waitingForDataRegistry;
 
-=======
-public class GeoPointWidget extends BaseGeoWidget {
-    public static final String LOCATION = "gp";
-    public static final String ACCURACY_THRESHOLD = "accuracyThreshold";
-    public static final String READ_ONLY = "readOnly";
-    public static final String DRAGGABLE_ONLY = "draggable";
-
-    public static final double DEFAULT_LOCATION_ACCURACY = 5.0;
-    private MapConfigurator mapConfigurator;
-    private boolean useMap;
-    private double accuracyThreshold;
-    private boolean draggable = true;
-    private String stringAnswer;
-
-    public GeoPointWidget(Context context, QuestionDetails questionDetails, QuestionDef questionDef, MapConfigurator mapConfigurator) {
-        super(context, questionDetails);
-        this.mapConfigurator = mapConfigurator;
-        determineMapProperties(questionDef);
-    }
-
-    private void determineMapProperties(QuestionDef questionDef) {
->>>>>>> 911881237... add unit tests
         // Determine the accuracy threshold to use.
         String acc = questionDef.getAdditionalAttribute(null, ACCURACY_THRESHOLD);
         accuracyThreshold = acc != null && !acc.isEmpty() ? Double.parseDouble(acc) : DEFAULT_LOCATION_ACCURACY;
 
-<<<<<<< HEAD
         stringAnswer = getFormEntryPrompt().getAnswerText();
-        boolean dataAvailable = false;
         if (stringAnswer != null && !stringAnswer.isEmpty()) {
-            dataAvailable = true;
             setData(stringAnswer);
-=======
-        // Determine whether to use the map and whether the point should be draggable.
-        if (mapConfigurator.isAvailable(getContext())) {
-            if (hasAppearance(getFormEntryPrompt(), PLACEMENT_MAP)) {
-                draggable = true;
-                useMap = true;
-            } else if (hasAppearance(getFormEntryPrompt(), MAPS)) {
-                draggable = false;
-                useMap = true;
-            }
->>>>>>> 911881237... add unit tests
         }
-        updateButtonLabelsAndVisibility(dataAvailable);
     }
 
-<<<<<<< HEAD
     @Override
     protected View onCreateAnswerView(Context context, FormEntryPrompt prompt, int answerFontSize) {
         binding = GeoWidgetAnswerBinding.inflate(((Activity) context).getLayoutInflater());
@@ -114,12 +75,6 @@ public class GeoPointWidget extends BaseGeoWidget {
         readOnly = prompt.isReadOnly();
         if (readOnly) {
             binding.simpleButton.setVisibility(GONE);
-=======
-    public void updateButtonLabelsAndVisibility(boolean dataAvailable) {
-        if (useMap) {
-            startGeoButton.setText(
-                dataAvailable ? R.string.view_change_location : R.string.get_point);
->>>>>>> 911881237... add unit tests
         } else {
             binding.simpleButton.setText(getDefaultButtonLabel());
             binding.simpleButton.setTextSize(TypedValue.COMPLEX_UNIT_DIP, answerFontSize);
@@ -160,7 +115,7 @@ public class GeoPointWidget extends BaseGeoWidget {
     }
 
     @Override
-    public void setBinaryData(Object answer) {
+    public void setData(Object answer) {
         stringAnswer = (String) answer;
         binding.geoAnswerText.setText(getAnswerToDisplay(stringAnswer));
 
@@ -168,12 +123,8 @@ public class GeoPointWidget extends BaseGeoWidget {
             stringAnswer = "";
         }
 
-<<<<<<< HEAD
         updateButtonLabelsAndVisibility(stringAnswer != null);
-=======
->>>>>>> 911881237... add unit tests
         widgetValueChanged();
-        updateButtonLabelsAndVisibility(true);
     }
 
     private String getAnswerToDisplay(String answer) {
