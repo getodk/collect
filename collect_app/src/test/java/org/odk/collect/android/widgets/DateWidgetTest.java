9fdadadc6c268fbd81a24c9b8b78671b27293465
SaumiaSinghal <saumiasinghal@gmail.com>
2020-10-20 19:07:17 +0530

    reslolve conflicts

--- collect_app/src/test/java/org/odk/collect/android/widgets/DateWidgetTest.java
+++ collect_app/src/test/java/org/odk/collect/android/widgets/DateWidgetTest.java
@@ -2,65 +2,86 @@ package org.odk.collect.android.widgets;
 
 import android.view.View;
 
-import androidx.annotation.NonNull;
 
 import org.javarosa.core.model.QuestionDef;
-import org.javarosa.core.model.data.DateData;
-import org.joda.time.LocalDateTime;
+import org.javarosa.form.api.FormEntryPrompt;
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
+import org.junit.runner.RunWith;
+import org.odk.collect.android.R;
 import org.odk.collect.android.formentry.questions.QuestionDetails;
-import org.odk.collect.android.widgets.base.GeneralDateTimeWidgetTest;
+import org.odk.collect.android.listeners.WidgetValueChangedListener;
+import org.robolectric.RobolectricTestRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.nullValue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.odk.collect.android.widgets.support.QuestionWidgetHelpers.mockValueChangedListener;
+import static org.odk.collect.android.widgets.support.QuestionWidgetHelpers.promptWithQuestionDefAndAnswer;
+import static org.odk.collect.android.widgets.support.QuestionWidgetHelpers.promptWithReadOnlyAndQuestionDef;
+import static org.odk.collect.android.widgets.support.QuestionWidgetHelpers.widgetTestActivity;
 
-public class DateWidgetTest extends GeneralDateTimeWidgetTest<DateWidget, DateData> {
+@RunWith(RobolectricTestRunner.class)
+public class DateWidgetTest {
 
-    @Mock
-    QuestionDef questionDef;
+    private QuestionDef questionDef;
+    private View.OnLongClickListener listener;
 
-    @Override
-    public void setUp() throws Exception {
-        super.setUp();
-
-        when(formEntryPrompt.getQuestion()).thenReturn(questionDef);
-        when(questionDef.getAppearanceAttr()).thenReturn("");
-    }
-
-    @NonNull
-    @Override
-    public DateWidget createWidget() {
-        return new DateWidget(activity, new QuestionDetails(formEntryPrompt, "formAnalyticsID"));
-    }
-
-    @NonNull
-    @Override
-    public DateData getNextAnswer() {
-        return new DateData(getNextDateTime().toDate());
-    }
-
-    @Override
-    public DateData getInitialAnswer() {
-        return getNextAnswer();
+    @Before
+    public void setUp() {
+        questionDef = mock(QuestionDef.class);
+        listener = mock(View.OnLongClickListener.class);
     }
 
     @Test
-    public void setData() {
-        DateWidget widget = getSpyWidget();
-        LocalDateTime date = new LocalDateTime().withYear(2010).withMonthOfYear(5).withDayOfMonth(12);
-        widget.setData(date);
-        assertFalse(widget.isNullAnswer);
-        assertEquals(widget.getAnswer().getDisplayText(), new DateData(date.toDate()).getDisplayText());
+    public void usingReadOnlyOption_doesNotShowButton() {
+        DateWidget widget = createWidget(promptWithReadOnlyAndQuestionDef(questionDef));
+        assertThat(widget.dateButton.getVisibility(), equalTo(View.GONE));
     }
 
     @Test
-    public void usingReadOnlyOptionShouldMakeAllClickableElementsDisabled() {
-        when(formEntryPrompt.isReadOnly()).thenReturn(true);
+    public void getAnswer_whenPromptAnswerDoesNotHaveAnswer_returnsNull() {
+        assertThat(createWidget(promptWithQuestionDefAndAnswer(questionDef, null)), nullValue());
+    }
 
-        assertThat(getSpyWidget().dateButton.getVisibility(), is(View.GONE));
+    @Test
+    public void clearAnswer_clearsWidgetAnswer() {
+        DateWidget widget = createWidget(promptWithQuestionDefAndAnswer(questionDef, null));
+        widget.clearAnswer();
+
+        assertThat(widget.getAnswer(), nullValue());
+        assertThat(widget.dateTextView.getText(), equalTo(widget.getContext().getString(R.string.no_date_selected)));
+    }
+
+    @Test
+    public void clearAnswer_callValueChangeListener() {
+        DateWidget widget = createWidget(promptWithQuestionDefAndAnswer(questionDef, null));
+        WidgetValueChangedListener valueChangedListener = mockValueChangedListener(widget);
+        widget.clearAnswer();
+
+        verify(valueChangedListener).widgetValueChanged(widget);
+    }
+
+    public void clickingButtonForLong_callsLongClickListener() {
+        DateWidget widget = createWidget(promptWithQuestionDefAndAnswer(questionDef, null));
+        widget.setOnLongClickListener(listener);
+        widget.dateButton.performLongClick();
+
+        verify(listener).onLongClick(widget.dateButton);
+    }
+
+    @Test
+    public void clickingAnswerTextViewForLong_callsLongClickListener() {
+        DateWidget widget = createWidget(promptWithQuestionDefAndAnswer(questionDef, null));
+        widget.setOnLongClickListener(listener);
+        widget.dateTextView.performLongClick();
+
+        verify(listener).onLongClick(widget.dateTextView);
+    }
+
+    private DateWidget createWidget(FormEntryPrompt prompt) {
+        return new DateWidget(widgetTestActivity(), new QuestionDetails(prompt, "formAnalyticsID"));
     }
 }
