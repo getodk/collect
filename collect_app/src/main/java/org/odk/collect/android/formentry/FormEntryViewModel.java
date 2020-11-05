0f9b51bc8b33c59c2c31e5e96763fa2846952ca7
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2020-11-05 16:22:01 +0100

    Check for null exception's cause in FormEntryViewModel.addRepeat

--- collect_app/src/main/java/org/odk/collect/android/formentry/FormEntryViewModel.java
+++ collect_app/src/main/java/org/odk/collect/android/formentry/FormEntryViewModel.java
@@ -83,14 +83,14 @@ public class FormEntryViewModel extends ViewModel implements RequiresFormControl
         try {
             formController.newRepeat();
         } catch (RuntimeException e) {
-            error.setValue(e.getCause().getMessage());
+            error.setValue(e.getCause() != null ? e.getCause().getMessage() : "Unknown issue occurred while adding a new group");
         }
         
         if (!formController.indexIsInFieldList()) {
             try {
                 formController.stepToNextScreenEvent();
             } catch (JavaRosaException e) {
-                error.setValue(e.getCause().getMessage());
+                error.setValue(e.getCause() != null ? e.getCause().getMessage() : "Unknown issue occurred while adding a new group");
             }
         }
     }
