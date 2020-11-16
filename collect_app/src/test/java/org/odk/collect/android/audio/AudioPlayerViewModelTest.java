cee757daa58d7177998b4a2ab5f1766dcc6c8fe7
SaumiaSinghal <saumiasinghal@gmail.com>
2020-11-16 14:59:21 +0530

    code refactor

---
+++ collect_app/src/test/java/org/odk/collect/android/audio/AudioPlayerViewModelTest.java
@@ -0,0 +1,439 @@
+package org.odk.collect.android.audio;
+
+import android.media.MediaPlayer;
+
+import androidx.lifecycle.LiveData;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
+import org.odk.collect.android.R;
+import org.odk.collect.android.support.FakeScheduler;
+import org.odk.collect.android.support.LiveDataTester;
+import org.robolectric.RobolectricTestRunner;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(RobolectricTestRunner.class)
+public class AudioPlayerViewModelTest {
+    private final MediaPlayer mediaPlayer = mock(MediaPlayer.class);
+    private final FakeScheduler fakeScheduler = new FakeScheduler();
+    private final LiveDataTester liveDataTester = new LiveDataTester();
+
+    private AudioPlayerViewModel viewModel;
+
+    @Before
+    public void setup() {
+        viewModel = new AudioPlayerViewModel(() -> mediaPlayer, fakeScheduler);
+    }
+
+    @After
+    public void teardown() {
+        liveDataTester.teardown();
+    }
+
+    @Test
+    public void play_resetsAndPreparesAndStartsMediaPlayer() throws Exception {
+        viewModel.play(new Clip("clip1", "file://audio.mp3"));
+
+        InOrder inOrder = Mockito.inOrder(mediaPlayer);
+
+        inOrder.verify(mediaPlayer).reset();
+        inOrder.verify(mediaPlayer).setDataSource("file://audio.mp3");
+        inOrder.verify(mediaPlayer).prepare();
+        inOrder.verify(mediaPlayer).start();
+    }
+
+    @Test
+    public void play_whenAlreadyingPlayingClip_startsMediaPlayer() {
+        viewModel.play(new Clip("clip1", "file://audio.mp3"));
+        viewModel.play(new Clip("clip1", "file://audio.mp3"));
+
+        verify(mediaPlayer, times(1)).reset();
+        verify(mediaPlayer, times(2)).start();
+    }
+
+    @Test
+    public void play_whenClipHasPositionSet_startsAtPosition() {
+        viewModel.setPosition("clip1", 4321);
+        viewModel.play(new Clip("clip1", "file://audio.mp3"));
+
+        InOrder inOrder = Mockito.inOrder(mediaPlayer);
+
+        inOrder.verify(mediaPlayer).seekTo(4321);
+        inOrder.verify(mediaPlayer).start();
+    }
+
+    @Test
+    public void playMultipleClips_updatesProgress_forAllClips() {
+        viewModel.play(new Clip("clip1", "file://audio.mp3"));
+        assertThat(fakeScheduler.checkRepeatRunning(), equalTo(true));
+
+        viewModel.onCleared();
+
+        viewModel.play(new Clip("clip1", "file://audio.mp3"));
+        assertThat(fakeScheduler.checkRepeatRunning(), equalTo(true));
+    }
+
+    @Test
+    public void playInOrder_playsClipsOneAfterTheOther_andUpdatesProgress() throws Exception {
+        viewModel.playInOrder(asList(
+                new Clip("clip1", "file://audio1.mp3"),
+                new Clip("clip2", "file://audio2.mp3")
+        ));
+
+        ArgumentCaptor<MediaPlayer.OnCompletionListener> captor = ArgumentCaptor.forClass(MediaPlayer.OnCompletionListener.class);
+        verify(mediaPlayer).setOnCompletionListener(captor.capture());
+        MediaPlayer.OnCompletionListener onCompletionListener = captor.getValue();
+
+        verify(mediaPlayer).setDataSource("file://audio1.mp3");
+        verify(mediaPlayer, times(1)).start();
+        assertThat(fakeScheduler.checkRepeatRunning(), equalTo(true));
+
+        onCompletionListener.onCompletion(mediaPlayer);
+
+        verify(mediaPlayer).setDataSource("file://audio2.mp3");
+        verify(mediaPlayer, times(2)).start();
+        assertThat(fakeScheduler.checkRepeatRunning(), equalTo(true));
+
+        onCompletionListener.onCompletion(mediaPlayer);
+        verify(mediaPlayer, times(2)).start();
+        assertThat(fakeScheduler.checkRepeatRunning(), equalTo(false));
+    }
+
+    @Test
+    public void playInOrder_whenThereIsAnErrorContinuesWithNextClip() throws Exception {
+        doThrow(IOException.class).when(mediaPlayer).setDataSource("file://missing.mp3");
+
+        viewModel.playInOrder(asList(
+                new Clip("clip1", "file://missing.mp3"),
+                new Clip("clip2", "file://not-missing.mp3")
+        ));
+
+        verify(mediaPlayer).setDataSource("file://not-missing.mp3");
+        verify(mediaPlayer, times(1)).start();
+    }
+
+    @Test
+    public void play_afterAPlayInOrder_doesNotContinuePlayingClips() throws Exception {
+        viewModel.playInOrder(asList(
+                new Clip("clip1", "file://audio1.mp3"),
+                new Clip("clip2", "file://audio2.mp3")
+        ));
+
+        viewModel.play(new Clip("clip3", "file://audio3.mp3"));
+
+        verify(mediaPlayer, times(2)).start();
+        verify(mediaPlayer).setDataSource("file://audio1.mp3");
+        verify(mediaPlayer).setDataSource("file://audio3.mp3");
+
+        ArgumentCaptor<MediaPlayer.OnCompletionListener> captor = ArgumentCaptor.forClass(MediaPlayer.OnCompletionListener.class);
+        verify(mediaPlayer).setOnCompletionListener(captor.capture());
+        MediaPlayer.OnCompletionListener onCompletionListener = captor.getValue();
+        onCompletionListener.onCompletion(mediaPlayer);
+
+        verify(mediaPlayer, never()).setDataSource("file://audio2.mp3");
+        verify(mediaPlayer, times(2)).start();
+    }
+
+    @Test
+    public void isPlaying_whenNothingPlaying_is_false() {
+        LiveData<Boolean> isPlaying = liveDataTester.activate(viewModel.isPlaying("clip1"));
+
+        assertThat(isPlaying.getValue(), equalTo(false));
+    }
+
+    @Test
+    public void isPlaying_whenClipIDPlaying_is_PLAYING() {
+        LiveData<Boolean> isPlaying = liveDataTester.activate(viewModel.isPlaying("clip1"));
+
+        viewModel.play(new Clip("clip1", "file://audio.mp3"));
+        assertThat(isPlaying.getValue(), equalTo(true));
+    }
+
+    @Test
+    public void isPlaying_whenDifferentClipIDPlaying_is_false() {
+        LiveData<Boolean> isPlaying = liveDataTester.activate(viewModel.isPlaying("clip2"));
+
+        viewModel.play(new Clip("clip1", "file://other.mp3"));
+        assertThat(isPlaying.getValue(), equalTo(false));
+    }
+
+    @Test
+    public void isPlaying_whenClipIDPlaying_thenCompleted_is_false() {
+        final LiveData<Boolean> isPlaying = liveDataTester.activate(viewModel.isPlaying("clip1"));
+
+        viewModel.play(new Clip("clip1", "file://audio.mp3"));
+
+        ArgumentCaptor<MediaPlayer.OnCompletionListener> captor = ArgumentCaptor.forClass(MediaPlayer.OnCompletionListener.class);
+        verify(mediaPlayer).setOnCompletionListener(captor.capture());
+        captor.getValue().onCompletion(mediaPlayer);
+
+        assertThat(isPlaying.getValue(), equalTo(false));
+    }
+
+    @Test
+    public void isPlaying_whenPlaybackFails_is_false() throws Exception {
+        doThrow(IOException.class).when(mediaPlayer).setDataSource("file://missing.mp3");
+
+        final LiveData<Boolean> isPlaying = liveDataTester.activate(viewModel.isPlaying("clip1"));
+        viewModel.play(new Clip("clip1", "file://missing.mp3"));
+        assertThat(isPlaying.getValue(), equalTo(false));
+    }
+
+    @Test
+    public void stop_stopsMediaPlayer() {
+        viewModel.play(new Clip("clip1", "file://audio.mp3"));
+        viewModel.stop();
+        verify(mediaPlayer).stop();
+    }
+
+    @Test
+    public void stop_beforePlay_doesntCallStopOnMediaPlayer() {
+        viewModel.stop();
+        verify(mediaPlayer, never()).stop();
+    }
+
+    @Test
+    public void stop_resetsPosition() {
+        final LiveData<Integer> position = liveDataTester.activate(viewModel.getPosition("clip1"));
+
+        viewModel.play(new Clip("clip1", "file://audio.mp3"));
+
+        when(mediaPlayer.getCurrentPosition()).thenReturn(1000);
+        fakeScheduler.runForeground();
+
+        viewModel.stop();
+        assertThat(position.getValue(), equalTo(0));
+    }
+
+    @Test
+    public void background_releasesMediaPlayer() {
+        viewModel.background();
+        verify(mediaPlayer).release();
+    }
+
+    @Test
+    public void background_cancelsScheduler() {
+        viewModel.play(new Clip("clip1", "file://audio.mp3"));
+        viewModel.background();
+
+        assertThat(fakeScheduler.hasBeenCancelled(), equalTo(true));
+    }
+
+    @Test
+    public void isPlaying_whenPlayingAndThenBackgrounding_is_false() {
+        LiveData<Boolean> isPlaying = liveDataTester.activate(viewModel.isPlaying("clip1"));
+
+        viewModel.play(new Clip("clip1", "file://audio.mp3"));
+        viewModel.background();
+
+        assertThat(isPlaying.getValue(), equalTo(false));
+    }
+
+    @Test
+    public void play_afterBackground_createsANewMediaPlayer() {
+        RecordingMockMediaPlayerFactory factory = new RecordingMockMediaPlayerFactory();
+        AudioPlayerViewModel viewModel = new AudioPlayerViewModel(factory, new FakeScheduler());
+
+        viewModel.play(new Clip("clip1", "file://audio.mp3"));
+        assertThat(factory.createdInstances.size(), equalTo(1));
+        verify(factory.createdInstances.get(0)).start();
+
+        viewModel.background();
+        viewModel.play(new Clip("clip1", "file://audio.mp3"));
+        assertThat(factory.createdInstances.size(), equalTo(2));
+        verify(factory.createdInstances.get(1)).start();
+    }
+
+    @Test
+    public void pause_pausesMediaPlayer() {
+        viewModel.pause();
+        verify(mediaPlayer).pause();
+    }
+
+    @Test
+    public void isPlaying_afterPause_is_false() {
+        LiveData<Boolean> isPlaying = liveDataTester.activate(viewModel.isPlaying("clip1"));
+
+        viewModel.play(new Clip("clip1", "file://audio.mp3"));
+        viewModel.pause();
+
+        assertThat(isPlaying.getValue(), equalTo(false));
+    }
+
+    @Test
+    public void isPlaying_afterPause_andThenPlay_is_true() {
+        final LiveData<Boolean> isPlaying = liveDataTester.activate(viewModel.isPlaying("clip1"));
+
+        viewModel.play(new Clip("clip1", "file://audio.mp3"));
+        viewModel.pause();
+        viewModel.play(new Clip("clip1", "file://audio.mp3"));
+
+        assertThat(isPlaying.getValue(), equalTo(true));
+    }
+
+    @Test
+    public void getPosition_returnsMediaPlayerPositionInMilliseconds() {
+        when(mediaPlayer.getCurrentPosition()).thenReturn(0);
+        LiveData<Integer> duration = liveDataTester.activate(viewModel.getPosition("clip1"));
+        assertThat(duration.getValue(), equalTo(0));
+
+        viewModel.play(new Clip("clip1", "file://audio.mp3"));
+
+        when(mediaPlayer.getCurrentPosition()).thenReturn(1000);
+        fakeScheduler.runForeground();
+        assertThat(duration.getValue(), equalTo(1000));
+
+        when(mediaPlayer.getCurrentPosition()).thenReturn(24135);
+        fakeScheduler.runForeground();
+        assertThat(duration.getValue(), equalTo(24135));
+    }
+
+    @Test
+    public void getPosition_worksWhenMultipleClipsArePlayed() {
+        when(mediaPlayer.getCurrentPosition()).thenReturn(0);
+        final LiveData<Integer> duration1 = liveDataTester.activate(viewModel.getPosition("clip1"));
+        final LiveData<Integer> duration2 = liveDataTester.activate(viewModel.getPosition("clip2"));
+
+        viewModel.play(new Clip("clip1", "file://audio.mp3"));
+        when(mediaPlayer.getCurrentPosition()).thenReturn(1000);
+        fakeScheduler.runForeground();
+        assertThat(duration1.getValue(), equalTo(1000));
+
+        viewModel.play(new Clip("clip2", "file://audio.mp3"));
+        when(mediaPlayer.getCurrentPosition()).thenReturn(2500);
+        fakeScheduler.runForeground();
+        assertThat(duration2.getValue(), equalTo(2500));
+    }
+
+    @Test
+    public void setPosition_whenClipIsPlaying_seeksMediaPlayer() {
+        when(mediaPlayer.getDuration()).thenReturn(100000);
+        viewModel.play(new Clip("clip1", "file://audio.mp3"));
+
+        viewModel.setPosition("clip1", 54321);
+        verify(mediaPlayer).seekTo(54321);
+    }
+
+    @Test
+    public void setPosition_whenClipIsNotPlaying_doesNothing() {
+        viewModel.setPosition("clip1", 54321);
+        verify(mediaPlayer, never()).seekTo(anyInt());
+    }
+
+    @Test
+    public void setPosition_updatesPosition() {
+        LiveData<Integer> duration = liveDataTester.activate(viewModel.getPosition("clip1"));
+
+        viewModel.setPosition("clip1", 54321);
+        assertThat(duration.getValue(), equalTo(54321));
+    }
+
+    @Test
+    public void onCleared_releasesMediaPlayer() {
+        viewModel.onCleared();
+        verify(mediaPlayer).release();
+    }
+
+    @Test
+    public void onCleared_cancelsScheduler() {
+        viewModel.play(new Clip("clip1", "file://audio.mp3"));
+        viewModel.onCleared();
+
+        assertThat(fakeScheduler.hasBeenCancelled(), equalTo(true));
+    }
+
+    @Test
+    public void whenPlaybackCompletes_cancelsScheduler() {
+        viewModel.play(new Clip("clip1", "file://audio.mp3"));
+
+        ArgumentCaptor<MediaPlayer.OnCompletionListener> captor = ArgumentCaptor.forClass(MediaPlayer.OnCompletionListener.class);
+        verify(mediaPlayer).setOnCompletionListener(captor.capture());
+        captor.getValue().onCompletion(mediaPlayer);
+
+        assertThat(fakeScheduler.hasBeenCancelled(), equalTo(true));
+    }
+
+    @Test
+    public void whenPlaybackCompletes_resetsPosition() {
+        final LiveData<Integer> position = liveDataTester.activate(viewModel.getPosition("clip1"));
+
+        viewModel.play(new Clip("clip1", "file://audio.mp3"));
+
+        when(mediaPlayer.getCurrentPosition()).thenReturn(1000);
+        fakeScheduler.runForeground();
+
+        ArgumentCaptor<MediaPlayer.OnCompletionListener> captor = ArgumentCaptor.forClass(MediaPlayer.OnCompletionListener.class);
+        verify(mediaPlayer).setOnCompletionListener(captor.capture());
+        captor.getValue().onCompletion(mediaPlayer);
+
+        assertThat(position.getValue(), equalTo(0));
+    }
+
+    @Test
+    public void getError_whenPlaybackFailsBecauseOfMissingFile_is_PlaybackFailed() throws Exception {
+        final LiveData<Exception> error = liveDataTester.activate(viewModel.getError());
+
+        doThrow(IOException.class).when(mediaPlayer).setDataSource("file://missing.mp3");
+        viewModel.play(new Clip("clip1", "file://missing.mp3"));
+
+        PlaybackFailedException playbackFailedException = new PlaybackFailedException("file://missing.mp3", R.string.file_missing);
+        assertThat(error.getValue(), equalTo(playbackFailedException));
+        assertThat(R.string.file_missing, equalTo(playbackFailedException.getExceptionMsg()));
+    }
+
+    @Test
+    public void getError_whenPlaybackFailsBecauseOfInvalidFile_is_PlaybackFailed() throws Exception {
+        final LiveData<Exception> error = liveDataTester.activate(viewModel.getError());
+
+        doThrow(IOException.class).when(mediaPlayer).setDataSource("file://invalid.mp3");
+        viewModel.play(new Clip("clip1", "file://invalid.mp3"));
+
+        PlaybackFailedException playbackFailedException = new PlaybackFailedException("file://invalid.mp3", R.string.file_invalid);
+        assertThat(error.getValue(), equalTo(playbackFailedException));
+        assertThat(R.string.file_invalid, equalTo(playbackFailedException.getExceptionMsg()));
+    }
+
+    @Test
+    public void dismissError_removesErrorValue() throws Exception {
+        final LiveData<Exception> error = liveDataTester.activate(viewModel.getError());
+
+        doThrow(IOException.class).when(mediaPlayer).setDataSource("file://missing.mp3");
+        viewModel.play(new Clip("clip1", "file://missing.mp3"));
+
+        viewModel.errorDisplayed();
+        assertThat(error.getValue(), equalTo(null));
+    }
+
+    private static class RecordingMockMediaPlayerFactory implements Supplier<MediaPlayer> {
+
+        List<MediaPlayer> createdInstances = new ArrayList<>();
+
+        @Override
+        public MediaPlayer get() {
+            MediaPlayer mock = mock(MediaPlayer.class);
+            createdInstances.add(mock);
+
+            return mock;
+        }
+    }
+}
