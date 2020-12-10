fbd7bd302aee5336e9cad30f73ad751b98417eb6
Callum Stott <callum@seadowg.com>
2020-12-10 14:14:54 +0000

    Remove multiclick test

--- collect_app/src/test/java/org/odk/collect/android/activities/MainActivityTest.java
+++ collect_app/src/test/java/org/odk/collect/android/activities/MainActivityTest.java
@@ -13,15 +13,10 @@ import org.robolectric.RobolectricTestRunner;
 import org.robolectric.shadows.ShadowActivity;
 import org.robolectric.shadows.ShadowEnvironment;
 import org.robolectric.shadows.ShadowIntent;
-import org.robolectric.shadows.ShadowSystemClock;
-
-import java.time.Duration;
 
 import static android.os.Environment.MEDIA_MOUNTED;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
 import static org.robolectric.Shadows.shadowOf;
 
 @RunWith(RobolectricTestRunner.class)
@@ -81,55 +76,5 @@ public class MainActivityTest {
         ShadowIntent shadowIntent = shadowOf(startedIntent);
         assertEquals(DeleteSavedFormActivity.class.getName(),
                 shadowIntent.getIntentClass().getName());
-    }
-
-    @Test
-    public void shouldButtonsBeResistantToMulticlicking() {
-        ShadowSystemClock.setNanoTime(0);
-        assertThat(mainMenuActivity.findViewById(R.id.enter_data).performClick(), is(true));
-        ShadowSystemClock.advanceBy(Duration.ofMillis(1000));
-        assertThat(mainMenuActivity.findViewById(R.id.enter_data).performClick(), is(false));
-        assertThat(mainMenuActivity.findViewById(R.id.review_data).performClick(), is(false));
-        ShadowSystemClock.advanceBy(Duration.ofMillis(1001));
-        assertThat(mainMenuActivity.findViewById(R.id.enter_data).performClick(), is(true));
-
-        ShadowSystemClock.setNanoTime(0);
-        assertThat(mainMenuActivity.findViewById(R.id.review_data).performClick(), is(true));
-        ShadowSystemClock.advanceBy(Duration.ofMillis(1000));
-        assertThat(mainMenuActivity.findViewById(R.id.review_data).performClick(), is(false));
-        ShadowSystemClock.advanceBy(Duration.ofMillis(1001));
-        assertThat(mainMenuActivity.findViewById(R.id.review_data).performClick(), is(true));
-
-        ShadowSystemClock.setNanoTime(0);
-        assertThat(mainMenuActivity.findViewById(R.id.send_data).performClick(), is(true));
-        ShadowSystemClock.advanceBy(Duration.ofMillis(1000));
-        assertThat(mainMenuActivity.findViewById(R.id.send_data).performClick(), is(false));
-        assertThat(mainMenuActivity.findViewById(R.id.view_sent_forms).performClick(), is(false));
-        ShadowSystemClock.advanceBy(Duration.ofMillis(1001));
-        assertThat(mainMenuActivity.findViewById(R.id.send_data).performClick(), is(true));
-
-        ShadowSystemClock.setNanoTime(0);
-        assertThat(mainMenuActivity.findViewById(R.id.view_sent_forms).performClick(), is(true));
-        ShadowSystemClock.advanceBy(Duration.ofMillis(1000));
-        assertThat(mainMenuActivity.findViewById(R.id.view_sent_forms).performClick(), is(false));
-        assertThat(mainMenuActivity.findViewById(R.id.get_forms).performClick(), is(false));
-        ShadowSystemClock.advanceBy(Duration.ofMillis(1001));
-        assertThat(mainMenuActivity.findViewById(R.id.view_sent_forms).performClick(), is(true));
-
-        ShadowSystemClock.setNanoTime(0);
-        assertThat(mainMenuActivity.findViewById(R.id.get_forms).performClick(), is(true));
-        ShadowSystemClock.advanceBy(Duration.ofMillis(1000));
-        assertThat(mainMenuActivity.findViewById(R.id.get_forms).performClick(), is(false));
-        assertThat(mainMenuActivity.findViewById(R.id.manage_forms).performClick(), is(false));
-        ShadowSystemClock.advanceBy(Duration.ofMillis(1001));
-        assertThat(mainMenuActivity.findViewById(R.id.get_forms).performClick(), is(true));
-
-        ShadowSystemClock.setNanoTime(0);
-        assertThat(mainMenuActivity.findViewById(R.id.manage_forms).performClick(), is(true));
-        ShadowSystemClock.advanceBy(Duration.ofMillis(1000));
-        assertThat(mainMenuActivity.findViewById(R.id.manage_forms).performClick(), is(false));
-        assertThat(mainMenuActivity.findViewById(R.id.enter_data).performClick(), is(false));
-        ShadowSystemClock.advanceBy(Duration.ofMillis(1001));
-        assertThat(mainMenuActivity.findViewById(R.id.manage_forms).performClick(), is(true));
     }
 }
