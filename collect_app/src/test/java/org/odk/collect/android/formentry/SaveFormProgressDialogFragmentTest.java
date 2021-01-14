72f39e4e739a10622cb1803633bdd753a951f255
Callum Stott <callum@seadowg.com>
2021-01-14 17:27:01 +0000

    Use FragmentScenario to test SaveFormProgressDialogFragment

--- collect_app/src/test/java/org/odk/collect/android/formentry/SaveFormProgressDialogFragmentTest.java
+++ collect_app/src/test/java/org/odk/collect/android/formentry/SaveFormProgressDialogFragmentTest.java
@@ -1,35 +1,24 @@
 package org.odk.collect.android.formentry;
 
-import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.testing.FragmentScenario;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.odk.collect.android.formentry.saving.SaveFormProgressDialogFragment;
-import org.robolectric.Robolectric;
-import org.robolectric.RobolectricTestRunner;
+import org.odk.collect.android.support.RobolectricHelpers;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.robolectric.Shadows.shadowOf;
 
-@RunWith(RobolectricTestRunner.class)
+@RunWith(AndroidJUnit4.class)
 public class SaveFormProgressDialogFragmentTest {
-
-    private FragmentManager fragmentManager;
-
-    @Before
-    public void setup() {
-        FragmentActivity activity = Robolectric.setupActivity(FragmentActivity.class);
-        fragmentManager = activity.getSupportFragmentManager();
-    }
 
     @Test
     public void dialogIsNotCancellable() {
-        SaveFormProgressDialogFragment fragment = new SaveFormProgressDialogFragment();
-        fragment.show(fragmentManager, "TAG");
-
-        assertThat(shadowOf(fragment.getDialog()).isCancelable(), equalTo(false));
+        FragmentScenario<SaveFormProgressDialogFragment> fragmentScenario = RobolectricHelpers.launchDialogFragment(SaveFormProgressDialogFragment.class);
+        fragmentScenario.onFragment(fragment -> {
+            assertThat(fragment.isCancelable(), equalTo(false));
+        });
     }
 }
