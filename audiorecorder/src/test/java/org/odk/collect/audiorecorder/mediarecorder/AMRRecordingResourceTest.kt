d3386e5173b9d28787210c4f48263215a8b69aad
Callum Stott <callum@seadowg.com>
2021-01-28 14:50:32 +0000

    Add test for lower API levels

--- audiorecorder/src/test/java/org/odk/collect/audiorecorder/mediarecorder/AMRRecordingResourceTest.kt
+++ audiorecorder/src/test/java/org/odk/collect/audiorecorder/mediarecorder/AMRRecordingResourceTest.kt
@@ -4,6 +4,8 @@ import android.media.MediaRecorder
 import org.junit.Test
 import org.mockito.Mockito.inOrder
 import org.mockito.Mockito.mock
+import org.mockito.Mockito.only
+import org.mockito.Mockito.verify
 
 class AMRRecordingResourceTest {
 
@@ -11,19 +13,32 @@ class AMRRecordingResourceTest {
      * Calls to stop() while paused hang which appears to be a problem in the Android
      * framework. Resuming and the stopping does work however.
      *
-     * @see <a href="https://gist.github.com/seadowg/e6cbab21e032fe2fcd9afb9b4f458ab9">Gist</a> for
-     * test demonstrating the issue in Android.
+     * @see [Gist with test demonstrating problem](https://gist.github.com/seadowg/e6cbab21e032fe2fcd9afb9b4f458ab9)
+     * @see [Issue in Google's Android tracker](https://issuetracker.google.com/issues/178630865)
      */
     @Test
-    fun whenPaused_stop_callsResumeThenStopOnMediaRecorder() {
+    fun whenAPI24OrHigher_whenPaused_stop_callsResumeThenStopOnMediaRecorder() {
         val mediaRecorder = mock(MediaRecorder::class.java)
         val inOrder = inOrder(mediaRecorder)
-        val amrRecordingResource = AMRRecordingResource(mediaRecorder, android.os.Build.VERSION_CODES.N)
+        val amrRecordingResource = AMRRecordingResource(mediaRecorder, 24)
 
         amrRecordingResource.pause()
 
         amrRecordingResource.stop()
         inOrder.verify(mediaRecorder).resume()
         inOrder.verify(mediaRecorder).stop()
+    }
+
+    /**
+     * Pause/resume is not supported in API 23 and below so we need to check that we're not calling
+     * [MediaRecorder.pause] in [AMRRecordingResource.stop].
+     */
+    @Test
+    fun whenAPI23OrLower_stop_callsStopOnMediaRecorder() {
+        val mediaRecorder = mock(MediaRecorder::class.java)
+        val amrRecordingResource = AMRRecordingResource(mediaRecorder, 23)
+
+        amrRecordingResource.stop()
+        verify(mediaRecorder, only()).stop()
     }
 }
