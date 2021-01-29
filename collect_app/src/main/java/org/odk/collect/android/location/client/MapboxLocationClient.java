d6e2107bc93500186d6e4db4c7cab981b9449e61
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-01-29 22:31:14 +0100

    Implemented MapboxLocationClient

---
+++ collect_app/src/main/java/org/odk/collect/android/location/client/MapboxLocationClient.java
@@ -0,0 +1,33 @@
+package org.odk.collect.android.location.client;
+
+import android.location.Location;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.location.LocationListener;
+import com.mapbox.android.core.location.LocationEngineCallback;
+import com.mapbox.android.core.location.LocationEngineResult;
+
+public class MapboxLocationClient implements LocationEngineCallback<LocationEngineResult> {
+    @Nullable
+    private final LocationListener locationListener;
+
+    public MapboxLocationClient(@Nullable LocationListener locationListener) {
+        this.locationListener = locationListener;
+    }
+
+    @Override
+    public void onSuccess(LocationEngineResult result) {
+        Location location = result.getLastLocation();
+        if (locationListener != null && location != null) {
+            if (location.isFromMockProvider() || location.getAccuracy() < 0) {
+                location.setAccuracy(0);
+            }
+            locationListener.onLocationChanged(location);
+        }
+    }
+
+    @Override
+    public void onFailure(@NonNull Exception exception) { }
+}
