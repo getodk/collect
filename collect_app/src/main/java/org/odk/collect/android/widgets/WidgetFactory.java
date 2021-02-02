4b5ca9dccd276d9d5b0b29a0bfe80b1c166654b1
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-02-02 15:17:54 +0100

    Fixed conflicts

--- collect_app/src/main/java/org/odk/collect/android/widgets/WidgetFactory.java
+++ collect_app/src/main/java/org/odk/collect/android/widgets/WidgetFactory.java
@@ -181,7 +181,7 @@ public class WidgetFactory {
                 }
                 break;
             case Constants.CONTROL_FILE_CAPTURE:
-                if (appearance.startsWith(WidgetAppearanceUtils.EX)) {
+                if (appearance.startsWith(Appearances.EX)) {
                     questionWidget = new ExArbitraryFileWidget(context, questionDetails, new MediaUtils(), questionMediaManager, waitingForDataRegistry, new ExternalAppIntentProvider(), activityAvailability);
                 } else {
                     questionWidget = new ArbitraryFileWidget(context, questionDetails, new MediaUtils(), questionMediaManager, waitingForDataRegistry);
