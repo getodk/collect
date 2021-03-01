07e5099b974a4cc1ed70d1ee56e3625979830bb1
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-03-01 12:50:55 +0100

    Implemented PreferencesRepository class

---
+++ collect_app/src/main/java/org/odk/collect/android/preferences/PreferencesRepository.kt
@@ -0,0 +1,23 @@
+package org.odk.collect.android.preferences
+
+import android.content.Context
+import androidx.preference.PreferenceManager
+
+open class PreferencesRepository(private val context: Context) {
+    fun getMetaPreferences(): PreferencesDataSource {
+        return PreferencesDataSource(context.getSharedPreferences(META_PREFS_NAME, Context.MODE_PRIVATE))
+    }
+
+    fun getGeneralPreferences(): PreferencesDataSource {
+        return PreferencesDataSource(PreferenceManager.getDefaultSharedPreferences(context), GeneralKeys.DEFAULTS)
+    }
+
+    fun getAdminPreferences(): PreferencesDataSource {
+        return PreferencesDataSource(context.getSharedPreferences(ADMIN_PREFS_NAME, Context.MODE_PRIVATE), AdminKeys.getDefaults())
+    }
+
+    companion object {
+        private const val META_PREFS_NAME = "meta"
+        private const val ADMIN_PREFS_NAME = "admin_prefs"
+    }
+}
