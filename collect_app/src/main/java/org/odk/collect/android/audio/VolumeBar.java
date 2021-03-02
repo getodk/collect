b091a3d24fbf2b0bf1416a78839eea038a4a9e44
Callum Stott <callum@seadowg.com>
2021-03-02 16:21:34 +0000

    Implemented segmented volume bar with hardcoded colors

--- collect_app/src/main/java/org/odk/collect/android/audio/VolumeBar.java
+++ collect_app/src/main/java/org/odk/collect/android/audio/VolumeBar.java
@@ -1,46 +1,85 @@
 package org.odk.collect.android.audio;
 
 import android.content.Context;
-import android.os.Build;
+import android.graphics.drawable.ColorDrawable;
 import android.util.AttributeSet;
-import android.view.LayoutInflater;
-import android.widget.FrameLayout;
-import android.widget.ProgressBar;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import org.odk.collect.android.databinding.VolumeBarBinding;
+import org.odk.collect.android.R;
 
-public class VolumeBar extends FrameLayout {
+import static org.odk.collect.android.utilities.ViewUtils.pxFromDp;
 
-    private ProgressBar progressBar;
+public class VolumeBar extends LinearLayout {
+
     private Integer lastAmplitude;
+    private int pips;
 
     public VolumeBar(@NonNull Context context) {
         super(context);
-        init(context);
+        init();
     }
 
     public VolumeBar(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        init(context);
+        init();
     }
 
     public VolumeBar(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context);
+        init();
     }
 
-    private void init(Context context) {
-        progressBar = VolumeBarBinding.inflate(LayoutInflater.from(context), this, true).getRoot();
+    private void init() {
+        setOrientation(LinearLayout.HORIZONTAL);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+
+        if (changed && getHeight() > 0) {
+            int pipSize = pxFromDp(getContext(), 24);
+            int marginSize = pxFromDp(getContext(), 4);
+            pips = getWidth() / (pipSize + marginSize);
+
+            this.removeAllViews();
+            for (int i = 0; i < pips; i++) {
+                View pip = new View(getContext());
+
+                LayoutParams layoutParams = new LayoutParams(pipSize, getHeight());
+                if (i != pips - 1) {
+                    layoutParams.setMarginEnd(marginSize);
+                } else {
+                    layoutParams.setMarginEnd(0);
+                }
+
+                pip.setLayoutParams(layoutParams);
+                addView(pip);
+            }
+        }
     }
 
     public void addAmplitude(int amplitude) {
         lastAmplitude = amplitude;
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            progressBar.setProgress((int) ((amplitude * 6) / 22760d * 100), true);
+        if (pips > 0) {
+            int segmentAmplitude = 22760 / pips;
+            int segmentsToFill = amplitude * 6 / segmentAmplitude;
+
+            int filledColor = getContext().getResources().getColor(R.color.blue_500);
+            int unfilledColor = getContext().getResources().getColor(R.color.gray600);
+
+            for (int i = 0; i < pips; i++) {
+                if (i < segmentsToFill) {
+                    getChildAt(i).setBackground(new ColorDrawable(filledColor));
+                } else {
+                    getChildAt(i).setBackground(new ColorDrawable(unfilledColor));
+                }
+            }
         }
     }
 
