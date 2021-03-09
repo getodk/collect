f808d41cf2c08306192ebc380be895974b8b93c1
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-03-09 12:36:23 +0100

    Implemented GeneralPreferencesDataStore

---
+++ collect_app/src/main/java/org/odk/collect/android/preferences/stores/GeneralPreferencesDataStore.kt
@@ -0,0 +1,54 @@
+package org.odk.collect.android.preferences.stores
+
+import androidx.preference.PreferenceDataStore
+import org.odk.collect.android.preferences.PreferencesDataSourceProvider
+
+class GeneralPreferencesDataStore(val preferencesDataSourceProvider: PreferencesDataSourceProvider) : PreferenceDataStore() {
+    override fun putString(key: String, value: String?) {
+        preferencesDataSourceProvider.getGeneralPreferences().save(key, value)
+    }
+
+    override fun putBoolean(key: String, value: Boolean) {
+        preferencesDataSourceProvider.getGeneralPreferences().save(key, value)
+    }
+
+    override fun putLong(key: String, value: Long) {
+        preferencesDataSourceProvider.getGeneralPreferences().save(key, value)
+    }
+
+    override fun putInt(key: String, value: Int) {
+        preferencesDataSourceProvider.getGeneralPreferences().save(key, value)
+    }
+
+    override fun putFloat(key: String, value: Float) {
+        preferencesDataSourceProvider.getGeneralPreferences().save(key, value)
+    }
+
+    override fun putStringSet(key: String, value: Set<String>?) {
+        preferencesDataSourceProvider.getGeneralPreferences().save(key, value)
+    }
+
+    override fun getString(key: String, defValue: String?): String? {
+        return preferencesDataSourceProvider.getGeneralPreferences().getString(key)
+    }
+
+    override fun getBoolean(key: String, defValue: Boolean): Boolean {
+        return preferencesDataSourceProvider.getGeneralPreferences().getBoolean(key)
+    }
+
+    override fun getLong(key: String, defValue: Long): Long {
+        return preferencesDataSourceProvider.getGeneralPreferences().getLong(key)
+    }
+
+    override fun getInt(key: String, defValue: Int): Int {
+        return preferencesDataSourceProvider.getGeneralPreferences().getInt(key)
+    }
+
+    override fun getFloat(key: String, defValue: Float): Float {
+        return preferencesDataSourceProvider.getGeneralPreferences().getFloat(key)
+    }
+
+    override fun getStringSet(key: String, defValue: Set<String>?): Set<String>? {
+        return preferencesDataSourceProvider.getGeneralPreferences().getStringSet(key)
+    }
+}
