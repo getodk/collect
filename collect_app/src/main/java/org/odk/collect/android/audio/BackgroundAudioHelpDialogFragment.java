047536a04ad70674a66e26fc3dec4e2364bd3ad1
Callum Stott <callum@seadowg.com>
2021-03-10 11:37:11 +0000

    Make sure scroll bars show on dialog

--- collect_app/src/main/java/org/odk/collect/android/audio/BackgroundAudioHelpDialogFragment.java
+++ collect_app/src/main/java/org/odk/collect/android/audio/BackgroundAudioHelpDialogFragment.java
@@ -2,6 +2,8 @@ package org.odk.collect.android.audio;
 
 import android.app.Dialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,8 +18,10 @@ public class BackgroundAudioHelpDialogFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        LayoutInflater inflater = LayoutInflater.from(requireContext());
+        View view = inflater.inflate(R.layout.background_audio_help_fragment_layout, null);
         return new MaterialAlertDialogBuilder(requireContext())
-                .setMessage(R.string.background_audio_help)
+                .setView(view)
                 .setPositiveButton(R.string.ok, null)
                 .create();
     }
