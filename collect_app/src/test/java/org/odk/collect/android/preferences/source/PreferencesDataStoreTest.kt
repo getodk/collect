bb0c3a8ee8b73431c6bfc2acc0e1f0f12cfb1c98
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-03-11 15:48:49 +0100

    Removed redundant times() from PreferencesDataStoreTest

--- collect_app/src/test/java/org/odk/collect/android/preferences/source/PreferencesDataStoreTest.kt
+++ collect_app/src/test/java/org/odk/collect/android/preferences/source/PreferencesDataStoreTest.kt
@@ -3,7 +3,6 @@ package org.odk.collect.android.preferences.source
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito.mock
-import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
 
 class PreferencesDataStoreTest {
@@ -19,72 +18,72 @@ class PreferencesDataStoreTest {
     @Test
     fun `When putString() called, should save() be called on generalPreferences`() {
         preferencesDataStore.putString("Key", "value")
-        verify(preferencesDataSource, times(1)).save("Key", "value")
+        verify(preferencesDataSource).save("Key", "value")
     }
 
     @Test
     fun `When putBoolean() called, should save() be called on generalPreferences`() {
         preferencesDataStore.putBoolean("Key", true)
-        verify(preferencesDataSource, times(1)).save("Key", true)
+        verify(preferencesDataSource).save("Key", true)
     }
 
     @Test
     fun `When putLong() called, should save() be called on generalPreferences`() {
         preferencesDataStore.putLong("Key", 3L)
-        verify(preferencesDataSource, times(1)).save("Key", 3L)
+        verify(preferencesDataSource).save("Key", 3L)
     }
 
     @Test
     fun `When putInt() called, should save() be called on generalPreferences`() {
         preferencesDataStore.putInt("Key", 5)
-        verify(preferencesDataSource, times(1)).save("Key", 5)
+        verify(preferencesDataSource).save("Key", 5)
     }
 
     @Test
     fun `When putFloat() called, should save() be called on generalPreferences`() {
         preferencesDataStore.putFloat("Key", 8.43f)
-        verify(preferencesDataSource, times(1)).save("Key", 8.43f)
+        verify(preferencesDataSource).save("Key", 8.43f)
     }
 
     @Test
     fun `When putStringSet() called, should save() be called on generalPreferences`() {
         preferencesDataStore.putStringSet("Key", setOf("x", "y", "z"))
-        verify(preferencesDataSource, times(1)).save("Key", setOf("x", "y", "z"))
+        verify(preferencesDataSource).save("Key", setOf("x", "y", "z"))
     }
 
     @Test
     fun `When getString() called, should getString() be called on generalPreferences`() {
         preferencesDataStore.getString("Key", "value")
-        verify(preferencesDataSource, times(1)).getString("Key")
+        verify(preferencesDataSource).getString("Key")
     }
 
     @Test
     fun `When getBoolean() called, should getBoolean() be called on generalPreferences`() {
         preferencesDataStore.getBoolean("Key", false)
-        verify(preferencesDataSource, times(1)).getBoolean("Key")
+        verify(preferencesDataSource).getBoolean("Key")
     }
 
     @Test
     fun `When getLong() called, should getLong() be called on generalPreferences`() {
         preferencesDataStore.getLong("Key", 0L)
-        verify(preferencesDataSource, times(1)).getLong("Key")
+        verify(preferencesDataSource).getLong("Key")
     }
 
     @Test
     fun `When getInt() called, should getInt() be called on generalPreferences`() {
         preferencesDataStore.getInt("Key", 0)
-        verify(preferencesDataSource, times(1)).getInt("Key")
+        verify(preferencesDataSource).getInt("Key")
     }
 
     @Test
     fun `When getFloat() called, should getFloat() be called on generalPreferences`() {
         preferencesDataStore.getFloat("Key", 1f)
-        verify(preferencesDataSource, times(1)).getFloat("Key")
+        verify(preferencesDataSource).getFloat("Key")
     }
 
     @Test
     fun `When getStringSet() called, should getStringSet() be called on generalPreferences`() {
         preferencesDataStore.getStringSet("Key", emptySet())
-        verify(preferencesDataSource, times(1)).getStringSet("Key")
+        verify(preferencesDataSource).getStringSet("Key")
     }
 }
