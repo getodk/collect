62c575a4f88d5fbe0c4f4be953566f7733b81e4c
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-03-16 15:51:47 +0100

    Settings for projects should be provided depending on currentProjectId

--- collect_app/src/main/java/org/odk/collect/android/preferences/keys/MetaKeys.java
+++ collect_app/src/main/java/org/odk/collect/android/preferences/keys/MetaKeys.java
@@ -1,5 +1,8 @@
 package org.odk.collect.android.preferences.keys;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MetaKeys {
 
     public static final String KEY_INSTALL_ID = "metadata_installid";
@@ -7,6 +10,15 @@ public class MetaKeys {
     public static final String KEY_GOOGLE_BUG_154855417_FIXED = "google_bug_154855417_fixed";
     public static final String LAST_UPDATED_NOTIFICATION = "last_updated_notification";
     public static final String SERVER_LIST = "server_list";
+    public static final String CURRENT_PROJECT_ID = "current_project_id";
+
+    public static final String DEFAULT_PROJECT_ID = "default";
+
+    public static Map<String, Object> getDefaults() {
+        Map<String, Object> defaults = new HashMap<>();
+        defaults.put(CURRENT_PROJECT_ID, DEFAULT_PROJECT_ID);
+        return defaults;
+    }
 
     private MetaKeys() {
 
