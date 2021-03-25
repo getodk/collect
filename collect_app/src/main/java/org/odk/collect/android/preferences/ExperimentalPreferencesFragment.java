c03adfd76e6f39a5b007c23611024ba75cf6d778
Neil Penman <neilpenman@gmail.com>
2021-03-25 20:19:37 +1000

    Fix crash in experimental preferences page

--- collect_app/src/main/java/org/odk/collect/android/preferences/ExperimentalPreferencesFragment.java
+++ collect_app/src/main/java/org/odk/collect/android/preferences/ExperimentalPreferencesFragment.java
@@ -1,12 +1,14 @@
 package org.odk.collect.android.preferences;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 
 import org.odk.collect.android.R;
 import org.odk.collect.android.activities.MainMenuActivity;
+import org.odk.collect.android.activities.SmapMain;
 import org.odk.collect.android.injection.DaggerUtils;
 
 import static org.odk.collect.android.activities.ActivityUtils.startActivityAndCloseAllOthers;
@@ -18,7 +20,9 @@ public class ExperimentalPreferencesFragment extends BasePreferenceFragment {
         setPreferencesFromResource(R.xml.experimental_preferences, rootKey);
 
         findPreference(GeneralKeys.KEY_MAGENTA_THEME).setOnPreferenceChangeListener((preference, newValue) -> {
-            startActivityAndCloseAllOthers(requireActivity(), MainMenuActivity.class);
+            Intent intent = new Intent(getActivity().getBaseContext(), SmapMain.class);     // smap
+            getActivity().startActivity(intent);
+            //startActivityAndCloseAllOthers(requireActivity(), MainMenuActivity.class);    // smap commented out
             return true;
         });
     }
