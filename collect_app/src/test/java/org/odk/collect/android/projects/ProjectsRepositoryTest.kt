26e5d28aa8404d6706744c2711a6bd2245462721
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-03-29 15:58:31 +0200

    Added unit tests

---
+++ collect_app/src/test/java/org/odk/collect/android/projects/ProjectsRepositoryTest.kt
@@ -0,0 +1,25 @@
+package org.odk.collect.android.projects
+
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.junit.Before
+import org.junit.Test
+
+abstract class ProjectsRepositoryTest {
+    lateinit var projectsRepository: ProjectsRepository
+
+    abstract fun buildSubject(): ProjectsRepository
+
+    @Before fun setup() {
+        projectsRepository = buildSubject()
+    }
+
+    @Test fun add_shouldSaveProjectToStorage() {
+        projectsRepository.add("Project 1")
+
+        val projects = projectsRepository.getAll()
+
+        assertThat(projects.size, `is`(1))
+        assertThat(projects[0].name, `is`("Project 1"))
+    }
+}
