6b420fc61a55f417a7465644573263ee4adbcb82
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-03-30 14:11:35 +0200

    Added more tests for ProjectsRepositoryTest

--- collect_app/src/test/java/org/odk/collect/android/projects/ProjectsRepositoryTest.kt
+++ collect_app/src/test/java/org/odk/collect/android/projects/ProjectsRepositoryTest.kt
@@ -2,6 +2,8 @@ package org.odk.collect.android.projects
 
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.not
+import org.hamcrest.Matchers.notNullValue
 import org.junit.Before
 import org.junit.Test
 
@@ -16,12 +18,50 @@ abstract class ProjectsRepositoryTest {
     }
 
     @Test
+    fun getAll_shouldReturnAllProjectsFromStorage() {
+        projectsRepository.add("ProjectX")
+        projectsRepository.add("ProjectY")
+        projectsRepository.add("ProjectZ")
+
+        val projects = projectsRepository.getAll()
+
+        assertThat(projects.size, `is`(3))
+        assertThat(projects[0].name, `is`("ProjectX"))
+        assertThat(projects[1].name, `is`("ProjectY"))
+        assertThat(projects[2].name, `is`("ProjectZ"))
+    }
+
+    @Test
     fun add_shouldSaveProjectToStorage() {
-        projectsRepository.add("Project 1")
+        projectsRepository.add("ProjectX")
 
         val projects = projectsRepository.getAll()
 
         assertThat(projects.size, `is`(1))
-        assertThat(projects[0].name, `is`("Project 1"))
+        assertThat(projects[0].name, `is`("ProjectX"))
+    }
+
+    @Test
+    fun delete_shouldDeleteProjectFromStorage() {
+        projectsRepository.add("ProjectX")
+        projectsRepository.add("ProjectY")
+
+        val projects = projectsRepository.getAll()
+        projectsRepository.delete(projects.first { it.name == "ProjectX" }.uuid)
+
+        assertThat(projects.size, `is`(1))
+        assertThat(projects[0].name, `is`("ProjectY"))
+    }
+
+    @Test
+    open fun add_shouldAddsUniqueId() {
+        projectsRepository.add("ProjectX")
+        projectsRepository.add("ProjectY")
+
+        val projects = projectsRepository.getAll()
+
+        assertThat(projects[0].uuid, `is`(notNullValue()))
+        assertThat(projects[1].uuid, `is`(notNullValue()))
+        assertThat(projects[0].uuid, not(projects[1].uuid))
     }
 }
