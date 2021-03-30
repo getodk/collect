e36bfbe73959ff6eec79918febb67f526d99f000
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-03-31 00:07:46 +0200

    Added oneCharacterOrEmojiOnly_shouldBeAcceptedAsProjectIcon test

--- collect_app/src/test/java/org/odk/collect/android/projects/AddProjectDialogTest.kt
+++ collect_app/src/test/java/org/odk/collect/android/projects/AddProjectDialogTest.kt
@@ -1,12 +1,19 @@
 package org.odk.collect.android.projects
 
-import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.Visibility
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isRoot
+import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
+import androidx.test.espresso.matcher.ViewMatchers.withHint
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.android.material.button.MaterialButton
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.odk.collect.android.R
@@ -32,8 +39,22 @@ class AddProjectDialogTest {
         val scenario = RobolectricHelpers.launchDialogFragment(AddProjectDialog::class.java)
         scenario.onFragment { f: AddProjectDialog ->
             val dialog = f.dialog
-            Espresso.onView(ViewMatchers.isRoot()).perform(ViewActions.pressBack())
+            onView(isRoot()).perform(ViewActions.pressBack())
             assertThat(dialog!!.isShowing, `is`(false))
+        }
+    }
+
+    @Test
+    fun oneCharacterOrEmojiOnly_shouldBeAcceptedAsProjectIcon() {
+        val scenario = RobolectricHelpers.launchDialogFragmentInContainer(AddProjectDialog::class.java)
+        scenario.onFragment {
+            // One character
+            onView(withHint(R.string.project_icon)).perform(ViewActions.typeText("XYZ"))
+            onView(Matchers.allOf(withText("X"), withEffectiveVisibility(Visibility.VISIBLE))).check(matches(isDisplayed()))
+
+            // or one emoji
+            onView(withHint(R.string.project_icon)).perform(ViewActions.replaceText("\uD83D\uDC22"))
+            onView(Matchers.allOf(withText("\uD83D\uDC22"), withEffectiveVisibility(Visibility.VISIBLE))).check(matches(isDisplayed()))
         }
     }
 }
