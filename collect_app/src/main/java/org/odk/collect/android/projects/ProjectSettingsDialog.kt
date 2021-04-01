9d5329fc2098b834b7e9f66ebd76d4a4f284c32d
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-04-01 15:22:53 +0200

    Set icon color based on the saved value

--- collect_app/src/main/java/org/odk/collect/android/projects/ProjectSettingsDialog.kt
+++ collect_app/src/main/java/org/odk/collect/android/projects/ProjectSettingsDialog.kt
@@ -87,7 +87,10 @@ class ProjectSettingsDialog : DialogFragment() {
         projectsRepository.getAll().forEach { project ->
             val projectView = LayoutInflater.from(context).inflate(R.layout.project_list_item, null)
 
-            projectView.findViewById<TextView>(R.id.project_icon).text = project.icon
+            projectView.findViewById<TextView>(R.id.project_icon).apply {
+                background.setTint(Color.parseColor(project.color))
+                text = project.icon
+            }
             projectView.findViewById<TextView>(R.id.project_name).text = project.name
 
             binding.projectList.addView(projectView)
