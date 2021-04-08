ffa147e241f4548d7baf2c44f7b787f3be63ecec
Callum Stott <callum@seadowg.com>
2021-04-08 12:54:11 +0100

    Replace query in repo

--- collect_app/src/main/java/org/odk/collect/android/database/DatabaseInstancesRepository.java
+++ collect_app/src/main/java/org/odk/collect/android/database/DatabaseInstancesRepository.java
@@ -2,12 +2,12 @@ package org.odk.collect.android.database;
 
 import android.content.ContentValues;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 
-import org.odk.collect.android.application.Collect;
 import org.odk.collect.android.instances.Instance;
 import org.odk.collect.android.instances.InstancesRepository;
 import org.odk.collect.android.provider.InstanceProvider;
-import org.odk.collect.android.provider.InstanceProviderAPI.InstanceColumns;
 import org.odk.collect.android.storage.StoragePathProvider;
 
 import java.util.ArrayList;
@@ -15,9 +15,17 @@ import java.util.List;
 
 import static android.provider.BaseColumns._ID;
 import static org.odk.collect.android.database.DatabaseConstants.INSTANCES_TABLE_NAME;
+import static org.odk.collect.android.provider.InstanceProviderAPI.InstanceColumns.CAN_EDIT_WHEN_COMPLETE;
 import static org.odk.collect.android.provider.InstanceProviderAPI.InstanceColumns.DELETED_DATE;
+import static org.odk.collect.android.provider.InstanceProviderAPI.InstanceColumns.DISPLAY_NAME;
+import static org.odk.collect.android.provider.InstanceProviderAPI.InstanceColumns.GEOMETRY;
+import static org.odk.collect.android.provider.InstanceProviderAPI.InstanceColumns.GEOMETRY_TYPE;
+import static org.odk.collect.android.provider.InstanceProviderAPI.InstanceColumns.INSTANCE_FILE_PATH;
 import static org.odk.collect.android.provider.InstanceProviderAPI.InstanceColumns.JR_FORM_ID;
 import static org.odk.collect.android.provider.InstanceProviderAPI.InstanceColumns.JR_VERSION;
+import static org.odk.collect.android.provider.InstanceProviderAPI.InstanceColumns.LAST_STATUS_CHANGE_DATE;
+import static org.odk.collect.android.provider.InstanceProviderAPI.InstanceColumns.STATUS;
+import static org.odk.collect.android.provider.InstanceProviderAPI.InstanceColumns.SUBMISSION_URI;
 
 /**
  * Mediates between {@link Instance} objects and the underlying SQLite database that stores them.
@@ -26,59 +34,75 @@ public final class DatabaseInstancesRepository implements InstancesRepository {
 
     @Override
     public Instance get(Long databaseId) {
-        String selection = InstanceColumns._ID + "=?";
+        String selection = _ID + "=?";
         String[] selectionArgs = {Long.toString(databaseId)};
 
-        List<Instance> result = getInstancesFromCursor(query(selection, selectionArgs));
-        return !result.isEmpty() ? result.get(0) : null;
+        try (Cursor cursor = query(selection, selectionArgs)) {
+            List<Instance> result = getInstancesFromCursor(cursor);
+            return !result.isEmpty() ? result.get(0) : null;
+        }
     }
 
     @Override
     public Instance getOneByPath(String instancePath) {
-        String selection = InstanceColumns.INSTANCE_FILE_PATH + "=?";
+        String selection = INSTANCE_FILE_PATH + "=?";
         String[] args = {new StoragePathProvider().getRelativeInstancePath(instancePath)};
-        List<Instance> instances = getInstancesFromCursor(query(selection, args));
-        if (instances.size() == 1) {
-            return instances.get(0);
-        } else {
-            return null;
+        try (Cursor cursor = query(selection, args)) {
+            List<Instance> instances = getInstancesFromCursor(cursor);
+            if (instances.size() == 1) {
+                return instances.get(0);
+            } else {
+                return null;
+            }
         }
     }
 
     @Override
     public List<Instance> getAll() {
-        return getInstancesFromCursor(query(null, null));
+        try (Cursor cursor = query(null, null)) {
+            return getInstancesFromCursor(cursor);
+        }
     }
 
     @Override
     public List<Instance> getAllNotDeleted() {
-        return getInstancesFromCursor(query(InstanceColumns.DELETED_DATE + " IS NULL ", null));
+        try (Cursor cursor = query(DELETED_DATE + " IS NULL ", null)) {
+            return getInstancesFromCursor(cursor);
+        }
     }
 
     @Override
     public List<Instance> getAllByStatus(String... status) {
-        Cursor instancesCursor = getCursorForAllByStatus(status);
-        return getInstancesFromCursor(instancesCursor);
+        try (Cursor instancesCursor = getCursorForAllByStatus(status)) {
+            return getInstancesFromCursor(instancesCursor);
+        }
     }
 
     @Override
     public int getCountByStatus(String... status) {
-        return getCursorForAllByStatus(status).getCount();
+        try (Cursor cursorForAllByStatus = getCursorForAllByStatus(status)) {
+            return cursorForAllByStatus.getCount();
+        }
     }
 
 
     @Override
     public List<Instance> getAllByFormId(String formId) {
-        Cursor c = query(JR_FORM_ID + " = ?", new String[]{formId});
-        return getInstancesFromCursor(c);
+        try (Cursor c = query(JR_FORM_ID + " = ?", new String[]{formId})) {
+            return getInstancesFromCursor(c);
+        }
     }
 
     @Override
     public List<Instance> getAllNotDeletedByFormIdAndVersion(String jrFormId, String jrVersion) {
         if (jrVersion != null) {
-            return getInstancesFromCursor(query(JR_FORM_ID + " = ? AND " + JR_VERSION + " = ? AND " + DELETED_DATE + " IS NULL", new String[]{jrFormId, jrVersion}));
+            try (Cursor cursor = query(JR_FORM_ID + " = ? AND " + JR_VERSION + " = ? AND " + DELETED_DATE + " IS NULL", new String[]{jrFormId, jrVersion})) {
+                return getInstancesFromCursor(cursor);
+            }
         } else {
-            return getInstancesFromCursor(query(JR_FORM_ID + " = ? AND " + JR_VERSION + " IS NULL AND " + DELETED_DATE + " IS NULL", new String[]{jrFormId}));
+            try (Cursor cursor = query(JR_FORM_ID + " = ? AND " + JR_VERSION + " IS NULL AND " + DELETED_DATE + " IS NULL", new String[]{jrFormId})) {
+                return getInstancesFromCursor(cursor);
+            }
         }
     }
 
@@ -135,16 +159,35 @@ public final class DatabaseInstancesRepository implements InstancesRepository {
     }
 
     private Cursor getCursorForAllByStatus(String[] status) {
-        StringBuilder selection = new StringBuilder(InstanceColumns.STATUS + "=?");
+        StringBuilder selection = new StringBuilder(STATUS + "=?");
         for (int i = 1; i < status.length; i++) {
-            selection.append(" or ").append(InstanceColumns.STATUS).append("=?");
+            selection.append(" or ").append(STATUS).append("=?");
         }
 
         return query(selection.toString(), status);
     }
 
     private Cursor query(String selection, String[] selectionArgs) {
-        return Collect.getInstance().getContentResolver().query(InstanceColumns.CONTENT_URI, null, selection, selectionArgs, null);
+        SQLiteDatabase readableDatabase = InstanceProvider.getDbHelper().getReadableDatabase();
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+        qb.setTables(INSTANCES_TABLE_NAME);
+
+        String[] projection = {
+                _ID,
+                DISPLAY_NAME,
+                SUBMISSION_URI,
+                CAN_EDIT_WHEN_COMPLETE,
+                INSTANCE_FILE_PATH,
+                JR_FORM_ID,
+                JR_VERSION,
+                STATUS,
+                LAST_STATUS_CHANGE_DATE,
+                DELETED_DATE,
+                GEOMETRY,
+                GEOMETRY_TYPE
+        };
+
+        return qb.query(readableDatabase, projection, selection, selectionArgs, null, null, null);
     }
 
     private long insert(ContentValues values) {
@@ -159,68 +202,63 @@ public final class DatabaseInstancesRepository implements InstancesRepository {
         InstanceProvider.getDbHelper().getWritableDatabase().update(
                 INSTANCES_TABLE_NAME,
                 values,
-                InstanceColumns._ID + "=?",
+                _ID + "=?",
                 new String[]{instanceId.toString()}
         );
     }
 
     private static ContentValues getValuesFromInstanceObject(Instance instance) {
         ContentValues values = new ContentValues();
-        values.put(InstanceColumns.DISPLAY_NAME, instance.getDisplayName());
-        values.put(InstanceColumns.SUBMISSION_URI, instance.getSubmissionUri());
-        values.put(InstanceColumns.CAN_EDIT_WHEN_COMPLETE, Boolean.toString(instance.canEditWhenComplete()));
-        values.put(InstanceColumns.INSTANCE_FILE_PATH, new StoragePathProvider().getRelativeInstancePath(instance.getInstanceFilePath()));
-        values.put(InstanceColumns.JR_FORM_ID, instance.getFormId());
-        values.put(InstanceColumns.JR_VERSION, instance.getFormVersion());
-        values.put(InstanceColumns.STATUS, instance.getStatus());
-        values.put(InstanceColumns.LAST_STATUS_CHANGE_DATE, instance.getLastStatusChangeDate());
-        values.put(InstanceColumns.DELETED_DATE, instance.getDeletedDate());
-        values.put(InstanceColumns.GEOMETRY, instance.getGeometry());
-        values.put(InstanceColumns.GEOMETRY_TYPE, instance.getGeometryType());
+        values.put(DISPLAY_NAME, instance.getDisplayName());
+        values.put(SUBMISSION_URI, instance.getSubmissionUri());
+        values.put(CAN_EDIT_WHEN_COMPLETE, Boolean.toString(instance.canEditWhenComplete()));
+        values.put(INSTANCE_FILE_PATH, new StoragePathProvider().getRelativeInstancePath(instance.getInstanceFilePath()));
+        values.put(JR_FORM_ID, instance.getFormId());
+        values.put(JR_VERSION, instance.getFormVersion());
+        values.put(STATUS, instance.getStatus());
+        values.put(LAST_STATUS_CHANGE_DATE, instance.getLastStatusChangeDate());
+        values.put(DELETED_DATE, instance.getDeletedDate());
+        values.put(GEOMETRY, instance.getGeometry());
+        values.put(GEOMETRY_TYPE, instance.getGeometryType());
         return values;
     }
 
     public static List<Instance> getInstancesFromCursor(Cursor cursor) {
         List<Instance> instances = new ArrayList<>();
-        if (cursor != null) {
-            try {
-                cursor.moveToPosition(-1);
-                while (cursor.moveToNext()) {
-                    int displayNameColumnIndex = cursor.getColumnIndex(InstanceColumns.DISPLAY_NAME);
-                    int submissionUriColumnIndex = cursor.getColumnIndex(InstanceColumns.SUBMISSION_URI);
-                    int canEditWhenCompleteIndex = cursor.getColumnIndex(InstanceColumns.CAN_EDIT_WHEN_COMPLETE);
-                    int instanceFilePathIndex = cursor.getColumnIndex(InstanceColumns.INSTANCE_FILE_PATH);
-                    int jrFormIdColumnIndex = cursor.getColumnIndex(InstanceColumns.JR_FORM_ID);
-                    int jrVersionColumnIndex = cursor.getColumnIndex(InstanceColumns.JR_VERSION);
-                    int statusColumnIndex = cursor.getColumnIndex(InstanceColumns.STATUS);
-                    int lastStatusChangeDateColumnIndex = cursor.getColumnIndex(InstanceColumns.LAST_STATUS_CHANGE_DATE);
-                    int deletedDateColumnIndex = cursor.getColumnIndex(InstanceColumns.DELETED_DATE);
-                    int geometryTypeColumnIndex = cursor.getColumnIndex(InstanceColumns.GEOMETRY_TYPE);
-                    int geometryColumnIndex = cursor.getColumnIndex(InstanceColumns.GEOMETRY);
+        cursor.moveToPosition(-1);
+        while (cursor.moveToNext()) {
+            int displayNameColumnIndex = cursor.getColumnIndex(DISPLAY_NAME);
+            int submissionUriColumnIndex = cursor.getColumnIndex(SUBMISSION_URI);
+            int canEditWhenCompleteIndex = cursor.getColumnIndex(CAN_EDIT_WHEN_COMPLETE);
+            int instanceFilePathIndex = cursor.getColumnIndex(INSTANCE_FILE_PATH);
+            int jrFormIdColumnIndex = cursor.getColumnIndex(JR_FORM_ID);
+            int jrVersionColumnIndex = cursor.getColumnIndex(JR_VERSION);
+            int statusColumnIndex = cursor.getColumnIndex(STATUS);
+            int lastStatusChangeDateColumnIndex = cursor.getColumnIndex(LAST_STATUS_CHANGE_DATE);
+            int deletedDateColumnIndex = cursor.getColumnIndex(DELETED_DATE);
+            int geometryTypeColumnIndex = cursor.getColumnIndex(GEOMETRY_TYPE);
+            int geometryColumnIndex = cursor.getColumnIndex(GEOMETRY);
 
-                    int databaseIdIndex = cursor.getColumnIndex(InstanceColumns._ID);
+            int databaseIdIndex = cursor.getColumnIndex(_ID);
 
-                    Instance instance = new Instance.Builder()
-                            .displayName(cursor.getString(displayNameColumnIndex))
-                            .submissionUri(cursor.getString(submissionUriColumnIndex))
-                            .canEditWhenComplete(Boolean.valueOf(cursor.getString(canEditWhenCompleteIndex)))
-                            .instanceFilePath(new StoragePathProvider().getAbsoluteInstanceFilePath(cursor.getString(instanceFilePathIndex)))
-                            .formId(cursor.getString(jrFormIdColumnIndex))
-                            .formVersion(cursor.getString(jrVersionColumnIndex))
-                            .status(cursor.getString(statusColumnIndex))
-                            .lastStatusChangeDate(cursor.getLong(lastStatusChangeDateColumnIndex))
-                            .deletedDate(cursor.isNull(deletedDateColumnIndex) ? null : cursor.getLong(deletedDateColumnIndex))
-                            .geometryType(cursor.getString(geometryTypeColumnIndex))
-                            .geometry(cursor.getString(geometryColumnIndex))
-                            .dbId(cursor.getLong(databaseIdIndex))
-                            .build();
+            Instance instance = new Instance.Builder()
+                    .displayName(cursor.getString(displayNameColumnIndex))
+                    .submissionUri(cursor.getString(submissionUriColumnIndex))
+                    .canEditWhenComplete(Boolean.valueOf(cursor.getString(canEditWhenCompleteIndex)))
+                    .instanceFilePath(new StoragePathProvider().getAbsoluteInstanceFilePath(cursor.getString(instanceFilePathIndex)))
+                    .formId(cursor.getString(jrFormIdColumnIndex))
+                    .formVersion(cursor.getString(jrVersionColumnIndex))
+                    .status(cursor.getString(statusColumnIndex))
+                    .lastStatusChangeDate(cursor.getLong(lastStatusChangeDateColumnIndex))
+                    .deletedDate(cursor.isNull(deletedDateColumnIndex) ? null : cursor.getLong(deletedDateColumnIndex))
+                    .geometryType(cursor.getString(geometryTypeColumnIndex))
+                    .geometry(cursor.getString(geometryColumnIndex))
+                    .dbId(cursor.getLong(databaseIdIndex))
+                    .build();
 
-                    instances.add(instance);
-                }
-            } finally {
-                cursor.close();
-            }
+            instances.add(instance);
         }
+
         return instances;
     }
 }
