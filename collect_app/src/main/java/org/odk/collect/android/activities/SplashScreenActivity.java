9bba0762752f5511aebf25326df5e8201e10510e
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-04-12 21:01:01 +0200

    Removed decodeFile() method from SplashScreenActivity

--- collect_app/src/main/java/org/odk/collect/android/activities/SplashScreenActivity.java
+++ collect_app/src/main/java/org/odk/collect/android/activities/SplashScreenActivity.java
@@ -16,11 +16,8 @@ package org.odk.collect.android.activities;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
@@ -32,17 +29,14 @@ import org.odk.collect.android.injection.DaggerUtils;
 import org.odk.collect.android.preferences.keys.GeneralKeys;
 import org.odk.collect.android.preferences.source.SettingsProvider;
 import org.odk.collect.android.permissions.PermissionsProvider;
+import org.odk.collect.android.utilities.FileUtils;
+import org.odk.collect.android.utilities.ScreenUtils;
 import org.odk.collect.shared.Md5;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 import javax.inject.Inject;
-
-import timber.log.Timber;
 
 import static org.odk.collect.android.analytics.AnalyticsEvents.SHOW_SPLASH_SCREEN;
 import static org.odk.collect.android.preferences.keys.GeneralKeys.KEY_SPLASH_PATH;
@@ -50,8 +44,6 @@ import static org.odk.collect.android.preferences.keys.GeneralKeys.KEY_SPLASH_PA
 public class SplashScreenActivity extends Activity {
 
     private static final int SPLASH_TIMEOUT = 2000; // milliseconds
-
-    private int imageMaxWidth;
 
     @Inject
     Analytics analytics;
@@ -72,9 +64,6 @@ public class SplashScreenActivity extends Activity {
     }
 
     private void init() {
-        DisplayMetrics displayMetrics = getApplicationContext().getResources().getDisplayMetrics();
-        imageMaxWidth = displayMetrics.widthPixels;
-
         setContentView(R.layout.splash_screen);
 
         boolean showSplash = settingsProvider.getGeneralSettings().getBoolean(GeneralKeys.KEY_SHOW_SPLASH);
@@ -95,59 +84,14 @@ public class SplashScreenActivity extends Activity {
         finish();
     }
 
-    // decodes image and scales it to reduce memory consumption
-    private Bitmap decodeFile(File f) {
-        Bitmap b = null;
-        try {
-            // Decode image size
-            BitmapFactory.Options o = new BitmapFactory.Options();
-            o.inJustDecodeBounds = true;
-
-            FileInputStream fis = new FileInputStream(f);
-            BitmapFactory.decodeStream(fis, null, o);
-            try {
-                fis.close();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                Timber.e(e, "Unable to close file input stream");
-            }
-
-            int scale = 1;
-            if (o.outHeight > imageMaxWidth || o.outWidth > imageMaxWidth) {
-                scale =
-                        (int) Math.pow(
-                                2,
-                                (int) Math.round(Math.log(imageMaxWidth
-                                        / (double) Math.max(o.outHeight, o.outWidth))
-                                        / Math.log(0.5)));
-            }
-
-            // Decode with inSampleSize
-            BitmapFactory.Options o2 = new BitmapFactory.Options();
-            o2.inSampleSize = scale;
-            fis = new FileInputStream(f);
-            b = BitmapFactory.decodeStream(fis, null, o2);
-            try {
-                fis.close();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                Timber.e(e, "Unable to close file input stream");
-            }
-        } catch (FileNotFoundException e) {
-            Timber.d(e);
-        }
-        return b;
-    }
-
     private void startSplashScreen(String path) {
-
         // add items to the splash screen here. makes things less distracting.
         ImageView customSplashView = findViewById(R.id.splash);
         LinearLayout defaultSplashView = findViewById(R.id.splash_default);
 
         File customSplash = new File(path);
         if (customSplash.exists()) {
-            customSplashView.setImageBitmap(decodeFile(customSplash));
+            customSplashView.setImageBitmap(FileUtils.getBitmapScaledToDisplay(customSplash, ScreenUtils.getScreenHeight(), ScreenUtils.getScreenWidth()));
             defaultSplashView.setVisibility(View.GONE);
             customSplashView.setVisibility(View.VISIBLE);
         }
