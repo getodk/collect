0c4bb0310aa19a01105e0bb69440b8c3440e8646
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-04-16 14:55:00 +0200

    Improved FirstLaunchDialogTest

--- collect_app/src/test/java/org/odk/collect/android/fragments/dialogs/FirstLaunchDialogTest.kt
+++ collect_app/src/test/java/org/odk/collect/android/fragments/dialogs/FirstLaunchDialogTest.kt
@@ -1,18 +1,21 @@
 package org.odk.collect.android.fragments.dialogs
 
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.action.ViewActions.pressBack
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
-import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isRoot
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.hamcrest.CoreMatchers
-import org.hamcrest.MatcherAssert
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.notNullValue
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.`when`
@@ -23,6 +26,8 @@ import org.odk.collect.android.activities.MainMenuActivity
 import org.odk.collect.android.application.Collect
 import org.odk.collect.android.injection.config.AppDependencyModule
 import org.odk.collect.android.preferences.source.SettingsProvider
+import org.odk.collect.android.projects.AddProjectDialog
+import org.odk.collect.android.projects.AddProjectDialog.Companion.STARTED_FROM_FIRST_LAUNCH_SCREEN
 import org.odk.collect.android.projects.ProjectImporter
 import org.odk.collect.android.projects.ProjectsRepository
 import org.odk.collect.android.support.RobolectricHelpers
@@ -37,9 +42,9 @@ class FirstLaunchDialogTest {
         val scenario = RobolectricHelpers.launchDialogFragment(FirstLaunchDialog::class.java)
         scenario.onFragment { f: FirstLaunchDialog ->
             val dialog = f.dialog
-            MatcherAssert.assertThat(dialog!!.isShowing, CoreMatchers.`is`(true))
-            Espresso.onView(ViewMatchers.isRoot()).perform(ViewActions.pressBack())
-            MatcherAssert.assertThat(dialog.isShowing, CoreMatchers.`is`(true))
+            assertThat(dialog!!.isShowing, `is`(true))
+            onView(isRoot()).perform(pressBack())
+            assertThat(dialog.isShowing, `is`(true))
         }
     }
 
@@ -54,7 +59,7 @@ class FirstLaunchDialogTest {
 
         val scenario = RobolectricHelpers.launchDialogFragmentInContainer(FirstLaunchDialog::class.java)
         scenario.onFragment {
-            Espresso.onView(withText(getString(ApplicationProvider.getApplicationContext<Collect>(), R.string.configure_later))).perform(click())
+            onView(withText(getString(ApplicationProvider.getApplicationContext<Collect>(), R.string.configure_later))).perform(click())
             verify(projectImporter).importDemoProject()
         }
     }
@@ -64,9 +69,37 @@ class FirstLaunchDialogTest {
         val scenario = RobolectricHelpers.launchDialogFragmentInContainer(FirstLaunchDialog::class.java)
         scenario.onFragment {
             Intents.init()
-            Espresso.onView(withText(getString(ApplicationProvider.getApplicationContext<Collect>(), R.string.configure_later))).perform(click())
+            onView(withText(getString(ApplicationProvider.getApplicationContext<Collect>(), R.string.configure_later))).perform(click())
             intended(hasComponent(MainMenuActivity::class.java.name))
             Intents.release()
+        }
+    }
+
+    @Test
+    fun clickingConfigureManually_shouldStartAddProjectDialogWithRequiredArgument() {
+        val scenario = RobolectricHelpers.launchDialogFragmentInContainer(FirstLaunchDialog::class.java)
+        scenario.onFragment {
+            onView(withText(getString(ApplicationProvider.getApplicationContext<Collect>(), R.string.configure_manually))).perform(click())
+            val fragment = it.activity!!.supportFragmentManager.findFragmentByTag(AddProjectDialog::class.qualifiedName)
+            assertThat(fragment, `is`(notNullValue()))
+            assertThat(fragment!!.arguments!![STARTED_FROM_FIRST_LAUNCH_SCREEN], `is`(true))
+        }
+    }
+
+    @Test
+    fun clickingConfigureManually_shouldNotDismissFirstLaunchDialog() {
+        val scenario = RobolectricHelpers.launchDialogFragmentInContainer(FirstLaunchDialog::class.java)
+        scenario.onFragment {
+            onView(withText(getString(ApplicationProvider.getApplicationContext<Collect>(), R.string.configure_manually))).perform(click())
+            assertThat(it.activity!!.supportFragmentManager.findFragmentByTag(it.tag), `is`(notNullValue()))
+        }
+    }
+
+    @Test
+    fun appIconShouldBeDisplayed() {
+        val scenario = RobolectricHelpers.launchDialogFragmentInContainer(FirstLaunchDialog::class.java)
+        scenario.onFragment {
+            onView(withId(R.id.logo)).check(matches(isDisplayed()))
         }
     }
 
@@ -83,7 +116,7 @@ class FirstLaunchDialogTest {
         val scenario = RobolectricHelpers.launchDialogFragmentInContainer(FirstLaunchDialog::class.java)
         scenario.onFragment {
             verify(versionInformation).versionToDisplay
-            Espresso.onView(withText(getString(ApplicationProvider.getApplicationContext<Collect>(), R.string.app_name, "v1.30.1.1"))).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+            onView(withText(getString(ApplicationProvider.getApplicationContext<Collect>(), R.string.app_name, "v1.30.1.1"))).check(matches(isDisplayed()))
         }
     }
 }
