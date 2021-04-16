254f2b9ea6cff512369c9bbd83e7d792579c6284
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-04-16 13:01:13 +0200

    Fixed theming

--- collect_app/src/main/java/org/odk/collect/android/fragments/dialogs/FirstLaunchDialog.kt
+++ collect_app/src/main/java/org/odk/collect/android/fragments/dialogs/FirstLaunchDialog.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.Toolbar
-import androidx.fragment.app.DialogFragment
 import kotlinx.android.synthetic.main.first_launch_dialog_layout.*
 import org.odk.collect.android.R
 import org.odk.collect.android.activities.ActivityUtils
@@ -23,11 +22,6 @@ class FirstLaunchDialog : MaterialFullScreenDialogFragment() {
 
     @Inject
     lateinit var versionInformation: VersionInformation
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setStyle(DialogFragment.STYLE_NO_TITLE, R.style.Theme_Collect_Light)
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.first_launch_dialog_layout, container, false)
