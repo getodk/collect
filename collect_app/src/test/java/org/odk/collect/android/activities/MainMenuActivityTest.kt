cd7230b90ced668b4eb1ec347aa335ba7f969fc8
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-04-23 17:26:51 +0200

    Removed old MianActivityTest and move tests to the new MainMenuActivityTest

--- collect_app/src/test/java/org/odk/collect/android/activities/MainMenuActivityTest.kt
+++ collect_app/src/test/java/org/odk/collect/android/activities/MainMenuActivityTest.kt
@@ -4,10 +4,13 @@ import android.app.Application
 import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.equalTo
@@ -24,6 +27,7 @@ import org.odk.collect.android.injection.config.AppDependencyModule
 import org.odk.collect.android.preferences.source.SettingsProvider
 import org.odk.collect.android.projects.CurrentProjectProvider
 import org.odk.collect.android.support.RobolectricHelpers
+import org.odk.collect.android.utilities.ApplicationConstants
 import org.odk.collect.android.version.VersionInformation
 import org.odk.collect.projects.Project
 
@@ -45,6 +49,8 @@ class MainMenuActivityTest {
         `when`(mainMenuViewModel.sentFormsCount).thenReturn(livedata)
         `when`(mainMenuViewModel.unsentFormsCount).thenReturn(livedata)
 
+        `when`(mainMenuViewModel.currentProject).thenReturn(Project("Project 1", "P", "#ffffff"))
+
         RobolectricHelpers.overrideAppDependencyModule(object : AppDependencyModule() {
             override fun providesMainMenuViewModel(versionInformation: VersionInformation, application: Application, settingsProvider: SettingsProvider, instancesCountRepository: InstancesCountRepository, currentProjectProvider: CurrentProjectProvider): MainMenuViewModel.Factory {
                 return object : MainMenuViewModel.Factory(versionInformation, application, settingsProvider, instancesCountRepository, currentProjectProvider) {
@@ -58,8 +64,6 @@ class MainMenuActivityTest {
 
     @Test
     fun `Project icon for current project should be displayed`() {
-        `when`(mainMenuViewModel.currentProject).thenReturn(Project("Project 1", "P", "#ffffff"))
-
         val scenario = ActivityScenario.launch(MainMenuActivity::class.java)
         scenario.onActivity { activity: MainMenuActivity ->
             val projectIcon = activity.findViewById<TextView>(R.id.project_icon)
@@ -69,6 +73,146 @@ class MainMenuActivityTest {
 
             val background = projectIcon.background as GradientDrawable
             assertThat(background.color!!.defaultColor, equalTo(Color.parseColor("#ffffff")))
+        }
+    }
+
+    @Test
+    fun `Fill Blank Form button should have proper text`() {
+        val scenario = ActivityScenario.launch(MainMenuActivity::class.java)
+        scenario.onActivity { activity: MainMenuActivity ->
+            val button: Button = activity.findViewById(R.id.enter_data)
+            assertThat(button.text, `is`(activity.getString(R.string.enter_data_button)))
+        }
+    }
+
+    @Test
+    fun `Fill Blank Form button should start list of blank forms`() {
+        val scenario = ActivityScenario.launch(MainMenuActivity::class.java)
+        scenario.onActivity { activity: MainMenuActivity ->
+            Intents.init()
+
+            val button: Button = activity.findViewById(R.id.enter_data)
+            button.performClick()
+            assertThat(Intents.getIntents()[0], hasComponent(FillBlankFormActivity::class.java.name))
+
+            Intents.release()
+        }
+    }
+
+    @Test
+    fun `Edit Saved Form button should have proper text`() {
+        val scenario = ActivityScenario.launch(MainMenuActivity::class.java)
+        scenario.onActivity { activity: MainMenuActivity ->
+            val button: Button = activity.findViewById(R.id.review_data)
+            assertThat(button.text, `is`(activity.getString(R.string.review_data_button)))
+        }
+    }
+
+    @Test
+    fun `Edit Saved Form button should start list of saved forms`() {
+        val scenario = ActivityScenario.launch(MainMenuActivity::class.java)
+        scenario.onActivity { activity: MainMenuActivity ->
+            Intents.init()
+
+            val button: Button = activity.findViewById(R.id.review_data)
+            button.performClick()
+            assertThat(Intents.getIntents()[0], hasComponent(InstanceChooserList::class.java.name))
+            assertThat(Intents.getIntents()[0].extras!!.get(ApplicationConstants.BundleKeys.FORM_MODE), `is`(ApplicationConstants.FormModes.EDIT_SAVED))
+
+            Intents.release()
+        }
+    }
+
+    @Test
+    fun `Send Finalized Form button should have proper text`() {
+        val scenario = ActivityScenario.launch(MainMenuActivity::class.java)
+        scenario.onActivity { activity: MainMenuActivity ->
+            val button: Button = activity.findViewById(R.id.send_data)
+            assertThat(button.text, `is`(activity.getString(R.string.send_data_button)))
+        }
+    }
+
+    @Test
+    fun `Send Finalized Form button should start list of finalized forms`() {
+        val scenario = ActivityScenario.launch(MainMenuActivity::class.java)
+        scenario.onActivity { activity: MainMenuActivity ->
+            Intents.init()
+
+            val button: Button = activity.findViewById(R.id.send_data)
+            button.performClick()
+            assertThat(Intents.getIntents()[0], hasComponent(InstanceUploaderListActivity::class.java.name))
+
+            Intents.release()
+        }
+    }
+
+    @Test
+    fun `View Sent Form button should have proper text`() {
+        val scenario = ActivityScenario.launch(MainMenuActivity::class.java)
+        scenario.onActivity { activity: MainMenuActivity ->
+            val button: Button = activity.findViewById(R.id.view_sent_forms)
+            assertThat(button.text, `is`(activity.getString(R.string.view_sent_forms)))
+        }
+    }
+
+    @Test
+    fun `View Sent Form button should start list of sent forms`() {
+        val scenario = ActivityScenario.launch(MainMenuActivity::class.java)
+        scenario.onActivity { activity: MainMenuActivity ->
+            Intents.init()
+
+            val button: Button = activity.findViewById(R.id.view_sent_forms)
+            button.performClick()
+            assertThat(Intents.getIntents()[0], hasComponent(InstanceChooserList::class.java.name))
+            assertThat(Intents.getIntents()[0].extras!!.get(ApplicationConstants.BundleKeys.FORM_MODE), `is`(ApplicationConstants.FormModes.VIEW_SENT))
+
+            Intents.release()
+        }
+    }
+
+    @Test
+    fun `Get Blank Form button should have proper text`() {
+        val scenario = ActivityScenario.launch(MainMenuActivity::class.java)
+        scenario.onActivity { activity: MainMenuActivity ->
+            val button: Button = activity.findViewById(R.id.get_forms)
+            assertThat(button.text, `is`(activity.getString(R.string.get_forms)))
+        }
+    }
+
+    @Test
+    fun `Get Blank Form button should start list of forms to download`() {
+        val scenario = ActivityScenario.launch(MainMenuActivity::class.java)
+        scenario.onActivity { activity: MainMenuActivity ->
+            Intents.init()
+
+            val button: Button = activity.findViewById(R.id.get_forms)
+            button.performClick()
+            assertThat(Intents.getIntents()[0], hasComponent(FormDownloadListActivity::class.java.name))
+
+            Intents.release()
+        }
+    }
+
+    @Test
+    fun `Delete Saved Form button should have proper text`() {
+        val scenario = ActivityScenario.launch(MainMenuActivity::class.java)
+        scenario.onActivity { activity: MainMenuActivity ->
+            val button: Button = activity.findViewById(R.id.manage_forms)
+            assertThat(button.text, `is`(activity.getString(R.string.manage_files)))
+        }
+    }
+
+    @Test
+    fun `Delete Saved Form button should start list of forms to delete`() {
+        val scenario = ActivityScenario.launch(MainMenuActivity::class.java)
+        scenario.onActivity { activity: MainMenuActivity ->
+            Intents.init()
+
+            val button: Button = activity.findViewById(R.id.manage_forms)
+            button.performClick()
+            assertThat(Intents.getIntents()[0], hasComponent(DeleteSavedFormActivity::class.java.name))
+
+            Intents.release()
         }
     }
 }
