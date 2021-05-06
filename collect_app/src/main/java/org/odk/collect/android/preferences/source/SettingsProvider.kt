409bc49de5ad9689291c9bd295c7388c71777b45
Callum Stott <callum@seadowg.com>
2021-05-06 11:05:08 +0100

    Cache settings in singleton

--- collect_app/src/main/java/org/odk/collect/android/preferences/source/SettingsProvider.kt
+++ collect_app/src/main/java/org/odk/collect/android/preferences/source/SettingsProvider.kt
@@ -11,23 +11,32 @@ import org.odk.collect.shared.Settings
 
 class SettingsProvider(private val context: Context) {
 
-    fun getMetaSettings() = SharedPreferencesSettings(context.getSharedPreferences(META_SETTINGS_NAME, Context.MODE_PRIVATE))
+    private val settings = mutableMapOf<String, Settings>()
+
+    fun getMetaSettings() = settings.getOrPut(META_SETTINGS_NAME) {
+        SharedPreferencesSettings(context.getSharedPreferences(META_SETTINGS_NAME, Context.MODE_PRIVATE))
+    }
 
     @JvmOverloads
     fun getGeneralSettings(projectId: String = NOT_SPECIFIED_UUID): Settings {
         val settingsId = getSettingsId(GENERAL_SETTINGS_NAME, projectId)
 
-        return if (settingsId == GENERAL_SETTINGS_NAME) {
-            SharedPreferencesSettings(PreferenceManager.getDefaultSharedPreferences(context), GeneralKeys.DEFAULTS)
-        } else {
-            SharedPreferencesSettings(context.getSharedPreferences(settingsId, Context.MODE_PRIVATE), GeneralKeys.DEFAULTS)
+        return settings.getOrPut(settingsId) {
+            if (settingsId == GENERAL_SETTINGS_NAME) {
+                SharedPreferencesSettings(PreferenceManager.getDefaultSharedPreferences(context), GeneralKeys.DEFAULTS)
+            } else {
+                SharedPreferencesSettings(context.getSharedPreferences(settingsId, Context.MODE_PRIVATE), GeneralKeys.DEFAULTS)
+            }
         }
     }
 
     @JvmOverloads
     fun getAdminSettings(projectId: String = NOT_SPECIFIED_UUID): Settings {
         val settingsId = getSettingsId(ADMIN_SETTINGS_NAME, projectId)
-        return SharedPreferencesSettings(context.getSharedPreferences(settingsId, Context.MODE_PRIVATE), AdminKeys.getDefaults())
+
+        return settings.getOrPut(settingsId) {
+            SharedPreferencesSettings(context.getSharedPreferences(settingsId, Context.MODE_PRIVATE), AdminKeys.getDefaults())
+        }
     }
 
     private fun getSettingsId(settingName: String, projectId: String) = if (projectId == NOT_SPECIFIED_UUID) {
