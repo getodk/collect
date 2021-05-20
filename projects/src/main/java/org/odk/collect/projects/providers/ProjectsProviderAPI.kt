10bd154ff9305f041a1e150bc948558fe41e93c7
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-05-20 12:57:23 +0200

    Improved CONTENT_URI used to access ProjectsProvider

--- projects/src/main/java/org/odk/collect/projects/providers/ProjectsProviderAPI.kt
+++ projects/src/main/java/org/odk/collect/projects/providers/ProjectsProviderAPI.kt
@@ -5,7 +5,7 @@ import android.net.Uri
 object ProjectsProviderAPI {
     const val AUTHORITY = "org.odk.collect.android.provider.odk.projects"
 
-    val CONTENT_URI = Uri.parse("content://$AUTHORITY/projects")
+    val CONTENT_URI = Uri.parse("content://$AUTHORITY/")
 
     const val CONTENT_TYPE = "vnd.android.cursor.dir/vnd.odk.project"
 
