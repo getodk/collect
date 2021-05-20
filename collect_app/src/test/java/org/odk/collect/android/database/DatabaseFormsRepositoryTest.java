661473c66049077503a76fc8e8fe9a59178ac9d9
Callum Stott <callum@seadowg.com>
2021-05-20 20:29:21 +0100

    Fix database test setup for for projects

--- collect_app/src/test/java/org/odk/collect/android/database/DatabaseFormsRepositoryTest.java
+++ collect_app/src/test/java/org/odk/collect/android/database/DatabaseFormsRepositoryTest.java
@@ -1,17 +1,19 @@
 package org.odk.collect.android.database;
 
+import android.app.Application;
+
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.odk.collect.android.application.Collect;
 import org.odk.collect.android.database.forms.DatabaseFormsRepository;
+import org.odk.collect.android.database.forms.FormsDatabaseProvider;
 import org.odk.collect.android.injection.DaggerUtils;
-import org.odk.collect.android.storage.StorageInitializer;
+import org.odk.collect.android.injection.config.AppDependencyComponent;
 import org.odk.collect.android.storage.StoragePathProvider;
 import org.odk.collect.android.storage.StorageSubdirectory;
-import org.odk.collect.testshared.RobolectricHelpers;
-import org.odk.collect.android.utilities.FormsRepositoryProvider;
+import org.odk.collect.android.support.CollectHelpers;
 import org.odk.collect.forms.FormsRepository;
 import org.odk.collect.formstest.FormsRepositoryTest;
 
@@ -21,22 +23,24 @@ import java.util.function.Supplier;
 public class DatabaseFormsRepositoryTest extends FormsRepositoryTest {
 
     private StoragePathProvider storagePathProvider;
+    private FormsDatabaseProvider formsDatabaseProvider;
 
     @Before
     public void setup() {
-        RobolectricHelpers.mountExternalStorage();
-        storagePathProvider = new StoragePathProvider();
-        new StorageInitializer().createOdkDirsOnStorage();
+        CollectHelpers.setupDemoProject();
+        AppDependencyComponent component = DaggerUtils.getComponent(ApplicationProvider.<Application>getApplicationContext());
+        storagePathProvider = component.storagePathProvider();
+        formsDatabaseProvider = component.formsDatabaseProvider();
     }
 
     @Override
     public FormsRepository buildSubject() {
-        return new FormsRepositoryProvider().get();
+        return new DatabaseFormsRepository(System::currentTimeMillis, storagePathProvider, formsDatabaseProvider);
     }
 
     @Override
     public FormsRepository buildSubject(Supplier<Long> clock) {
-        return new DatabaseFormsRepository(clock, new StoragePathProvider(), DaggerUtils.getComponent(Collect.getInstance()).formsDatabaseProvider());
+        return new DatabaseFormsRepository(clock, storagePathProvider, formsDatabaseProvider);
     }
 
     @Override
