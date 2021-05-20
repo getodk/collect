377d122e9f2092150ea60ed16eafab25785adbe4
Callum Stott <callum@seadowg.com>
2021-05-20 14:41:25 +0100

    Fix setup for provider tests

--- collect_app/src/test/java/org/odk/collect/android/provider/InstanceProviderTest.java
+++ collect_app/src/test/java/org/odk/collect/android/provider/InstanceProviderTest.java
@@ -5,7 +5,6 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Environment;
 
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -13,9 +12,9 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.odk.collect.android.storage.StorageInitializer;
+import org.odk.collect.android.injection.DaggerUtils;
+import org.odk.collect.android.support.CollectHelpers;
 import org.odk.collect.formstest.InstanceUtils;
-import org.robolectric.shadows.ShadowEnvironment;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -51,13 +50,11 @@ public class InstanceProviderTest {
 
     @Before
     public void setup() {
-        // Fake that external storage is mounted (it isn't by default in Robolectric)
-        ShadowEnvironment.setExternalStorageState(Environment.MEDIA_MOUNTED);
-        new StorageInitializer().createOdkDirsOnStorage();
+        CollectHelpers.setupDemoProject();
 
         Context context = ApplicationProvider.getApplicationContext();
+        externalFilesDir = new File(DaggerUtils.getComponent(context).storagePathProvider().getProjectRootDirPath());
         contentResolver = context.getContentResolver();
-        externalFilesDir = context.getExternalFilesDir(null);
     }
 
     @Test
