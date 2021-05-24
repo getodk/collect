191c8807391c4ca26a6f130561d5c3c1dc42e7bc
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-05-24 10:12:52 +0200

    Implemented generating project name and icon from url

--- projects/src/main/java/org/odk/collect/projects/AddProjectDialog.kt
+++ projects/src/main/java/org/odk/collect/projects/AddProjectDialog.kt
@@ -2,6 +2,8 @@ package org.odk.collect.projects
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,12 +12,23 @@ import org.jetbrains.annotations.TestOnly
 import org.odk.collect.androidshared.OneSignTextWatcher
 import org.odk.collect.material.MaterialFullScreenDialogFragment
 import org.odk.collect.projects.databinding.AddProjectDialogLayoutBinding
+import java.net.URL
 
 class AddProjectDialog : MaterialFullScreenDialogFragment() {
+
+    companion object {
+        const val IS_PROJECT_NAME_SET_BY_USER = "IS_PROJECT_NAME_SET_BY_USER"
+        const val IS_PROJECT_ICON_SET_BY_USER = "IS_PROJECT_ICON_SET_BY_USER"
+        const val VALUE_CHANGED_PROGRAMMATICALLY = "VALUE_CHANGED_PROGRAMMATICALLY"
+    }
 
     private lateinit var binding: AddProjectDialogLayoutBinding
 
     private var listener: AddProjectDialogListener? = null
+
+    private var projectNameSetByUser = false
+
+    private var projectIconSetByUser = false
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -23,6 +36,12 @@ class AddProjectDialog : MaterialFullScreenDialogFragment() {
         if (context is AddProjectDialogListener) {
             listener = context
         }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        projectNameSetByUser = savedInstanceState?.getBoolean(IS_PROJECT_NAME_SET_BY_USER, false) ?: false
+        projectIconSetByUser = savedInstanceState?.getBoolean(IS_PROJECT_ICON_SET_BY_USER, false) ?: false
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -34,6 +53,50 @@ class AddProjectDialog : MaterialFullScreenDialogFragment() {
         super.onViewCreated(view, savedInstanceState)
         setUpToolbar()
 
+        binding.urlInputText.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                generateNameAndIcon()
+            }
+        }
+
+        binding.urlInputText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                generateNameAndIcon()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+
+        binding.projectNameInputText.post {
+            binding.projectNameInputText.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                    if (binding.projectNameInputText.tag != VALUE_CHANGED_PROGRAMMATICALLY) {
+                        projectNameSetByUser = true
+                    }
+                }
+
+                override fun afterTextChanged(s: Editable) {}
+            })
+        }
+
+        binding.projectIconInputText.post {
+            binding.projectIconInputText.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                    if (binding.projectIconInputText.tag != VALUE_CHANGED_PROGRAMMATICALLY) {
+                        projectIconSetByUser = true
+                    }
+                }
+
+                override fun afterTextChanged(s: Editable) {}
+            })
+        }
+
         binding.projectIconInputText.addTextChangedListener(OneSignTextWatcher(binding.projectIconInputText))
 
         binding.cancelButton.setOnClickListener {
@@ -44,6 +107,40 @@ class AddProjectDialog : MaterialFullScreenDialogFragment() {
             val newProject = NewProject(getUrl(), getUsername(), getPassword(), getProjectName(), getProjectIcon(), getProjectColor())
             listener?.onProjectAdded(newProject)
             dismiss()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(IS_PROJECT_NAME_SET_BY_USER, projectNameSetByUser)
+        outState.putBoolean(IS_PROJECT_ICON_SET_BY_USER, projectIconSetByUser)
+    }
+
+    private fun generateNameAndIcon() {
+        val urlString = getUrl()
+        if (urlString.isNotEmpty()) {
+            try {
+                val url = URL(urlString)
+                var projectName = url.host
+                if (projectName.startsWith("www.")) {
+                    projectName = projectName.substring(4)
+                }
+                projectName = projectName.substring(0, projectName.indexOf("."))
+
+                if (!projectNameSetByUser) {
+                    binding.projectNameInputText.tag = VALUE_CHANGED_PROGRAMMATICALLY
+                    binding.projectNameInputText.setText(projectName)
+                    binding.projectNameInputText.tag = ""
+                }
+
+                if (!projectIconSetByUser) {
+                    val projectIcon = projectName.first().toUpperCase().toString()
+                    binding.projectIconInputText.tag = VALUE_CHANGED_PROGRAMMATICALLY
+                    binding.projectIconInputText.setText(projectIcon)
+                    binding.projectIconInputText.tag = ""
+                }
+            } catch (e: Exception) {
+            }
         }
     }
 
