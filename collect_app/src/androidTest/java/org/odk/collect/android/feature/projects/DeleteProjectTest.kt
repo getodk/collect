cf2ad07273eda11dbef27a8da4e311eeb7bae2d3
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-05-27 14:17:14 +0200

    Fixed DeleteProjectTest

--- collect_app/src/androidTest/java/org/odk/collect/android/feature/projects/DeleteProjectTest.kt
+++ collect_app/src/androidTest/java/org/odk/collect/android/feature/projects/DeleteProjectTest.kt
@@ -22,9 +22,8 @@ class DeleteProjectTest {
         rule.startAtMainMenu()
             .openProjectSettings()
             .clickAddProject()
-            .inputUrl("Turtle nesting")
-            .inputUsername("T")
-            .inputPassword("#0000FF")
+            .inputUrl("https://my-server.com")
+            .inputUsername("John")
             .addProject()
 
             // Delete Demo project
@@ -33,8 +32,8 @@ class DeleteProjectTest {
             .deleteProject()
 
             // Assert switching to Turtle nesting
-            .checkIsToastWithMessageDisplayed(R.string.switched_project, "Turtle nesting")
-            .assertProjectIcon("T", "#0000FF")
+            .checkIsToastWithMessageDisplayed(R.string.switched_project, "my-server.com")
+            .assertProjectIcon("M", "#3e9fcc")
 
             // Delete Turtle nesting project
             .openProjectSettings()
