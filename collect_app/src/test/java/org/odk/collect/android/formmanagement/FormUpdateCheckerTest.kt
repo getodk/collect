458d0d5b98edc82ca634c6258dd12daee2c2ad28
Callum Stott <callum@seadowg.com>
2021-05-31 17:44:08 +0100

    Add test for auto download

--- collect_app/src/test/java/org/odk/collect/android/formmanagement/FormUpdateCheckerTest.kt
+++ collect_app/src/test/java/org/odk/collect/android/formmanagement/FormUpdateCheckerTest.kt
@@ -10,20 +10,31 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.kotlin.any
+import org.mockito.kotlin.doAnswer
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 import org.odk.collect.android.injection.DaggerUtils
+import org.odk.collect.android.preferences.keys.GeneralKeys
 import org.odk.collect.android.provider.FormsProviderAPI.CONTENT_URI
+import org.odk.collect.android.storage.StorageSubdirectory
 import org.odk.collect.android.support.BooleanChangeLock
+import org.odk.collect.forms.FormListItem
 import org.odk.collect.forms.FormSource
+import org.odk.collect.formstest.FormUtils
 import org.odk.collect.projects.Project
+import org.odk.collect.shared.Md5.getMd5Hash
 
 @RunWith(AndroidJUnit4::class)
 class FormUpdateCheckerTest {
 
     private val application = ApplicationProvider.getApplicationContext<Application>()
     private val component = DaggerUtils.getComponent(application)
+
+    private val formsRepositoryProvider = component.formsRepositoryProvider()
+    private val storagePathProvider = component.storagePathProvider()
+    private val settingsProvider = component.settingsProvider()
 
     private val formSource = mock<FormSource> {
         on { fetchFormList() } doReturn emptyList()
@@ -33,10 +44,6 @@ class FormUpdateCheckerTest {
 
     @Before
     fun setup() {
-        val settingsProvider = component.settingsProvider()
-        val storagePathProvider = component.storagePathProvider()
-        val formsRepositoryProvider = component.formsRepositoryProvider()
-
         val formSourceProvider = mock<FormSourceProvider> { on { get(any()) } doReturn formSource }
 
         updateChecker = FormUpdateChecker(
@@ -66,6 +73,48 @@ class FormUpdateCheckerTest {
     fun `checkForUpdates() uses projectId`() {
         val project = setupProject()
         assertThat(updateChecker.checkForUpdates(project.uuid), `is`(true))
+    }
+
+    @Test
+    fun `checkForUpdates() downloads updates when auto download is enabled`() {
+        val project = setupProject()
+        addFormLocally(project, "formId", "1")
+
+        val updatedXForm = FormUtils.createXFormBody("formId", "2")
+        addFormToServer(updatedXForm, "formId", "2")
+
+        settingsProvider.getGeneralSettings(project.uuid)
+            .save(GeneralKeys.KEY_AUTOMATIC_UPDATE, true)
+
+        updateChecker.checkForUpdates(project.uuid)
+        assertThat(
+            formsRepositoryProvider.get(project.uuid).getAllByFormIdAndVersion("formId", "2").size,
+            `is`(1)
+        )
+    }
+
+    private fun addFormToServer(updatedXForm: String, formId: String, formVersion: String) {
+        whenever(formSource.fetchFormList()).doReturn(
+            listOf(
+                FormListItem(
+                    "http://$formId",
+                    formId,
+                    formVersion,
+                    getMd5Hash(updatedXForm),
+                    "blah",
+                    null
+                )
+            )
+        )
+        whenever(formSource.fetchForm("http://$formId")).doAnswer { updatedXForm.byteInputStream() }
+    }
+
+    private fun addFormLocally(project: Project.Saved, formId: String, formVersion: String) {
+        val formsDir = storagePathProvider.getOdkDirPath(StorageSubdirectory.FORMS, project.uuid)
+        val formsRepository = formsRepositoryProvider.get(project.uuid)
+        formsRepository.save(
+            FormUtils.buildForm(formId, formVersion, formsDir).build()
+        )
     }
 
     private fun setupProject(): Project.Saved {
