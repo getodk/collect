b866741408a07b5b865e99087efbb7b5de4aaa00
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-06-02 21:46:32 +0200

    Improved ProjectCreatorTest

--- collect_app/src/test/java/org/odk/collect/android/projects/ProjectCreatorTest.kt
+++ collect_app/src/test/java/org/odk/collect/android/projects/ProjectCreatorTest.kt
@@ -1,12 +1,15 @@
 package org.odk.collect.android.projects
 
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mockito.`when`
+import org.mockito.Mockito.anyString
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.verifyNoMoreInteractions
+import org.mockito.kotlin.whenever
 import org.odk.collect.android.configure.SettingsImporter
 import org.odk.collect.android.database.forms.FormsDatabaseProvider
 import org.odk.collect.android.database.instances.InstancesDatabaseProvider
@@ -18,18 +21,18 @@ class ProjectCreatorTest {
     private val newProject = Project.New("my-server.com", "M", "#3e9fcc")
     private val savedProject = Project.Saved("1", newProject)
 
-    private val projectImporter = mock<ProjectImporter> {
+    private var projectImporter = mock<ProjectImporter> {
         on { importNewProject(newProject) } doReturn savedProject
     }
 
-    private val projectsRepository = mock<ProjectsRepository> {
+    private var projectsRepository = mock<ProjectsRepository> {
         on { getAll() } doReturn listOf(savedProject)
     }
 
-    private val currentProjectProvider = mock<CurrentProjectProvider> {}
-    private val settingsImporter = mock<SettingsImporter> {}
-    private val formsDatabaseProvider = mock<FormsDatabaseProvider> {}
-    private val instancesDatabaseProvider = mock<InstancesDatabaseProvider> {}
+    private var currentProjectProvider = mock<CurrentProjectProvider> {}
+    private var settingsImporter = mock<SettingsImporter> {}
+    private var formsDatabaseProvider = mock<FormsDatabaseProvider> {}
+    private var instancesDatabaseProvider = mock<InstancesDatabaseProvider> {}
 
     private lateinit var projectCreator: ProjectCreator
 
@@ -45,25 +48,53 @@ class ProjectCreatorTest {
     }
 
     @Test
+    fun `Importing settings should be triggered with proper params`() {
+        projectCreator.createNewProject(json)
+        verify(settingsImporter).fromJSON(json, "1")
+    }
+
+    @Test
+    fun `When importing settings failed createNewProject() should return false`() {
+        whenever(settingsImporter.fromJSON(anyString(), anyString())).thenReturn(false)
+
+        projectCreator.createNewProject(json)
+        assertThat(projectCreator.createNewProject(json), `is`(false))
+    }
+
+    @Test
+    fun `When importing settings succeeded createNewProject() should return true`() {
+        whenever(settingsImporter.fromJSON(anyString(), anyString())).thenReturn(true)
+
+        projectCreator.createNewProject(json)
+        assertThat(projectCreator.createNewProject(json), `is`(true))
+    }
+
+    @Test
+    fun `When importing settings failed should created project be deleted`() {
+        whenever(settingsImporter.fromJSON(anyString(), anyString())).thenReturn(false)
+
+        projectCreator.createNewProject(json)
+        verify(projectsRepository).delete(savedProject.uuid)
+    }
+
+    @Test
     fun `New project id should be set if it's the only project`() {
+        whenever(settingsImporter.fromJSON(anyString(), anyString())).thenReturn(true)
+
         projectCreator.createNewProject(json)
         verify(currentProjectProvider).setCurrentProject("1")
 
-        `when`(projectsRepository.getAll()).thenReturn(listOf(savedProject, Project.Saved("2", "Project X", "X", "#cccccc")))
+        whenever(projectsRepository.getAll()).thenReturn(listOf(savedProject, Project.Saved("2", "Project X", "X", "#cccccc")))
         projectCreator.createNewProject(json)
         verifyNoMoreInteractions(currentProjectProvider)
     }
 
     @Test
     fun `Databases should be released if new project has been set`() {
+        whenever(settingsImporter.fromJSON(anyString(), anyString())).thenReturn(true)
+
         projectCreator.createNewProject(json)
         verify(formsDatabaseProvider).releaseDatabaseHelper()
         verify(instancesDatabaseProvider).releaseDatabaseHelper()
-    }
-
-    @Test
-    fun `Importing settings should be triggered`() {
-        projectCreator.createNewProject(json)
-        verify(settingsImporter).fromJSON(json, "1")
     }
 }
