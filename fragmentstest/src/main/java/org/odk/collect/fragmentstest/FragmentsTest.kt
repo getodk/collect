74c53d245c7a3180d2bdc2b907bb22a2bdca670e
Callum Stott <callum@seadowg.com>
2021-06-18 18:52:53 +0100

    Add docs for new helpers

--- fragmentstest/src/main/java/org/odk/collect/fragmentstest/FragmentsTest.kt
+++ fragmentstest/src/main/java/org/odk/collect/fragmentstest/FragmentsTest.kt
@@ -9,8 +9,19 @@ import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.matcher.RootMatchers.isDialog
 import org.hamcrest.Matcher
 
+/**
+ * Helpers for writing tests for Fragment that (hopefully) make interacting with [FragmentScenario]
+ * less troublesome.
+ */
 object FragmentsTest {
 
+    /**
+     * Creates a FragmentScenario using a test theme that supports AppCompat and MaterialComponents
+     * attributes but also won't explode (like MaterialComponents themes do).
+     *
+     * This uses [FragmentScenario.launch] here instead of [FragmentScenario.launchInContainer] as
+     * per docs at https://developer.android.com/guide/fragments/test#dialog.
+     */
     @JvmOverloads
     @JvmStatic
     fun <F : Fragment?> launchDialogFragment(
@@ -25,6 +36,10 @@ object FragmentsTest {
         )
     }
 
+    /**
+     * Slightly easier way to perform actions/assertions against a [DialogFragment]'s UI. Using
+     * [onView] normally will match on a root view outside of where the dialog is displayed.
+     */
     @JvmStatic
     fun onViewInDialog(viewMatcher: Matcher<View>): ViewInteraction {
         return onView(viewMatcher).inRoot(isDialog())
