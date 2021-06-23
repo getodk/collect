b392e15cbbef3751a56a1da17f21d1de1e57012e
Callum Stott <callum@seadowg.com>
2021-06-23 15:56:09 +0100

    Add test to check that deleted instances don't crash

--- collect_app/src/androidTest/java/org/odk/collect/android/feature/instancemanagement/InstancesAdbTest.kt
+++ collect_app/src/androidTest/java/org/odk/collect/android/feature/instancemanagement/InstancesAdbTest.kt
@@ -1,21 +1,26 @@
 package org.odk.collect.android.feature.instancemanagement
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
 import org.junit.runner.RunWith
+import org.odk.collect.android.storage.StorageSubdirectory
 import org.odk.collect.android.support.CollectTestRule
+import org.odk.collect.android.support.TestDependencies
 import org.odk.collect.android.support.TestRuleChain
 import org.odk.collect.android.support.pages.MainMenuPage
+import java.io.File
 
 @RunWith(AndroidJUnit4::class)
 class InstancesAdbTest {
 
+    private val testDependencies = TestDependencies()
     private val rule = CollectTestRule()
 
     @get:Rule
-    val chain: RuleChain = TestRuleChain.chain().around(rule)
+    val chain: RuleChain = TestRuleChain.chain(testDependencies).around(rule)
 
     @Test
     fun canAddInstanceOnDisk() {
@@ -26,5 +31,22 @@ class InstancesAdbTest {
             .pressBack(MainMenuPage()) // Return to main menu to trigger instance disk sync
             .clickEditSavedForm(1)
             .assertText("One Question")
+    }
+
+    @Test
+    fun deletingInstanceOnDisk_andThenOpeningInstance_doesNotCrash() {
+        val mainMenuPage = rule.startAtMainMenu()
+            .copyForm("one-question.xml")
+            .startBlankForm("One Question")
+            .swipeToEndScreen()
+            .clickSaveAndExit();
+
+        val instancesDir = testDependencies.storagePathProvider.getOdkDirPath(StorageSubdirectory.INSTANCES)
+        val instanceDeleted = File(instancesDir).listFiles()[0].deleteRecursively()
+        Assert.assertTrue(instanceDeleted)
+
+        mainMenuPage
+            .clickEditSavedForm(1)
+            .clickOnText("One Question")
     }
 }
