b43cd2ab1a2f9e0d095efbe6f9fa45efb26f196b
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-06-24 14:55:56 +0200

    Improved ProjectDetailsCreatorTest

--- collect_app/src/test/java/org/odk/collect/android/projects/ProjectDetailsCreatorTest.kt
+++ collect_app/src/test/java/org/odk/collect/android/projects/ProjectDetailsCreatorTest.kt
@@ -15,43 +15,87 @@ class ProjectDetailsCreatorTest {
     val projectDetailsCreator = ProjectDetailsCreator(ApplicationProvider.getApplicationContext())
 
     @Test
-    fun `Project name should be generated from url`() {
-        assertThat(projectDetailsCreator.getProject("https://my-project.com").name, `is`("my-project.com"))
-        assertThat(projectDetailsCreator.getProject("https://your-project.com/one").name, `is`("your-project.com"))
-        assertThat(projectDetailsCreator.getProject("http://www.my-project.com").name, `is`("www.my-project.com"))
-        assertThat(projectDetailsCreator.getProject("").name, `is`("Project")) // default project name for invalid urls
-        assertThat(projectDetailsCreator.getProject("something").name, `is`("Project")) // default project name for invalid urls
+    fun `If project name is included in project details should be used`() {
+        assertThat(projectDetailsCreator.createProjectFromDetails(name = "Project X").name, `is`("Project X"))
     }
 
     @Test
-    fun `Project icon should be generated from url`() {
-        assertThat(projectDetailsCreator.getProject("https://my-project.com").icon, `is`("M"))
-        assertThat(projectDetailsCreator.getProject("https://your-project.com/one").icon, `is`("Y"))
-        assertThat(projectDetailsCreator.getProject("http://www.my-project.com").icon, `is`("W"))
-        assertThat(projectDetailsCreator.getProject("").icon, `is`("P")) // default project icon for invalid urls
-        assertThat(projectDetailsCreator.getProject("something").icon, `is`("P")) // default project icon for invalid urls
+    fun `If project name is not included in project details or empty should be generated based on url`() {
+        // Case1: not included
+        assertThat(projectDetailsCreator.createProjectFromDetails(url = "https://my-server.com").name, `is`("my-server.com"))
+
+        // Case2: empty
+        assertThat(projectDetailsCreator.createProjectFromDetails(url = "https://my-server.com", name = " ").name, `is`("my-server.com"))
+    }
+
+    @Test
+    fun `If project icon is included in project details should be used`() {
+        // Case1: normal chars
+        assertThat(projectDetailsCreator.createProjectFromDetails(icon = "X").icon, `is`("X"))
+
+        // Case2: emoticons
+        assertThat(projectDetailsCreator.createProjectFromDetails(icon = "\uD83D\uDC22").icon, `is`("\uD83D\uDC22"))
+    }
+
+    @Test
+    fun `If project icon is not included in project details or empty should be generated based on project name`() {
+        // Case1: not included
+        assertThat(projectDetailsCreator.createProjectFromDetails(name = "Project X").icon, `is`("P"))
+
+        // Case2: empty
+        assertThat(projectDetailsCreator.createProjectFromDetails(name = "My Project X", icon = " ").icon, `is`("M"))
+    }
+
+    @Test
+    fun `If project icon is included in project details but longer than one sign, only the first sign should be used`() {
+        // Case1: normal chars
+        assertThat(projectDetailsCreator.createProjectFromDetails(icon = "XX").icon, `is`("X"))
+
+        // Case2: emoticons
+        assertThat(projectDetailsCreator.createProjectFromDetails(icon = "\uD83D\uDC22XX").icon, `is`("\uD83D\uDC22"))
+    }
+
+    @Test
+    fun `If project color is included in project details should be used`() {
+        assertThat(projectDetailsCreator.createProjectFromDetails(color = "#cccccc").color, `is`("#cccccc"))
+    }
+
+    @Test
+    fun `If project color is not included in project details should be generated based on project name`() {
+        // Case1: any project name
+        assertThat(projectDetailsCreator.createProjectFromDetails(name = "Project X").color, `is`("#9f50b0"))
+
+        // Case2: demo project
+        assertThat(projectDetailsCreator.createProjectFromDetails(name = Project.DEMO_PROJECT_NAME).color, `is`(Project.DEMO_PROJECT_COLOR))
+    }
+
+    @Test
+    fun `If project color is included in project details but invalid should be generated based on project name`() {
+        assertThat(projectDetailsCreator.createProjectFromDetails(name = "Project X", icon = "#cc").color, `is`("#9f50b0"))
+    }
+
+    @Test
+    fun `Test generating project name from various urls`() {
+        assertThat(projectDetailsCreator.createProjectFromDetails(url = "https://my-project.com").name, `is`("my-project.com"))
+        assertThat(projectDetailsCreator.createProjectFromDetails(url = "https://your-project.com/one").name, `is`("your-project.com"))
+        assertThat(projectDetailsCreator.createProjectFromDetails(url = "http://www.my-project.com").name, `is`("www.my-project.com"))
+        assertThat(projectDetailsCreator.createProjectFromDetails(url = "https://demo.getodk.org").name, `is`(Project.DEMO_PROJECT_NAME))
+        assertThat(projectDetailsCreator.createProjectFromDetails(url = "").name, `is`(Project.DEMO_PROJECT_NAME))
+        assertThat(projectDetailsCreator.createProjectFromDetails(url = "something").name, `is`("Project")) // default project name for invalid urls
     }
 
     @Test
     fun `Generated project color should be the same for identical project names`() {
-        assertThat(projectDetailsCreator.getProject("https://my-project.com").color, `is`(projectDetailsCreator.getProject("https://my-project.com").color))
-        assertThat(projectDetailsCreator.getProject("https://your-project.com/one").color, `is`(projectDetailsCreator.getProject("https://your-project.com/one").color))
-        assertThat(projectDetailsCreator.getProject("http://www.my-project.com").color, `is`(projectDetailsCreator.getProject("http://www.my-project.com").color))
-        assertThat(projectDetailsCreator.getProject("").color, `is`(projectDetailsCreator.getProject("something").color)) // default project color for invalid urls
+        assertThat(projectDetailsCreator.createProjectFromDetails(url = "https://my-project.com").color, `is`(projectDetailsCreator.createProjectFromDetails(url = "https://my-project.com").color))
+        assertThat(projectDetailsCreator.createProjectFromDetails(url = "https://your-project.com/one").color, `is`(projectDetailsCreator.createProjectFromDetails(url = "https://your-project.com/one").color))
+        assertThat(projectDetailsCreator.createProjectFromDetails(url = "http://www.my-project.com").color, `is`(projectDetailsCreator.createProjectFromDetails(url = "http://www.my-project.com").color))
+        assertThat(projectDetailsCreator.createProjectFromDetails(url = "qwerty").color, `is`(projectDetailsCreator.createProjectFromDetails(url = "something").color)) // default project color for invalid urls
     }
 
     @Test
     fun `Generated project color should be different for different project names`() {
-        assertThat(projectDetailsCreator.getProject("https://my-project.com").color, not(projectDetailsCreator.getProject("http://www.my-project.com").color))
-        assertThat(projectDetailsCreator.getProject("https://your-project.com/one").color, not(projectDetailsCreator.getProject("http://www.my-project.com").color))
-        assertThat(projectDetailsCreator.getProject("http://www.my-project.com").color, not(projectDetailsCreator.getProject("https://your-project.com/one").color))
-    }
-
-    @Test
-    fun `Generated project should have demo details if demo url is used`() {
-        val project = projectDetailsCreator.getProject("https://demo.getodk.org")
-        assertThat(project.name, `is`(Project.DEMO_PROJECT_NAME))
-        assertThat(project.icon, `is`(Project.DEMO_PROJECT_ICON))
-        assertThat(project.color, `is`(Project.DEMO_PROJECT_COLOR))
+        assertThat(projectDetailsCreator.createProjectFromDetails(url = "https://my-project.com").color, not(projectDetailsCreator.createProjectFromDetails(url = "http://www.my-project.com").color))
+        assertThat(projectDetailsCreator.createProjectFromDetails(url = "https://your-project.com/one").color, not(projectDetailsCreator.createProjectFromDetails(url = "http://www.my-project.com").color))
+        assertThat(projectDetailsCreator.createProjectFromDetails(url = "http://www.my-project.com").color, not(projectDetailsCreator.createProjectFromDetails(url = "https://your-project.com/one").color))
     }
 }
