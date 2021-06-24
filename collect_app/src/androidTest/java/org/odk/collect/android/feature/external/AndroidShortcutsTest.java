4e2b42bb47febced91133470d94764811ed14d90
Callum Stott <callum@seadowg.com>
2021-06-24 18:01:48 +0100

    Use (hopefully) more robust way of launching activity in test

--- collect_app/src/androidTest/java/org/odk/collect/android/feature/external/AndroidShortcutsTest.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/feature/external/AndroidShortcutsTest.java
@@ -3,6 +3,7 @@ package org.odk.collect.android.feature.external;
 import android.content.Intent;
 
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Rule;
@@ -46,7 +47,8 @@ public class AndroidShortcutsTest {
          /*
         This can't use ActivityScenario.launch because of: https://github.com/android/android-test/issues/496
          */
-        scenario.onActivity(activity -> activity.startActivity(shortcutIntent));
+        shortcutIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        ApplicationProvider.getApplicationContext().startActivity(shortcutIntent);
         return new FormEntryPage(formName);
     }
 }
