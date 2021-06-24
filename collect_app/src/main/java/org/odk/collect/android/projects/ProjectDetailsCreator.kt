c37e5c402352fe47a928908e33cc64ff51b34539
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-06-24 14:55:28 +0200

    Refactored ProjectDetailsCreator

--- collect_app/src/main/java/org/odk/collect/android/projects/ProjectDetailsCreator.kt
+++ collect_app/src/main/java/org/odk/collect/android/projects/ProjectDetailsCreator.kt
@@ -5,32 +5,74 @@ import androidx.core.content.ContextCompat
 import org.odk.collect.android.R
 import org.odk.collect.projects.Project
 import java.net.URL
+import java.util.Locale
+import java.util.regex.Pattern
 import kotlin.math.abs
 
 class ProjectDetailsCreator(private val context: Context) {
 
-    fun getProject(urlString: String): Project {
-        return if (urlString.startsWith(context.getString(R.string.default_server_url))) {
-            Project.DEMO_PROJECT
+    fun createProjectFromDetails(url: String = "", name: String = "", icon: String = "", color: String = ""): Project {
+        val projectName = if (name.isNotBlank()) {
+            name
+        } else {
+            getProjectNameFromUrl(url)
+        }
+
+        val projectIcon = if (icon.isNotBlank()) {
+            if (Character.codePointCount(icon, 0, icon.length) == 1) {
+                icon.toUpperCase(Locale.US)
+            } else {
+                getFirstSing(icon)
+            }
+        } else {
+            projectName.first().toUpperCase().toString()
+        }
+
+        val projectColor = if (isProjectColorValid(color)) {
+            color
+        } else {
+            getProjectColorFromProjectName(projectName)
+        }
+
+        return Project.New(projectName, projectIcon, projectColor)
+    }
+
+    private fun getProjectNameFromUrl(url: String): String {
+        return if (url.isBlank() || url.startsWith(context.getString(R.string.default_server_url))) {
+            Project.DEMO_PROJECT_NAME
         } else {
             try {
-                val url = URL(urlString)
-                val projectName = url.host
-                val projectIcon = projectName.first().toUpperCase().toString()
-                val projectColor = getProjectColorForProjectName(projectName)
-
-                Project.New(projectName, projectIcon, projectColor)
+                URL(url).host
             } catch (e: Exception) {
-                Project.New("Project", "P", "#3e9fcc")
+                "Project"
             }
         }
     }
 
-    private fun getProjectColorForProjectName(projectName: String): String {
+    private fun getProjectColorFromProjectName(projectName: String): String {
+        if (projectName == Project.DEMO_PROJECT_NAME) {
+            return Project.DEMO_PROJECT_COLOR
+        }
+
         val colorId = (abs(projectName.hashCode()) % 15) + 1
         val colorName = "color$colorId"
         val colorValue = context.resources.getIdentifier(colorName, "color", context.packageName)
 
         return "#${Integer.toHexString(ContextCompat.getColor(context, colorValue)).substring(2)}"
+    }
+
+    private fun isProjectColorValid(hexColor: String): Boolean {
+        return Pattern
+            .compile("^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})\$", Pattern.CASE_INSENSITIVE)
+            .matcher(hexColor)
+            .matches()
+    }
+
+    private fun getFirstSing(value: String): String {
+        return if (Character.codePointCount(value, 0, value.length) == 1) {
+            value
+        } else {
+            getFirstSing(value.substring(0, value.length - 1))
+        }
     }
 }
