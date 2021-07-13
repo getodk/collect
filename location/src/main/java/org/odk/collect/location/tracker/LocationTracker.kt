698b2c797045bbdaf8e5f7b7e94615be859c4ca8
Callum Stott <callum@seadowg.com>
2021-07-13 13:42:49 +0100

    Revise docs for LocationTracker

--- location/src/main/java/org/odk/collect/location/tracker/LocationTracker.kt
+++ location/src/main/java/org/odk/collect/location/tracker/LocationTracker.kt
@@ -8,14 +8,18 @@ import org.odk.collect.location.Location
 interface LocationTracker {
 
     /**
-     * The last location tracked by this [LocationTracker].
+     * The last location tracked. Will return `null` if a location hasn't been determined
+     * or [LocationTracker.start] hasn't been called yet.
      */
     fun getCurrentLocation(): Location?
 
     /**
-     * Starts tracking the device's location at a fixed interval
+     * Starts tracking the device's location at a fixed interval.
      */
     fun start()
 
+    /**
+     * Stops tracking location. Does not reset the value returned by [LocationTracker.getCurrentLocation].
+     */
     fun stop()
 }
