8d15ad9698c5a39f06509061ddc93d3e636c56c1
Hélène Martin <lognaturel@gmail.com>
2021-07-20 14:10:20 -0700

    Enable configuration from QR code image

--- collect_app/src/main/java/org/odk/collect/android/projects/QrCodeProjectCreatorDialog.kt
+++ collect_app/src/main/java/org/odk/collect/android/projects/QrCodeProjectCreatorDialog.kt
@@ -1,30 +1,39 @@
 package org.odk.collect.android.projects
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.widget.Toolbar
 import com.google.zxing.client.android.BeepManager
 import com.google.zxing.integration.android.IntentIntegrator
 import com.journeyapps.barcodescanner.BarcodeResult
 import com.journeyapps.barcodescanner.CaptureManager
+import org.odk.collect.android.R
 import org.odk.collect.android.activities.ActivityUtils
 import org.odk.collect.android.activities.MainMenuActivity
+import org.odk.collect.android.configure.SettingsImporter
+import org.odk.collect.android.configure.qr.QRCodeDecoder
 import org.odk.collect.android.databinding.QrCodeProjectCreatorDialogLayoutBinding
 import org.odk.collect.android.injection.DaggerUtils
 import org.odk.collect.android.listeners.PermissionListener
 import org.odk.collect.android.permissions.PermissionsProvider
 import org.odk.collect.android.preferences.source.SettingsProvider
+import org.odk.collect.android.utilities.ActivityAvailability
 import org.odk.collect.android.utilities.CodeCaptureManagerFactory
 import org.odk.collect.android.utilities.CompressionUtils
 import org.odk.collect.android.utilities.DialogUtils
 import org.odk.collect.android.utilities.ToastUtils
+import org.odk.collect.android.utilities.ToastUtils.showShortToast
 import org.odk.collect.android.views.BarcodeViewDecoder
 import org.odk.collect.material.MaterialFullScreenDialogFragment
 import org.odk.collect.projects.ProjectsRepository
-import org.odk.collect.projects.R
+import timber.log.Timber
 import javax.inject.Inject
 
 class QrCodeProjectCreatorDialog : MaterialFullScreenDialogFragment(), DuplicateProjectConfirmationDialog.DuplicateProjectConfirmationListener {
@@ -57,15 +66,43 @@ class QrCodeProjectCreatorDialog : MaterialFullScreenDialogFragment(), Duplicate
     private lateinit var beepManager: BeepManager
     lateinit var binding: QrCodeProjectCreatorDialogLayoutBinding
 
+    @Inject
+    lateinit var activityAvailability: ActivityAvailability
+
+    @Inject
+    lateinit var qrCodeDecoder: QRCodeDecoder
+
+    @Inject
+    lateinit var settingsImporter: SettingsImporter
+
+    private val imageQrCodeImportResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+        val imageUri: Uri? = result.data?.data
+        if (imageUri != null) {
+            requireActivity().contentResolver.openInputStream(imageUri).use {
+                try {
+                    val settingsJson = qrCodeDecoder.decode(it)
+                    createProjectOrError(settingsJson)
+                } catch (e: QRCodeDecoder.InvalidException) {
+                    showShortToast(R.string.invalid_qrcode)
+                } catch (e: QRCodeDecoder.NotFoundException) {
+                    showShortToast(R.string.qr_code_not_found)
+                }
+            }
+        }
+    }
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
         DaggerUtils.getComponent(context).inject(this)
+
         settingsConnectionMatcher = SettingsConnectionMatcher(projectsRepository, settingsProvider)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = QrCodeProjectCreatorDialogLayoutBinding.inflate(inflater)
-        setUpToolbar()
+
+        configureMenu()
+
         binding.configureManuallyButton.setOnClickListener {
             DialogUtils.showIfNotShowing(ManualProjectCreatorDialog::class.java, requireActivity().supportFragmentManager)
         }
@@ -86,6 +123,26 @@ class QrCodeProjectCreatorDialog : MaterialFullScreenDialogFragment(), Duplicate
             }
         )
         return binding.root
+    }
+
+    private fun configureMenu() {
+        binding.toolbar.menu.removeItem(R.id.menu_item_share)
+
+        binding.toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.menu_item_scan_sd_card -> {
+                    val photoPickerIntent = Intent(Intent.ACTION_PICK)
+                    photoPickerIntent.type = "image/*"
+                    if (activityAvailability.isActivityAvailable(photoPickerIntent)) {
+                        imageQrCodeImportResultLauncher.launch(photoPickerIntent)
+                    } else {
+                        showShortToast(getString(R.string.activity_not_found, getString(R.string.choose_image)))
+                        Timber.w(getString(R.string.activity_not_found, getString(R.string.choose_image)))
+                    }
+                }
+            }
+            false
+        }
     }
 
     override fun onCloseClicked() {
@@ -117,13 +174,8 @@ class QrCodeProjectCreatorDialog : MaterialFullScreenDialogFragment(), Duplicate
         super.onDestroy()
     }
 
-    override fun getToolbar(): Toolbar {
+    override fun getToolbar(): Toolbar? {
         return binding.toolbar
-    }
-
-    private fun setUpToolbar() {
-        toolbar.setTitle(R.string.add_project)
-        toolbar.navigationIcon = null
     }
 
     private fun startScanning(savedInstanceState: Bundle?) {
@@ -135,25 +187,27 @@ class QrCodeProjectCreatorDialog : MaterialFullScreenDialogFragment(), Duplicate
                 try {
                     beepManager.playBeepSoundAndVibrate()
                 } catch (e: Exception) {
-                    // ignore
+                    // ignore because beeping isn't essential and this can crash the whole app
                 }
 
-                try {
-                    val settingsJson = CompressionUtils.decompress(barcodeResult.text)
-
-                    settingsConnectionMatcher.getProjectWithMatchingConnection(settingsJson)?.let { uuid ->
-                        val confirmationArgs = Bundle()
-                        confirmationArgs.putString(DuplicateProjectConfirmationKeys.SETTINGS_JSON, settingsJson)
-                        confirmationArgs.putString(DuplicateProjectConfirmationKeys.MATCHING_PROJECT, uuid)
-                        DialogUtils.showIfNotShowing(DuplicateProjectConfirmationDialog::class.java, confirmationArgs, childFragmentManager)
-                    } ?: run {
-                        createProject(settingsJson)
-                    }
-                } catch (e: Exception) {
-                    ToastUtils.showShortToast(getString(R.string.invalid_qrcode))
-                }
+                createProjectOrError(CompressionUtils.decompress(barcodeResult.text))
             }
         )
+    }
+
+    private fun createProjectOrError(settingsJson: String) {
+        try {
+            settingsConnectionMatcher.getProjectWithMatchingConnection(settingsJson)?.let { uuid ->
+                val confirmationArgs = Bundle()
+                confirmationArgs.putString(DuplicateProjectConfirmationKeys.SETTINGS_JSON, settingsJson)
+                confirmationArgs.putString(DuplicateProjectConfirmationKeys.MATCHING_PROJECT, uuid)
+                DialogUtils.showIfNotShowing(DuplicateProjectConfirmationDialog::class.java, confirmationArgs, childFragmentManager)
+            } ?: run {
+                createProject(settingsJson)
+            }
+        } catch (e: Exception) {
+            showShortToast(getString(R.string.invalid_qrcode))
+        }
     }
 
     override fun createProject(settingsJson: String) {
