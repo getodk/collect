bf436518339c916f74c5a1ad35255c6ff67b8772
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-07-26 12:02:53 +0200

    Removed unused SERVER_LIST key from MetaKeys

--- collect_app/src/main/java/org/odk/collect/android/preferences/keys/MetaKeys.java
+++ collect_app/src/main/java/org/odk/collect/android/preferences/keys/MetaKeys.java
@@ -6,7 +6,6 @@ public class MetaKeys {
     public static final String KEY_MAPBOX_INITIALIZED = "mapbox_initialized";
     public static final String KEY_GOOGLE_BUG_154855417_FIXED = "google_bug_154855417_fixed";
     public static final String LAST_UPDATED_NOTIFICATION = "last_updated_notification";
-    public static final String SERVER_LIST = "server_list";
     public static final String CURRENT_PROJECT_ID = "current_project_id";
     public static final String KEY_PROJECTS = "projects";
     public static final String FIRST_LAUNCH = "first_launch";
