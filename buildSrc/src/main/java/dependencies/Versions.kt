247e15af58845cc226adcd7fc0769e1d6be29de5
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-08-12 08:33:42 +0200

    Moved dependencies used in collect_app module to the new module

--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -1,4 +1,10 @@
 package dependencies
 
 object Versions {
+    const val androidx_core_ktx = "1.3.2"
+    const val androidx_fragment_ktx = "1.3.5"
+    const val android_material = "1.3.0"
+    const val okhttp3 = "4.9.0"
+    const val timber = "4.7.1"
+    const val dagger = "2.35.1"
 }
