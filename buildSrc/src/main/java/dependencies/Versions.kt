bb07aa1cd16d3caccc864b617671f5ba88f68f02
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-08-12 12:25:35 +0200

    Moved sdk versions to Versions class

--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -1,6 +1,10 @@
 package dependencies
 
 object Versions {
+    const val android_compile_sdk = 30
+    const val android_min_sdk = 21
+    const val android_target_sdk = 29
+
     const val androidx_core_ktx = "1.6.0"
     const val androidx_fragment = "1.3.6"
     const val android_material = "1.4.0"
