d675caf65a148a14d7c958d7c9f4d659888b8922
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-08-12 11:46:12 +0200

    Updated dependencies

--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -1,11 +1,11 @@
 package dependencies
 
 object Versions {
-    const val androidx_core_ktx = "1.3.2"
-    const val androidx_fragment = "1.3.5"
-    const val android_material = "1.3.0"
-    const val okhttp3 = "4.9.0"
-    const val timber = "4.7.1"
-    const val dagger = "2.35.1"
-    const val kotlin = "1.4.30"
+    const val androidx_core_ktx = "1.6.0"
+    const val androidx_fragment = "1.3.6"
+    const val android_material = "1.4.0"
+    const val okhttp3 = "4.9.1"
+    const val timber = "5.0.0"
+    const val dagger = "2.38.1"
+    const val kotlin = "1.5.21"
 }
