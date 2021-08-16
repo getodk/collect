9628b3647189809504ab4ec32a05f87d0d5f4350
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-08-16 10:59:38 +0200

    Remove Version consts that are used only once

--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -5,11 +5,8 @@ object Versions {
     const val android_min_sdk = 21
     const val android_target_sdk = 29
 
-    const val androidx_core_ktx = "1.6.0"
     const val androidx_fragment = "1.3.6"
-    const val android_material = "1.4.0"
     const val okhttp3 = "4.9.1"
-    const val timber = "5.0.0"
     const val dagger = "2.38.1"
     const val kotlin = "1.5.21"
     const val mockito = "3.11.1" // the newest version is buggy https://github.com/mockito/mockito/issues/2370
