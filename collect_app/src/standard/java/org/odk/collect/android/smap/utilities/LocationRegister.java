9f578473982eb3a9e83bef3f30eeaa38bd9a1f41
Neil Penman <neilpenman@gmail.com>
2021-08-18 21:57:36 +1000

    Add standard location register

---
+++ collect_app/src/standard/java/org/odk/collect/android/smap/utilities/LocationRegister.java
@@ -0,0 +1,18 @@
+package org.odk.collect.android.smap.utilities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.preference.PreferenceManager;
+
+import org.odk.collect.android.database.TraceUtilities;
+import org.odk.collect.android.preferences.GeneralKeys;
+
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import timber.log.Timber;
+
+public class LocationRegister {
+    public void register(Context context, Location location) {
+       // Do nothing
+    }
+}
