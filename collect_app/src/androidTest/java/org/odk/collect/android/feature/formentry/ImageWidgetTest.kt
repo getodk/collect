7894da1bdefbe740a555684434f9c1ef0161b82c
Callum Stott <callum@seadowg.com>
2021-09-06 12:25:47 +0100

    Use RecoredIntentRule

--- collect_app/src/androidTest/java/org/odk/collect/android/feature/formentry/ImageWidgetTest.kt
+++ collect_app/src/androidTest/java/org/odk/collect/android/feature/formentry/ImageWidgetTest.kt
@@ -6,7 +6,6 @@ import android.content.Intent
 import androidx.core.content.FileProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intending
 import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
@@ -18,6 +17,7 @@ import org.junit.Test
 import org.junit.rules.RuleChain
 import org.odk.collect.android.BuildConfig
 import org.odk.collect.android.R
+import org.odk.collect.android.RecordedIntentsRule
 import org.odk.collect.android.application.Collect
 import org.odk.collect.android.storage.StoragePathProvider
 import org.odk.collect.android.support.CollectTestRule
@@ -33,21 +33,17 @@ class ImageWidgetTest {
     @get:Rule
     var copyFormChain: RuleChain = TestRuleChain.chain()
         .around(ResetStateRule())
+        .around(RecordedIntentsRule())
         .around(rule)
 
     @Test // https://github.com/getodk/collect/issues/4819
     fun attachingGifsShouldBePossible() {
-        Intents.init()
-        val resultIntent = Intent()
-        val file = File.createTempFile("file", "gif", File(StoragePathProvider().odkRootDirPath))
-        FileUtils.copyFileFromAssets("media" + File.separator + "file.gif", file.path)
-        resultIntent.data = FileProvider.getUriForFile(
-            Collect.getInstance(),
-            BuildConfig.APPLICATION_ID + ".provider",
-            file
+        intending(not(IntentMatchers.isInternal())).respondWith(
+            Instrumentation.ActivityResult(
+                Activity.RESULT_OK,
+                gifFileResultIntent()
+            )
         )
-
-        intending(not(IntentMatchers.isInternal())).respondWith(Instrumentation.ActivityResult(Activity.RESULT_OK, resultIntent))
 
         rule.startAtMainMenu()
             .copyForm("image_widget.xml")
@@ -57,5 +53,17 @@ class ImageWidgetTest {
         onView(withTagValue(`is`("ImageView")))
             .perform(nestedScrollTo())
             .check(matches(isDisplayed()))
+    }
+
+    private fun gifFileResultIntent(): Intent {
+        val resultIntent = Intent()
+        val file = File.createTempFile("file", "gif", File(StoragePathProvider().odkRootDirPath))
+        FileUtils.copyFileFromAssets("media" + File.separator + "file.gif", file.path)
+        resultIntent.data = FileProvider.getUriForFile(
+            Collect.getInstance(),
+            BuildConfig.APPLICATION_ID + ".provider",
+            file
+        )
+        return resultIntent
     }
 }
