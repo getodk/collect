15caf7e3f8f90e41bced78294a7070698d0efd5c
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-09-17 12:34:12 +0200

    Added new exceptions to FormDownloadException

--- collect_app/src/main/java/org/odk/collect/android/formmanagement/FormDownloadException.java
+++ collect_app/src/main/java/org/odk/collect/android/formmanagement/FormDownloadException.java
@@ -8,4 +8,20 @@ public class FormDownloadException extends Exception {
     public FormDownloadException(String message) {
         super(message);
     }
+
+    public static class DownloadingInterruptedException extends FormDownloadException {
+
+    }
+
+    public static class FormWithNoHashException extends FormDownloadException {
+
+    }
+
+    public static class FormParsingException extends FormDownloadException {
+
+    }
+
+    public static class DiskException extends FormDownloadException {
+
+    }
 }
