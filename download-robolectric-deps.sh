3817f2a24e1820f531f1d1a7d11bf6433aa2602e
Callum Stott <callum@seadowg.com>
2021-09-23 13:56:06 +0100

    Add geo to robolectric deps download

--- download-robolectric-deps.sh
+++ download-robolectric-deps.sh
@@ -4,8 +4,9 @@ wget -nc https://repo1.maven.org/maven2/org/robolectric/android-all/6.0.1_r3-rob
 wget -nc https://repo1.maven.org/maven2/org/robolectric/android-all/7.0.0_r1-robolectric-r1/android-all-7.0.0_r1-robolectric-r1.jar -P robolectric-deps
 wget -nc https://repo1.maven.org/maven2/org/robolectric/android-all/11-robolectric-6757853/android-all-11-robolectric-6757853.jar -P robolectric-deps
 
-cp robolectric-deps.properties collect_app/src/test/resources
-cp robolectric-deps.properties audiorecorder/src/test/resources
-cp robolectric-deps.properties projects/src/test/resources
-cp robolectric-deps.properties location/src/test/resources
-cp robolectric-deps.properties androidshared/src/test/resources
+mkdir -p collect_app/src/test/resources && cp robolectric-deps.properties "$_"
+mkdir -p audiorecorder/src/test/resources && cp robolectric-deps.properties "$_"
+mkdir -p projects/src/test/resources && cp robolectric-deps.properties "$_"
+mkdir -p location/src/test/resources && cp robolectric-deps.properties "$_"
+mkdir -p androidshared/src/test/resources && cp robolectric-deps.properties "$_"
+mkdir -p geo/src/test/resources && cp robolectric-deps.properties "$_"
