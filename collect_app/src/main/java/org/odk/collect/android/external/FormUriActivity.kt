529a590af5e531639c9e57b91c1e0f0e2272b710
Neil Penman <neilpenman@gmail.com>
2021-10-02 17:11:43 +1000

    Minor miscelaneous changes

---
+++ collect_app/src/main/java/org/odk/collect/android/external/FormUriActivity.kt
@@ -0,0 +1,66 @@
+package org.odk.collect.android.external
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import org.odk.collect.android.R
+import org.odk.collect.android.activities.FormEntryActivity
+import org.odk.collect.android.injection.DaggerUtils
+//import org.odk.collect.android.projects.CurrentProjectProvider  // smap
+import org.odk.collect.android.utilities.ApplicationConstants
+import org.odk.collect.android.utilities.ThemeUtils
+//import org.odk.collect.projects.ProjectsRepository    // smap
+import javax.inject.Inject
+
+class FormUriActivity : Activity() {
+
+    //@Inject
+    //lateinit var currentProjectProvider: CurrentProjectProvider  // smap
+
+    //@Inject
+    //lateinit var projectsRepository: ProjectsRepository   // smap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //DaggerUtils.getComponent(this).inject(this)  // smap
+        setTheme(ThemeUtils(this).appTheme)
+
+        //val firstProject = projectsRepository.getAll().first()  // smap
+        val uri = intent.data
+        val uriProjectId = uri?.getQueryParameter("projectId")
+        //val projectId = uriProjectId ?: firstProject.uuid  // smap
+
+        logAnalytics(uriProjectId)
+
+        if (true /*projectId == currentProjectProvider.getCurrentProject().uuid*/) {
+            startActivity(
+                Intent(this, FormEntryActivity::class.java).also {
+                    it.data = uri
+                    intent.extras?.let { sourceExtras -> it.putExtras(sourceExtras) }
+                }
+            )
+        } else {
+            AlertDialog.Builder(this)
+                .setMessage(R.string.wrong_project_selected_for_form)
+                .setPositiveButton(R.string.ok) { _, _ -> finish() }
+                .create()
+                .show()
+        }
+    }
+
+    private fun logAnalytics(uriProjectId: String?) {
+        /* smap
+        if (uriProjectId != null) {
+            Analytics.log(AnalyticsEvents.FORM_ACTION_WITH_PROJECT_ID)
+        } else {
+            Analytics.log(AnalyticsEvents.FORM_ACTION_WITHOUT_PROJECT_ID)
+        }
+
+        if (intent.getStringExtra(ApplicationConstants.BundleKeys.FORM_MODE) != null) {
+            Analytics.log(AnalyticsEvents.FORM_ACTION_WITH_FORM_MODE_EXTRA)
+        }
+        */
+
+    }
+}
