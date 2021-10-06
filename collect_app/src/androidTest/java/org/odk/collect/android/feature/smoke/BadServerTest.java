5274ff3e75c7860366382691afeea87d7c384b53
Callum Stott <callum@seadowg.com>
2021-10-06 11:01:53 +0100

    Add faster way to set up server in project

--- collect_app/src/androidTest/java/org/odk/collect/android/feature/smoke/BadServerTest.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/feature/smoke/BadServerTest.java
@@ -1,54 +1,34 @@
 package org.odk.collect.android.feature.smoke;
 
-import android.Manifest;
-import android.webkit.MimeTypeMap;
-
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.rule.GrantPermissionRule;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
 import org.junit.runner.RunWith;
-import org.odk.collect.android.injection.config.AppDependencyModule;
-import org.odk.collect.android.openrosa.OpenRosaHttpInterface;
 import org.odk.collect.android.support.CollectTestRule;
-import org.odk.collect.android.support.ResetStateRule;
-import org.odk.collect.android.support.StubOpenRosaServer;
-import org.odk.collect.utilities.UserAgentProvider;
+import org.odk.collect.android.support.TestDependencies;
+import org.odk.collect.android.support.TestRuleChain;
 
 @RunWith(AndroidJUnit4.class)
 public class BadServerTest {
 
-    public final StubOpenRosaServer server = new StubOpenRosaServer();
 
-    public CollectTestRule rule = new CollectTestRule();
+    private final CollectTestRule rule = new CollectTestRule(false);
+    private final TestDependencies testDependencies = new TestDependencies();
+
 
     @Rule
-    public RuleChain copyFormChain = RuleChain
-            .outerRule(GrantPermissionRule.grant(
-                    Manifest.permission.READ_EXTERNAL_STORAGE,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                    Manifest.permission.READ_PHONE_STATE,
-                    Manifest.permission.GET_ACCOUNTS
-            ))
-            .around(new ResetStateRule(new AppDependencyModule() {
-                @Override
-                public OpenRosaHttpInterface provideHttpInterface(MimeTypeMap mimeTypeMap, UserAgentProvider userAgentProvider) {
-                    return server;
-                }
-            }))
-            .around(rule);
+    public RuleChain chain = TestRuleChain.chain(testDependencies).around(rule);
 
     @Test
     // The hash from the form list wasn't used for a long time so some server implementations omitted it even though
     // it's required by the spec. Now we explicitly show an error.
     public void whenHashNotIncludedInFormList_showError() {
-        server.removeHashInFormList();
-        server.addForm("One Question", "one-question", "1", "one-question.xml");
+        testDependencies.server.removeHashInFormList();
+        testDependencies.server.addForm("One Question", "one-question", "1", "one-question.xml");
 
-        rule.startAtMainMenu()
-                .setServer(server.getURL())
+        rule.withProject(testDependencies.server.getURL())
                 .clickGetBlankForm()
                 .clickGetSelected()
                 .assertMessage("1 of 1 downloads failed!")
