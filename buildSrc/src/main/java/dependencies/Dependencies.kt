cf19ffa20ea8ec0204a75d2bc7536969815e675f
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-10-12 13:31:13 +0200

    Fixed crash when using mapbox on Android 11

--- buildSrc/src/main/java/dependencies/Dependencies.kt
+++ buildSrc/src/main/java/dependencies/Dependencies.kt
@@ -29,6 +29,7 @@ object Dependencies {
     const val play_services_maps = "com.google.android.gms:play-services-maps:17.0.1"
     const val play_services_location = "com.google.android.gms:play-services-location:18.0.0"
     const val mapbox_android_sdk = "com.mapbox.mapboxsdk:mapbox-android-sdk:9.2.1"
+    const val mapbox_android_telemetry = "com.mapbox.mapboxsdk:mapbox-android-telemetry:6.1.0" // We need this to fix https://github.com/mapbox/mapbox-gl-native-android/issues/425. Once we update Mapbox Android SDK we can get rid of it.
     const val mapbox_android_plugin_annotation = "com.mapbox.mapboxsdk:mapbox-android-plugin-annotation-v9:0.8.0" // Upgrading will require more changes in our codebase https://github.com/getodk/collect/issues/4305
     const val osmdroid = "org.osmdroid:osmdroid-android:6.1.11"
     const val guava = "com.google.guava:guava:30.1.1-android"
