cf5ec4c63f4865e50aa6537c4f40cc6257568825
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-10-15 16:06:59 +0200

    Fixed error prone registerToClearAnswerOnLongPress() method for string widget

--- collect_app/src/main/java/org/odk/collect/android/widgets/StringWidget.java
+++ collect_app/src/main/java/org/odk/collect/android/widgets/StringWidget.java
@@ -105,21 +105,17 @@ public class StringWidget extends QuestionWidget {
     }
 
     /**
-     * Registers all subviews except for the EditText to clear on long press. This makes it possible
-     * to long-press to paste or perform other text editing functions.
+     * Registers all subviews except for the answer_container (which contains the EditText) to clear on long press.
+     * This makes it possible to long-press to paste or perform other text editing functions.
      */
     @Override
     protected void registerToClearAnswerOnLongPress(FormEntryActivity activity, ViewGroup viewGroup) {
-        for (int i = 0; i < viewGroup.getChildCount(); i++) {
-            View child = viewGroup.getChildAt(i);
-            if (child.getId() == R.id.help_layout) {
-                child.setId(getId());
-                activity.registerForContextMenu(child);
-            } else if (child instanceof ViewGroup) {
-                registerToClearAnswerOnLongPress(activity, (ViewGroup) child);
-            } else if (!(child instanceof EditText)) {
-                child.setId(getId());
-                activity.registerForContextMenu(child);
+        ViewGroup view = findViewById(R.id.question_widget_container);
+        for (int i = 0; i < view.getChildCount(); i++) {
+            View childView = view.getChildAt(i);
+            if (childView.getId() != R.id.answer_container) {
+                childView.setId(getId());
+                activity.registerForContextMenu(childView);
             }
         }
     }
