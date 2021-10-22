a325eae9de7c84a02a804206a53ac16697d1c6f0
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-10-22 08:57:55 +0200

    Converted some simple classes from support package to kotlin

---
+++ collect_app/src/test/java/org/odk/collect/android/support/TestScreenContextActivity.kt
@@ -0,0 +1,25 @@
+package org.odk.collect.android.support
+
+import android.view.View
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.LifecycleOwner
+import org.odk.collect.android.utilities.ScreenContext
+import java.util.ArrayList
+
+class TestScreenContextActivity : FragmentActivity(), ScreenContext {
+    @JvmField
+    val viewsRegisterForContextMenu: MutableList<View> = ArrayList()
+
+    override fun getActivity(): FragmentActivity {
+        return this
+    }
+
+    override fun getViewLifecycle(): LifecycleOwner {
+        return this
+    }
+
+    override fun registerForContextMenu(view: View) {
+        super.registerForContextMenu(view)
+        viewsRegisterForContextMenu.add(view)
+    }
+}
