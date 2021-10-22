3e84a0757aa5a449e548ef573c1efe2657b475ba
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-10-22 10:47:24 +0200

    Added launchForResult() to IntentLauncher

--- androidshared/src/main/java/org/odk/collect/androidshared/utils/IntentLauncher.kt
+++ androidshared/src/main/java/org/odk/collect/androidshared/utils/IntentLauncher.kt
@@ -1,5 +1,6 @@
 package org.odk.collect.androidshared.utils
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 
@@ -7,6 +8,16 @@ object IntentLauncher {
     fun launch(context: Context, intent: Intent, onError: () -> Unit) {
         try {
             context.startActivity(intent)
+        } catch (e: Exception) {
+            onError()
+        } catch (e: Error) {
+            onError()
+        }
+    }
+
+    fun launchForResult(activity: Activity, intent: Intent, requestCode: Int, onError: () -> Unit) {
+        try {
+            activity.startActivityForResult(intent, requestCode)
         } catch (e: Exception) {
             onError()
         } catch (e: Error) {
