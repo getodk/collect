86f0b06ac23e10197e8b9a78fde0721db52f7280
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-10-27 18:05:22 +0200

    Removed unused DummyIntentLauncher class

--- testshared/src/main/java/org/odk/collect/testshared/ErrorIntentLauncher.kt
+++ testshared/src/main/java/org/odk/collect/testshared/ErrorIntentLauncher.kt
@@ -6,26 +6,6 @@ import android.content.Intent
 import androidx.activity.result.ActivityResultLauncher
 import org.odk.collect.androidshared.utils.IntentLauncher
 
-class DummyIntentLauncher : IntentLauncher {
-    override fun launch(context: Context, intent: Intent, onError: () -> Unit) {
-    }
-
-    override fun launchForResult(
-        activity: Activity,
-        intent: Intent,
-        requestCode: Int,
-        onError: () -> Unit
-    ) {
-    }
-
-    override fun launchForResult(
-        resultLauncher: ActivityResultLauncher<Intent>,
-        intent: Intent,
-        onError: () -> Unit
-    ) {
-    }
-}
-
 class ErrorIntentLauncher : IntentLauncher {
     override fun launch(context: Context, intent: Intent, onError: () -> Unit) {
         onError()
