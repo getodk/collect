5f56a89b7c739b224cfe32dbc30d355cfaad9778
Callum Stott <callum@seadowg.com>
2021-11-03 10:40:35 +0000

    Prevent early clicks with proper page assertions

--- collect_app/src/androidTest/java/org/odk/collect/android/feature/formentry/ContextMenuTest.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/feature/formentry/ContextMenuTest.java
@@ -4,10 +4,10 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
 import org.odk.collect.android.R;
+import org.odk.collect.android.support.AdbFormLoadingUtils;
 import org.odk.collect.android.support.CopyFormRule;
 import org.odk.collect.android.support.FormActivityTestRule;
-import org.odk.collect.android.support.AdbFormLoadingUtils;
-import org.odk.collect.android.support.ResetStateRule;
+import org.odk.collect.android.support.TestRuleChain;
 import org.odk.collect.android.support.pages.FormEntryPage;
 
 public class ContextMenuTest {
@@ -17,13 +17,13 @@ public class ContextMenuTest {
     public FormActivityTestRule activityTestRule = AdbFormLoadingUtils.getFormActivityTestRuleFor(STRING_WIDGETS_TEST_FORM);
 
     @Rule
-    public RuleChain copyFormChain = RuleChain
-            .outerRule(new ResetStateRule())
+    public RuleChain copyFormChain = TestRuleChain.chain()
             .around(new CopyFormRule(STRING_WIDGETS_TEST_FORM, true));
 
     @Test
     public void whenRemoveStringAnswer_ShouldAppropriateQuestionBeCleared() {
-        new FormEntryPage("string_widgets")
+        new FormEntryPage("fl")
+                .assertOnPage()
                 .answerQuestion(0, "TestString")
                 .answerQuestion(1, "1234")
                 .assertText("TestString")
@@ -42,7 +42,8 @@ public class ContextMenuTest {
 
     @Test
     public void whenLongPressedOnEditText_ShouldNotRemoveAnswerOptionAppear() {
-        new FormEntryPage("string_widgets")
+        new FormEntryPage("fl")
+                .assertOnPage()
                 .longPressOnView(R.id.answer_container, 0)
                 .assertTextDoesNotExist(R.string.clear_answer);
     }
--- collect_app/src/androidTest/java/org/odk/collect/android/feature/formentry/backgroundlocation/SetGeopointActionTest.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/feature/formentry/backgroundlocation/SetGeopointActionTest.java
@@ -1,26 +1,24 @@
 package org.odk.collect.android.feature.formentry.backgroundlocation;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import android.Manifest;
 
 import androidx.test.core.app.ApplicationProvider;
-import androidx.test.espresso.Espresso;
 import androidx.test.rule.GrantPermissionRule;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
 import org.odk.collect.android.R;
-import org.odk.collect.android.support.ActivityHelpers;
+import org.odk.collect.android.support.AdbFormLoadingUtils;
 import org.odk.collect.android.support.CopyFormRule;
 import org.odk.collect.android.support.FormActivityTestRule;
-import org.odk.collect.android.support.AdbFormLoadingUtils;
-import org.odk.collect.android.support.ResetStateRule;
-
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import org.odk.collect.android.support.TestRuleChain;
+import org.odk.collect.android.support.pages.FormEntryPage;
 
 public class SetGeopointActionTest {
     private static final String SETGEOPOINT_ACTION_FORM = "setgeopoint-action.xml";
@@ -28,21 +26,25 @@ public class SetGeopointActionTest {
     public FormActivityTestRule rule = AdbFormLoadingUtils.getFormActivityTestRuleFor(SETGEOPOINT_ACTION_FORM);
 
     @Rule
-    public RuleChain copyFormChain = RuleChain
-            .outerRule(GrantPermissionRule.grant(Manifest.permission.ACCESS_FINE_LOCATION))
-            .around(new ResetStateRule())
+    public RuleChain copyFormChain = TestRuleChain.chain()
+            .around(GrantPermissionRule.grant(Manifest.permission.ACCESS_FINE_LOCATION))
             .around(new CopyFormRule(SETGEOPOINT_ACTION_FORM, true))
             .around(rule);
 
     @Test
     public void locationCollectionSnackbar_ShouldBeDisplayedAtFormLaunch() {
+        new FormEntryPage("setgeopoint-action-instance-load").assertOnPage();
         onView(withId(com.google.android.material.R.id.snackbar_text))
                 .check(matches(withText(String.format(ApplicationProvider.getApplicationContext().getString(R.string.background_location_enabled), "⋮"))));
     }
 
+    /**
+     * Could be replaced in test for {@link org.odk.collect.android.formentry.FormEntryMenuDelegate}
+     */
     @Test
     public void locationCollectionToggle_ShouldBeAvailable() {
-        Espresso.openActionBarOverflowOrOptionsMenu(ActivityHelpers.getActivity());
-        onView(withText(R.string.track_location)).check(matches(isDisplayed()));
+        new FormEntryPage("setgeopoint-action-instance-load").assertOnPage()
+                .clickOptionsIcon()
+                .assertText(R.string.track_location);
     }
 }
