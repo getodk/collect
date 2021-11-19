e82f6d7ec87857ba18347daa9259d79d34930b54
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-11-19 14:58:33 +0100

    Added tests for FormUriActivityTest

---
+++ collect_app/src/test/java/org/odk/collect/android/external/FormUriActivityTest.kt
@@ -0,0 +1,147 @@
+package org.odk.collect.android.external
+
+import android.app.Activity
+import android.app.Application
+import android.content.Intent
+import android.net.Uri
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasData
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra
+import androidx.test.espresso.matcher.RootMatchers.isDialog
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.gson.Gson
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+import org.odk.collect.android.R
+import org.odk.collect.android.activities.FormEntryActivity
+import org.odk.collect.android.injection.config.AppDependencyModule
+import org.odk.collect.android.preferences.source.SettingsProvider
+import org.odk.collect.android.projects.CurrentProjectProvider
+import org.odk.collect.android.support.CollectHelpers
+import org.odk.collect.projects.InMemProjectsRepository
+import org.odk.collect.projects.Project
+import org.odk.collect.projects.ProjectsRepository
+import org.odk.collect.shared.strings.UUIDGenerator
+
+@RunWith(AndroidJUnit4::class)
+class FormUriActivityTest {
+    private lateinit var projectsRepository: ProjectsRepository
+    private lateinit var currentProjectProvider: CurrentProjectProvider
+    private val context = ApplicationProvider.getApplicationContext<Application>()
+    private val firstProject = Project.DEMO_PROJECT
+    private val secondProject = Project.Saved("123", "Second project", "S", "#cccccc")
+
+    @Before
+    fun setup() {
+        projectsRepository = InMemProjectsRepository()
+        currentProjectProvider = mock()
+
+        projectsRepository.save(firstProject)
+        projectsRepository.save(secondProject)
+        whenever(currentProjectProvider.getCurrentProject()).thenReturn(firstProject)
+
+        CollectHelpers.overrideAppDependencyModule(object : AppDependencyModule() {
+            override fun providesProjectsRepository(
+                uuidGenerator: UUIDGenerator?,
+                gson: Gson?,
+                settingsProvider: SettingsProvider?
+            ): ProjectsRepository {
+                return projectsRepository
+            }
+
+            override fun providesCurrentProjectProvider(
+                settingsProvider: SettingsProvider?,
+                projectsRepository: ProjectsRepository?
+            ): CurrentProjectProvider {
+                return currentProjectProvider
+            }
+        })
+    }
+
+    @Test
+    fun `When there is project id specified in uri and it does not match current project id then display alert dialog`() {
+        val scenario = ActivityScenario.launch<FormUriActivity>(getIntent(secondProject.uuid))
+
+        onView(withText(R.string.wrong_project_selected_for_form)).inRoot(isDialog()).check(matches(isDisplayed()))
+        onView(withId(android.R.id.button1)).perform(click())
+
+        assertThat(scenario.result.resultCode, `is`(Activity.RESULT_CANCELED))
+    }
+
+    @Test
+    fun `When there is project id specified in uri and it matches current project id then start form filling`() {
+        Intents.init()
+
+        ActivityScenario.launch<FormUriActivity>(getIntent(firstProject.uuid))
+
+        Intents.intended(hasComponent(FormEntryActivity::class.java.name))
+
+        Intents.release()
+    }
+
+    @Test
+    fun `When there is no project id specified in uri and first available project id does not match current project id then display alert dialog`() {
+        whenever(currentProjectProvider.getCurrentProject()).thenReturn(secondProject)
+
+        val scenario = ActivityScenario.launch<FormUriActivity>(getIntent())
+
+        onView(withText(R.string.wrong_project_selected_for_form)).inRoot(isDialog()).check(matches(isDisplayed()))
+        onView(withId(android.R.id.button1)).perform(click())
+
+        assertThat(scenario.result.resultCode, `is`(Activity.RESULT_CANCELED))
+    }
+
+    @Test
+    fun `When there is no project id specified in uri and first available project id matches current project id then start form filling`() {
+        Intents.init()
+
+        ActivityScenario.launch<FormUriActivity>(getIntent())
+
+        Intents.intended(hasComponent(FormEntryActivity::class.java.name))
+
+        Intents.release()
+    }
+
+    @Test
+    fun `When everything is fine form filling is started with given data`() {
+        Intents.init()
+
+        ActivityScenario.launch<FormUriActivity>(getIntent(firstProject.uuid))
+
+        Intents.intended(hasComponent(FormEntryActivity::class.java.name))
+        Intents.intended(hasData(FormsContract.getUri(firstProject.uuid, 1)))
+        Intents.intended(hasExtra("KEY_1", "Text"))
+
+        Intents.release()
+    }
+
+    // TODO: Replace the explicit FormUriActivity intent with an implicit one Intent.ACTION_EDIT once it's possible https://github.com/android/android-test/issues/496
+    private fun getIntent(projectId: String? = null) = Intent(context, FormUriActivity::class.java).apply {
+        data = if (projectId == null) {
+            val uri = FormsContract.getUri("", 1)
+            Uri.Builder()
+                .scheme(uri.scheme)
+                .authority(uri.authority)
+                .path(uri.path)
+                .query(null)
+                .build()
+        } else {
+            FormsContract.getUri(projectId, 1)
+        }
+        putExtra("KEY_1", "Text")
+    }
+}
