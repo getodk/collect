40f8e3c119d9c12f5af6c7deecf5266c83b00545
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-12-03 13:03:40 +0100

    Updated FormUpdatesDownloadedNotificationBuilder to behave according to the new requirements

--- collect_app/src/main/java/org/odk/collect/android/notifications/FormUpdatesDownloadedNotificationBuilder.kt
+++ collect_app/src/main/java/org/odk/collect/android/notifications/FormUpdatesDownloadedNotificationBuilder.kt
@@ -6,55 +6,74 @@ import android.app.PendingIntent
 import android.content.Intent
 import androidx.core.app.NotificationCompat
 import org.odk.collect.android.R
-import org.odk.collect.android.activities.FormDownloadListActivity
-import org.odk.collect.android.activities.NotificationActivity
+import org.odk.collect.android.activities.FillBlankFormActivity
 import org.odk.collect.android.formmanagement.ServerFormDetails
-import org.odk.collect.android.utilities.ApplicationConstants.RequestCodes.FORMS_DOWNLOADED_NOTIFICATION
+import org.odk.collect.android.utilities.ApplicationConstants
 import org.odk.collect.android.utilities.IconUtils
-import org.odk.collect.android.utilities.TranslationHandler
+import org.odk.collect.errors.ErrorActivity
+import org.odk.collect.errors.ErrorItem
 import org.odk.collect.strings.localization.getLocalizedString
+import java.io.Serializable
 
 class FormUpdatesDownloadedNotificationBuilder(private val application: Application) {
 
     fun build(result: Map<ServerFormDetails, String>): Notification {
-        val intent = Intent(application, NotificationActivity::class.java)
-        intent.putExtra(
-            NotificationActivity.NOTIFICATION_TITLE,
-            TranslationHandler.getString(application, R.string.download_forms_result)
-        )
-        intent.putExtra(
-            NotificationActivity.NOTIFICATION_MESSAGE,
-            FormDownloadListActivity.getDownloadResultMessage(result)
-        )
+        val allFormsDownloadedSuccessfully = allFormsDownloadedSuccessfully(result)
+
+        val intent = if (allFormsDownloadedSuccessfully) {
+            Intent(application, FillBlankFormActivity::class.java)
+        } else {
+            Intent(application, ErrorActivity::class.java).apply {
+                putExtra(ErrorActivity.EXTRA_ERRORS, getFailures(result) as Serializable)
+            }
+        }
+
         val contentIntent = PendingIntent.getActivity(
             application,
-            FORMS_DOWNLOADED_NOTIFICATION,
+            ApplicationConstants.RequestCodes.FORMS_DOWNLOADED_NOTIFICATION,
             intent,
             PendingIntent.FLAG_UPDATE_CURRENT
         )
 
-        val content =
-            application.getLocalizedString(if (allFormsDownloadedSuccessfully(result)) R.string.success else R.string.failures)
+        val title =
+            if (allFormsDownloadedSuccessfully) application.getLocalizedString(R.string.forms_download_succeeded)
+            else application.getLocalizedString(R.string.forms_download_failed)
+
+        val message =
+            if (allFormsDownloadedSuccessfully) application.getLocalizedString(R.string.all_downloads_succeeded)
+            else application.getLocalizedString(
+                R.string.some_downloads_failed,
+                getNumberOfFailures(result),
+                result.size
+            )
 
         return NotificationCompat.Builder(
             application,
             NotificationManagerNotifier.COLLECT_NOTIFICATION_CHANNEL
         ).apply {
             setContentIntent(contentIntent)
-            setContentTitle(
-                TranslationHandler.getString(
-                    application,
-                    R.string.odk_auto_download_notification_title
-                )
-            )
-            setContentText(content)
+            setContentTitle(title)
+            setContentText(message)
             setSmallIcon(IconUtils.getNotificationAppIcon())
             setAutoCancel(true)
         }.build()
     }
 
-    private fun allFormsDownloadedSuccessfully(result: Map<ServerFormDetails, String>) =
-        result.values.all {
-            it == application.getLocalizedString(R.string.success)
-        }
+    private fun allFormsDownloadedSuccessfully(result: Map<ServerFormDetails, String>) = result.values.all {
+        it == application.getLocalizedString(R.string.success)
+    }
+
+    private fun getNumberOfFailures(result: Map<ServerFormDetails, String>) = result.count {
+        it.value != application.getLocalizedString(R.string.success)
+    }
+
+    private fun getFailures(result: Map<ServerFormDetails, String>) = result.filter {
+        it.value != application.getLocalizedString(R.string.success)
+    }.map {
+        ErrorItem(
+            it.key.formName ?: "",
+            application.getLocalizedString(R.string.form_details, listOf(it.key.formId, it.key.formVersion)),
+            it.value
+        )
+    }
 }
