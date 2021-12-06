a301fed6937bf3e9f874731152625714635b6c58
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-12-06 18:04:23 +0100

    Converted NotificationDrawerRule to kotlin

---
+++ collect_app/src/androidTest/java/org/odk/collect/android/support/NotificationDrawerRule.kt
@@ -0,0 +1,27 @@
+package org.odk.collect.android.support
+
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+import org.odk.collect.android.support.pages.NotificationDrawer
+
+class NotificationDrawerRule : TestRule {
+    private val notificationDrawer = NotificationDrawer()
+
+    override fun apply(base: Statement, description: Description): Statement {
+        return object : Statement() {
+            @Throws(Throwable::class)
+            override fun evaluate() {
+                try {
+                    base.evaluate()
+                } finally {
+                    notificationDrawer.teardown()
+                }
+            }
+        }
+    }
+
+    fun open(): NotificationDrawer {
+        return notificationDrawer.open()
+    }
+}
