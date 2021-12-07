5ff744af5525c1fd1c966a0c0bff2d5a10e5df97
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-12-07 21:49:28 +0100

    Updated FormsSubmissionNotification to display proper content and open proper activities

--- collect_app/src/main/java/org/odk/collect/android/notifications/builders/FormsSubmissionNotificationBuilder.kt
+++ collect_app/src/main/java/org/odk/collect/android/notifications/builders/FormsSubmissionNotificationBuilder.kt
@@ -6,40 +6,41 @@ import android.app.PendingIntent
 import android.content.Intent
 import androidx.core.app.NotificationCompat
 import org.odk.collect.android.R
-import org.odk.collect.android.activities.NotificationActivity
+import org.odk.collect.android.activities.InstanceUploaderListActivity
+import org.odk.collect.android.activities.MainMenuActivity
 import org.odk.collect.android.notifications.NotificationManagerNotifier
 import org.odk.collect.android.utilities.ApplicationConstants.RequestCodes
 import org.odk.collect.android.utilities.IconUtils
-import org.odk.collect.android.utilities.TranslationHandler
 import org.odk.collect.strings.localization.getLocalizedString
 
 class FormsSubmissionNotificationBuilder(private val application: Application) {
 
     fun build(submissionFailed: Boolean, message: String, projectName: String): Notification {
-        val notifyIntent = Intent(application, NotificationActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK
-            putExtra(
-                NotificationActivity.NOTIFICATION_TITLE,
-                application.getLocalizedString(R.string.upload_results)
-            )
-            putExtra(NotificationActivity.NOTIFICATION_MESSAGE, message.trim { it <= ' ' })
-        }
+        val notifyIntent =
+            if (submissionFailed) Intent(application, InstanceUploaderListActivity::class.java)
+            else Intent(application, MainMenuActivity::class.java)
 
         val pendingNotify = PendingIntent.getActivity(
-            application, RequestCodes.FORMS_UPLOADED_NOTIFICATION,
-            notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT
+            application,
+            RequestCodes.FORMS_UPLOADED_NOTIFICATION,
+            notifyIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
         )
 
+        val title =
+            if (submissionFailed) application.getLocalizedString(R.string.forms_upload_failed)
+            else application.getLocalizedString(R.string.forms_upload_succeeded)
+
         val content =
-            if (submissionFailed) application.getLocalizedString(R.string.failures)
-            else TranslationHandler.getString(application, R.string.success)
+            if (submissionFailed) message
+            else application.getLocalizedString(R.string.all_uploads_succeeded)
 
         return NotificationCompat.Builder(
             application,
             NotificationManagerNotifier.COLLECT_NOTIFICATION_CHANNEL
         ).apply {
             setContentIntent(pendingNotify)
-            setContentTitle(TranslationHandler.getString(application, R.string.odk_auto_note))
+            setContentTitle(title)
             setContentText(content)
             setSubText(projectName)
             setSmallIcon(IconUtils.getNotificationAppIcon())
