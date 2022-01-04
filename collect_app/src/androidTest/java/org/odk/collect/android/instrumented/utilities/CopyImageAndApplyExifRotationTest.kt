214716b0c250363a96ca0aedf5d78b8c2b347813
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2022-01-04 13:40:51 +0100

    Improved CopyImageAndApplyExifRotationTest

--- collect_app/src/androidTest/java/org/odk/collect/android/instrumented/utilities/CopyImageAndApplyExifRotationTest.kt
+++ collect_app/src/androidTest/java/org/odk/collect/android/instrumented/utilities/CopyImageAndApplyExifRotationTest.kt
@@ -13,16 +13,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.odk.collect.android.utilities
+package org.odk.collect.android.instrumented.utilities
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import androidx.exifinterface.media.ExifInterface
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.odk.collect.android.utilities.FileUtils
 import timber.log.Timber
 import java.io.File
 import java.io.IOException
@@ -44,59 +46,69 @@ class CopyImageAndApplyExifRotationTest {
     @Test
     fun copyAndRotateImageNinety() {
         attributes[ExifInterface.TAG_ORIENTATION] = ExifInterface.ORIENTATION_ROTATE_90.toString()
-        saveTestBitmapToFile(sourceFile.absolutePath, 3000, 4000, attributes)
+        saveTestBitmapToFile(sourceFile.absolutePath, attributes)
         FileUtils.copyImageAndApplyExifRotation(sourceFile, destinationFile)
         val image = FileUtils.getBitmap(destinationFile.absolutePath, BitmapFactory.Options())
 
-        assertEquals(4000, image.width)
-        assertEquals(3000, image.height)
+        assertEquals(2, image.width)
+        assertEquals(1, image.height)
+        assertEquals(Color.GREEN, image.getPixel(0, 0))
+        assertEquals(Color.RED, image.getPixel(1, 0))
         verifyNoExifOrientationInDestinationFile(destinationFile.absolutePath)
     }
 
     @Test
     fun copyAndRotateImageTwoSeventy() {
         attributes[ExifInterface.TAG_ORIENTATION] = ExifInterface.ORIENTATION_ROTATE_270.toString()
-        saveTestBitmapToFile(sourceFile.absolutePath, 3000, 4000, attributes)
+        saveTestBitmapToFile(sourceFile.absolutePath, attributes)
         FileUtils.copyImageAndApplyExifRotation(sourceFile, destinationFile)
         val image = FileUtils.getBitmap(destinationFile.absolutePath, BitmapFactory.Options())
 
-        assertEquals(4000, image.width)
-        assertEquals(3000, image.height)
+        assertEquals(2, image.width)
+        assertEquals(1, image.height)
+        assertEquals(Color.RED, image.getPixel(0, 0))
+        assertEquals(Color.GREEN, image.getPixel(1, 0))
         verifyNoExifOrientationInDestinationFile(destinationFile.absolutePath)
     }
 
     @Test
     fun copyAndRotateImageOneEighty() {
         attributes[ExifInterface.TAG_ORIENTATION] = ExifInterface.ORIENTATION_ROTATE_180.toString()
-        saveTestBitmapToFile(sourceFile.absolutePath, 3000, 4000, attributes)
+        saveTestBitmapToFile(sourceFile.absolutePath, attributes)
         FileUtils.copyImageAndApplyExifRotation(sourceFile, destinationFile)
         val image = FileUtils.getBitmap(destinationFile.absolutePath, BitmapFactory.Options())
 
-        assertEquals(3000, image.width)
-        assertEquals(4000, image.height)
+        assertEquals(1, image.width)
+        assertEquals(2, image.height)
+        assertEquals(Color.GREEN, image.getPixel(0, 0))
+        assertEquals(Color.RED, image.getPixel(0, 1))
         verifyNoExifOrientationInDestinationFile(destinationFile.absolutePath)
     }
 
     @Test
     fun copyAndRotateImageUndefined() {
         attributes[ExifInterface.TAG_ORIENTATION] = ExifInterface.ORIENTATION_UNDEFINED.toString()
-        saveTestBitmapToFile(sourceFile.absolutePath, 3000, 4000, attributes)
+        saveTestBitmapToFile(sourceFile.absolutePath, attributes)
         FileUtils.copyImageAndApplyExifRotation(sourceFile, destinationFile)
         val image = FileUtils.getBitmap(destinationFile.absolutePath, BitmapFactory.Options())
 
-        assertEquals(3000, image.width)
-        assertEquals(4000, image.height)
+        assertEquals(1, image.width)
+        assertEquals(2, image.height)
+        assertEquals(Color.RED, image.getPixel(0, 0))
+        assertEquals(Color.GREEN, image.getPixel(0, 1))
         verifyNoExifOrientationInDestinationFile(destinationFile.absolutePath)
     }
 
     @Test
     fun copyAndRotateImageNoExif() {
-        saveTestBitmapToFile(sourceFile.absolutePath, 3000, 4000, null)
+        saveTestBitmapToFile(sourceFile.absolutePath, null)
         FileUtils.copyImageAndApplyExifRotation(sourceFile, destinationFile)
         val image = FileUtils.getBitmap(destinationFile.absolutePath, BitmapFactory.Options())
 
-        assertEquals(3000, image.width)
-        assertEquals(4000, image.height)
+        assertEquals(1, image.width)
+        assertEquals(2, image.height)
+        assertEquals(Color.RED, image.getPixel(0, 0))
+        assertEquals(Color.GREEN, image.getPixel(0, 1))
         verifyNoExifOrientationInDestinationFile(destinationFile.absolutePath)
     }
 
@@ -114,11 +126,12 @@ class CopyImageAndApplyExifRotationTest {
 
     private fun saveTestBitmapToFile(
         filePath: String,
-        width: Int,
-        height: Int,
         attributes: Map<String, String>?
     ) {
-        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+        val bitmap = Bitmap.createBitmap(1, 2, Bitmap.Config.ARGB_8888)
+        bitmap.setPixel(0, 0, Color.RED)
+        bitmap.setPixel(0, 1, Color.GREEN)
+
         FileUtils.saveBitmapToFile(bitmap, filePath)
         if (attributes != null) {
             try {
@@ -143,7 +156,7 @@ class CopyImageAndApplyExifRotationTest {
     }
 
     private fun createTempImageFile(imageName: String): File {
-        val temp = File.createTempFile(imageName, ".jpg")
+        val temp = File.createTempFile(imageName, ".png")
         temp.deleteOnExit()
         return temp
     }
