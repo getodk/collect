10d765b74554ab4e71b16b58561650831b5da05d
Callum Stott <callum@seadowg.com>
2022-01-10 12:12:35 +0000

    Add reference to bug

--- testshared/src/main/java/org/odk/collect/testshared/Extensions.kt
+++ testshared/src/main/java/org/odk/collect/testshared/Extensions.kt
@@ -7,7 +7,8 @@ object Extensions {
 
     /**
      * Calling finish() doesn't seem to move an Activity to the DESTROYED state when using
-     * ActivityScenario but `isFinishing` appears to work correctly.
+     * ActivityScenario but `isFinishing` appears to work correctly. Bug for this is tracked
+     * at https://github.com/android/android-test/issues/978.
      */
     val <T : Activity> ActivityScenario<T>.isFinishing: Boolean
         get() {
