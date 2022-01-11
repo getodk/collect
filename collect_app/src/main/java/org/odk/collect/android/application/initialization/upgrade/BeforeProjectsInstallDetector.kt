744d0aa121c1cfbb98e8a53e8f60916499791b1e
Callum Stott <callum@seadowg.com>
2022-01-11 09:53:13 +0000

    Add docs for BeforeProjectsInstallDetector

--- collect_app/src/main/java/org/odk/collect/android/application/initialization/upgrade/BeforeProjectsInstallDetector.kt
+++ collect_app/src/main/java/org/odk/collect/android/application/initialization/upgrade/BeforeProjectsInstallDetector.kt
@@ -5,7 +5,14 @@ import androidx.preference.PreferenceManager
 import org.odk.collect.android.utilities.FileUtils
 import org.odk.collect.upgrade.InstallDetector
 import java.io.File
+import org.odk.collect.upgrade.AppUpgrader
 
+/**
+ * Implementation of [InstallDetector] that looks for signs that a version of Collect
+ * is installed from before Projects were introduced (< v2021.2). [AppUpgrader] was
+ * introduced in that release as well so it and versions after it can use [AppUpgrader]'s
+ * built in version tracking.
+ */
 class BeforeProjectsInstallDetector(private val context: Context) : InstallDetector {
 
     override fun installDetected(): Boolean {
