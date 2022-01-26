4f3acbfca759d6820340db38b54c4305d1656f0b
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2022-01-26 12:04:39 +0100

    Updated dependencies and gradle

--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -5,9 +5,9 @@ object Versions {
     const val android_min_sdk = 21
     const val android_target_sdk = 30
 
-    const val androidx_fragment = "1.3.6"
-    const val okhttp3 = "4.9.2"
-    const val dagger = "2.40"
-    const val kotlin = "1.5.31"
-    const val mockito = "4.0.0"
+    const val androidx_fragment = "1.3.6" // Upgrading will require compileSdk 31
+    const val okhttp3 = "4.9.3"
+    const val dagger = "2.40.5"
+    const val kotlin = "1.6.10"
+    const val mockito = "4.3.1"
 }
