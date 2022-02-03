ef4a1f22b10254f4d37f1ce87546a3077cd81ca3
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2022-02-03 16:47:36 +0100

    Updated dependencies that were blocked by older comileSdkVersion

--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -5,7 +5,7 @@ object Versions {
     const val android_min_sdk = 21
     const val android_target_sdk = 30
 
-    const val androidx_fragment = "1.3.6" // Upgrading will require compileSdk 31
+    const val androidx_fragment = "1.4.1"
     const val okhttp3 = "4.9.3"
     const val dagger = "2.40.5"
     const val kotlin = "1.6.10"
