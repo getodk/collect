b836affdd4ac6fe2c57017b552665651b4bf1221
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2022-02-09 20:52:09 +0100

    Converted SyncFormsTaskSpecTest to kotlin

---
+++ collect_app/src/test/java/org/odk/collect/android/backgroundwork/SyncFormsTaskSpecTest.kt
@@ -0,0 +1,55 @@
+package org.odk.collect.android.backgroundwork
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.odk.collect.analytics.Analytics
+import org.odk.collect.android.formmanagement.FormSourceProvider
+import org.odk.collect.android.formmanagement.FormsUpdater
+import org.odk.collect.android.formmanagement.matchexactly.SyncStatusAppState
+import org.odk.collect.android.injection.config.AppDependencyModule
+import org.odk.collect.android.notifications.Notifier
+import org.odk.collect.android.storage.StoragePathProvider
+import org.odk.collect.android.support.CollectHelpers
+import org.odk.collect.android.utilities.ChangeLockProvider
+import org.odk.collect.android.utilities.FormsRepositoryProvider
+import org.odk.collect.android.utilities.InstancesRepositoryProvider
+import org.odk.collect.settings.SettingsProvider
+
+@RunWith(AndroidJUnit4::class)
+class SyncFormsTaskSpecTest {
+    private val formsUpdater = mock<FormsUpdater>()
+
+    @Before
+    fun setup() {
+        CollectHelpers.overrideAppDependencyModule(object : AppDependencyModule() {
+            override fun providesFormUpdateChecker(
+                context: Context,
+                notifier: Notifier,
+                analytics: Analytics,
+                storagePathProvider: StoragePathProvider,
+                settingsProvider: SettingsProvider,
+                formsRepositoryProvider: FormsRepositoryProvider,
+                formSourceProvider: FormSourceProvider,
+                syncStatusAppState: SyncStatusAppState,
+                instancesRepositoryProvider: InstancesRepositoryProvider,
+                changeLockProvider: ChangeLockProvider
+            ): FormsUpdater {
+                return formsUpdater
+            }
+        })
+    }
+
+    @Test
+    fun `calls synchronize with projectId`() {
+        val inputData = HashMap<String, String>()
+        inputData[SyncFormsTaskSpec.DATA_PROJECT_ID] = "projectId"
+        SyncFormsTaskSpec().getTask(ApplicationProvider.getApplicationContext(), inputData).get()
+        verify(formsUpdater).matchFormsWithServer("projectId")
+    }
+}
