335145a594b537845ec0489ad19c9f53ace1976e
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2022-02-11 10:57:20 +0100

    Converted RefreshFormListDialogFragment to kotlin

---
+++ collect_app/src/main/java/org/odk/collect/android/formentry/RefreshFormListDialogFragment.kt
@@ -0,0 +1,30 @@
+package org.odk.collect.android.formentry
+
+import android.content.Context
+import android.content.DialogInterface
+import org.odk.collect.android.R
+import org.odk.collect.material.MaterialProgressDialogFragmentNew
+
+class RefreshFormListDialogFragment : MaterialProgressDialogFragmentNew() {
+    lateinit var listener: RefreshFormListDialogFragmentListener
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is RefreshFormListDialogFragmentListener) {
+            listener = context
+        }
+        title = getString(R.string.downloading_data)
+        message = getString(R.string.please_wait)
+        isCancelable = false
+        negativeButtonTitle = getString(R.string.cancel_loading_form)
+        negativeButtonListener =
+            DialogInterface.OnClickListener { _: DialogInterface?, _: Int ->
+                listener.onCancelFormLoading()
+                dismiss()
+            }
+    }
+
+    interface RefreshFormListDialogFragmentListener {
+        fun onCancelFormLoading()
+    }
+}
