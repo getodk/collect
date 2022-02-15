a60a0dfd3680e8aa9316893e1140a2a84e7a1639
Callum Stott <callum@seadowg.com>
2022-02-15 13:32:08 +0000

    Convert FormMapTest to Kotlin

---
+++ collect_app/src/androidTest/java/org/odk/collect/android/feature/maps/FormMapTest.kt
@@ -0,0 +1,96 @@
+package org.odk.collect.android.feature.maps
+
+import android.Manifest
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Context
+import android.location.Location
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.GrantPermissionRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+import org.junit.runner.RunWith
+import org.odk.collect.android.R
+import org.odk.collect.android.support.rules.CollectTestRule
+import org.odk.collect.android.support.rules.TestRuleChain
+import org.odk.collect.androidtest.RecordedIntentsRule
+import org.odk.collect.externalapp.ExternalAppUtils.getReturnIntent
+import org.odk.collect.geo.GeoUtils
+
+@RunWith(AndroidJUnit4::class)
+class FormMapTest {
+
+    private val rule = CollectTestRule()
+
+    @get:Rule
+    var copyFormChain: RuleChain = TestRuleChain.chain()
+        .around(GrantPermissionRule.grant(Manifest.permission.ACCESS_COARSE_LOCATION))
+        .around(RecordedIntentsRule())
+        .around(rule)
+
+    @Test
+    fun gettingBlankFormList_showsMapIcon_onlyForFormsWithGeometry() {
+        rule.startAtMainMenu()
+            .copyForm(SINGLE_GEOPOINT_FORM)
+            .copyForm(NO_GEOPOINT_FORM)
+            .clickFillBlankForm()
+            .checkMapIconDisplayedForForm("Single geopoint")
+            .checkMapIconNotDisplayedForForm("basic")
+    }
+
+    @Test
+    fun clickingOnMapIcon_opensMapForForm() {
+        rule.startAtMainMenu()
+            .copyForm(SINGLE_GEOPOINT_FORM)
+            .copyForm(NO_GEOPOINT_FORM)
+            .clickFillBlankForm()
+            .clickOnMapIconForForm("Single geopoint")
+            .assertText("Single geopoint")
+    }
+
+    @Test
+    fun fillingBlankForm_addsInstanceToMap() {
+        stubGeopointIntent()
+
+        rule.startAtMainMenu()
+            .copyForm(SINGLE_GEOPOINT_FORM)
+            .copyForm(NO_GEOPOINT_FORM)
+            .clickFillBlankForm()
+            .clickOnMapIconForForm("Single geopoint")
+            .clickFillBlankFormButton("Single geopoint")
+            .inputText("Foo")
+            .swipeToNextQuestion("Location")
+            .clickWidgetButton()
+            .swipeToEndScreen()
+            .clickSaveAndExitBackToMap()
+            .assertText(
+                ApplicationProvider.getApplicationContext<Context>().resources.getString(
+                    R.string.geometry_status,
+                    1,
+                    1
+                )
+            )
+    }
+
+    private fun stubGeopointIntent() {
+        val location = Location("gps")
+        location.latitude = 125.1
+        location.longitude = 10.1
+        location.altitude = 5.0
+
+        val intent = getReturnIntent(GeoUtils.formatLocationResultString(location))
+        val result = Instrumentation.ActivityResult(Activity.RESULT_OK, intent)
+
+        intending(hasComponent("org.odk.collect.geo.GeoPointActivity"))
+            .respondWith(result)
+    }
+
+    companion object {
+        private const val SINGLE_GEOPOINT_FORM = "single-geopoint.xml"
+        private const val NO_GEOPOINT_FORM = "basic.xml"
+    }
+}
