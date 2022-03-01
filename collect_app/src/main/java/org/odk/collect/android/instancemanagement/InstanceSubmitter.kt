ecb168b356fb90b6d4caf209b480134cc2c19ad8
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2022-03-01 18:29:32 +0100

    Factored out deleteInstance method

--- collect_app/src/main/java/org/odk/collect/android/instancemanagement/InstanceSubmitter.kt
+++ collect_app/src/main/java/org/odk/collect/android/instancemanagement/InstanceSubmitter.kt
@@ -64,24 +64,7 @@ class InstanceSubmitter(
                 uploader.uploadOneSubmission(instance, destinationUrl)
                 result[instance] = null
 
-                // If the submission was successful, delete the instance if either the app-level
-                // delete preference is set or the form definition requests auto-deletion.
-                // TODO: this could take some time so might be better to do in a separate process,
-                // perhaps another worker. It also feels like this could fail and if so should be
-                // communicated to the user. Maybe successful delete should also be communicated?
-                if (InstanceUploaderUtils.shouldFormBeDeleted(
-                        formsRepository, instance.formId, instance.formVersion,
-                        generalSettings.getBoolean(ProjectKeys.KEY_DELETE_AFTER_SEND)
-                    )
-                ) {
-                    InstanceDeleter(
-                        InstancesRepositoryProvider(Collect.getInstance()).get(),
-                        FormsRepositoryProvider(Collect.getInstance()).get()
-                    ).delete(instance.dbId)
-                }
-                val action = if (isGoogleSheetsProtocol()) "HTTP-Sheets auto" else "HTTP auto"
-                val label = Collect.getFormIdentifierHash(instance.formId, instance.formVersion)
-                analytics.logEvent(AnalyticsEvents.SUBMISSION, action, label)
+                deleteInstance(instance)
             } catch (e: FormUploadException) {
                 Timber.d(e)
                 result[instance] = e
@@ -119,4 +102,25 @@ class InstanceSubmitter(
 
     private fun isGoogleSheetsProtocol() =
         generalSettings.getString(ProjectKeys.KEY_PROTOCOL) == ProjectKeys.PROTOCOL_GOOGLE_SHEETS
+
+    private fun deleteInstance(instance: Instance) {
+        // If the submission was successful, delete the instance if either the app-level
+        // delete preference is set or the form definition requests auto-deletion.
+        // TODO: this could take some time so might be better to do in a separate process,
+        // perhaps another worker. It also feels like this could fail and if so should be
+        // communicated to the user. Maybe successful delete should also be communicated?
+        if (InstanceUploaderUtils.shouldFormBeDeleted(
+                formsRepository, instance.formId, instance.formVersion,
+                generalSettings.getBoolean(ProjectKeys.KEY_DELETE_AFTER_SEND)
+            )
+        ) {
+            InstanceDeleter(
+                InstancesRepositoryProvider(Collect.getInstance()).get(),
+                FormsRepositoryProvider(Collect.getInstance()).get()
+            ).delete(instance.dbId)
+        }
+        val action = if (isGoogleSheetsProtocol()) "HTTP-Sheets auto" else "HTTP auto"
+        val label = Collect.getFormIdentifierHash(instance.formId, instance.formVersion)
+        analytics.logEvent(AnalyticsEvents.SUBMISSION, action, label)
+    }
 }
