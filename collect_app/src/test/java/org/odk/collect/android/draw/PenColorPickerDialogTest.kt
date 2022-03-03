e5bd2960272ef47f0725c73b995e3cfc7eb87b9c
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2022-03-03 16:01:49 +0100

    Fixed PenColorPickerDialogTest to use FragmentScenarioLauncherRule

--- collect_app/src/test/java/org/odk/collect/android/draw/PenColorPickerDialogTest.kt
+++ collect_app/src/test/java/org/odk/collect/android/draw/PenColorPickerDialogTest.kt
@@ -6,19 +6,24 @@ import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.odk.collect.android.R
 import org.odk.collect.fragmentstest.DialogFragmentTest
+import org.odk.collect.fragmentstest.FragmentScenarioLauncherRule
 
 @RunWith(AndroidJUnit4::class)
 class PenColorPickerDialogTest {
 
+    @get:Rule
+    val launcherRule = FragmentScenarioLauncherRule()
+
     @Test
     fun `dialog should be cancelable`() {
-        val scenario = DialogFragmentTest.launchDialogFragment(PenColorPickerDialog::class.java)
+        val scenario = launcherRule.launchDialogFragment(PenColorPickerDialog::class.java)
         scenario.onFragment {
             assertThat(it.isCancelable, `is`(true))
         }
@@ -26,7 +31,7 @@ class PenColorPickerDialogTest {
 
     @Test
     fun `pen color in view model should be set after clicking ok`() {
-        val scenario = DialogFragmentTest.launchDialogFragment(PenColorPickerDialog::class.java)
+        val scenario = launcherRule.launchDialogFragment(PenColorPickerDialog::class.java)
         scenario.onFragment {
             val testViewModel = mock<PenColorPickerViewModel>()
             it.model = testViewModel
