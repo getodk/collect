c3ced1b11fe36a029ce886e00ae1fe3d3459ded8
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2022-03-14 15:15:49 +0100

    Cleaned FormDeleter removing the case with forms with the same id and version

--- collect_app/src/main/java/org/odk/collect/android/formmanagement/FormDeleter.kt
+++ collect_app/src/main/java/org/odk/collect/android/formmanagement/FormDeleter.kt
@@ -12,12 +12,7 @@ class FormDeleter(
             val instancesForVersion = instancesRepository.getAllNotDeletedByFormIdAndVersion(
                 form.formId, form.version
             )
-            // If there's more than one form with the same formid/version, trust the user that they want to truly delete this one
-            // because otherwise it may not ever be removed (instance deletion only deletes one corresponding form).
-            val formsWithSameFormIdVersion = formsRepository.getAllByFormIdAndVersion(
-                form.formId, form.version
-            )
-            if (instancesForVersion.isEmpty() || formsWithSameFormIdVersion.size > 1) {
+            if (instancesForVersion.isEmpty()) {
                 formsRepository.delete(id)
             } else {
                 formsRepository.softDelete(form.dbId)
