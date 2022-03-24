bb59f8929cb352f456c275993796b6d8fd4df8e7
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2022-03-24 10:26:33 +0100

    Added new layout for form list items

--- collect_app/src/main/java/org/odk/collect/android/formlist/FormListAdapter.kt
+++ collect_app/src/main/java/org/odk/collect/android/formlist/FormListAdapter.kt
@@ -1,3 +1,21 @@
 package org.odk.collect.android.formlist
 
-class FormListAdapter
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import org.odk.collect.android.databinding.FormListItemBinding
+
+class FormListAdapter(private val formItems: List<FormListItem>) : RecyclerView.Adapter<FormListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = FormListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    }
+
+    override fun getItemCount() = formItems.size
+
+    class ViewHolder(binding: FormListItemBinding) : RecyclerView.ViewHolder(binding.root)
+}
