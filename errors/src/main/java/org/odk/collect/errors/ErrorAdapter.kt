81e0fd288fdde5add3cc87aaffd028d13ef08b49
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2022-03-31 11:16:50 +0200

    Fixed using binding in ErrorAdapter

--- errors/src/main/java/org/odk/collect/errors/ErrorAdapter.kt
+++ errors/src/main/java/org/odk/collect/errors/ErrorAdapter.kt
@@ -7,24 +7,22 @@ import org.odk.collect.errors.databinding.ErrorItemBinding
 
 class ErrorAdapter(private val errors: List<ErrorItem>) : RecyclerView.Adapter<ErrorAdapter.ViewHolder>() {
 
-    private lateinit var binding: ErrorItemBinding
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        binding = ErrorItemBinding.inflate(LayoutInflater.from(parent.context))
+        val binding = ErrorItemBinding.inflate(LayoutInflater.from(parent.context))
         return ViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(errors[position])
-
-    override fun getItemCount() = errors.size
-
-    class ViewHolder(val binding: ErrorItemBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(error: ErrorItem) {
-            binding.apply {
-                title.text = error.title
-                secondaryText.text = error.secondaryText
-                supportingText.text = error.supportingText
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        with(holder) {
+            with(errors[position]) {
+                binding.title.text = this.title
+                binding.secondaryText.text = this.secondaryText
+                binding.supportingText.text = this.supportingText
             }
         }
     }
+
+    override fun getItemCount() = errors.size
+
+    inner class ViewHolder(val binding: ErrorItemBinding) : RecyclerView.ViewHolder(binding.root)
 }
