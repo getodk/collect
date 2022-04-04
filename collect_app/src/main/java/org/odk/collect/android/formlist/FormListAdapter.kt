02b2b4a8a5cf6616c5c402864b340ff8831d3eef
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2022-04-04 11:33:42 +0200

    Fixed using binding in FormListAdapter

--- collect_app/src/main/java/org/odk/collect/android/formlist/FormListAdapter.kt
+++ collect_app/src/main/java/org/odk/collect/android/formlist/FormListAdapter.kt
@@ -1,6 +1,5 @@
 package org.odk.collect.android.formlist
 
-import android.content.Context
 import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
@@ -17,54 +16,46 @@ class FormListAdapter : RecyclerView.Adapter<FormListAdapter.ViewHolder>() {
     private var formItems = emptyList<FormListItem>()
     lateinit var listener: OnFormItemClickListener
 
-    private lateinit var binding: FormListItemBinding
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        binding = FormListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = FormListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(formItems[position])
+        with(holder) {
+            with(formItems[position]) {
+                binding.formTitle.text = this.formName
+
+                binding.formSubtitle.text = binding.root.context.getString(R.string.version_number, this.formVersion)
+                binding.formSubtitle.visibility = if (this.formVersion.isNotBlank()) View.VISIBLE else View.GONE
+
+                binding.formSubtitle2.text = try {
+                    SimpleDateFormat(binding.root.context.getString(R.string.added_on_date_at_time), Locale.getDefault()).format(this.dateOfCreation)
+                } catch (e: IllegalArgumentException) {
+                    Timber.e(e)
+                    ""
+                }
+
+                binding.mapButton.visibility = if (this.geometryPath.isNotBlank()) View.VISIBLE else View.GONE
+
+                binding.root.setOnClickListener {
+                    if (MultiClickGuard.allowClick(javaClass.name)) {
+                        listener.onFormClick(this.contentUri)
+                    }
+                }
+
+                binding.mapButton.setOnClickListener {
+                    if (MultiClickGuard.allowClick(javaClass.name)) {
+                        listener.onMapButtonClick(this.databaseId)
+                    }
+                }
+            }
+        }
     }
 
     override fun getItemCount() = formItems.size
 
-    inner class ViewHolder(private val binding: FormListItemBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: FormListItem) {
-            binding.apply {
-                formTitle.text = item.formName
-
-                formSubtitle.text = binding.root.context.getString(R.string.version_number, item.formVersion)
-                formSubtitle.visibility = if (item.formVersion.isNotBlank()) View.VISIBLE else View.GONE
-
-                formSubtitle2.text = getSubtitle2Text(binding.root.context, item.dateOfCreation)
-
-                mapButton.visibility = if (item.geometryPath.isNotBlank()) View.VISIBLE else View.GONE
-
-                root.setOnClickListener {
-                    if (MultiClickGuard.allowClick(javaClass.name)) {
-                        listener.onFormClick(item.contentUri)
-                    }
-                }
-
-                mapButton.setOnClickListener {
-                    if (MultiClickGuard.allowClick(javaClass.name)) {
-                        listener.onMapButtonClick(item.databaseId)
-                    }
-                }
-            }
-        }
-
-        private fun getSubtitle2Text(context: Context, date: Long): String {
-            return try {
-                SimpleDateFormat(context.getString(R.string.added_on_date_at_time), Locale.getDefault()).format(date)
-            } catch (e: IllegalArgumentException) {
-                Timber.e(e)
-                ""
-            }
-        }
-    }
+    inner class ViewHolder(val binding: FormListItemBinding) : RecyclerView.ViewHolder(binding.root)
 
     fun setData(formItems: List<FormListItem>) {
         this.formItems = formItems.toList()
