8eeaf57de7ad950c675d0f0cda272816fc5fb0a1
Hélène Martin <lognaturel@gmail.com>
2022-04-11 09:37:31 -0700

    Launch select from map with zoom on current location

--- geo/src/main/java/org/odk/collect/geo/selection/SelectionMapFragment.kt
+++ geo/src/main/java/org/odk/collect/geo/selection/SelectionMapFragment.kt
@@ -35,7 +35,8 @@ import javax.inject.Inject
 class SelectionMapFragment(
     val selectionMapData: SelectionMapData,
     val skipSummary: Boolean = false,
-    val showNewItemButton: Boolean = true
+    val showNewItemButton: Boolean = true,
+    val zoomToFeatureBoundingBoxOnLoad: Boolean = true
 ) : Fragment() {
 
     @Inject
@@ -320,7 +321,7 @@ class SelectionMapFragment(
         if (selectedFeatureId != null) {
             onFeatureClicked(selectedFeatureId)
             viewportInitialized = true
-        } else if (!viewportInitialized && points.isNotEmpty()) {
+        } else if (zoomToFeatureBoundingBoxOnLoad && !viewportInitialized && points.isNotEmpty()) {
             map.zoomToBoundingBox(points, 0.8, false)
             viewportInitialized = true
         }
