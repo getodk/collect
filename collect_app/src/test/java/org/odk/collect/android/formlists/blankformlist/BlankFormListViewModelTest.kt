f5c09d8a9eebe504647d83b7d1b2e7555d0bc2bc
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2022-05-19 14:12:48 +0200

    Improved BlankFormListViewModelTest

--- collect_app/src/test/java/org/odk/collect/android/formlists/blankformlist/BlankFormListViewModelTest.kt
+++ collect_app/src/test/java/org/odk/collect/android/formlists/blankformlist/BlankFormListViewModelTest.kt
@@ -4,10 +4,8 @@ import android.app.Application
 import androidx.lifecycle.MutableLiveData
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import junit.framework.TestCase.assertFalse
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.kotlin.doReturn
@@ -25,7 +23,6 @@ import org.odk.collect.android.utilities.FormsDirDiskFormsSynchronizer
 import org.odk.collect.androidtest.getOrAwaitValue
 import org.odk.collect.forms.Form
 import org.odk.collect.forms.FormSourceException
-import org.odk.collect.formstest.FormUtils
 import org.odk.collect.formstest.InMemFormsRepository
 import org.odk.collect.settings.keys.ProjectKeys
 import org.odk.collect.shared.settings.InMemSettings
@@ -34,49 +31,7 @@ import org.odk.collect.testshared.TestScheduler
 
 @RunWith(AndroidJUnit4::class)
 class BlankFormListViewModelTest {
-    private val form1 = Form.Builder()
-        .dbId(1)
-        .formId("2")
-        .version("1")
-        .displayName("Form 2")
-        .date(0)
-        .formFilePath(FormUtils.createXFormFile("2", "1").absolutePath)
-        .build()
-
-    private val form2 = Form.Builder()
-        .dbId(2)
-        .formId("1")
-        .version("1")
-        .displayName("Form 1")
-        .date(1)
-        .formFilePath(FormUtils.createXFormFile("1", "1").absolutePath)
-        .build()
-
-    private val form3 = Form.Builder()
-        .dbId(3)
-        .formId("2x")
-        .version("1")
-        .displayName("Form 2x")
-        .date(3)
-        .formFilePath(FormUtils.createXFormFile("2x", "1").absolutePath)
-        .build()
-
-    private val form4 = Form.Builder()
-        .dbId(4)
-        .formId("4")
-        .version("1")
-        .displayName("Form 4")
-        .date(3)
-        .deleted(true)
-        .formFilePath(FormUtils.createXFormFile("4", "1").absolutePath)
-        .build()
-
-    private val formsRepository = InMemFormsRepository().apply {
-        save(form1)
-        save(form2)
-        save(form3)
-        save(form4)
-    }
+    private val formsRepository = InMemFormsRepository()
     private val context = ApplicationProvider.getApplicationContext<Application>()
     private val syncRepository: SyncStatusAppState = mock()
     private val formsUpdater: FormsUpdater = mock()
@@ -90,78 +45,16 @@ class BlankFormListViewModelTest {
     private val changeLock = BooleanChangeLock()
     private lateinit var viewModel: BlankFormListViewModel
 
-    private val formListItem1 = BlankFormListItem(
-        databaseId = form1.dbId,
-        formId = form1.formId,
-        formName = form1.displayName,
-        formVersion = form1.version ?: "",
-        geometryPath = form1.geometryXpath ?: "",
-        dateOfCreation = form1.date,
-        dateOfLastUsage = 0,
-        contentUri = FormsContract.getUri(projectId, form1.dbId)
-    )
-
-    private val formListItem2 = BlankFormListItem(
-        databaseId = form2.dbId,
-        formId = form2.formId,
-        formName = form2.displayName,
-        formVersion = form2.version ?: "",
-        geometryPath = form2.geometryXpath ?: "",
-        dateOfCreation = form2.date,
-        dateOfLastUsage = 0,
-        contentUri = FormsContract.getUri(projectId, form2.dbId)
-    )
-
-    private val formListItem3 = BlankFormListItem(
-        databaseId = form3.dbId,
-        formId = form3.formId,
-        formName = form3.displayName,
-        formVersion = form3.version ?: "",
-        geometryPath = form3.geometryXpath ?: "",
-        dateOfCreation = form3.date,
-        dateOfLastUsage = 0,
-        contentUri = FormsContract.getUri(projectId, form3.dbId)
-    )
-
-    private val formListItem4 = BlankFormListItem(
-        databaseId = form4.dbId,
-        formId = form4.formId,
-        formName = form4.displayName,
-        formVersion = form4.version ?: "",
-        geometryPath = form4.geometryXpath ?: "",
-        dateOfCreation = form4.date,
-        dateOfLastUsage = 0,
-        contentUri = FormsContract.getUri(projectId, form4.dbId)
-    )
-
-    @Before
-    fun setup() {
-        whenever(changeLockProvider.getFormLock(projectId)).thenReturn(changeLock)
-        whenever(formsDirDiskFormsSynchronizer.synchronizeAndReturnError()).thenReturn("Result text")
-
-        viewModel = BlankFormListViewModel(
-            formsRepository,
-            context,
-            syncRepository,
-            formsUpdater,
-            scheduler,
-            generalSettings,
-            analytics,
-            changeLockProvider,
-            formsDirDiskFormsSynchronizer,
-            projectId
-        )
-
-        generalSettings.save(ProjectKeys.KEY_SERVER_URL, "https://sample.com")
-    }
-
     @Test
     fun `syncWithStorage should be triggered when viewModel is initialized`() {
+        createViewModel()
         verify(formsDirDiskFormsSynchronizer).synchronizeAndReturnError()
     }
 
     @Test
     fun `syncWithStorage return correct result`() {
+        whenever(formsDirDiskFormsSynchronizer.synchronizeAndReturnError()).thenReturn("Result text")
+        createViewModel()
         assertThat(viewModel.syncResult.value?.value, `is`("Result text"))
     }
 
@@ -169,25 +62,15 @@ class BlankFormListViewModelTest {
     fun `syncWithStorage should not be triggered when viewModel is initialized if forms lock is locked`() {
         changeLock.lock()
         val formsDirDiskFormsSynchronizer: FormsDirDiskFormsSynchronizer = mock()
-
-        BlankFormListViewModel(
-            formsRepository,
-            context,
-            syncRepository,
-            formsUpdater,
-            scheduler,
-            generalSettings,
-            analytics,
-            changeLockProvider,
-            formsDirDiskFormsSynchronizer,
-            projectId
-        )
+        createViewModel()
 
         verifyNoInteractions(formsDirDiskFormsSynchronizer)
     }
 
     @Test
     fun `syncWithServer when task finishes sets result to true`() {
+        createViewModel()
+        generalSettings.save(ProjectKeys.KEY_SERVER_URL, "https://sample.com")
         doReturn(true).whenever(formsUpdater).matchFormsWithServer(projectId)
         val result = viewModel.syncWithServer()
         assertThat(result.value, `is`(true))
@@ -195,6 +78,8 @@ class BlankFormListViewModelTest {
 
     @Test
     fun `syncWithServer when there is an error sets result to false`() {
+        createViewModel()
+        generalSettings.save(ProjectKeys.KEY_SERVER_URL, "https://sample.com")
         doReturn(false).whenever(formsUpdater).matchFormsWithServer(projectId)
         val result = viewModel.syncWithServer()
         assertThat(result.value, `is`(false))
@@ -202,6 +87,8 @@ class BlankFormListViewModelTest {
 
     @Test
     fun `isMatchExactlyEnabled returns correct value based on settings`() {
+        createViewModel()
+
         generalSettings.save(ProjectKeys.KEY_PROTOCOL, ProjectKeys.PROTOCOL_SERVER)
 
         assertThat(viewModel.isMatchExactlyEnabled(), `is`(false))
@@ -213,6 +100,8 @@ class BlankFormListViewModelTest {
 
     @Test
     fun `isSyncingWithServer follows repository isSyncing`() {
+        createViewModel()
+
         val liveData = MutableLiveData(true)
         whenever(syncRepository.isSyncing(projectId)).thenReturn(liveData)
 
@@ -224,6 +113,8 @@ class BlankFormListViewModelTest {
 
     @Test
     fun `isOutOfSyncWithServer follows repository syncError`() {
+        createViewModel()
+
         val liveData = MutableLiveData<FormSourceException?>(FormSourceException.FetchError())
         whenever(syncRepository.getSyncError(projectId)).thenReturn(liveData)
 
@@ -235,6 +126,8 @@ class BlankFormListViewModelTest {
 
     @Test
     fun `isAuthenticationRequired follows repository syncError`() {
+        createViewModel()
+
         val liveData = MutableLiveData<FormSourceException?>(FormSourceException.FetchError())
         whenever(syncRepository.getSyncError(projectId)).thenReturn(liveData)
 
@@ -248,116 +141,138 @@ class BlankFormListViewModelTest {
 
     @Test
     fun `original list of forms should be fetched from database initially and sorted by name ASC`() {
-        val firstForm = viewModel.formsToDisplay.value!![0]
-        val secondForm = viewModel.formsToDisplay.value!![1]
-        val thirdForm = viewModel.formsToDisplay.value!![2]
+        saveForms(BlankFormFixtures.blankForm1, BlankFormFixtures.blankForm2)
+        createViewModel()
 
-        assertThat(firstForm, `is`(formListItem2))
-        assertThat(secondForm, `is`(formListItem1))
-        assertThat(thirdForm, `is`(formListItem3))
+        assertFormItem(viewModel.formsToDisplay.value!![0], BlankFormFixtures.blankForm1)
+        assertFormItem(viewModel.formsToDisplay.value!![1], BlankFormFixtures.blankForm2)
     }
 
     @Test
     fun `deleted forms should be ignored`() {
-        assertThat(viewModel.formsToDisplay.value!!.size, `is`(3))
-        assertFalse(viewModel.formsToDisplay.value!!.contains(formListItem4))
+        saveForms(BlankFormFixtures.blankForm1, BlankFormFixtures.blankForm4)
+        createViewModel()
+
+        assertThat(viewModel.formsToDisplay.value!!.size, `is`(1))
+        assertFormItem(viewModel.formsToDisplay.value!![0], BlankFormFixtures.blankForm1)
     }
 
     @Test
     fun `list of forms should be sorted when sorting order is changed`() {
+        saveForms(BlankFormFixtures.blankForm1, BlankFormFixtures.blankForm2, BlankFormFixtures.blankForm5)
+        createViewModel()
+
         // Sort by name DESC
         viewModel.sortingOrder = 1
 
-        var firstForm = viewModel.formsToDisplay.value!![0]
-        var secondForm = viewModel.formsToDisplay.value!![1]
-        var thirdForm = viewModel.formsToDisplay.value!![2]
-
-        assertThat(firstForm, `is`(formListItem3))
-        assertThat(secondForm, `is`(formListItem1))
-        assertThat(thirdForm, `is`(formListItem2))
+        assertFormItem(viewModel.formsToDisplay.value!![0], BlankFormFixtures.blankForm2)
+        assertFormItem(viewModel.formsToDisplay.value!![1], BlankFormFixtures.blankForm1)
+        assertFormItem(viewModel.formsToDisplay.value!![2], BlankFormFixtures.blankForm5)
 
         // Sort by date ASC
         viewModel.sortingOrder = 2
 
-        firstForm = viewModel.formsToDisplay.value!![0]
-        secondForm = viewModel.formsToDisplay.value!![1]
-        thirdForm = viewModel.formsToDisplay.value!![2]
-
-        assertThat(firstForm, `is`(formListItem1))
-        assertThat(secondForm, `is`(formListItem2))
-        assertThat(thirdForm, `is`(formListItem3))
+        assertFormItem(viewModel.formsToDisplay.value!![0], BlankFormFixtures.blankForm1)
+        assertFormItem(viewModel.formsToDisplay.value!![1], BlankFormFixtures.blankForm2)
+        assertFormItem(viewModel.formsToDisplay.value!![2], BlankFormFixtures.blankForm5)
 
         // Sort by date DESC
         viewModel.sortingOrder = 3
 
-        firstForm = viewModel.formsToDisplay.value!![0]
-        secondForm = viewModel.formsToDisplay.value!![1]
-        thirdForm = viewModel.formsToDisplay.value!![2]
-
-        assertThat(firstForm, `is`(formListItem3))
-        assertThat(secondForm, `is`(formListItem2))
-        assertThat(thirdForm, `is`(formListItem1))
+        assertFormItem(viewModel.formsToDisplay.value!![0], BlankFormFixtures.blankForm5)
+        assertFormItem(viewModel.formsToDisplay.value!![1], BlankFormFixtures.blankForm2)
+        assertFormItem(viewModel.formsToDisplay.value!![2], BlankFormFixtures.blankForm1)
 
         // Sort by name ASC
         viewModel.sortingOrder = 0
 
-        firstForm = viewModel.formsToDisplay.value!![0]
-        secondForm = viewModel.formsToDisplay.value!![1]
-        thirdForm = viewModel.formsToDisplay.value!![2]
-
-        assertThat(firstForm, `is`(formListItem2))
-        assertThat(secondForm, `is`(formListItem1))
-        assertThat(thirdForm, `is`(formListItem3))
+        assertFormItem(viewModel.formsToDisplay.value!![0], BlankFormFixtures.blankForm5)
+        assertFormItem(viewModel.formsToDisplay.value!![1], BlankFormFixtures.blankForm1)
+        assertFormItem(viewModel.formsToDisplay.value!![2], BlankFormFixtures.blankForm2)
     }
 
     @Test
     fun `list of forms should be filtered when filterText is changed`() {
+        saveForms(BlankFormFixtures.blankForm1, BlankFormFixtures.blankForm2, BlankFormFixtures.blankForm3)
+        createViewModel()
+
         viewModel.filterText = "2"
 
-        var firstForm = viewModel.formsToDisplay.value!![0]
-        var secondForm = viewModel.formsToDisplay.value!![1]
-
         assertThat(viewModel.formsToDisplay.value?.size, `is`(2))
-        assertThat(firstForm, `is`(formListItem1))
-        assertThat(secondForm, `is`(formListItem3))
+        assertFormItem(viewModel.formsToDisplay.value!![0], BlankFormFixtures.blankForm2)
+        assertFormItem(viewModel.formsToDisplay.value!![1], BlankFormFixtures.blankForm3)
 
         viewModel.filterText = "2x"
 
-        firstForm = viewModel.formsToDisplay.value!![0]
-
         assertThat(viewModel.formsToDisplay.value?.size, `is`(1))
-        assertThat(firstForm, `is`(formListItem3))
+        assertFormItem(viewModel.formsToDisplay.value!![0], BlankFormFixtures.blankForm3)
 
         viewModel.filterText = ""
 
-        firstForm = viewModel.formsToDisplay.value!![0]
-        secondForm = viewModel.formsToDisplay.value!![1]
-        val thirdForm = viewModel.formsToDisplay.value!![2]
-
         assertThat(viewModel.formsToDisplay.value?.size, `is`(3))
-        assertThat(firstForm, `is`(formListItem2))
-        assertThat(secondForm, `is`(formListItem1))
-        assertThat(thirdForm, `is`(formListItem3))
+        assertFormItem(viewModel.formsToDisplay.value!![0], BlankFormFixtures.blankForm1)
+        assertFormItem(viewModel.formsToDisplay.value!![1], BlankFormFixtures.blankForm2)
+        assertFormItem(viewModel.formsToDisplay.value!![2], BlankFormFixtures.blankForm3)
     }
 
     @Test
     fun `filtering and sorting should work together`() {
+        saveForms(BlankFormFixtures.blankForm1, BlankFormFixtures.blankForm2, BlankFormFixtures.blankForm3)
+        createViewModel()
+
         viewModel.filterText = "2"
 
-        var firstForm = viewModel.formsToDisplay.value!![0]
-        var secondForm = viewModel.formsToDisplay.value!![1]
-
         assertThat(viewModel.formsToDisplay.value?.size, `is`(2))
-        assertThat(firstForm, `is`(formListItem1))
-        assertThat(secondForm, `is`(formListItem3))
+        assertFormItem(viewModel.formsToDisplay.value!![0], BlankFormFixtures.blankForm2)
+        assertFormItem(viewModel.formsToDisplay.value!![1], BlankFormFixtures.blankForm3)
 
         viewModel.sortingOrder = 1
 
-        firstForm = viewModel.formsToDisplay.value!![0]
-        secondForm = viewModel.formsToDisplay.value!![1]
-
         assertThat(viewModel.formsToDisplay.value?.size, `is`(2))
-        assertThat(firstForm, `is`(formListItem3))
-        assertThat(secondForm, `is`(formListItem1))
+        assertFormItem(viewModel.formsToDisplay.value!![0], BlankFormFixtures.blankForm3)
+        assertFormItem(viewModel.formsToDisplay.value!![1], BlankFormFixtures.blankForm2)
+    }
+
+    private fun saveForms(vararg forms: Form) {
+        formsRepository.deleteAll()
+
+        forms.forEach {
+            formsRepository.save(it)
+        }
+    }
+
+    private fun createViewModel() {
+        whenever(changeLockProvider.getFormLock(projectId)).thenReturn(changeLock)
+
+        viewModel = BlankFormListViewModel(
+            formsRepository,
+            context,
+            syncRepository,
+            formsUpdater,
+            scheduler,
+            generalSettings,
+            analytics,
+            changeLockProvider,
+            formsDirDiskFormsSynchronizer,
+            projectId
+        )
+    }
+
+    private fun assertFormItem(blankFormListItem: BlankFormListItem, form: Form) {
+        assertThat(
+            blankFormListItem,
+            `is`(
+                BlankFormListItem(
+                    databaseId = form.dbId,
+                    formId = form.formId,
+                    formName = form.displayName,
+                    formVersion = form.version ?: "",
+                    geometryPath = form.geometryXpath ?: "",
+                    dateOfCreation = form.date,
+                    dateOfLastUsage = 0,
+                    contentUri = FormsContract.getUri(projectId, form.dbId)
+                )
+            )
+        )
     }
 }
