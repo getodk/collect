d69c9c0db9687ebeb09bff0e6d298d1713c8db99
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2022-06-30 17:24:48 +0200

    Initialize schemaStream and schemaJsonObject by lazy

--- settings/src/main/java/org/odk/collect/settings/validation/JsonSchemaSettingsValidator.kt
+++ settings/src/main/java/org/odk/collect/settings/validation/JsonSchemaSettingsValidator.kt
@@ -8,19 +8,24 @@ import com.networknt.schema.ValidatorTypeCode
 import org.json.JSONObject
 import org.odk.collect.settings.importing.SettingsValidator
 import java.io.InputStream
-import java.nio.charset.Charset
 
 internal class JsonSchemaSettingsValidator(private val schemaProvider: () -> InputStream) :
     SettingsValidator {
 
+    private val schemaString: String by lazy {
+        schemaProvider().bufferedReader().use { it.readText() }
+    }
+
+    private val schemaJsonObject: JSONObject by lazy {
+        JSONObject(schemaString)
+    }
+
     override fun isValid(json: String): Boolean {
         return try {
-            schemaProvider().use { schemaStream ->
-                val schemaFactory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V201909)
-                val schema = schemaFactory.getSchema(schemaStream)
-                val errors = schema.validate(ObjectMapper().readTree(json))
-                errors.none { it.type != ValidatorTypeCode.ENUM.value }
-            }
+            val schemaFactory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V201909)
+            val schema = schemaFactory.getSchema(schemaString)
+            val errors = schema.validate(ObjectMapper().readTree(json))
+            errors.none { it.type != ValidatorTypeCode.ENUM.value }
         } catch (e: JsonParseException) {
             false
         }
@@ -28,17 +33,11 @@ internal class JsonSchemaSettingsValidator(private val schemaProvider: () -> Inp
 
     override fun isKeySupported(parentJsonObjectName: String, key: String): Boolean {
         return try {
-            schemaProvider().use { schemaStream ->
-                val schemaJsonObject = JSONObject(
-                    schemaStream.readBytes().toString(Charset.defaultCharset())
-                )
-
-                return schemaJsonObject
-                    .getJSONObject("properties")
-                    .getJSONObject(parentJsonObjectName)
-                    .getJSONObject("properties")
-                    .has(key)
-            }
+            return schemaJsonObject
+                .getJSONObject("properties")
+                .getJSONObject(parentJsonObjectName)
+                .getJSONObject("properties")
+                .has(key)
         } catch (e: JsonParseException) {
             false
         }
@@ -46,29 +45,23 @@ internal class JsonSchemaSettingsValidator(private val schemaProvider: () -> Inp
 
     override fun isValueSupported(parentJsonObjectName: String, key: String, value: Any): Boolean {
         return try {
-            schemaProvider().use { schemaStream ->
-                val schemaJsonObject = JSONObject(
-                    schemaStream.readBytes().toString(Charset.defaultCharset())
-                )
+            val settingJsonObject = schemaJsonObject
+                .getJSONObject("properties")
+                .getJSONObject(parentJsonObjectName)
+                .getJSONObject("properties")
+                .getJSONObject(key)
 
-                val settingJsonObject = schemaJsonObject
-                    .getJSONObject("properties")
-                    .getJSONObject(parentJsonObjectName)
-                    .getJSONObject("properties")
-                    .getJSONObject(key)
+            return if (settingJsonObject.has("enum")) {
+                val supportedValues = settingJsonObject.getJSONArray("enum")
 
-                return if (settingJsonObject.has("enum")) {
-                    val supportedValues = settingJsonObject.getJSONArray("enum")
-
-                    for (i in 0 until supportedValues.length()) {
-                        if (supportedValues[i] == value) {
-                            return true
-                        }
+                for (i in 0 until supportedValues.length()) {
+                    if (supportedValues[i] == value) {
+                        return true
                     }
-                    false
-                } else {
-                    true
                 }
+                false
+            } else {
+                true
             }
         } catch (e: JsonParseException) {
             false
