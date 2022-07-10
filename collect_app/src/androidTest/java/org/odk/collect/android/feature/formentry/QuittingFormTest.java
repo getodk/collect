2a48ec57d361685fcb73ec79c0113b66056e72e8
Callum Stott <callum@seadowg.com>
2022-07-10 18:38:56 +0100

    Add tests for save on back with constraints

--- collect_app/src/androidTest/java/org/odk/collect/android/feature/formentry/QuittingFormTest.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/feature/formentry/QuittingFormTest.java
@@ -6,6 +6,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
 import org.junit.runner.RunWith;
+import org.odk.collect.android.R;
+import org.odk.collect.android.support.pages.FormEntryPage;
 import org.odk.collect.android.support.pages.MainMenuPage;
 import org.odk.collect.android.support.pages.SaveOrIgnoreDialog;
 import org.odk.collect.android.support.rules.CollectTestRule;
@@ -68,5 +70,75 @@ public class QuittingFormTest {
                 .clickOnForm("Two Question")
                 .assertText("Reuben")
                 .assertTextDoesNotExist("10");
+    }
+
+    @Test
+    public void whenFillingForm_withViolatedConstraints_pressingBack_andClickingSaveChanges_savesCurrentAnswers() {
+        rule.startAtMainMenu()
+                .copyForm("two-question-required.xml")
+                .startBlankForm("Two Question Required")
+                .answerQuestion("What is your name?", "Reuben")
+
+                .pressBack(new SaveOrIgnoreDialog<>("Two Question Required", new MainMenuPage()))
+                .clickSaveChanges()
+
+                .clickEditSavedForm(1)
+                .clickOnForm("Two Question Required")
+                .assertText("Reuben");
+    }
+
+    @Test
+    public void whenEditingANonFinalizedForm_withViolatedConstraints_pressingBack_andClickingSaveChanges_savesCurrentAnswers() {
+        rule.startAtMainMenu()
+                .copyForm("two-question-required.xml")
+                .startBlankForm("Two Question Required")
+                .answerQuestion("What is your name?", "Reuben")
+
+                .pressBack(new SaveOrIgnoreDialog<>("Two Question Required", new MainMenuPage()))
+                .clickSaveChanges()
+
+                .clickEditSavedForm(1)
+                .clickOnForm("Two Question Required")
+                .clickGoToStart()
+                .answerQuestion("What is your name?", "Another Reuben")
+
+                .pressBack(new SaveOrIgnoreDialog<>("Two Question Required", new MainMenuPage()))
+                .clickSaveChanges()
+
+                .clickEditSavedForm(1)
+                .clickOnForm("Two Question Required")
+                .assertText("Another Reuben");
+    }
+
+    @Test
+    public void whenEditingAFinalizedForm_withViolatedConstraints_pressingBack_andClickingSaveChanges_showsError() {
+        rule.startAtMainMenu()
+                .copyForm("two-question-required.xml")
+                .startBlankForm("Two Question Required")
+                .answerQuestion("What is your name?", "Reuben")
+                .swipeToNextQuestion("What is your age?", true)
+                .answerQuestion("What is your age?", "32")
+                .swipeToEndScreen()
+                .clickSaveAndExit()
+
+                .clickEditSavedForm(1)
+                .clickOnForm("Two Question Required")
+                .clickGoToStart()
+                .answerQuestion("What is your name?", "Another Reuben")
+                .swipeToNextQuestion("What is your age?", true)
+                .longPressOnQuestion("What is your age?", true)
+                .removeResponse()
+
+                .closeSoftKeyboard()
+                .pressBack(new SaveOrIgnoreDialog<>("Two Question Required", new FormEntryPage("Two Question Required")))
+                .clickSaveChanges()
+                .checkIsToastWithMessageDisplayed(R.string.data_saved_error)
+
+                .pressBack(new SaveOrIgnoreDialog<>("Two Question Required", new MainMenuPage()))
+                .clickIgnoreChanges()
+
+                .clickEditSavedForm(1)
+                .clickOnForm("Two Question Required")
+                .assertText("Reuben");
     }
 }
