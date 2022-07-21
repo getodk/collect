4b22c6f55245f46a5b0c272d7bf3cb0ca28b7653
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2022-07-21 09:49:08 +0200

    Updated Dependencies and Versions

--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -6,8 +6,8 @@ object Versions {
     const val android_target_sdk = 31
 
     const val androidx_fragment = "1.4.1"
-    const val okhttp3 = "4.9.3"
-    const val dagger = "2.41"
-    const val kotlin = "1.6.10"
-    const val mockito = "4.5.1"
+    const val okhttp3 = "4.10.0"
+    const val dagger = "2.42"
+    const val kotlin = "1.7.10"
+    const val mockito = "4.6.1"
 }
