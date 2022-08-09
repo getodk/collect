33f4666a1c6fe66863cb9128e136a347c8693bc8
Callum Stott <callum@seadowg.com>
2022-08-09 19:07:08 +0100

    Remove getFormController from guidance tests

--- collect_app/src/androidTest/java/org/odk/collect/android/feature/formentry/GuidanceHintFormTest.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/feature/formentry/GuidanceHintFormTest.java
@@ -6,21 +6,16 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static junit.framework.Assert.assertFalse;
 import static org.hamcrest.CoreMatchers.not;
-
-import android.text.TextUtils;
 
 import androidx.test.espresso.matcher.ViewMatchers;
 
-import org.javarosa.form.api.FormEntryPrompt;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
 import org.odk.collect.android.R;
 import org.odk.collect.android.TestSettingsProvider;
-import org.odk.collect.android.application.Collect;
 import org.odk.collect.android.preferences.GuidanceHint;
 import org.odk.collect.android.support.rules.FormActivityTestRule;
 import org.odk.collect.android.support.rules.TestRuleChain;
@@ -55,13 +50,9 @@ public class GuidanceHintFormTest {
         onView(withId(R.id.menu_goto)).perform(click());
         onView(withId(R.id.jumpBeginningButton)).perform(click());
 
-        FormEntryPrompt prompt = Collect.getInstance().getFormController().getQuestionPrompt();
-        String guidance = prompt.getSpecialFormQuestionText(prompt.getQuestion().getHelpTextID(), "guidance");
-        assertFalse(TextUtils.isEmpty(guidance));
-
         Screengrab.screenshot("guidance_hint");
 
-        onView(withId(R.id.guidance_text_view)).check(matches(withText(guidance)));
+        onView(withId(R.id.guidance_text_view)).check(matches(withText("If the age is less than 18, the remainder of the survey will be hidden.")));
     }
 
     @Test
@@ -71,12 +62,8 @@ public class GuidanceHintFormTest {
         onView(withId(R.id.menu_goto)).perform(click());
         onView(withId(R.id.jumpBeginningButton)).perform(click());
 
-        FormEntryPrompt prompt = Collect.getInstance().getFormController().getQuestionPrompt();
-        String guidance = prompt.getSpecialFormQuestionText(prompt.getQuestion().getHelpTextID(), "guidance");
-        assertFalse(TextUtils.isEmpty(guidance));
-
         onView(withId(R.id.guidance_text_view)).check(matches(not(isDisplayed())));
         onView(withId(R.id.help_icon)).perform(click());
-        onView(withId(R.id.guidance_text_view)).check(matches(withText(guidance)));
+        onView(withId(R.id.guidance_text_view)).check(matches(withText("If the age is less than 18, the remainder of the survey will be hidden.")));
     }
 }
