d27d00800d583de5c7b83152a7ca220b82d7f00b
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2022-08-16 08:57:52 +0200

    Converted CrashReportingTree to kotlin

---
+++ collect_app/src/main/java/org/odk/collect/android/application/initialization/CrashReportingTree.kt
@@ -0,0 +1,16 @@
+package org.odk.collect.android.application.initialization
+
+import android.util.Log
+import org.odk.collect.analytics.Analytics
+import timber.log.Timber
+
+internal class CrashReportingTree(private val analytics: Analytics) : Timber.Tree() {
+
+    override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+        when (priority) {
+            Log.WARN -> analytics.logMessage("W/$tag: $message")
+
+            Log.ERROR -> analytics.logNonFatal(t ?: Throwable("E/$tag: $message"))
+        }
+    }
+}
