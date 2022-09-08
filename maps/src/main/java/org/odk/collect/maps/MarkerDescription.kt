ffba5f3da82eafd77db1aa7fc3b3ed91419b12e1
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2022-09-08 11:27:54 +0200

    Extended MarkerDescription to hold color and symbol

--- maps/src/main/java/org/odk/collect/maps/MarkerDescription.kt
+++ maps/src/main/java/org/odk/collect/maps/MarkerDescription.kt
@@ -4,5 +4,7 @@ data class MarkerDescription(
     val point: MapPoint,
     val isDraggable: Boolean,
     @get:MapFragment.IconAnchor @param:MapFragment.IconAnchor val iconAnchor: String,
-    val iconDrawableId: Int
+    val iconDrawableId: Int,
+    val color: String? = null,
+    val symbol: String? = null
 )
