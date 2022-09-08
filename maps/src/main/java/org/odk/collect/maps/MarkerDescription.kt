538933f213c286961c406139df593e32ea96cd5a
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2022-09-08 11:23:05 +0200

    Converted MarkerDescription to kotlin

---
+++ maps/src/main/java/org/odk/collect/maps/MarkerDescription.kt
@@ -0,0 +1,8 @@
+package org.odk.collect.maps
+
+data class MarkerDescription(
+    val point: MapPoint,
+    val isDraggable: Boolean,
+    @get:MapFragment.IconAnchor @param:MapFragment.IconAnchor val iconAnchor: String,
+    val iconDrawableId: Int
+)
