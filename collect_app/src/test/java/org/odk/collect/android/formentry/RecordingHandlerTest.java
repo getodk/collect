9016bb1476d7345008f9de280451eb4fba011f63
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2022-10-17 21:48:39 +0200

    Added failing tests

--- collect_app/src/test/java/org/odk/collect/android/formentry/RecordingHandlerTest.java
+++ collect_app/src/test/java/org/odk/collect/android/formentry/RecordingHandlerTest.java
@@ -1,21 +1,26 @@
 package org.odk.collect.android.formentry;
 
 
+import static junit.framework.TestCase.assertFalse;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 
+import org.javarosa.core.model.FormDef;
 import org.javarosa.core.model.data.StringData;
 import org.javarosa.core.model.instance.TreeReference;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.odk.collect.android.audio.AudioFileAppender;
 import org.odk.collect.android.javarosawrapper.FormController;
 import org.odk.collect.android.widgets.support.FakeQuestionMediaManager;
@@ -34,6 +39,7 @@ public class RecordingHandlerTest {
 
     private final FakeQuestionMediaManager questionMediaManager = new FakeQuestionMediaManager();
     private final FormController formController = mock(FormController.class);
+    private final FormDef formDef = mock(FormDef.class);
     private final AudioFileAppender amrAppender = mock(AudioFileAppender.class);
     private final AudioFileAppender m4aAppender = mock(AudioFileAppender.class);
 
@@ -42,6 +48,33 @@ public class RecordingHandlerTest {
     @Before
     public void setup() {
         recordingHandler = new RecordingHandler(questionMediaManager, new FakeLifecycleOwner(), mock(AudioRecorder.class), amrAppender, m4aAppender);
+        doReturn(formDef).when(formController).getFormDef();
+    }
+
+    @Test
+    public void whenBackgroundRecordingM4A_andThereIsNoRecordingAlreadySavedForReference_savesNewAnswer() throws Exception {
+        TreeReference treeReference = new TreeReference();
+
+        File newRecording = File.createTempFile("new", ".m4a");
+        RecordingSession recordingSession = new RecordingSession(new HashSet<TreeReference>() {
+            {
+                add(treeReference);
+            }
+        }, newRecording, 0, 0, false);
+
+        recordingHandler.handle(formController, recordingSession, success -> {
+        });
+
+        ArgumentCaptor<StringData> dataAc = ArgumentCaptor.forClass(StringData.class);
+        ArgumentCaptor<TreeReference> refAc = ArgumentCaptor.forClass(TreeReference.class);
+        ArgumentCaptor<Boolean> midSurveyAc = ArgumentCaptor.forClass(Boolean.class);
+
+        verify(formDef).setValue(dataAc.capture(), refAc.capture(), midSurveyAc.capture());
+        assertThat(dataAc.getValue(), is(new StringData(questionMediaManager.getAnswerFile(newRecording.getName()).getName())));
+        assertThat(refAc.getValue(), is(treeReference));
+        assertFalse(midSurveyAc.getValue());
+
+        verifyNoInteractions(m4aAppender);
     }
 
     @Test
@@ -64,10 +97,70 @@ public class RecordingHandlerTest {
         });
 
         verify(m4aAppender).append(existingRecording, questionMediaManager.getAnswerFile(newRecording.getName()));
+        verifyNoInteractions(formDef);
     }
 
     @Test
-    public void whenBackgroundRecordinAMR_andRecordingAlreadySavedForReference_appendsAudioFiles() throws Exception {
+    public void whenBackgroundRecordingM4A_andRecordingAlreadySavedForReferenceButTheAudioFileDoesNotExist_savesNewAnswer() throws Exception {
+        File recording = File.createTempFile("existing", ".m4a");
+        File existingRecording = questionMediaManager.createAnswerFile(recording).getValue().getOrNull();
+        assertThat(existingRecording, not(nullValue()));
+
+        existingRecording.delete();
+
+        TreeReference treeReference = new TreeReference();
+        when(formController.getAnswer(treeReference)).thenReturn(new StringData(existingRecording.getName()));
+
+        File newRecording = File.createTempFile("new", ".m4a");
+        RecordingSession recordingSession = new RecordingSession(new HashSet<TreeReference>() {
+            {
+                add(treeReference);
+            }
+        }, newRecording, 0, 0, false);
+
+        recordingHandler.handle(formController, recordingSession, success -> {
+        });
+
+        ArgumentCaptor<StringData> dataAc = ArgumentCaptor.forClass(StringData.class);
+        ArgumentCaptor<TreeReference> refAc = ArgumentCaptor.forClass(TreeReference.class);
+        ArgumentCaptor<Boolean> midSurveyAc = ArgumentCaptor.forClass(Boolean.class);
+
+        verify(formDef).setValue(dataAc.capture(), refAc.capture(), midSurveyAc.capture());
+        assertThat(dataAc.getValue(), is(new StringData(questionMediaManager.getAnswerFile(newRecording.getName()).getName())));
+        assertThat(refAc.getValue(), is(treeReference));
+        assertFalse(midSurveyAc.getValue());
+
+        verifyNoInteractions(m4aAppender);
+    }
+
+    @Test
+    public void whenBackgroundRecordingAMR_andThereIsNoRecordingAlreadySavedForReference_savesNewAnswer() throws Exception {
+        TreeReference treeReference = new TreeReference();
+
+        File newRecording = File.createTempFile("new", ".amr");
+        RecordingSession recordingSession = new RecordingSession(new HashSet<TreeReference>() {
+            {
+                add(treeReference);
+            }
+        }, newRecording, 0, 0, false);
+
+        recordingHandler.handle(formController, recordingSession, success -> {
+        });
+
+        ArgumentCaptor<StringData> dataAc = ArgumentCaptor.forClass(StringData.class);
+        ArgumentCaptor<TreeReference> refAc = ArgumentCaptor.forClass(TreeReference.class);
+        ArgumentCaptor<Boolean> midSurveyAc = ArgumentCaptor.forClass(Boolean.class);
+
+        verify(formDef).setValue(dataAc.capture(), refAc.capture(), midSurveyAc.capture());
+        assertThat(dataAc.getValue(), is(new StringData(questionMediaManager.getAnswerFile(newRecording.getName()).getName())));
+        assertThat(refAc.getValue(), is(treeReference));
+        assertFalse(midSurveyAc.getValue());
+
+        verifyNoInteractions(amrAppender);
+    }
+
+    @Test
+    public void whenBackgroundRecordingAMR_andRecordingAlreadySavedForReference_appendsAudioFiles() throws Exception {
         File recording = File.createTempFile("existing", ".amr");
         File existingRecording = questionMediaManager.createAnswerFile(recording).getValue().getOrNull();
         assertThat(existingRecording, not(nullValue()));
@@ -86,6 +179,40 @@ public class RecordingHandlerTest {
         });
 
         verify(amrAppender).append(existingRecording, questionMediaManager.getAnswerFile(newRecording.getName()));
+        verifyNoInteractions(formDef);
+    }
+
+    @Test
+    public void whenBackgroundRecordingAMR_andRecordingAlreadySavedForReferenceButTheAudioFileDoesNotExist_savesNewAnswer() throws Exception {
+        File recording = File.createTempFile("existing", ".amr");
+        File existingRecording = questionMediaManager.createAnswerFile(recording).getValue().getOrNull();
+        assertThat(existingRecording, not(nullValue()));
+
+        existingRecording.delete();
+
+        TreeReference treeReference = new TreeReference();
+        when(formController.getAnswer(treeReference)).thenReturn(new StringData(existingRecording.getName()));
+
+        File newRecording = File.createTempFile("new", ".amr");
+        RecordingSession recordingSession = new RecordingSession(new HashSet<TreeReference>() {
+            {
+                add(treeReference);
+            }
+        }, newRecording, 0, 0, false);
+
+        recordingHandler.handle(formController, recordingSession, success -> {
+        });
+
+        ArgumentCaptor<StringData> dataAc = ArgumentCaptor.forClass(StringData.class);
+        ArgumentCaptor<TreeReference> refAc = ArgumentCaptor.forClass(TreeReference.class);
+        ArgumentCaptor<Boolean> midSurveyAc = ArgumentCaptor.forClass(Boolean.class);
+
+        verify(formDef).setValue(dataAc.capture(), refAc.capture(), midSurveyAc.capture());
+        assertThat(dataAc.getValue(), is(new StringData(questionMediaManager.getAnswerFile(newRecording.getName()).getName())));
+        assertThat(refAc.getValue(), is(treeReference));
+        assertFalse(midSurveyAc.getValue());
+
+        verifyNoInteractions(amrAppender);
     }
 
     @Test
