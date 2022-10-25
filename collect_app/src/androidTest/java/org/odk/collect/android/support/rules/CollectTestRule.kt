8834bdfdc6be7e43c94e7aebf9c80c3d7f649542
Callum Stott <callum@seadowg.com>
2022-10-25 15:03:51 +0100

    Use proper launch intent to prevent problems with notification clicks in tests

--- collect_app/src/androidTest/java/org/odk/collect/android/support/rules/CollectTestRule.kt
+++ collect_app/src/androidTest/java/org/odk/collect/android/support/rules/CollectTestRule.kt
@@ -2,9 +2,9 @@ package org.odk.collect.android.support.rules
 
 import android.app.Activity
 import android.app.Instrumentation
+import android.content.Context
 import android.content.Intent
 import androidx.test.core.app.ApplicationProvider
-import org.odk.collect.android.activities.MainMenuActivity
 import org.odk.collect.android.external.AndroidShortcutsActivity
 import org.odk.collect.android.support.pages.FirstLaunchPage
 import org.odk.collect.android.support.pages.MainMenuPage
@@ -14,17 +14,20 @@ import org.odk.collect.androidtest.ActivityScenarioLauncherRule
 import java.util.function.Consumer
 
 class CollectTestRule @JvmOverloads constructor(
-    private val useDemoProject: Boolean = true
+    private val useDemoProject: Boolean = true,
 ) : ActivityScenarioLauncherRule() {
 
     override fun before() {
         super.before()
 
+        val launchIntent = ApplicationProvider.getApplicationContext<Context>().packageManager
+            .getLaunchIntentForPackage("org.odk.collect.android")!!.also {
+                it.addCategory(Intent.CATEGORY_LAUNCHER)
+            }
+
+
         val firstLaunchPage = launch(
-            Intent(
-                ApplicationProvider.getApplicationContext(),
-                MainMenuActivity::class.java
-            ),
+            launchIntent,
             FirstLaunchPage()
         ).assertOnPage()
 
@@ -49,8 +52,12 @@ class CollectTestRule @JvmOverloads constructor(
     }
 
     fun <D : Page<D>> relaunch(destination: D): D {
-        launch(MainMenuActivity::class.java)
-        return destination.assertOnPage()
+        val launchIntent = ApplicationProvider.getApplicationContext<Context>().packageManager
+            .getLaunchIntentForPackage("org.odk.collect.android")!!.also {
+                it.addCategory(Intent.CATEGORY_LAUNCHER)
+            }
+
+        return launch(launchIntent, destination)
     }
 
     fun <T : Page<T>> launch(intent: Intent, destination: T): T {
@@ -61,7 +68,7 @@ class CollectTestRule @JvmOverloads constructor(
     fun <T : Page<T>> launchForResult(
         intent: Intent,
         destination: T,
-        actions: Consumer<T>
+        actions: Consumer<T>,
     ): Instrumentation.ActivityResult {
         val scenario = launchForResult<Activity>(intent)
         destination.assertOnPage()
