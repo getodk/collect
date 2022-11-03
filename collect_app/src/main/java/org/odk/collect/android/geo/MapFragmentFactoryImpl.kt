ec30219b2b4361227d1fb4e03aa3f435e7a0cb14
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2022-11-03 16:53:55 +0100

    Fixed MapFragmentFactoryImpl

--- collect_app/src/main/java/org/odk/collect/android/geo/MapFragmentFactoryImpl.kt
+++ collect_app/src/main/java/org/odk/collect/android/geo/MapFragmentFactoryImpl.kt
@@ -4,17 +4,19 @@ import org.odk.collect.android.application.MapboxClassInstanceCreator
 import org.odk.collect.maps.MapFragment
 import org.odk.collect.maps.MapFragmentFactory
 import org.odk.collect.osmdroid.OsmDroidMapFragment
+import org.odk.collect.settings.SettingsProvider
 import org.odk.collect.settings.keys.ProjectKeys.BASEMAP_SOURCE_CARTO
 import org.odk.collect.settings.keys.ProjectKeys.BASEMAP_SOURCE_MAPBOX
 import org.odk.collect.settings.keys.ProjectKeys.BASEMAP_SOURCE_OSM
 import org.odk.collect.settings.keys.ProjectKeys.BASEMAP_SOURCE_STAMEN
 import org.odk.collect.settings.keys.ProjectKeys.BASEMAP_SOURCE_USGS
 import org.odk.collect.settings.keys.ProjectKeys.KEY_BASEMAP_SOURCE
-import org.odk.collect.shared.settings.Settings
 
-class MapFragmentFactoryImpl(private val settings: Settings) : MapFragmentFactory {
+class MapFragmentFactoryImpl(private val settingsProvider: SettingsProvider) : MapFragmentFactory {
 
     override fun createMapFragment(): MapFragment {
+        val settings = settingsProvider.getUnprotectedSettings()
+
         return when {
             isBasemapOSM(settings.getString(KEY_BASEMAP_SOURCE)) -> OsmDroidMapFragment()
             settings.getString(KEY_BASEMAP_SOURCE) == BASEMAP_SOURCE_MAPBOX -> MapboxClassInstanceCreator.createMapboxMapFragment() ?: GoogleMapFragment()
