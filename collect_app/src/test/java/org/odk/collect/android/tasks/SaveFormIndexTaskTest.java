a5f211ef2eb0b9494a0671480bff249b4e8c4c17
Callum Stott <callum@seadowg.com>
2022-11-11 17:01:23 +0000

    Move save point index test to JVM

--- collect_app/src/test/java/org/odk/collect/android/tasks/SaveFormIndexTaskTest.java
+++ collect_app/src/test/java/org/odk/collect/android/tasks/SaveFormIndexTaskTest.java
@@ -1,44 +1,31 @@
-/*
- * Copyright 2017 Nafundi
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package org.odk.collect.android.instrumented.utilities;
+package org.odk.collect.android.tasks;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
 import org.javarosa.core.model.FormIndex;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.odk.collect.android.javarosawrapper.FormController;
 import org.odk.collect.android.storage.StoragePathProvider;
 import org.odk.collect.android.storage.StorageSubdirectory;
-import org.odk.collect.android.tasks.SaveFormIndexTask;
-import org.odk.collect.android.tasks.SaveFormToDisk;
+import org.odk.collect.android.support.CollectHelpers;
 
 import java.io.File;
 
 // Verify that a FormIndex can be saved to and restored from a file
-@RunWith(MockitoJUnitRunner.class)
-public class FormIndexSavepointTest {
+@RunWith(AndroidJUnit4.class)
+public class SaveFormIndexTaskTest {
 
-    @Mock
-    private FormController formController;
+    @Before
+    public void setup() {
+        CollectHelpers.setupDemoProject(); // Allows using StoragePathProvider
+    }
 
     @Test
     public void saveAndReadFormIndexTest() {
@@ -46,6 +33,7 @@ public class FormIndexSavepointTest {
 
         // for loadFormIndexFromFile
         File instancePath = new File(new StoragePathProvider().getOdkDirPath(StorageSubdirectory.INSTANCES) + File.separator + instanceName);
+        FormController formController = mock(FormController.class);
         when(formController.getInstanceFile()).thenReturn(instancePath);
 
         FormIndex originalFormIndex = FormIndex.createBeginningOfFormIndex();
