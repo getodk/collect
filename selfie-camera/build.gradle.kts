f9442ae8a249ea02a23b0193bea31f7940268273
Callum Stott <callum@seadowg.com>
2022-11-24 14:51:22 +0000

    Spike out replacing Camera2 with CameraX

--- selfie-camera/build.gradle.kts
+++ selfie-camera/build.gradle.kts
@@ -50,4 +50,8 @@ dependencies {
     implementation(project(":permissions"))
 
     implementation(Dependencies.timber)
+    implementation("androidx.camera:camera-core:1.1.0")
+    implementation("androidx.camera:camera-view:1.1.0")
+    implementation("androidx.camera:camera-lifecycle:1.1.0")
+    implementation("androidx.camera:camera-camera2:1.1.0")
 }
---
+++ selfie-camera/src/main/java/org/odk/collect/selfiecamera/CaptureSelfieActivity.kt
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2017 Nafundi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.odk.collect.selfiecamera
+
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import org.odk.collect.androidshared.ui.ToastUtils.showLongToast
+import org.odk.collect.permissions.PermissionsProvider
+import org.odk.collect.shared.injection.ObjectProviderHost
+import org.odk.collect.strings.localization.LocalizedActivity
+import java.io.File
+
+class CaptureSelfieActivity : LocalizedActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val objectProvider = (application as ObjectProviderHost).getObjectProvider()
+        val permissionsProvider = objectProvider.provide(PermissionsProvider::class.java)
+
+        if (!permissionsProvider.isCameraPermissionGranted) {
+            finish()
+            return
+        }
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_capture_selfie)
+
+        val cameraProvider = ProcessCameraProvider.getInstance(this)
+        cameraProvider.addListener(
+            { setupCameraProvider(cameraProvider.get()) },
+            ContextCompat.getMainExecutor(this)
+        )
+
+        showLongToast(this, R.string.take_picture_instruction)
+    }
+
+    private fun setupCameraProvider(cameraProvider: ProcessCameraProvider) {
+        val previewView = findViewById<PreviewView>(R.id.preview)
+
+        val preview = Preview.Builder().build()
+        preview.setSurfaceProvider(previewView.surfaceProvider)
+
+        val imageCapture = ImageCapture.Builder()
+            .setTargetRotation(previewView.display.rotation)
+            .build()
+
+        val outputFile = File(intent.getStringExtra(EXTRA_TMP_IMAGE_PATH))
+        val outputFileOptions = ImageCapture.OutputFileOptions.Builder(outputFile).build()
+        previewView.setOnClickListener {
+            imageCapture.takePicture(
+                outputFileOptions,
+                ContextCompat.getMainExecutor(this),
+                object : ImageCapture.OnImageSavedCallback {
+                    override fun onError(error: ImageCaptureException) {
+                        // Ignored
+                    }
+
+                    override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                        setResult(RESULT_OK)
+                        finish()
+                    }
+                }
+            )
+        }
+
+        val cameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
+            .build()
+
+        cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+    }
+
+    companion object {
+        const val EXTRA_TMP_IMAGE_PATH = "tmpImagePath"
+    }
+}
