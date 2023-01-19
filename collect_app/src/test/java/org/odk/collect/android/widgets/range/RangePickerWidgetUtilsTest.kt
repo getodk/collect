a2607fbe9a4b84e9f09241274d5277cb3faf2003
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-01-19 18:33:48 +0100

    Added tests

--- collect_app/src/test/java/org/odk/collect/android/widgets/range/RangePickerWidgetUtilsTest.kt
+++ collect_app/src/test/java/org/odk/collect/android/widgets/range/RangePickerWidgetUtilsTest.kt
@@ -2,7 +2,11 @@ package org.odk.collect.android.widgets.range
 
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
+import org.javarosa.core.model.data.IntegerData
+import org.javarosa.form.api.FormEntryPrompt
 import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
 import java.math.BigDecimal
 
 class RangePickerWidgetUtilsTest {
@@ -76,6 +80,34 @@ class RangePickerWidgetUtilsTest {
         assertThat(
             RangePickerWidgetUtils.getNumbersFromRangeAsc(rangeStart, rangeStep, rangeEnd, true),
             equalTo(arrayOf("-5", "-3", "-1", "1", "3", "5"))
+        )
+    }
+
+    @Test
+    fun `getProgressFromPrompt() returns the position of the value in the list`() {
+        val prompt = mock<FormEntryPrompt>().apply {
+            whenever(this.answerValue).thenReturn(IntegerData(2))
+        }
+
+        val listOfValuesAsc = arrayOf("1", "2", "3", "4", "5")
+
+        assertThat(
+            RangePickerWidgetUtils.getProgressFromPrompt(prompt, listOfValuesAsc),
+            equalTo(1)
+        )
+    }
+
+    @Test
+    fun `getProgressFromPrompt()  returns 0 if the current value does not exist in the list of values`() {
+        val prompt = mock<FormEntryPrompt>().apply {
+            whenever(this.answerValue).thenReturn(IntegerData(10))
+        }
+
+        val listOfValuesAsc = arrayOf("1", "2", "3", "4", "5")
+
+        assertThat(
+            RangePickerWidgetUtils.getProgressFromPrompt(prompt, listOfValuesAsc),
+            equalTo(0)
         )
     }
 }
