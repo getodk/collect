7a1270516a993b41a3a8fc8a66d464c835fb5e84
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-01-30 23:34:39 +0100

    Added comments in the DatabaseConstants class

--- collect_app/src/main/java/org/odk/collect/android/database/DatabaseConstants.java
+++ collect_app/src/main/java/org/odk/collect/android/database/DatabaseConstants.java
@@ -4,10 +4,12 @@ public final class DatabaseConstants {
 
     public static final String FORMS_DATABASE_NAME = "forms.db";
     public static final String FORMS_TABLE_NAME = "forms";
+    // Please always test upgrades manually when you change this value
     public static final int FORMS_DATABASE_VERSION = 12;
 
     public static final String INSTANCES_DATABASE_NAME = "instances.db";
     public static final String INSTANCES_TABLE_NAME = "instances";
+    // Please always test upgrades manually when you change this value
     public static final int INSTANCES_DATABASE_VERSION = 6;
 
     private DatabaseConstants() {
