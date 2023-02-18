705ad0526c4b4979c5816b6c390b4fbfa6bae218
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-02-18 11:49:39 +0100

    Removed unused consts from ApplicationConstants

--- collect_app/src/main/java/org/odk/collect/android/utilities/ApplicationConstants.java
+++ collect_app/src/main/java/org/odk/collect/android/utilities/ApplicationConstants.java
@@ -20,9 +20,6 @@ import com.google.zxing.integration.android.IntentIntegrator;
 
 public class ApplicationConstants {
 
-    // based on http://www.sqlite.org/limits.html
-    public static final int SQLITE_MAX_VARIABLE_NUMBER = 999;
-
     private ApplicationConstants() {
 
     }
@@ -30,10 +27,8 @@ public class ApplicationConstants {
     public abstract static class BundleKeys {
         public static final String FORM_MODE = "formMode";
         public static final String SUCCESS_KEY = "SUCCESSFUL";
-        public static final String ERROR_REASON = "ERROR_MSG";
         public static final String FORM_IDS = "FORM_IDS";
         public static final String MESSAGE = "MESSAGE";
-        public static final String FORM_ID = "FORM_ID";
         public static final String USERNAME = "USERNAME";
         public static final String PASSWORD = "PASSWORD";
         public static final String URL = "URL";
