be4aa4937524cbe28337af134d3a3fe75cc8a9cd
Callum Stott <callum@seadowg.com>
2023-03-16 10:23:11 +0000

    Add audit assertions for save point cases

--- collect_app/src/androidTest/java/org/odk/collect/android/feature/formentry/SavePointTest.kt
+++ collect_app/src/androidTest/java/org/odk/collect/android/feature/formentry/SavePointTest.kt
@@ -1,18 +1,23 @@
 package org.odk.collect.android.feature.formentry
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
 import org.junit.runner.RunWith
+import org.odk.collect.android.support.StorageUtils
+import org.odk.collect.android.support.pages.AppClosedPage
 import org.odk.collect.android.support.pages.FormEntryPage
+import org.odk.collect.android.support.pages.SaveOrIgnoreDialog
 import org.odk.collect.android.support.rules.FormActivityTestRule
 import org.odk.collect.android.support.rules.TestRuleChain
 
 @RunWith(AndroidJUnit4::class)
 class SavePointTest {
 
-    private val rule = FormActivityTestRule("two-question.xml", "Two Question")
+    private val rule = FormActivityTestRule("two-question-audit.xml", "Two Question")
 
     @get:Rule
     val ruleChain: RuleChain = TestRuleChain.chain().around(rule)
@@ -30,6 +35,21 @@ class SavePointTest {
             .assertTextDoesNotExist("46")
             .pressBack(FormEntryPage("Two Question"))
             .assertQuestion("What is your name?")
+            .pressBack(SaveOrIgnoreDialog("Two Question", AppClosedPage()))
+            .clickSaveChanges()
+
+        val auditLog = StorageUtils.getAuditLogForFirstInstance()
+        assertThat(auditLog.size, equalTo(7))
+
+        assertThat(auditLog[0].get("event"), equalTo("form start"))
+        assertThat(auditLog[1].get("event"), equalTo("question"))
+        // Second question event not logged - possibly a problem
+
+        assertThat(auditLog[2].get("event"), equalTo("form resume"))
+        assertThat(auditLog[3].get("event"), equalTo("jump"))
+        assertThat(auditLog[4].get("event"), equalTo("question"))
+        assertThat(auditLog[5].get("event"), equalTo("form save"))
+        assertThat(auditLog[6].get("event"), equalTo("form exit"))
     }
 
     @Test
@@ -42,6 +62,20 @@ class SavePointTest {
             .assertText("Alexei")
             .pressBack(FormEntryPage("Two Question"))
             .assertQuestion("What is your name?")
+            .pressBack(SaveOrIgnoreDialog("Two Question", AppClosedPage()))
+            .clickSaveChanges()
+
+        val auditLog = StorageUtils.getAuditLogForFirstInstance()
+        assertThat(auditLog.size, equalTo(6))
+
+        assertThat(auditLog[0].get("event"), equalTo("form start"))
+        // Question event not logged - possibly a problem
+
+        assertThat(auditLog[1].get("event"), equalTo("form resume"))
+        assertThat(auditLog[2].get("event"), equalTo("jump"))
+        assertThat(auditLog[3].get("event"), equalTo("question"))
+        assertThat(auditLog[4].get("event"), equalTo("form save"))
+        assertThat(auditLog[5].get("event"), equalTo("form exit"))
     }
 
     /**
