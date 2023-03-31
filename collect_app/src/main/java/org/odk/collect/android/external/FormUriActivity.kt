5f570206aac614a811fdc0c5d317dd99edba3011
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-03-31 18:31:06 +0200

    Check if form exists before starting FormEntryActivity

--- collect_app/src/main/java/org/odk/collect/android/external/FormUriActivity.kt
+++ collect_app/src/main/java/org/odk/collect/android/external/FormUriActivity.kt
@@ -9,6 +9,9 @@ import org.odk.collect.android.R
 import org.odk.collect.android.activities.FormEntryActivity
 import org.odk.collect.android.injection.DaggerUtils
 import org.odk.collect.android.projects.CurrentProjectProvider
+import org.odk.collect.android.utilities.ContentUriHelper
+import org.odk.collect.android.utilities.FormsRepositoryProvider
+import org.odk.collect.android.utilities.InstancesRepositoryProvider
 import org.odk.collect.projects.ProjectsRepository
 import javax.inject.Inject
 
@@ -19,6 +22,12 @@ class FormUriActivity : ComponentActivity() {
 
     @Inject
     lateinit var projectsRepository: ProjectsRepository
+
+    @Inject
+    lateinit var formsRepositoryProvider: FormsRepositoryProvider
+
+    @Inject
+    lateinit var instanceRepositoryProvider: InstancesRepositoryProvider
 
     private var formFillingAlreadyStarted = false
 
@@ -36,6 +45,7 @@ class FormUriActivity : ComponentActivity() {
             !assertProjectListNotEmpty() -> Unit
             !assertCurrentProjectUsed() -> Unit
             !assertValidUri() -> Unit
+            !assertFormExists() -> Unit
             !assertFormFillingNotAlreadyStarted(savedInstanceState) -> Unit
             else -> startForm()
         }
@@ -86,6 +96,28 @@ class FormUriActivity : ComponentActivity() {
         return if (!isUriValid) {
             MaterialAlertDialogBuilder(this)
                 .setMessage(R.string.unrecognized_uri)
+                .setPositiveButton(R.string.ok) { _, _ -> finish() }
+                .create()
+                .show()
+            false
+        } else {
+            true
+        }
+    }
+
+    private fun assertFormExists(): Boolean {
+        val uri = intent.data!!
+        val uriMimeType = contentResolver.getType(uri)
+
+        val doesFormExist = if (uriMimeType == FormsContract.CONTENT_ITEM_TYPE) {
+            formsRepositoryProvider.get().get(ContentUriHelper.getIdFromUri(uri)) != null
+        } else {
+            instanceRepositoryProvider.get().get(ContentUriHelper.getIdFromUri(uri)) != null
+        }
+
+        return if (!doesFormExist) {
+            MaterialAlertDialogBuilder(this)
+                .setMessage(R.string.bad_uri)
                 .setPositiveButton(R.string.ok) { _, _ -> finish() }
                 .create()
                 .show()
--- collect_app/src/test/java/org/odk/collect/android/external/FormUriActivityTest.kt
+++ collect_app/src/test/java/org/odk/collect/android/external/FormUriActivityTest.kt
@@ -2,6 +2,7 @@ package org.odk.collect.android.external
 
 import android.app.Activity
 import android.app.Application
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.test.core.app.ApplicationProvider
@@ -25,19 +26,26 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.kotlin.mock
-import org.mockito.kotlin.times
 import org.mockito.kotlin.whenever
 import org.odk.collect.android.R
 import org.odk.collect.android.activities.FormEntryActivity
 import org.odk.collect.android.injection.config.AppDependencyModule
 import org.odk.collect.android.projects.CurrentProjectProvider
+import org.odk.collect.android.storage.StoragePathProvider
 import org.odk.collect.android.support.CollectHelpers
+import org.odk.collect.android.utilities.FormsRepositoryProvider
+import org.odk.collect.android.utilities.InstancesRepositoryProvider
 import org.odk.collect.androidtest.ActivityScenarioLauncherRule
 import org.odk.collect.androidtest.RecordedIntentsRule
+import org.odk.collect.forms.instances.Instance
+import org.odk.collect.formstest.FormUtils
+import org.odk.collect.formstest.InMemFormsRepository
+import org.odk.collect.formstest.InMemInstancesRepository
 import org.odk.collect.projects.InMemProjectsRepository
 import org.odk.collect.projects.Project
 import org.odk.collect.projects.ProjectsRepository
 import org.odk.collect.settings.SettingsProvider
+import org.odk.collect.shared.TempFiles
 import org.odk.collect.shared.strings.UUIDGenerator
 
 @RunWith(AndroidJUnit4::class)
@@ -47,6 +55,14 @@ class FormUriActivityTest {
     private val context = ApplicationProvider.getApplicationContext<Application>()
     private val firstProject = Project.DEMO_PROJECT
     private val secondProject = Project.Saved("123", "Second project", "S", "#cccccc")
+    private val formsRepository = InMemFormsRepository()
+    private val instancesRepository = InMemInstancesRepository()
+    private val formsRepositoryProvider = mock<FormsRepositoryProvider>().apply {
+        whenever(get()).thenReturn(formsRepository)
+    }
+    private val instancesRepositoryProvider = mock<InstancesRepositoryProvider>().apply {
+        whenever(get()).thenReturn(instancesRepository)
+    }
 
     @get:Rule
     val activityRule = RecordedIntentsRule()
@@ -57,6 +73,18 @@ class FormUriActivityTest {
     @Before
     fun setup() {
         projectsRepository = InMemProjectsRepository()
+
+        formsRepository.save(
+            FormUtils.buildForm("1", "1", TempFiles.createTempDir().absolutePath).build()
+        )
+
+        instancesRepository.save(
+            Instance.Builder()
+                .formId("1")
+                .formVersion("1")
+                .status(Instance.STATUS_INCOMPLETE)
+                .build()
+        )
 
         CollectHelpers.overrideAppDependencyModule(object : AppDependencyModule() {
             override fun providesProjectsRepository(
@@ -73,6 +101,17 @@ class FormUriActivityTest {
             ): CurrentProjectProvider {
                 return currentProjectProvider
             }
+
+            override fun providesFormsRepositoryProvider(application: Application?): FormsRepositoryProvider {
+                return formsRepositoryProvider
+            }
+
+            override fun providesInstancesRepositoryProvider(
+                context: Context?,
+                storagePathProvider: StoragePathProvider?
+            ): InstancesRepositoryProvider {
+                return instancesRepositoryProvider
+            }
         })
     }
 
@@ -87,10 +126,10 @@ class FormUriActivityTest {
     }
 
     @Test
-    fun `When there is project id specified in uri and it does not match current project id then display alert dialog`() {
+    fun `When there is project id specified in uri but it does not match current project id then display alert dialog`() {
         saveTestProjects()
 
-        val scenario = launcherRule.launchForResult<FormUriActivity>(getIntent(secondProject.uuid))
+        val scenario = launcherRule.launchForResult<FormUriActivity>(getBlankFormIntent(secondProject.uuid))
 
         onView(withText(R.string.wrong_project_selected_for_form)).inRoot(isDialog())
             .check(matches(isDisplayed()))
@@ -100,21 +139,85 @@ class FormUriActivityTest {
     }
 
     @Test
-    fun `When there is project id specified in uri and it matches current project id then start form filling`() {
+    fun `When there is no project id specified in uri and first available project id does not match current project id then display alert dialog`() {
         saveTestProjects()
 
-        launcherRule.launch<FormUriActivity>(getIntent(firstProject.uuid))
+        whenever(currentProjectProvider.getCurrentProject()).thenReturn(secondProject)
 
-        Intents.intended(hasComponent(FormEntryActivity::class.java.name))
-        Intents.intended(hasData(FormsContract.getUri(firstProject.uuid, 1)))
-        Intents.intended(hasExtra("KEY_1", "Text"))
+        val scenario = launcherRule.launchForResult<FormUriActivity>(getBlankFormIntent())
+
+        onView(withText(R.string.wrong_project_selected_for_form)).inRoot(isDialog())
+            .check(matches(isDisplayed()))
+        onView(withId(android.R.id.button1)).perform(click())
+
+        assertThat(scenario.result.resultCode, `is`(Activity.RESULT_CANCELED))
+    }
+
+    @Test
+    fun `When uri is null then display alert dialog`() {
+        saveTestProjects()
+
+        val scenario = launcherRule.launchForResult<FormUriActivity>(
+            getBlankFormIntent().apply {
+                data = null
+            }
+        )
+
+        onView(withText(R.string.unrecognized_uri)).inRoot(isDialog())
+            .check(matches(isDisplayed()))
+        onView(withId(android.R.id.button1)).perform(click())
+
+        assertThat(scenario.result.resultCode, `is`(Activity.RESULT_CANCELED))
+    }
+
+    @Test
+    fun `When uri is invalid then display alert dialog`() {
+        saveTestProjects()
+
+        val scenario = launcherRule.launchForResult<FormUriActivity>(
+            getBlankFormIntent().apply {
+                data = Uri.parse("blah")
+            }
+        )
+
+        onView(withText(R.string.unrecognized_uri)).inRoot(isDialog())
+            .check(matches(isDisplayed()))
+        onView(withId(android.R.id.button1)).perform(click())
+
+        assertThat(scenario.result.resultCode, `is`(Activity.RESULT_CANCELED))
+    }
+
+    @Test
+    fun `When uri represents a blank form that does not exist then display alert dialog`() {
+        saveTestProjects()
+
+        val scenario = launcherRule.launchForResult<FormUriActivity>(getBlankFormIntent(firstProject.uuid, 2))
+
+        onView(withText(R.string.bad_uri)).inRoot(isDialog())
+            .check(matches(isDisplayed()))
+        onView(withId(android.R.id.button1)).perform(click())
+
+        assertThat(scenario.result.resultCode, `is`(Activity.RESULT_CANCELED))
+    }
+
+    @Test
+    fun `When uri represents a saved form that does not exist then display alert dialog`() {
+        saveTestProjects()
+
+        val scenario = launcherRule.launchForResult<FormUriActivity>(getSavedIntent(firstProject.uuid, 2))
+
+        onView(withText(R.string.bad_uri)).inRoot(isDialog())
+            .check(matches(isDisplayed()))
+        onView(withId(android.R.id.button1)).perform(click())
+
+        assertThat(scenario.result.resultCode, `is`(Activity.RESULT_CANCELED))
     }
 
     @Test
     fun `Form filling should not be started again after recreating the activity or getting back to it`() {
         saveTestProjects()
 
-        val scenario = launcherRule.launch<FormUriActivity>(getIntent(firstProject.uuid))
+        val scenario = launcherRule.launch<FormUriActivity>(getBlankFormIntent(firstProject.uuid))
 
         scenario.recreate()
 
@@ -124,25 +227,32 @@ class FormUriActivityTest {
     }
 
     @Test
-    fun `When there is no project id specified in uri and first available project id does not match current project id then display alert dialog`() {
+    fun `When there is project id specified in uri that represents a blank form and it matches current project id then start form filling`() {
         saveTestProjects()
 
-        whenever(currentProjectProvider.getCurrentProject()).thenReturn(secondProject)
+        launcherRule.launch<FormUriActivity>(getBlankFormIntent(firstProject.uuid))
 
-        val scenario = launcherRule.launchForResult<FormUriActivity>(getIntent())
-
-        onView(withText(R.string.wrong_project_selected_for_form)).inRoot(isDialog())
-            .check(matches(isDisplayed()))
-        onView(withId(android.R.id.button1)).perform(click())
-
-        assertThat(scenario.result.resultCode, `is`(Activity.RESULT_CANCELED))
+        Intents.intended(hasComponent(FormEntryActivity::class.java.name))
+        Intents.intended(hasData(FormsContract.getUri(firstProject.uuid, 1)))
+        Intents.intended(hasExtra("KEY_1", "Text"))
     }
 
     @Test
-    fun `When there is no project id specified in uri and first available project id matches current project id then start form filling`() {
+    fun `When there is project id specified in uri that represents a saved form and it matches current project id then start form filling`() {
         saveTestProjects()
 
-        launcherRule.launch<FormUriActivity>(getIntent())
+        launcherRule.launch<FormUriActivity>(getSavedIntent(firstProject.uuid))
+
+        Intents.intended(hasComponent(FormEntryActivity::class.java.name))
+        Intents.intended(hasData(InstancesContract.getUri(firstProject.uuid, 1)))
+        Intents.intended(hasExtra("KEY_1", "Text"))
+    }
+
+    @Test
+    fun `When there is no project id specified in uri that represents a blank form and first available project id matches current project id then start form filling`() {
+        saveTestProjects()
+
+        launcherRule.launch<FormUriActivity>(getBlankFormIntent())
 
         Intents.intended(hasComponent(FormEntryActivity::class.java.name))
         val uri = FormsContract.getUri("", 1)
@@ -160,44 +270,31 @@ class FormUriActivityTest {
     }
 
     @Test
-    fun `When uri is null then display alert dialog`() {
+    fun `When there is no project id specified in uri that represents a saved form and first available project id matches current project id then start form filling`() {
         saveTestProjects()
 
-        val scenario = launcherRule.launchForResult<FormUriActivity>(
-            getIntent().apply {
-                data = null
-            }
+        launcherRule.launch<FormUriActivity>(getSavedIntent())
+
+        Intents.intended(hasComponent(FormEntryActivity::class.java.name))
+        val uri = InstancesContract.getUri("", 1)
+        Intents.intended(
+            hasData(
+                Uri.Builder()
+                    .scheme(uri.scheme)
+                    .authority(uri.authority)
+                    .path(uri.path)
+                    .query(null)
+                    .build()
+            )
         )
-
-        onView(withText(R.string.unrecognized_uri)).inRoot(isDialog())
-            .check(matches(isDisplayed()))
-        onView(withId(android.R.id.button1)).perform(click())
-
-        assertThat(scenario.result.resultCode, `is`(Activity.RESULT_CANCELED))
-    }
-
-    @Test
-    fun `When uri is invalid then display alert dialog`() {
-        saveTestProjects()
-
-        val scenario = launcherRule.launchForResult<FormUriActivity>(
-            getIntent().apply {
-                data = Uri.parse("blah")
-            }
-        )
-
-        onView(withText(R.string.unrecognized_uri)).inRoot(isDialog())
-            .check(matches(isDisplayed()))
-        onView(withId(android.R.id.button1)).perform(click())
-
-        assertThat(scenario.result.resultCode, `is`(Activity.RESULT_CANCELED))
+        Intents.intended(hasExtra("KEY_1", "Text"))
     }
 
     // TODO: Replace the explicit FormUriActivity intent with an implicit one Intent.ACTION_EDIT once it's possible https://github.com/android/android-test/issues/496
-    private fun getIntent(projectId: String? = null) =
+    private fun getBlankFormIntent(projectId: String? = null, formDbId: Long = 1) =
         Intent(context, FormUriActivity::class.java).apply {
             data = if (projectId == null) {
-                val uri = FormsContract.getUri("", 1)
+                val uri = FormsContract.getUri("", formDbId)
                 Uri.Builder()
                     .scheme(uri.scheme)
                     .authority(uri.authority)
@@ -205,7 +302,23 @@ class FormUriActivityTest {
                     .query(null)
                     .build()
             } else {
-                FormsContract.getUri(projectId, 1)
+                FormsContract.getUri(projectId, formDbId)
+            }
+            putExtra("KEY_1", "Text")
+        }
+
+    private fun getSavedIntent(projectId: String? = null, formDbId: Long = 1) =
+        Intent(context, FormUriActivity::class.java).apply {
+            data = if (projectId == null) {
+                val uri = InstancesContract.getUri("", formDbId)
+                Uri.Builder()
+                    .scheme(uri.scheme)
+                    .authority(uri.authority)
+                    .path(uri.path)
+                    .query(null)
+                    .build()
+            } else {
+                InstancesContract.getUri(projectId, formDbId)
             }
             putExtra("KEY_1", "Text")
         }
