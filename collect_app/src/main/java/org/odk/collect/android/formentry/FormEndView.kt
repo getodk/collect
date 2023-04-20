85caede7f844cfc43e5d765d422d7bf43c8e9305
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-04-20 16:19:19 +0200

    Converted FormEndView to koltin

---
+++ collect_app/src/main/java/org/odk/collect/android/formentry/FormEndView.kt
@@ -0,0 +1,35 @@
+package org.odk.collect.android.formentry
+
+import android.content.Context
+import android.view.LayoutInflater
+import androidx.core.widget.NestedScrollView
+import org.odk.collect.android.R
+import org.odk.collect.android.databinding.FormEntryEndBinding
+import org.odk.collect.android.listeners.SwipeHandler
+
+class FormEndView(
+    context: Context,
+    formTitle: String,
+    private val listener: Listener
+) : SwipeHandler.View(context) {
+
+    private val binding = FormEntryEndBinding.inflate(LayoutInflater.from(context), this, true)
+
+    init {
+        binding.description.text = context.getString(R.string.save_enter_data_description, formTitle)
+
+        binding.finalize.setOnClickListener {
+            listener.onSaveClicked(true)
+        }
+    }
+
+    override fun shouldSuppressFlingGesture() = false
+
+    override fun getVerticalScrollView(): NestedScrollView? {
+        return findViewById(R.id.scroll_view)
+    }
+
+    interface Listener {
+        fun onSaveClicked(markAsFinalized: Boolean)
+    }
+}
