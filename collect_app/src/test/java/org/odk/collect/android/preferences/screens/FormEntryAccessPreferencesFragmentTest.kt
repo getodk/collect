6465ce474639d706b5cf2fade2c26115916fed1b
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-04-21 11:13:14 +0200

    Added tests to make sure both 'Save as draft' and 'Finalize' buttons can't be disabled at the same time

---
+++ collect_app/src/test/java/org/odk/collect/android/preferences/screens/FormEntryAccessPreferencesFragmentTest.kt
@@ -0,0 +1,112 @@
+package org.odk.collect.android.preferences.screens
+
+import androidx.preference.CheckBoxPreference
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.odk.collect.android.TestSettingsProvider
+import org.odk.collect.android.support.CollectHelpers
+import org.odk.collect.fragmentstest.FragmentScenarioLauncherRule
+import org.odk.collect.settings.keys.ProtectedProjectKeys
+import org.odk.collect.shared.settings.Settings
+
+@RunWith(AndroidJUnit4::class)
+class FormEntryAccessPreferencesFragmentTest {
+
+    private lateinit var adminSettings: Settings
+
+    @get:Rule
+    val launcherRule = FragmentScenarioLauncherRule()
+
+    @Before
+    fun setup() {
+        CollectHelpers.setupDemoProject()
+        adminSettings = TestSettingsProvider.getProtectedSettings()
+    }
+
+    @Test
+    fun `when the 'Save as draft' option is unchecked, the 'Finalize' option can't be changed`() {
+        adminSettings.save(ProtectedProjectKeys.KEY_SAVE_AS_DRAFT, false)
+        adminSettings.save(ProtectedProjectKeys.KEY_FINALIZE, true)
+
+        val scenario = launcherRule.launch(FormEntryAccessPreferencesFragment::class.java)
+        scenario.onFragment { fragment: FormEntryAccessPreferencesFragment ->
+            assertThat(
+                fragment.findPreference<CheckBoxPreference>(ProtectedProjectKeys.KEY_SAVE_AS_DRAFT)!!.isEnabled,
+                equalTo(true)
+            )
+
+            assertThat(
+                fragment.findPreference<CheckBoxPreference>(ProtectedProjectKeys.KEY_FINALIZE)!!.isEnabled,
+                equalTo(false)
+            )
+        }
+    }
+
+    @Test
+    fun `when the 'Finalize' option is unchecked, the 'Save as draft' option can't be changed`() {
+        adminSettings.save(ProtectedProjectKeys.KEY_SAVE_AS_DRAFT, true)
+        adminSettings.save(ProtectedProjectKeys.KEY_FINALIZE, false)
+
+        val scenario = launcherRule.launch(FormEntryAccessPreferencesFragment::class.java)
+        scenario.onFragment { fragment: FormEntryAccessPreferencesFragment ->
+            assertThat(
+                fragment.findPreference<CheckBoxPreference>(ProtectedProjectKeys.KEY_SAVE_AS_DRAFT)!!.isEnabled,
+                equalTo(false)
+            )
+
+            assertThat(
+                fragment.findPreference<CheckBoxPreference>(ProtectedProjectKeys.KEY_FINALIZE)!!.isEnabled,
+                equalTo(true)
+            )
+        }
+    }
+
+    @Test
+    fun `when the user unchecks the 'Save as draft' option, the 'Finalize' option becomes disabled`() {
+        val scenario = launcherRule.launch(FormEntryAccessPreferencesFragment::class.java)
+        scenario.onFragment { fragment: FormEntryAccessPreferencesFragment ->
+            assertThat(
+                fragment.findPreference<CheckBoxPreference>(ProtectedProjectKeys.KEY_FINALIZE)!!.isEnabled,
+                equalTo(true)
+            )
+
+            fragment.findPreference<CheckBoxPreference>(ProtectedProjectKeys.KEY_SAVE_AS_DRAFT)!!.performClick()
+
+            assertThat(
+                fragment.findPreference<CheckBoxPreference>(ProtectedProjectKeys.KEY_FINALIZE)!!.isEnabled,
+                equalTo(false)
+            )
+            assertThat(
+                fragment.findPreference<CheckBoxPreference>(ProtectedProjectKeys.KEY_FINALIZE)!!.isChecked,
+                equalTo(true)
+            )
+        }
+    }
+
+    @Test
+    fun `when the user unchecks the 'Finalize' option, the 'Save as draft' option becomes disabled`() {
+        val scenario = launcherRule.launch(FormEntryAccessPreferencesFragment::class.java)
+        scenario.onFragment { fragment: FormEntryAccessPreferencesFragment ->
+            assertThat(
+                fragment.findPreference<CheckBoxPreference>(ProtectedProjectKeys.KEY_SAVE_AS_DRAFT)!!.isEnabled,
+                equalTo(true)
+            )
+
+            fragment.findPreference<CheckBoxPreference>(ProtectedProjectKeys.KEY_FINALIZE)!!.performClick()
+
+            assertThat(
+                fragment.findPreference<CheckBoxPreference>(ProtectedProjectKeys.KEY_SAVE_AS_DRAFT)!!.isEnabled,
+                equalTo(false)
+            )
+            assertThat(
+                fragment.findPreference<CheckBoxPreference>(ProtectedProjectKeys.KEY_SAVE_AS_DRAFT)!!.isChecked,
+                equalTo(true)
+            )
+        }
+    }
+}
