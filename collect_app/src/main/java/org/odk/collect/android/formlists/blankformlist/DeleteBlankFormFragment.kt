2dbe4dd0867328925000a5661809504dcdf0fffc
Callum Stott <callum@seadowg.com>
2023-04-26 10:38:56 +0100

    Remove unused import

--- collect_app/src/main/java/org/odk/collect/android/formlists/blankformlist/DeleteBlankFormFragment.kt
+++ collect_app/src/main/java/org/odk/collect/android/formlists/blankformlist/DeleteBlankFormFragment.kt
@@ -1,7 +1,6 @@
 package org.odk.collect.android.formlists.blankformlist
 
 import android.content.Context
-import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
