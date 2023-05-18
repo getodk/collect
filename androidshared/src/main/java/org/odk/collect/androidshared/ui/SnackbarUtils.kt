1a043162a764d5e039d37f7e59ed781436d9f42c
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-05-18 18:32:25 +0200

    Improved SnackbarUtils to allow setting actions

--- androidshared/src/main/java/org/odk/collect/androidshared/ui/SnackbarUtils.kt
+++ androidshared/src/main/java/org/odk/collect/androidshared/ui/SnackbarUtils.kt
@@ -28,30 +28,50 @@ import org.odk.collect.androidshared.R
 object SnackbarUtils {
     @JvmStatic
     @JvmOverloads
-    fun showShortSnackbar(parentView: View, message: String, anchorView: View? = null, displayDismissButton: Boolean = false) {
-        showSnackbar(parentView, message, 3500, anchorView, displayDismissButton)
+    fun showShortSnackbar(
+        parentView: View,
+        message: String,
+        anchorView: View? = null,
+        action: Action? = null,
+        displayDismissButton: Boolean = false
+    ) {
+        return showSnackbar(parentView, message, 3500, anchorView, action, displayDismissButton)
     }
 
     @JvmStatic
     @JvmOverloads
-    fun showLongSnackbar(parentView: View, message: String, anchorView: View? = null, displayDismissButton: Boolean = false) {
-        showSnackbar(parentView, message, 5500, anchorView, displayDismissButton)
+    fun showLongSnackbar(
+        parentView: View,
+        message: String,
+        anchorView: View? = null,
+        action: Action? = null,
+        displayDismissButton: Boolean = false
+    ) {
+        return showSnackbar(parentView, message, 5500, anchorView, action, displayDismissButton)
     }
 
     /**
      * Displays snackbar with {@param message} and multi-line message enabled.
      *
-     * @param parentView    The view to find a parent from.
-     * @param anchorView    The view this snackbar should be anchored above.
-     * @param message       The text to show.  Can be formatted text.
+     * @param parentView            The view to find a parent from.
+     * @param anchorView            The view this snackbar should be anchored above.
+     * @param message               The text to show. Can be formatted text.
+     * @param displayDismissButton  True if the dismiss button should be displayed, false otherwise.
      */
-    private fun showSnackbar(parentView: View, message: String, duration: Int, anchorView: View?, displayDismissButton: Boolean) {
+    private fun showSnackbar(
+        parentView: View,
+        message: String,
+        duration: Int,
+        anchorView: View?,
+        action: Action? = null,
+        displayDismissButton: Boolean
+    ) {
         if (message.isBlank()) {
             return
         }
 
         Snackbar.make(parentView, message.trim(), duration).apply {
-            val textView = this.view.findViewById<TextView>(R.id.snackbar_text)
+            val textView = view.findViewById<TextView>(R.id.snackbar_text)
             textView.isSingleLine = false
 
             if (anchorView?.visibility != View.GONE) {
@@ -75,6 +95,15 @@ object SnackbarUtils {
                     (it.parent as ViewGroup).addView(dismissButton, params)
                 }
             }
+
+            if (action != null) {
+                setAction(action.text, action.listener)
+            }
         }.show()
     }
+
+    data class Action(
+        val text: String,
+        val listener: View.OnClickListener
+    )
 }
