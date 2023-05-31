e55ca237c1687360c5744d6ef91b4a81427ce99b
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-05-31 13:22:53 +0200

    Test

---
+++ collect_app/src/test/java/org/odk/collect/android/KotlinSortingTest.kt
@@ -0,0 +1,47 @@
+package org.odk.collect.android
+
+import org.junit.Test
+
+class KotlinSortingTest {
+
+    @Test
+    fun `test sorting objects by descending`() {
+        val adam = Person("Adam", 1)
+        val mark = Person("Mark", 2)
+
+        val items = listOf(adam, mark)
+
+        for (i in 1..100) {
+            val sortedItems = items.sortedByDescending {
+                it.age
+            }
+
+            if (sortedItems[0] != mark) {
+                throw Exception("Sorting failed in loop number $i")
+            }
+        }
+    }
+
+    @Test
+    fun `test sorting objects by ascending`() {
+        val adam = Person("Adam", 1)
+        val mark = Person("Mark", 2)
+
+        val items = listOf(mark, adam)
+
+        for (i in 1..100) {
+            val sortedItems = items.sortedBy {
+                it.age
+            }
+
+            if (sortedItems[0] != adam) {
+                throw Exception("Sorting failed in loop number $i")
+            }
+        }
+    }
+
+    data class Person(
+        val name: String,
+        val age: Int
+    )
+}
