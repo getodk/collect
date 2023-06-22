54b1274a2edb745d275f30a6d01fdb3eebb85072
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-06-22 15:31:33 +0200

    Made lines thicker

--- maps/src/main/java/org/odk/collect/maps/MapConsts.kt
+++ maps/src/main/java/org/odk/collect/maps/MapConsts.kt
@@ -1,7 +1,7 @@
 package org.odk.collect.maps
 
 object MapConsts {
-    const val POLYLINE_STROKE_WIDTH = 5
-    const val MAPBOX_POLYLINE_STROKE_WIDTH = 2
+    const val POLYLINE_STROKE_WIDTH = 8
+    const val MAPBOX_POLYLINE_STROKE_WIDTH = 4
     const val POLYGON_FILL_COLOR_OPACITY = 68
 }
