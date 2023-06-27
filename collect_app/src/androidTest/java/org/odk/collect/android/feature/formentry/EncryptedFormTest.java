4e6cd134db0d2db234aa97763632f749be8a4462
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-06-27 22:52:09 +0200

    Added new tests

--- collect_app/src/androidTest/java/org/odk/collect/android/feature/formentry/EncryptedFormTest.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/feature/formentry/EncryptedFormTest.java
@@ -38,6 +38,23 @@ public class EncryptedFormTest {
             .around(rule);
 
     @Test
+    public void instanceOfEncryptedForm_cantBeViewedAfterFinalizing() {
+        rule.startAtMainMenu()
+                .copyForm("encrypted.xml")
+                .setServer(testDependencies.server.getURL())
+
+                .startBlankForm("encrypted")
+                .assertQuestion("Question 1")
+                .swipeToEndScreen()
+                .clickFinalize()
+
+                .clickSendFinalizedForm(1)
+                .clickOnText("encrypted")
+                .checkIsToastWithMessageDisplayed(R.string.encrypted_form)
+                .assertOnPage();
+    }
+
+    @Test
     public void instanceOfEncryptedForm_cantBeViewedAfterSending() {
         rule.startAtMainMenu()
                 .copyForm("encrypted.xml")
