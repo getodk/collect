fbcf4cf52928bae7debb91af8666c0685334bef3
Callum Stott <callum@seadowg.com>
2023-07-07 16:34:08 +0100

    Move dynamic preload code to package

--- collect_app/src/main/java/org/odk/collect/android/tasks/FormLoaderTask.java
+++ collect_app/src/main/java/org/odk/collect/android/tasks/FormLoaderTask.java
@@ -38,7 +38,7 @@ import org.javarosa.xform.parse.XFormParser;
 import org.javarosa.xform.util.XFormUtils;
 import org.javarosa.xpath.XPathTypeMismatchException;
 import org.odk.collect.android.application.Collect;
-import org.odk.collect.android.application.initialization.DynamicPreloadExtra;
+import org.odk.collect.android.dynamicpreload.DynamicPreloadExtra;
 import org.odk.collect.android.dynamicpreload.ExternalAnswerResolver;
 import org.odk.collect.android.dynamicpreload.ExternalDataManager;
 import org.odk.collect.android.dynamicpreload.ExternalDataReader;
