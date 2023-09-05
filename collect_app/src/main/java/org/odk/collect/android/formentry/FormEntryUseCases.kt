932362fa3823d1b383596aaf5b4ea7d47242c6b3
Callum Stott <callum@seadowg.com>
2023-09-05 13:52:32 +0100

    Spike out use cases for form entry loading and init

---
+++ collect_app/src/main/java/org/odk/collect/android/formentry/FormEntryUseCases.kt
@@ -0,0 +1,98 @@
+package org.odk.collect.android.formentry
+
+import org.apache.commons.io.FileUtils.readFileToByteArray
+import org.javarosa.core.model.FormDef
+import org.javarosa.core.model.instance.InstanceInitializationFactory
+import org.javarosa.core.model.instance.TreeReference
+import org.javarosa.core.model.instance.utils.DefaultAnswerResolver
+import org.javarosa.core.reference.ReferenceManager
+import org.javarosa.form.api.FormEntryController
+import org.javarosa.xform.parse.XFormParser
+import org.javarosa.xform.util.XFormUtils
+import org.odk.collect.android.externaldata.ExternalAnswerResolver
+import org.odk.collect.android.javarosawrapper.FormController
+import org.odk.collect.android.javarosawrapper.JavaRosaFormController
+import org.odk.collect.android.tasks.FormLoaderTask.FormEntryControllerFactory
+import org.odk.collect.android.utilities.FileUtils
+import org.odk.collect.android.utilities.FormDefCache
+import org.odk.collect.android.utilities.FormUtils
+import java.io.File
+
+object FormEntryUseCases {
+
+    @JvmStatic
+    fun loadXForm(xForm: File, formMediaDir: File): FormDef? {
+        FormUtils.setupReferenceManagerForForm(ReferenceManager.instance(), formMediaDir)
+        return createFormDefFromCacheOrXml(xForm)
+    }
+
+    @JvmStatic
+    fun initializeInstance(formDef: FormDef, formMediaDir: File, formEntryControllerFactory: FormEntryControllerFactory, instance: File): FormController {
+        val formEntryController = formEntryControllerFactory.create(formDef)
+        val instanceInit = InstanceInitializationFactory()
+
+        importInstance(instance, formEntryController)
+        formDef.initialize(false, instanceInit)
+
+        return JavaRosaFormController(
+            formMediaDir,
+            formEntryController,
+            instance
+        )
+    }
+
+    private fun createFormDefFromCacheOrXml(xForm: File): FormDef? {
+        val formDefFromCache = FormDefCache.readCache(xForm)
+        if (formDefFromCache != null) {
+            return formDefFromCache
+        }
+
+        val lastSavedSrc = FileUtils.getOrCreateLastSavedSrc(xForm)
+        return XFormUtils.getFormFromFormXml(xForm.absolutePath, lastSavedSrc)?.also {
+            FormDefCache.writeCache(it, xForm.path)
+        }
+    }
+
+    private fun importInstance(instanceFile: File, formEntryController: FormEntryController) {
+        // convert files into a byte array
+        val fileBytes = readFileToByteArray(instanceFile)
+
+        // get the root of the saved and template instances
+        val savedRoot = XFormParser.restoreDataModel(fileBytes, null).root
+        val templateRoot = formEntryController.model.form.instance.root.deepCopy(true)
+
+        // weak check for matching forms
+        if (savedRoot.name != templateRoot.name || savedRoot.mult != 0) {
+            return
+        }
+
+        // populate the data model
+        val tr = TreeReference.rootRef()
+        tr.add(templateRoot.name, TreeReference.INDEX_UNBOUND)
+
+        // Here we set the Collect's implementation of the IAnswerResolver.
+        // We set it back to the default after select choices have been populated.
+        XFormParser.setAnswerResolver(ExternalAnswerResolver())
+        templateRoot.populate(savedRoot, formEntryController.model.form)
+        XFormParser.setAnswerResolver(DefaultAnswerResolver())
+
+        // FormInstanceParser.parseInstance is responsible for initial creation of instances. It explicitly sets the
+        // main instance name to null so we force this again on deserialization because some code paths rely on the main
+        // instance not having a name. Must be before the call on setRoot because setRoot also sets the root's name.
+        formEntryController.model.form.instance.name = null
+
+        // populated model to current form
+        formEntryController.model.form.instance.root = templateRoot
+
+        // fix any language issues
+        // :
+        // http://bitbucket.org/javarosa/main/issue/5/itext-n-appearing-in-restored-instances
+        if (formEntryController.model.languages != null) {
+            formEntryController.model.form
+                .localeChanged(
+                    formEntryController.model.language,
+                    formEntryController.model.form.localizer
+                )
+        }
+    }
+}
