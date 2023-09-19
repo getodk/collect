6052a8408d3d7888c33d586651aaee198cbbeac1
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-09-19 11:30:06 +0200

    Added failing test

--- collect_app/src/androidTest/java/org/odk/collect/android/feature/formmanagement/UpdateFormAttachmentsTest.kt
+++ collect_app/src/androidTest/java/org/odk/collect/android/feature/formmanagement/UpdateFormAttachmentsTest.kt
@@ -7,6 +7,7 @@ import org.junit.rules.RuleChain
 import org.junit.runner.RunWith
 import org.odk.collect.android.R
 import org.odk.collect.android.support.TestDependencies
+import org.odk.collect.android.support.pages.FormEntryPage
 import org.odk.collect.android.support.pages.MainMenuPage
 import org.odk.collect.android.support.rules.CollectTestRule
 import org.odk.collect.android.support.rules.TestRuleChain.chain
@@ -112,5 +113,42 @@ class UpdateFormAttachmentsTest {
             .clickBlankForms()
             .assertTextThatContainsExists("Added on")
             .assertTextThatContainsExists("Added on", 1)
+    }
+
+    @Test
+    fun whenAnUpdatedFormIsDownloaded_copyLastSavedFileFromPreviousFormVersion() {
+        testDependencies.server.addForm(
+            "One Question Last Saved",
+            "one_question_last_saved",
+            "1",
+            "one-question-last-saved-v1.xml"
+        )
+
+        val mainMenuPage = rule.withProject(testDependencies.server.url)
+            .clickGetBlankForm()
+            .clickGetSelected()
+            .assertMessage("All downloads succeeded!")
+            .clickOKOnDialog(MainMenuPage())
+            .startBlankForm("One Question Last Saved")
+            .fillOutAndFinalize(
+                FormEntryPage.QuestionAndAnswer("what is your age", "32")
+            )
+
+        testDependencies.server.removeForm("One Question Last Saved")
+
+        testDependencies.server.addForm(
+            "One Question Last Saved",
+            "one_question_last_saved",
+            "2",
+            "one-question-last-saved-v2.xml"
+        )
+
+        mainMenuPage.clickGetBlankForm()
+            .assertText(org.odk.collect.strings.R.string.newer_version_of_a_form_info)
+            .clickGetSelected()
+            .assertMessage("All downloads succeeded!")
+            .clickOKOnDialog(MainMenuPage())
+            .startBlankForm("One Question Last Saved")
+            .assertText("32")
     }
 }
