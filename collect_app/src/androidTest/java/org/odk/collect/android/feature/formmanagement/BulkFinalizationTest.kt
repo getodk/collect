51698059794d0c7ce1942e0e4cadb3c3c5c7c599
Callum Stott <callum@seadowg.com>
2023-10-05 13:16:17 +0100

    Fail to finalize instances with save points

--- collect_app/src/androidTest/java/org/odk/collect/android/feature/formmanagement/BulkFinalizationTest.kt
+++ collect_app/src/androidTest/java/org/odk/collect/android/feature/formmanagement/BulkFinalizationTest.kt
@@ -86,7 +86,30 @@ class BulkFinalizationTest {
     }
 
     @Test
-    fun doesNotFinalizeOtherTypesOfInstance() {
+    fun doesNotFinalizeInstancesWithSavePoints() {
+        rule.startAtMainMenu()
+            .copyForm("one-question.xml")
+            .startBlankForm("One Question")
+            .swipeToEndScreen()
+            .clickSaveAsDraft()
+
+            .clickDrafts()
+            .clickOnForm("One Question")
+            .killAndReopenApp(MainMenuPage())
+
+            .clickDrafts()
+            .clickOptionsIcon(string.finalize_all_forms)
+            .clickOnString(string.finalize_all_forms)
+            .checkIsSnackbarWithQuantityDisplayed(plurals.bulk_finalize_failure, 1)
+            .assertText("One Question")
+            .pressBack(MainMenuPage())
+
+            .assertNumberOfEditableForms(1)
+            .assertNumberOfFinalizedForms(0)
+    }
+
+    @Test
+    fun doesNotFinalizeAlreadyFinalizedInstances() {
         rule.startAtMainMenu()
             .copyForm("one-question.xml")
             .startBlankForm("One Question")
