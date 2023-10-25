47158742a8c1fba2f713db0776dd00cebb1fbaf7
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-10-25 15:13:14 +0200

    Merge pull request #5797 from grzesiek2010/COLLECT-5796
    Fixed searching in the list of drafts

--- collect_app/src/main/java/org/odk/collect/android/dao/CursorLoaderFactory.java
+++ collect_app/src/main/java/org/odk/collect/android/dao/CursorLoaderFactory.java
@@ -51,10 +51,10 @@ public class CursorLoaderFactory {
 
             cursorLoader = getInstancesCursorLoader(selection, selectionArgs, sortOrder);
         } else {
-            String selection = DatabaseInstanceColumns.STATUS + " =? " +
+            String selection = "(" + DatabaseInstanceColumns.STATUS + "=? or " + DatabaseInstanceColumns.STATUS + "=? or " + DatabaseInstanceColumns.STATUS + "=?)" +
                     "and " + DatabaseInstanceColumns.DISPLAY_NAME + " LIKE ?";
             String[] selectionArgs = {
-                    Instance.STATUS_INCOMPLETE,
+                    Instance.STATUS_INCOMPLETE, Instance.STATUS_INVALID, Instance.STATUS_VALID,
                     "%" + charSequence + "%"
             };
 
