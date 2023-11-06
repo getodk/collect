101f8584629d4da91b6e596044786339fa1bb0a9
Callum Stott <callum@seadowg.com>
2023-11-06 09:56:29 +0000

    Use XPathProcessor to detect pulldata

--- collect_app/src/main/java/org/odk/collect/android/dynamicpreload/DynamicPreloadXFormParserFactory.kt
+++ collect_app/src/main/java/org/odk/collect/android/dynamicpreload/DynamicPreloadXFormParserFactory.kt
@@ -1,11 +1,13 @@
 package org.odk.collect.android.dynamicpreload
 
 import org.javarosa.core.model.FormDef
+import org.javarosa.core.model.QuestionDef
 import org.javarosa.core.util.externalizable.ExtUtil
 import org.javarosa.core.util.externalizable.Externalizable
 import org.javarosa.core.util.externalizable.PrototypeFactory
 import org.javarosa.xform.parse.IXFormParserFactory
 import org.javarosa.xform.parse.XFormParser
+import org.javarosa.xpath.expr.XPathExpression
 import java.io.DataInputStream
 import java.io.DataOutputStream
 
@@ -19,10 +21,28 @@ class DynamicPreloadXFormParserFactory(base: IXFormParserFactory) :
     }
 }
 
-class DynamicPreloadParseProcessor : XFormParser.FormDefProcessor {
+class DynamicPreloadParseProcessor :
+    XFormParser.XPathProcessor,
+    XFormParser.QuestionProcessor,
+    XFormParser.FormDefProcessor {
+
+    private var containsPullData = false
+    private var containsSearch = false
+
+    override fun processXPath(xPathExpression: XPathExpression) {
+        if (xPathExpression.containsFunc("pulldata")) {
+            containsPullData = true
+        }
+    }
+
+    override fun processQuestion(question: QuestionDef) {
+        if (ExternalDataUtil.getSearchXPathExpression(question.appearanceAttr) != null) {
+            containsSearch = true
+        }
+    }
 
     override fun processFormDef(formDef: FormDef) {
-        formDef.extras.put(DynamicPreloadExtra(true))
+        formDef.extras.put(DynamicPreloadExtra(containsPullData || containsSearch))
     }
 }
 
