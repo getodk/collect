cc7ec0500fedf6c0c8358533cac539a183b27057
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-11-09 11:24:50 +0100

    Added tests

--- collect_app/src/test/java/org/odk/collect/android/application/initialization/GoogleDriveProjectsDeleterTest.kt
+++ collect_app/src/test/java/org/odk/collect/android/application/initialization/GoogleDriveProjectsDeleterTest.kt
@@ -6,6 +6,8 @@ import org.junit.Test
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.verifyNoMoreInteractions
+import org.mockito.kotlin.whenever
+import org.odk.collect.android.projects.DeleteProjectResult
 import org.odk.collect.android.projects.ProjectDeleter
 import org.odk.collect.projects.InMemProjectsRepository
 import org.odk.collect.projects.Project
@@ -41,5 +43,62 @@ class GoogleDriveProjectsDeleterTest {
         verify(projectDeleter).deleteProject("2")
         verify(projectDeleter).deleteProject("3")
         verifyNoMoreInteractions(projectDeleter)
+    }
+
+    @Test
+    fun `GD projects that cannot be deleted because of unsent forms should be converted to ODK protocol`() {
+        projectsRepository.save(Project.Saved("1", "project", "Q", "#000000"))
+        settingsProvider.getUnprotectedSettings("1").save(ProjectKeys.KEY_PROTOCOL, ProjectKeys.PROTOCOL_GOOGLE_SHEETS)
+        whenever(projectDeleter.deleteProject("1")).thenReturn(DeleteProjectResult.UnsentInstances)
+
+        googleDriveProjectsDeleter.run()
+
+        assertThat(settingsProvider.getUnprotectedSettings("1").getString(ProjectKeys.KEY_PROTOCOL), equalTo(ProjectKeys.PROTOCOL_SERVER))
+        assertThat(settingsProvider.getUnprotectedSettings("1").getString(ProjectKeys.KEY_SERVER_URL), equalTo("https://example.com"))
+    }
+
+    @Test
+    fun `GD projects that cannot be deleted because of running background jobs should be converted to ODK protocol`() {
+        projectsRepository.save(Project.Saved("1", "project", "Q", "#000000"))
+        settingsProvider.getUnprotectedSettings("1").save(ProjectKeys.KEY_PROTOCOL, ProjectKeys.PROTOCOL_GOOGLE_SHEETS)
+        whenever(projectDeleter.deleteProject("1")).thenReturn(DeleteProjectResult.RunningBackgroundJobs)
+
+        googleDriveProjectsDeleter.run()
+
+        assertThat(settingsProvider.getUnprotectedSettings("1").getString(ProjectKeys.KEY_PROTOCOL), equalTo(ProjectKeys.PROTOCOL_SERVER))
+        assertThat(settingsProvider.getUnprotectedSettings("1").getString(ProjectKeys.KEY_SERVER_URL), equalTo("https://example.com"))
+    }
+
+    @Test
+    fun `If GD projects deletion results with 'DeletedSuccessfullyLastProject' there is no attempt to convert it to ODK protocol`() {
+        projectsRepository.save(Project.Saved("1", "project", "Q", "#000000"))
+        settingsProvider.getUnprotectedSettings("1").save(ProjectKeys.KEY_PROTOCOL, ProjectKeys.PROTOCOL_GOOGLE_SHEETS)
+        whenever(projectDeleter.deleteProject("1")).thenReturn(DeleteProjectResult.DeletedSuccessfullyLastProject)
+
+        googleDriveProjectsDeleter.run()
+
+        assertThat(settingsProvider.getUnprotectedSettings("1").getString(ProjectKeys.KEY_PROTOCOL), equalTo(ProjectKeys.PROTOCOL_GOOGLE_SHEETS))
+    }
+
+    @Test
+    fun `If GD projects deletion results with 'DeletedSuccessfullyInactiveProject' there is no attempt to convert it to ODK protocol`() {
+        projectsRepository.save(Project.Saved("1", "project", "Q", "#000000"))
+        settingsProvider.getUnprotectedSettings("1").save(ProjectKeys.KEY_PROTOCOL, ProjectKeys.PROTOCOL_GOOGLE_SHEETS)
+        whenever(projectDeleter.deleteProject("1")).thenReturn(DeleteProjectResult.DeletedSuccessfullyInactiveProject)
+
+        googleDriveProjectsDeleter.run()
+
+        assertThat(settingsProvider.getUnprotectedSettings("1").getString(ProjectKeys.KEY_PROTOCOL), equalTo(ProjectKeys.PROTOCOL_GOOGLE_SHEETS))
+    }
+
+    @Test
+    fun `If GD projects deletion results with 'DeletedSuccessfullyCurrentProject' there is no attempt to convert it to ODK protocol`() {
+        projectsRepository.save(Project.Saved("1", "project", "Q", "#000000"))
+        settingsProvider.getUnprotectedSettings("1").save(ProjectKeys.KEY_PROTOCOL, ProjectKeys.PROTOCOL_GOOGLE_SHEETS)
+        whenever(projectDeleter.deleteProject("1")).thenReturn(DeleteProjectResult.DeletedSuccessfullyCurrentProject(mock()))
+
+        googleDriveProjectsDeleter.run()
+
+        assertThat(settingsProvider.getUnprotectedSettings("1").getString(ProjectKeys.KEY_PROTOCOL), equalTo(ProjectKeys.PROTOCOL_GOOGLE_SHEETS))
     }
 }
