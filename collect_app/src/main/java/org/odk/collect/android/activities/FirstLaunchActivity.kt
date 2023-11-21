b507b041e521b46b5e659ecf2e9900380b978cf8
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-11-21 21:42:03 +0100

    Introduce viewmodel to the loading state during configuration changes

--- collect_app/src/main/java/org/odk/collect/android/activities/FirstLaunchActivity.kt
+++ collect_app/src/main/java/org/odk/collect/android/activities/FirstLaunchActivity.kt
@@ -2,7 +2,10 @@ package org.odk.collect.android.activities
 
 import android.os.Bundle
 import android.text.SpannableStringBuilder
+import androidx.activity.viewModels
 import androidx.core.text.color
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import org.odk.collect.analytics.Analytics
 import org.odk.collect.android.analytics.AnalyticsEvents
 import org.odk.collect.android.databinding.FirstLaunchLayoutBinding
@@ -13,6 +16,7 @@ import org.odk.collect.android.projects.ProjectsDataService
 import org.odk.collect.android.projects.QrCodeProjectCreatorDialog
 import org.odk.collect.android.version.VersionInformation
 import org.odk.collect.androidshared.livedata.MutableNonNullLiveData
+import org.odk.collect.androidshared.livedata.NonNullLiveData
 import org.odk.collect.androidshared.system.ContextUtils.getThemeAttributeValue
 import org.odk.collect.androidshared.ui.DialogFragmentUtils
 import org.odk.collect.async.Scheduler
@@ -39,6 +43,14 @@ class FirstLaunchActivity : LocalizedActivity() {
 
     @Inject
     lateinit var scheduler: Scheduler
+
+    private val viewModel: FirstLaunchViewModel by viewModels {
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return FirstLaunchViewModel(scheduler, projectsRepository, projectsDataService) as T
+            }
+        }
+    }
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -76,12 +88,9 @@ class FirstLaunchActivity : LocalizedActivity() {
                     }
 
                 setOnClickListener {
-                    Analytics.log(AnalyticsEvents.TRY_DEMO)
-
-                    val isLoading = MutableNonNullLiveData(true)
                     MaterialProgressDialogFragment.showOn(
                         this@FirstLaunchActivity,
-                        isLoading,
+                        viewModel.isLoading,
                         supportFragmentManager
                     ) {
                         MaterialProgressDialogFragment().also { dialog ->
@@ -89,21 +98,41 @@ class FirstLaunchActivity : LocalizedActivity() {
                         }
                     }
 
-                    scheduler.immediate(
-                        background = {
-                            projectsRepository.save(Project.DEMO_PROJECT)
-                            projectsDataService.setCurrentProject(Project.DEMO_PROJECT_ID)
-                        },
-                        foreground = {
-                            isLoading.value = false
+                    viewModel.isLoading.observe(this@FirstLaunchActivity) { isLoading ->
+                        if (!isLoading) {
                             ActivityUtils.startActivityAndCloseAllOthers(
                                 this@FirstLaunchActivity,
                                 MainMenuActivity::class.java
                             )
                         }
-                    )
+                    }
+                    viewModel.tryDemo()
                 }
             }
         }
+    }
+}
+
+private class FirstLaunchViewModel(
+    private val scheduler: Scheduler,
+    private val projectsRepository: ProjectsRepository,
+    private val projectsDataService: ProjectsDataService
+) : ViewModel() {
+    private val _isLoading = MutableNonNullLiveData(false)
+    val isLoading: NonNullLiveData<Boolean> = _isLoading
+
+    fun tryDemo() {
+        Analytics.log(AnalyticsEvents.TRY_DEMO)
+
+        _isLoading.value = true
+        scheduler.immediate(
+            background = {
+                projectsRepository.save(Project.DEMO_PROJECT)
+                projectsDataService.setCurrentProject(Project.DEMO_PROJECT_ID)
+            },
+            foreground = {
+                _isLoading.value = false
+            }
+        )
     }
 }
