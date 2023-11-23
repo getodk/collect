188d4812685c07ab1f9ad324ea5a91e2a234a372
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-11-23 11:23:44 +0100

    Delay showing the keyboard

--- collect_app/src/main/java/org/odk/collect/android/utilities/SoftKeyboardController.kt
+++ collect_app/src/main/java/org/odk/collect/android/utilities/SoftKeyboardController.kt
@@ -1,15 +1,21 @@
 package org.odk.collect.android.utilities
 
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
 object SoftKeyboardController {
     fun showSoftKeyboard(view: View) {
-        if (view.requestFocus()) {
-            val inputMethodManager = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            inputMethodManager.showSoftInput(view, 0)
-        }
+        // Subtle delay in displaying the keyboard is necessary for the keyboard to be displayed at all,
+        // for example, when returning from the hierarchy view.
+        Handler(Looper.getMainLooper()).postDelayed({
+            if (view.requestFocus()) {
+                val inputMethodManager = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.showSoftInput(view, 0)
+            }
+        }, 100)
     }
 
     fun hideSoftKeyboard(view: View) {
