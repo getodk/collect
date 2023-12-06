ec0837b78a1df7bd0f4b85ac11b32110f6a4deb4
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-12-06 13:33:42 +0100

    Converted FormEntryMenuProvider and its test to kotlin

---
+++ collect_app/src/main/java/org/odk/collect/android/formentry/FormEntryMenuProvider.kt
@@ -0,0 +1,152 @@
+package org.odk.collect.android.formentry
+
+import android.content.DialogInterface
+import android.content.Intent
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuProvider
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import org.odk.collect.android.R
+import org.odk.collect.android.activities.FormHierarchyActivity
+import org.odk.collect.android.formentry.backgroundlocation.BackgroundLocationViewModel
+import org.odk.collect.android.formentry.questions.AnswersProvider
+import org.odk.collect.android.preferences.screens.ProjectPreferencesActivity
+import org.odk.collect.android.utilities.ApplicationConstants
+import org.odk.collect.androidshared.system.PlayServicesChecker
+import org.odk.collect.androidshared.ui.DialogFragmentUtils.showIfNotShowing
+import org.odk.collect.androidshared.ui.multiclicksafe.MultiClickGuard.allowClick
+import org.odk.collect.audiorecorder.recording.AudioRecorder
+import org.odk.collect.settings.SettingsProvider
+import org.odk.collect.settings.keys.ProjectKeys
+import org.odk.collect.settings.keys.ProtectedProjectKeys
+
+class FormEntryMenuProvider(
+    private val activity: AppCompatActivity,
+    private val answersProvider: AnswersProvider,
+    private val formEntryViewModel: FormEntryViewModel,
+    private val audioRecorder: AudioRecorder,
+    private val backgroundLocationViewModel: BackgroundLocationViewModel,
+    private val backgroundAudioViewModel: BackgroundAudioViewModel,
+    private val settingsProvider: SettingsProvider,
+    private val formEntryMenuClickListener: FormEntryMenuClickListener
+) : MenuProvider {
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.form_menu, menu)
+    }
+
+    override fun onPrepareMenu(menu: Menu) {
+        val formController = formEntryViewModel.formController
+
+        var useability: Boolean = settingsProvider.getProtectedSettings().getBoolean(ProtectedProjectKeys.KEY_SAVE_MID)
+        menu.findItem(R.id.menu_save).isVisible = useability
+
+        useability = settingsProvider.getProtectedSettings().getBoolean(ProtectedProjectKeys.KEY_JUMP_TO)
+        menu.findItem(R.id.menu_goto).isVisible = useability
+
+        useability = settingsProvider.getProtectedSettings().getBoolean(ProtectedProjectKeys.KEY_CHANGE_LANGUAGE) && formController != null && formController.getLanguages() != null && formController.getLanguages()!!.size > 1
+        menu.findItem(R.id.menu_languages).isVisible = useability
+
+        useability = settingsProvider.getProtectedSettings().getBoolean(ProtectedProjectKeys.KEY_ACCESS_SETTINGS)
+        menu.findItem(R.id.menu_preferences).isVisible = useability
+
+        if (formController != null &&
+            formController.currentFormCollectsBackgroundLocation() &&
+            PlayServicesChecker().isGooglePlayServicesAvailable(activity)
+        ) {
+            val backgroundLocation = menu.findItem(R.id.track_location)
+            backgroundLocation.isVisible = true
+            backgroundLocation.isChecked = settingsProvider.getUnprotectedSettings().getBoolean(ProjectKeys.KEY_BACKGROUND_LOCATION)
+        }
+
+        menu.findItem(R.id.menu_add_repeat).isVisible = formEntryViewModel.canAddRepeat()
+        menu.findItem(R.id.menu_record_audio).isVisible = formEntryViewModel.hasBackgroundRecording().value
+        menu.findItem(R.id.menu_record_audio).isChecked = backgroundAudioViewModel.isBackgroundRecordingEnabled.value
+    }
+
+    override fun onMenuItemSelected(item: MenuItem): Boolean {
+        if (!allowClick(javaClass.name)) {
+            return true
+        }
+
+        return when (item.itemId) {
+            R.id.menu_add_repeat -> {
+                if (audioRecorder.isRecording() && !backgroundAudioViewModel.isBackgroundRecording) {
+                    showIfNotShowing(RecordingWarningDialogFragment::class.java, activity.supportFragmentManager)
+                } else {
+                    formEntryViewModel.updateAnswersForScreen(answersProvider.answers, false)
+                    formEntryViewModel.promptForNewRepeat()
+                }
+                true
+            }
+            R.id.menu_preferences -> {
+                if (audioRecorder.isRecording()) {
+                    showIfNotShowing(RecordingWarningDialogFragment::class.java, activity.supportFragmentManager)
+                } else {
+                    formEntryViewModel.updateAnswersForScreen(answersProvider.answers, false)
+                    val pref = Intent(activity, ProjectPreferencesActivity::class.java)
+                    activity.startActivityForResult(pref, ApplicationConstants.RequestCodes.CHANGE_SETTINGS)
+                }
+                true
+            }
+            R.id.track_location -> {
+                backgroundLocationViewModel.backgroundLocationPreferenceToggled(settingsProvider.getUnprotectedSettings())
+                true
+            }
+            R.id.menu_goto -> {
+                if (audioRecorder.isRecording() && !backgroundAudioViewModel.isBackgroundRecording) {
+                    showIfNotShowing(RecordingWarningDialogFragment::class.java, activity.supportFragmentManager)
+                } else {
+                    formEntryViewModel.updateAnswersForScreen(answersProvider.answers, false)
+                    formEntryViewModel.openHierarchy()
+                    val i = Intent(activity, FormHierarchyActivity::class.java)
+                    i.putExtra(FormHierarchyActivity.EXTRA_SESSION_ID, formEntryViewModel.sessionId)
+                    activity.startActivityForResult(i, ApplicationConstants.RequestCodes.HIERARCHY_ACTIVITY)
+                }
+                true
+            }
+            R.id.menu_record_audio -> {
+                val enabled = !item.isChecked
+                if (!enabled) {
+                    MaterialAlertDialogBuilder(activity)
+                        .setMessage(org.odk.collect.strings.R.string.stop_recording_confirmation)
+                        .setPositiveButton(org.odk.collect.strings.R.string.disable_recording) { _: DialogInterface?, _: Int -> backgroundAudioViewModel.setBackgroundRecordingEnabled(false) }
+                        .setNegativeButton(org.odk.collect.strings.R.string.cancel, null)
+                        .create()
+                        .show()
+                } else {
+                    MaterialAlertDialogBuilder(activity)
+                        .setMessage(org.odk.collect.strings.R.string.background_audio_recording_enabled_explanation)
+                        .setCancelable(false)
+                        .setPositiveButton(org.odk.collect.strings.R.string.ok, null)
+                        .create()
+                        .show()
+                    backgroundAudioViewModel.setBackgroundRecordingEnabled(true)
+                }
+                true
+            }
+            R.id.menu_validate -> {
+                formEntryViewModel.saveScreenAnswersToFormController(answersProvider.answers, false)
+                formEntryViewModel.validate()
+                true
+            }
+            R.id.menu_languages -> {
+                formEntryMenuClickListener.changeLanguage()
+                true
+            }
+            R.id.menu_save -> {
+                formEntryMenuClickListener.save()
+                true
+            }
+            else -> {
+                false
+            }
+        }
+    }
+
+    interface FormEntryMenuClickListener {
+        fun changeLanguage()
+        fun save()
+    }
+}
