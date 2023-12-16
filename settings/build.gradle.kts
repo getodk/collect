832199dc403ee36af22eabb266dd3dd66a30a5e8
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-12-16 19:40:05 +0100

    Converted build.gradle from the settings module to kotlin

---
+++ settings/build.gradle.kts
@@ -0,0 +1,56 @@
+import dependencies.Dependencies
+import dependencies.Versions
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlin-kapt")
+}
+
+apply(from = "../config/quality.gradle")
+
+android {
+    compileSdk = Versions.android_compile_sdk
+
+    defaultConfig {
+        minSdk = Versions.android_min_sdk
+        targetSdk = Versions.android_target_sdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    packaging {
+        resources {
+            // These library licenses will be referenced in-app
+            excludes += setOf("META-INF/ASL-2.0.txt", "META-INF/LGPL-3.0.txt")
+
+            // Pick first occurrence of any files that cause conflicts
+            pickFirst("schema")
+        }
+    }
+
+    namespace = "org.odk.collect.settings"
+}
+
+dependencies {
+    implementation(project(":shared"))
+    implementation(project(":projects"))
+
+    implementation(Dependencies.json_schema_validator)
+
+    testImplementation(Dependencies.junit)
+    testImplementation(Dependencies.hamcrest)
+    testImplementation(Dependencies.mockito_kotlin)
+}
