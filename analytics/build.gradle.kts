b49f8a3288c793f2842f2360e85e691a6ee91f5f
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-12-16 19:11:55 +0100

    Converted build.gradle from the analytics module to kotlin

---
+++ analytics/build.gradle.kts
@@ -0,0 +1,44 @@
+import dependencies.Dependencies
+import dependencies.Versions
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
+
+apply(from = "../config/quality.gradle")
+
+android {
+    compileSdk = Versions.android_compile_sdk
+
+    defaultConfig {
+        minSdk = Versions.android_min_sdk
+        targetSdk = Versions.android_target_sdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    namespace = "org.odk.collect.analytics"
+}
+
+dependencies {
+    implementation(Dependencies.kotlin_stdlib)
+    implementation(Dependencies.androidx_core_ktx)
+    implementation(Dependencies.firebase_crashlytics)
+    implementation(Dependencies.firebase_analytics) {
+        exclude(group = "com.google.guava")
+    }
+}
