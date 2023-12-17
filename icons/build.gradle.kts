edb220b79b47ca32c3ff13b1c02683c9e6e801dd
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-12-17 18:36:49 +0100

    Converted build.gradle from the icons module to kotlin

---
+++ icons/build.gradle.kts
@@ -0,0 +1,38 @@
+import dependencies.Dependencies
+import dependencies.Versions
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
+
+apply(from = "../config/quality.gradle")
+
+android {
+    compileSdk = Versions.android_compile_sdk
+
+    defaultConfig {
+        minSdk = Versions.android_min_sdk
+        targetSdk = Versions.android_target_sdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    namespace = "org.odk.collect.icons"
+}
+
+dependencies {
+    implementation(Dependencies.android_material)
+}
