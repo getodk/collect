ca3d3edca08c71d47c464cdf70cf0bd24e52314f
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-12-17 19:01:33 +0100

    Converted build.gradle from the fragmentstest module to kotlin

---
+++ fragmentstest/build.gradle.kts
@@ -0,0 +1,45 @@
+import dependencies.Dependencies
+import dependencies.Versions
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
+
+apply(from = "../config/quality.gradle")
+
+android {
+    compileSdk = Versions.android_compile_sdk
+
+    defaultConfig {
+        minSdk = Versions.android_min_sdk
+        targetSdk = Versions.android_target_sdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    namespace = "org.odk.collect.fragmentstest"
+}
+
+dependencies {
+    implementation(Dependencies.kotlin_stdlib)
+    implementation(Dependencies.androidx_test_espresso_core)
+    implementation(Dependencies.android_material)
+
+    //noinspection FragmentGradleConfiguration
+    debugImplementation(Dependencies.androidx_fragment_testing) {
+        exclude(group = "androidx.test", module = "monitor") //fixes issue https://github.com/android/android-test/issues/731
+    }
+}
