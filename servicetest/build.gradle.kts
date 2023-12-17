4edc9e11a4a794493daf9f61b73077bf34b95e9b
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-12-17 18:26:11 +0100

    Converted build.gradle from the servicetest module to kotlin

---
+++ servicetest/build.gradle.kts
@@ -0,0 +1,41 @@
+import dependencies.Dependencies
+import dependencies.Versions
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
+
+apply(from = "../config/quality.gradle")
+
+android {
+    compileSdk = Versions.android_compile_sdk
+
+    defaultConfig {
+        minSdk = Versions.android_min_sdk
+        targetSdk = Versions.android_target_sdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    namespace = "org.odk.collect.servicetest"
+}
+
+dependencies {
+    coreLibraryDesugaring(Dependencies.desugar)
+
+    implementation(Dependencies.androidx_lifecycle_runtime_ktx)
+    implementation(Dependencies.robolectric)
+}
