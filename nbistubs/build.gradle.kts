d9eb469442d61f28a39ab7c1808c33b6241f8b4e
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-12-17 18:32:27 +0100

    Converted build.gradle from the nbistubs module to kotlin

---
+++ nbistubs/build.gradle.kts
@@ -0,0 +1,25 @@
+import dependencies.Versions
+
+plugins {
+    id("com.android.library")
+}
+
+android {
+    compileSdk = Versions.android_compile_sdk
+
+    defaultConfig {
+        minSdk = Versions.android_min_sdk
+        targetSdk = Versions.android_target_sdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+
+    namespace = "org.odk.collect.nbistubs"
+}
