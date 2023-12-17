872b35d6522ab9df1b601e9b2e4c28ffe7bc2d33
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2023-12-17 18:42:54 +0100

    Converted build.gradle from the externalapp module to kotlin

---
+++ externalapp/build.gradle.kts
@@ -0,0 +1,50 @@
+import dependencies.Dependencies
+import dependencies.Versions
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("kotlin-kapt")
+}
+
+apply(from = "../config/quality.gradle")
+
+android {
+    compileSdk = Versions.android_compile_sdk
+
+    defaultConfig {
+        minSdk = Versions.android_min_sdk
+        targetSdk = Versions.android_target_sdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+    namespace = "org.odk.collect.externalapp"
+}
+
+dependencies {
+    implementation(Dependencies.androidx_core_ktx)
+
+    testImplementation(Dependencies.junit)
+    testImplementation(Dependencies.hamcrest)
+    testImplementation(Dependencies.androidx_test_ext_junit)
+    testImplementation(Dependencies.robolectric)
+    testImplementation(Dependencies.mockito_kotlin)
+}
