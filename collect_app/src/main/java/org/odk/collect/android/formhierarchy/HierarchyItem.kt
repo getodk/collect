398ff29385430f80a035cc9abf1953e8a9464203
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2024-01-16 20:39:25 +0100

    Removed redundant constructor

--- collect_app/src/main/java/org/odk/collect/android/formhierarchy/HierarchyItem.kt
+++ collect_app/src/main/java/org/odk/collect/android/formhierarchy/HierarchyItem.kt
@@ -2,18 +2,12 @@ package org.odk.collect.android.formhierarchy
 
 import org.javarosa.core.model.FormIndex
 
-data class HierarchyItem(
+data class HierarchyItem @JvmOverloads constructor(
     val formIndex: FormIndex,
     val hierarchyItemType: HierarchyItemType,
     val primaryText: CharSequence,
-    val secondaryText: String?
-) {
-    constructor(
-        formIndex: FormIndex,
-        hierarchyItemType: HierarchyItemType,
-        primaryText: CharSequence
-    ) : this(formIndex, hierarchyItemType, primaryText, null)
-}
+    val secondaryText: String? = null
+)
 
 enum class HierarchyItemType(val id: Int) {
     QUESTION(0), VISIBLE_GROUP(1), REPEATABLE_GROUP(2), REPEAT_INSTANCE(3)
