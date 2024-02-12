f932ebc8bb547c6d3990e5646bb66c86d5710ba3
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2024-02-12 22:26:48 +0100

    Updated Dependencies and Versions

--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -5,14 +5,14 @@ object Versions {
     const val android_min_sdk = 21
     const val android_target_sdk = 33
 
-    const val androidx_fragment = "1.6.1"
-    const val dagger = "2.48.1"
+    const val androidx_fragment = "1.6.2"
+    const val dagger = "2.50"
     const val espresso = "3.5.1"
     const val glide = "4.16.0"
     const val okhttp3 = "4.12.0"
-    const val robolectric = "4.9"
-    const val work = "2.8.1"
-    const val lifecycle = "2.6.2"
-    const val camerax = "1.3.0"
-    const val navigation = "2.7.4"
+    const val robolectric = "4.11.1"
+    const val work = "2.9.0"
+    const val lifecycle = "2.7.0"
+    const val camerax = "1.3.1"
+    const val navigation = "2.7.7"
 }
