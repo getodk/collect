0e35f1d6d832160b1721fed8fe95ac6bc415e7c7
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2024-02-22 23:09:47 +0100

    Removed FormEntrySettingsTest in favour of MovingBackwardsTest

--- collect_app/src/androidTest/java/org/odk/collect/android/feature/settings/MovingBackwardsTest.kt
+++ collect_app/src/androidTest/java/org/odk/collect/android/feature/settings/MovingBackwardsTest.kt
@@ -5,7 +5,8 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
 import org.junit.runner.RunWith
-import org.odk.collect.android.R
+import org.odk.collect.android.support.pages.AccessControlPage
+import org.odk.collect.android.support.pages.ProjectSettingsPage
 import org.odk.collect.android.support.rules.CollectTestRule
 import org.odk.collect.android.support.rules.TestRuleChain
 
@@ -26,6 +27,15 @@ class MovingBackwardsTest {
             .clickOnString(org.odk.collect.strings.R.string.finalize)
 
             // before disabling moving backward
+            .pressBack(AccessControlPage())
+            .pressBack(ProjectSettingsPage())
+            .openFormManagement()
+            .openConstraintProcessing()
+            .clickOnString(org.odk.collect.strings.R.string.constraint_behavior_on_finalize)
+            .pressBack(ProjectSettingsPage())
+
+            .clickAccessControl()
+            .clickFormEntrySettings()
             .assertGoToPromptEnabled()
             .assertGoToPromptChecked()
 
@@ -53,6 +63,14 @@ class MovingBackwardsTest {
 
             .assertFinalizeDisabled()
             .assertFinalizeChecked()
+
+            .pressBack(AccessControlPage())
+            .pressBack(ProjectSettingsPage())
+            .openFormManagement()
+            .scrollToConstraintProcessing()
+            .checkIfConstraintProcessingIsDisabled()
+            .assertTextDoesNotExist(org.odk.collect.strings.R.string.constraint_behavior_on_finalize)
+            .assertText(org.odk.collect.strings.R.string.constraint_behavior_on_swipe)
     }
 
     @Test
@@ -62,15 +80,52 @@ class MovingBackwardsTest {
             .clickSettings()
             .clickAccessControl()
             .clickFormEntrySettings()
+            .clickOnString(org.odk.collect.strings.R.string.finalize)
+
+            // before disabling moving backward
+            .pressBack(AccessControlPage())
+            .pressBack(ProjectSettingsPage())
+            .openFormManagement()
+            .openConstraintProcessing()
+            .clickOnString(org.odk.collect.strings.R.string.constraint_behavior_on_finalize)
+            .pressBack(ProjectSettingsPage())
+
+            .clickAccessControl()
+            .clickFormEntrySettings()
+            .assertGoToPromptEnabled()
+            .assertGoToPromptChecked()
+
+            .assertSaveAsDraftInFormEntryEnabled()
+            .assertSaveAsDraftInFormEntryChecked()
+
+            .assertSaveAsDraftInFormEndDisabled()
+            .assertSaveAsDraftInFormEndChecked()
+
+            .assertFinalizeEnabled()
+            .assertFinalizeUnchecked()
+
             .clickMovingBackwards()
             .clickOnString(org.odk.collect.strings.R.string.no)
+
+            // after disabling moving backward - the state of the 4 related options is reversed
             .assertGoToPromptEnabled()
-            .assertSaveAsDraftInFormEntryEnabled()
-            .assertSaveAsDraftInFormEndEnabled()
-            .assertFinalizeEnabled()
             .assertGoToPromptChecked()
+
+            .assertSaveAsDraftInFormEntryEnabled()
             .assertSaveAsDraftInFormEntryChecked()
+
+            .assertSaveAsDraftInFormEndDisabled()
             .assertSaveAsDraftInFormEndChecked()
-            .assertFinalizeChecked()
+
+            .assertFinalizeEnabled()
+            .assertFinalizeUnchecked()
+
+            .pressBack(AccessControlPage())
+            .pressBack(ProjectSettingsPage())
+            .openFormManagement()
+            .scrollToConstraintProcessing()
+            .checkIfConstraintProcessingIsEnabled()
+            .assertText(org.odk.collect.strings.R.string.constraint_behavior_on_finalize)
+            .assertTextDoesNotExist(org.odk.collect.strings.R.string.constraint_behavior_on_swipe)
     }
 }
