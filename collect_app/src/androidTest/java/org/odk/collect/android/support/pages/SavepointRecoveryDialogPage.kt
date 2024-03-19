170f5ac8a8e67033c46e75f005a878067ec03230
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2024-03-19 22:49:28 +0100

    Use getTranslatedString in SavepointRecoveryDialogPage

--- collect_app/src/androidTest/java/org/odk/collect/android/support/pages/SavepointRecoveryDialogPage.kt
+++ collect_app/src/androidTest/java/org/odk/collect/android/support/pages/SavepointRecoveryDialogPage.kt
@@ -1,20 +1,15 @@
 package org.odk.collect.android.support.pages
 
-import android.app.Application
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.RootMatchers.isDialog
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import org.odk.collect.strings.R
-import org.odk.collect.strings.localization.getLocalizedString
 
 class SavepointRecoveryDialogPage : Page<SavepointRecoveryDialogPage>() {
     override fun assertOnPage(): SavepointRecoveryDialogPage {
-        val title = ApplicationProvider
-            .getApplicationContext<Application>()
-            .getLocalizedString(R.string.savepoint_recovery_dialog_title)
+        val title = getTranslatedString(R.string.savepoint_recovery_dialog_title)
         onView(withText(title)).inRoot(isDialog()).check(matches(isDisplayed()))
         return this
     }
