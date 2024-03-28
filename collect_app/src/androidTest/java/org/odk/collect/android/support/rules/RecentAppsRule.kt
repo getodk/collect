4ce2b75bc60754c99ac2e912c0690865a1fb421e
Callum Stott <callum@seadowg.com>
2024-03-28 18:15:33 +0000

    Add API 34 implementation for RecentAppsRule

--- collect_app/src/androidTest/java/org/odk/collect/android/support/rules/RecentAppsRule.kt
+++ collect_app/src/androidTest/java/org/odk/collect/android/support/rules/RecentAppsRule.kt
@@ -15,16 +15,20 @@ import org.odk.collect.testshared.DummyActivity
 class RecentAppsRule : ExternalResource() {
 
     override fun before() {
-        assumeTrue(SUPPORTED_SDKS.contains(Build.VERSION.SDK_INT)) // Skip if we're not using API 30
-        removeRecentAppsTooltips()
+        assumeTrue(SUPPORTED_SDKS.contains(Build.VERSION.SDK_INT))
+
+        if (Build.VERSION.SDK_INT == 30) {
+            removeRecentAppsTooltips()
+        }
     }
 
     fun killApp() {
-        if (Build.VERSION.SDK_INT == 30) {
+        if (Build.VERSION.SDK_INT == 30 || Build.VERSION.SDK_INT == 34) {
             val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
             device.pressRecentApps()
-            device
-                .findObject(UiSelector().descriptionContains("Collect"))
+
+            device.wait(Until.hasObject(By.descContains("Collect")), 1000)
+            device.findObject(UiSelector().descriptionContains("Collect"))
                 .swipeUp(10).also {
                     CollectHelpers.simulateProcessRestart() // the process is not restarted automatically (probably to keep the test running) so we have simulate it
                 }
@@ -34,42 +38,38 @@ class RecentAppsRule : ExternalResource() {
     }
 
     /**
-     * Makes sure `Page#killAndReopenApp` doesn't run into problems with tooltips by opening
+     * Makes sure [killApp] doesn't run into problems with tooltips by opening
      * Recent Apps and dismissing before any test runs. Only needs to run once per test process.
      */
     private fun removeRecentAppsTooltips() {
-        if (Build.VERSION.SDK_INT == 30) {
-            val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
 
-            // Open dummy activity so there is something in Recent Apps
-            InstrumentationRegistry.getInstrumentation().targetContext.apply {
-                val intent = Intent(this.applicationContext, DummyActivity::class.java)
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                startActivity(intent)
-                device.wait(Until.hasObject(By.textStartsWith(DummyActivity.TEXT)), 1000)
-            }
-
-            // Open Recent Apps and dismiss tooltips if they're there
-            device.pressRecentApps()
-            val foundToolTip = device.wait(
-                Until.hasObject(By.textStartsWith("Select text and images to copy")),
-                1000
-            )
-            if (foundToolTip) {
-                device.pressBack() // the first time we open the list of recent apps, a tooltip might be displayed and we need to close it
-            }
-
-            // Close recent apps
-            device.pressBack()
-
-            // Close dummy activity
-            device.pressBack()
-        } else {
-            throw NotImplementedError()
+        // Open dummy activity so there is something in Recent Apps
+        InstrumentationRegistry.getInstrumentation().targetContext.apply {
+            val intent = Intent(this.applicationContext, DummyActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            device.wait(Until.hasObject(By.textStartsWith(DummyActivity.TEXT)), 1000)
         }
+
+        // Open Recent Apps and dismiss tooltips if they're there
+        device.pressRecentApps()
+        val foundToolTip = device.wait(
+            Until.hasObject(By.textStartsWith("Select text and images to copy")),
+            1000
+        )
+        if (foundToolTip) {
+            device.pressBack() // the first time we open the list of recent apps, a tooltip might be displayed and we need to close it
+        }
+
+        // Close recent apps
+        device.pressBack()
+
+        // Close dummy activity
+        device.pressBack()
     }
 
     companion object {
-        private val SUPPORTED_SDKS = listOf(30)
+        private val SUPPORTED_SDKS = listOf(30, 34)
     }
 }
