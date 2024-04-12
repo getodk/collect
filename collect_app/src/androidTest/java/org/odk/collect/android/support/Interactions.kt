f9e2b4722b900b21c1a6be30d0ccc1008f43af08
Callum Stott <callum@seadowg.com>
2024-04-12 13:39:08 +0100

    Add docs for new methods

--- collect_app/src/androidTest/java/org/odk/collect/android/support/Interactions.kt
+++ collect_app/src/androidTest/java/org/odk/collect/android/support/Interactions.kt
@@ -10,6 +10,12 @@ import org.odk.collect.android.support.WaitFor.tryAgainOnFail
 
 object Interactions {
 
+    /**
+     * Click on the view matched by [view]. The root to use can optionally be specified with
+     * [root] (otherwise Espresso will use heuristics to determine the most likely root). If
+     * initially clicking on the view fails, this will then attempt to scroll to the view and
+     * retry the click.
+     */
     fun clickOn(view: Matcher<View>, root: Matcher<Root>? = null) {
         val onView = if (root != null) {
             onView(view).inRoot(root)
@@ -24,6 +30,14 @@ object Interactions {
         }
     }
 
+    /**
+     * Like [clickOn], but an [assertion] can be made after the click. If this fails, the click
+     * action will be reattempted.
+     *
+     * This can be useful in cases where [clickOn] itself appears to succeed, but the test fails
+     * because the click never actually occurs (most likely due to some flakiness in
+     * [androidx.test.espresso.action.ViewActions.click]).
+     */
     fun clickOn(view: Matcher<View>, root: Matcher<Root>? = null, assertion: () -> Unit) {
         tryAgainOnFail {
             clickOn(view, root)
