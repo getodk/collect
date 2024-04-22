da99164a19fa04eaead1a1eb9baed23e6e704f95
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2024-04-22 22:15:04 +0200

    Improved the test so that it fails because of the incorrect path displayed in the hierarchy

--- collect_app/src/androidTest/java/org/odk/collect/android/feature/formentry/FormHierarchyTest.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/feature/formentry/FormHierarchyTest.java
@@ -197,6 +197,7 @@ public class FormHierarchyTest {
                 .clickGoToArrow()
                 .clickGoUpIcon()
                 .clickGoUpIcon()
+                .assertText("Outer")
                 .assertNotRemovableGroup();
     }
 }
