fd6a5e3c54c8a36f4f03b6c86a144cc0f56d566b
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2024-06-27 08:59:09 +0200

    Naming improvements

--- androidshared/src/main/java/org/odk/collect/androidshared/async/TrackableWorker.kt
+++ androidshared/src/main/java/org/odk/collect/androidshared/async/TrackableWorker.kt
@@ -12,13 +12,13 @@ class TrackableWorker(private val scheduler: Scheduler) {
     private val _isWorking = MutableNonNullLiveData(false)
     val isWorking: NonNullLiveData<Boolean> = _isWorking
 
-    private var counter = AtomicInteger(0)
+    private var activeBackgroundJobsCounter = AtomicInteger(0)
 
     fun <T> immediate(background: Supplier<T>, foreground: Consumer<T>) {
-        counter.incrementAndGet()
+        activeBackgroundJobsCounter.incrementAndGet()
         _isWorking.value = true
         scheduler.immediate(background) { result ->
-            if (counter.decrementAndGet() == 0) {
+            if (activeBackgroundJobsCounter.decrementAndGet() == 0) {
                 _isWorking.value = false
             }
             foreground.accept(result)
