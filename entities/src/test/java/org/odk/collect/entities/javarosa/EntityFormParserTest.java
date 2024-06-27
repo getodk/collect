bde3f87503d7a4e7c6518a71fd61686ed906a5a9
Callum Stott <callum@seadowg.com>
2024-06-27 10:46:59 +0100

    Move entities tests to Collect

---
+++ entities/src/test/java/org/odk/collect/entities/javarosa/EntityFormParserTest.java
@@ -0,0 +1,46 @@
+package org.odk.collect.entities.javarosa;
+
+import org.javarosa.core.model.data.IntegerData;
+import org.javarosa.core.model.instance.TreeElement;
+import org.javarosa.entities.EntityAction;
+import org.javarosa.entities.internal.EntityFormParser;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.javarosa.entities.internal.EntityConstants.ATTRIBUTE_CREATE;
+import static org.javarosa.entities.internal.EntityConstants.ATTRIBUTE_UPDATE;
+import static org.javarosa.entities.internal.EntityConstants.ELEMENT_ENTITY;
+import static org.javarosa.entities.internal.EntityConstants.ELEMENT_LABEL;
+
+public class EntityFormParserTest {
+
+    @Test
+    public void parseAction_findsCreateWithTrueString() {
+        TreeElement entityElement = new TreeElement(ELEMENT_ENTITY);
+        entityElement.setAttribute(null, ATTRIBUTE_CREATE, "true");
+
+        EntityAction action = EntityFormParser.parseAction(entityElement);
+        assertThat(action, equalTo(EntityAction.CREATE));
+    }
+
+    @Test
+    public void parseAction_findsUpdateWithTrueString() {
+        TreeElement entityElement = new TreeElement(ELEMENT_ENTITY);
+        entityElement.setAttribute(null, ATTRIBUTE_UPDATE, "true");
+
+        EntityAction dataset = EntityFormParser.parseAction(entityElement);
+        assertThat(dataset, equalTo(EntityAction.UPDATE));
+    }
+
+    @Test
+    public void parseLabel_whenLabelIsAnInt_convertsToString() {
+        TreeElement labelElement = new TreeElement(ELEMENT_LABEL);
+        labelElement.setAnswer(new IntegerData(0));
+        TreeElement entityElement = new TreeElement(ELEMENT_ENTITY);
+        entityElement.addChild(labelElement);
+
+        String label = EntityFormParser.parseLabel(entityElement);
+        assertThat(label, equalTo("0"));
+    }
+}
