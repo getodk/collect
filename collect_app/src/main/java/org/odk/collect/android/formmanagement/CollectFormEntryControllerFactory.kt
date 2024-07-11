474f3ee46fa7793605c664cb3d4826f30a82da38
Callum Stott <callum@seadowg.com>
2024-07-11 12:25:07 +0100

    Spike out name query filter strategy for entities

--- collect_app/src/main/java/org/odk/collect/android/formmanagement/CollectFormEntryControllerFactory.kt
+++ collect_app/src/main/java/org/odk/collect/android/formmanagement/CollectFormEntryControllerFactory.kt
@@ -7,6 +7,7 @@ import org.odk.collect.android.application.Collect
 import org.odk.collect.android.dynamicpreload.ExternalDataManagerImpl
 import org.odk.collect.android.dynamicpreload.handler.ExternalDataHandlerPull
 import org.odk.collect.android.tasks.FormLoaderTask.FormEntryControllerFactory
+import org.odk.collect.entities.javarosa.filter.LocalEntitiesFilterStrategy
 import org.odk.collect.entities.javarosa.finalization.EntityFormFinalizationProcessor
 import java.io.File
 
@@ -20,7 +21,11 @@ class CollectFormEntryControllerFactory :
         val externalDataHandlerPull = ExternalDataHandlerPull(externalDataManager)
         formDef.evaluationContext.addFunctionHandler(externalDataHandlerPull)
 
+        val projectsDataService = Collect.getInstance().component.currentProjectProvider()
+        val projectId = projectsDataService.getCurrentProject().uuid
+
         return FormEntryController(FormEntryModel(formDef)).also {
+            it.addFilterStrategy(LocalEntitiesFilterStrategy(Collect.getInstance().component.entitiesRepositoryProvider().create(projectId)))
             it.addPostProcessor(EntityFormFinalizationProcessor())
         }
     }
---
+++ entities/src/main/java/org/odk/collect/entities/javarosa/filter/LocalEntitiesFilterStrategy.kt
@@ -0,0 +1,66 @@
+package org.odk.collect.entities.javarosa.filter
+
+import org.javarosa.core.model.CompareToNodeExpression
+import org.javarosa.core.model.condition.EvaluationContext
+import org.javarosa.core.model.condition.FilterStrategy
+import org.javarosa.core.model.instance.DataInstance
+import org.javarosa.core.model.instance.TreeReference
+import org.javarosa.xpath.expr.XPathEqExpr
+import org.javarosa.xpath.expr.XPathExpression
+import org.odk.collect.entities.storage.EntitiesRepository
+import org.odk.collect.entities.storage.Entity
+import java.util.function.Supplier
+
+class LocalEntitiesFilterStrategy(private val entitiesRepository: EntitiesRepository) :
+    FilterStrategy {
+    override fun filter(
+        sourceInstance: DataInstance<*>,
+        nodeSet: TreeReference,
+        predicate: XPathExpression,
+        children: MutableList<TreeReference>,
+        evaluationContext: EvaluationContext,
+        next: Supplier<MutableList<TreeReference>>
+    ): List<TreeReference> {
+        val candidate = CompareToNodeExpression.parse(predicate)
+
+        return when (val original = candidate?.original) {
+            is XPathEqExpr -> {
+                if (original.isEqual) {
+                    when {
+                        (candidate.nodeSide.steps[0].name.name == "name") -> {
+                            val value = candidate.evalContextSide(sourceInstance, evaluationContext)
+                            val entity =
+                                entitiesRepository.getById(
+                                    sourceInstance.instanceId,
+                                    value as String
+                                )
+
+                            if (entity != null) {
+                                listOf(convertToReference(nodeSet, entity))
+                            } else {
+                                emptyList()
+                            }
+                        }
+
+                        else -> next.get()
+                    }
+                } else {
+                    next.get()
+                }
+            }
+
+            else -> {
+                next.get()
+            }
+        }
+    }
+
+    private fun convertToReference(
+        nodeSet: TreeReference,
+        entity: Entity.Saved
+    ): TreeReference {
+        return nodeSet.clone().also {
+            it.setMultiplicity(it.size() - 1, entity.index)
+        }
+    }
+}
