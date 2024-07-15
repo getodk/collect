c7a928f647dc831f37d371bfb0a2e99876ff6882
Callum Stott <callum@seadowg.com>
2024-07-15 18:49:02 +0100

    Add test for no matching entities case

--- entities/src/test/java/org/odk/collect/entities/javarosa/filter/LocalEntitiesFilterStrategyTest.kt
+++ entities/src/test/java/org/odk/collect/entities/javarosa/filter/LocalEntitiesFilterStrategyTest.kt
@@ -3,7 +3,11 @@ package org.odk.collect.entities.javarosa.filter
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
 import org.javarosa.core.model.FormDef
+import org.javarosa.core.model.condition.EvaluationContext
+import org.javarosa.core.model.condition.FilterStrategy
 import org.javarosa.core.model.data.StringData
+import org.javarosa.core.model.instance.DataInstance
+import org.javarosa.core.model.instance.TreeReference
 import org.javarosa.form.api.FormEntryController
 import org.javarosa.form.api.FormEntryModel
 import org.javarosa.test.BindBuilderXFormsElement.bind
@@ -17,15 +21,58 @@ import org.javarosa.test.XFormsElement.mainInstance
 import org.javarosa.test.XFormsElement.model
 import org.javarosa.test.XFormsElement.t
 import org.javarosa.test.XFormsElement.title
+import org.javarosa.xpath.expr.XPathExpression
 import org.junit.Test
 import org.odk.collect.entities.storage.InMemEntitiesRepository
+import java.util.function.Supplier
 
 class LocalEntitiesFilterStrategyTest {
 
+    private val entitiesRepository = InMemEntitiesRepository()
+    private val fallthroughFilterStrategy = FallthroughFilterStrategy()
+
     private val controllerSupplier: (FormDef) -> FormEntryController = { formDef ->
         FormEntryController(FormEntryModel(formDef)).also {
-            it.addFilterStrategy(LocalEntitiesFilterStrategy(InMemEntitiesRepository()))
+            it.addFilterStrategy(LocalEntitiesFilterStrategy(entitiesRepository))
+            it.addFilterStrategy(fallthroughFilterStrategy)
         }
+    }
+
+    @Test
+    fun `returns empty nodeset when no entity matches name`() {
+        entitiesRepository.addList("things")
+
+        val scenario = Scenario.init(
+            "Secondary instance form",
+            html(
+                head(
+                    title("Secondary instance form"),
+                    model(
+                        mainInstance(
+                            t(
+                                "data id=\"create-entity-form\"",
+                                t("question"),
+                                t("calculate")
+                            )
+                        ),
+                        instance(
+                            "things",
+                            t("item", t("label", "Thing"), t("name", "thing"))
+                        ),
+                        bind("/data/question").type("string"),
+                        bind("/data/calculate").type("string")
+                            .calculate("instance('things')/root/item[name='other']/label")
+                    )
+                ),
+                body(
+                    input("/data/calculate")
+                )
+            ),
+            controllerSupplier
+        )
+
+        assertThat(fallthroughFilterStrategy.fellThrough, equalTo(false))
+        assertThat(scenario.answerOf<StringData>("/data/calculate"), equalTo(null))
     }
 
     @Test
@@ -60,5 +107,23 @@ class LocalEntitiesFilterStrategyTest {
         )
 
         assertThat(scenario.answerOf<StringData>("/data/calculate").value, equalTo("Thing"))
+    }
+}
+
+class FallthroughFilterStrategy : FilterStrategy {
+
+    var fellThrough = false
+        private set
+
+    override fun filter(
+        sourceInstance: DataInstance<*>,
+        nodeSet: TreeReference,
+        predicate: XPathExpression,
+        children: MutableList<TreeReference>,
+        evaluationContext: EvaluationContext,
+        next: Supplier<MutableList<TreeReference>>
+    ): MutableList<TreeReference> {
+        fellThrough = true
+        return next.get()
     }
 }
