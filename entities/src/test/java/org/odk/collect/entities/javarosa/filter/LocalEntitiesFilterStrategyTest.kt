1dbbdfc2a972d368fbc6a9d4b78e6872156ce97d
Callum Stott <callum@seadowg.com>
2024-07-15 17:55:00 +0100

    Switch to using custom FormEntryController setup

--- entities/src/test/java/org/odk/collect/entities/javarosa/filter/LocalEntitiesFilterStrategyTest.kt
+++ entities/src/test/java/org/odk/collect/entities/javarosa/filter/LocalEntitiesFilterStrategyTest.kt
@@ -2,7 +2,10 @@ package org.odk.collect.entities.javarosa.filter
 
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
+import org.javarosa.core.model.FormDef
 import org.javarosa.core.model.data.StringData
+import org.javarosa.form.api.FormEntryController
+import org.javarosa.form.api.FormEntryModel
 import org.javarosa.test.BindBuilderXFormsElement.bind
 import org.javarosa.test.Scenario
 import org.javarosa.test.XFormsElement.body
@@ -14,45 +17,48 @@ import org.javarosa.test.XFormsElement.mainInstance
 import org.javarosa.test.XFormsElement.model
 import org.javarosa.test.XFormsElement.t
 import org.javarosa.test.XFormsElement.title
-import org.javarosa.xform.util.XFormUtils
 import org.junit.Test
 import org.odk.collect.entities.storage.InMemEntitiesRepository
-import java.io.InputStreamReader
 
 class LocalEntitiesFilterStrategyTest {
 
+    private val controllerSupplier: (FormDef) -> FormEntryController = { formDef ->
+        FormEntryController(FormEntryModel(formDef)).also {
+            it.addFilterStrategy(LocalEntitiesFilterStrategy(InMemEntitiesRepository()))
+        }
+    }
+
     @Test
     fun `does not effect name queries on non entity instances`() {
-        val xForm = html(
-            head(
-                title("Secondary instance form"),
-                model(
-                    mainInstance(
-                        t(
-                            "data id=\"create-entity-form\"",
-                            t("question"),
-                            t("calculate")
-                        )
-                    ),
-                    instance(
-                        "secondary",
-                        t("item", t("label", "Thing"), t("name", "thing"))
-                    ),
-                    bind("/data/question").type("string"),
-                    bind("/data/calculate").type("string")
-                        .calculate("instance('secondary')/root/item[name='thing']/label")
+        val scenario = Scenario.init(
+            "Secondary instance form",
+            html(
+                head(
+                    title("Secondary instance form"),
+                    model(
+                        mainInstance(
+                            t(
+                                "data id=\"create-entity-form\"",
+                                t("question"),
+                                t("calculate")
+                            )
+                        ),
+                        instance(
+                            "secondary",
+                            t("item", t("label", "Thing"), t("name", "thing"))
+                        ),
+                        bind("/data/question").type("string"),
+                        bind("/data/calculate").type("string")
+                            .calculate("instance('secondary')/root/item[name='thing']/label")
+                    )
+                ),
+                body(
+                    input("/data/calculate")
                 )
             ),
-            body(
-                input("/data/calculate")
-            )
+            controllerSupplier
         )
 
-        val xml = xForm.asXml()
-        val formDef = XFormUtils.getFormRaw(InputStreamReader(xml.byteInputStream()))
-        formDef.addFilterStrategy(LocalEntitiesFilterStrategy(InMemEntitiesRepository()))
-
-        val scenario = Scenario.init(formDef)
         assertThat(scenario.answerOf<StringData>("/data/calculate").value, equalTo("Thing"))
     }
 }
