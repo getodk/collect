91902805910566e8b697e3a6544875d44765aa90
Callum Stott <callum@seadowg.com>
2024-07-16 10:27:28 +0100

    Replace partials when querying by ID

--- entities/src/main/java/org/odk/collect/entities/javarosa/filter/LocalEntitiesFilterStrategy.kt
+++ entities/src/main/java/org/odk/collect/entities/javarosa/filter/LocalEntitiesFilterStrategy.kt
@@ -3,10 +3,13 @@ package org.odk.collect.entities.javarosa.filter
 import org.javarosa.core.model.CompareToNodeExpression
 import org.javarosa.core.model.condition.EvaluationContext
 import org.javarosa.core.model.condition.FilterStrategy
+import org.javarosa.core.model.data.StringData
 import org.javarosa.core.model.instance.DataInstance
+import org.javarosa.core.model.instance.TreeElement
 import org.javarosa.core.model.instance.TreeReference
 import org.javarosa.xpath.expr.XPathEqExpr
 import org.javarosa.xpath.expr.XPathExpression
+import org.odk.collect.entities.browser.EntityItemElement
 import org.odk.collect.entities.storage.EntitiesRepository
 import org.odk.collect.entities.storage.Entity
 import java.util.function.Supplier
@@ -40,7 +43,9 @@ class LocalEntitiesFilterStrategy(private val entitiesRepository: EntitiesReposi
                                 )
 
                             if (entity != null) {
-                                listOf(convertToReference(nodeSet, entity))
+                                val element = convertToElement(sourceInstance, entity)
+                                sourceInstance.replacePartialElements(listOf(element))
+                                listOf(element.ref)
                             } else {
                                 emptyList()
                             }
@@ -59,12 +64,30 @@ class LocalEntitiesFilterStrategy(private val entitiesRepository: EntitiesReposi
         }
     }
 
-    private fun convertToReference(
-        nodeSet: TreeReference,
+    private fun convertToElement(
+        sourceInstance: DataInstance<*>,
         entity: Entity.Saved
-    ): TreeReference {
-        return nodeSet.clone().also {
-            it.setMultiplicity(it.size() - 1, entity.index)
+    ): TreeElement {
+        val name = TreeElement(EntityItemElement.ID)
+        val label = TreeElement(EntityItemElement.LABEL)
+        val version = TreeElement(EntityItemElement.VERSION)
+
+        name.value = StringData(entity.id)
+        label.value = StringData(entity.label)
+        version.value = StringData(entity.version.toString())
+
+        val item = TreeElement("item", entity.index)
+        item.addChild(name)
+        item.addChild(label)
+        item.addChild(version)
+
+        entity.properties.forEach { property ->
+            val propertyElement = TreeElement(property.first)
+            propertyElement.value = StringData(property.second)
+            item.addChild(propertyElement)
         }
+
+        item.parent = sourceInstance.root
+        return item
     }
 }
--- entities/src/test/java/org/odk/collect/entities/javarosa/filter/LocalEntitiesFilterStrategyTest.kt
+++ entities/src/test/java/org/odk/collect/entities/javarosa/filter/LocalEntitiesFilterStrategyTest.kt
@@ -7,6 +7,7 @@ import org.javarosa.core.model.condition.EvaluationContext
 import org.javarosa.core.model.condition.FilterStrategy
 import org.javarosa.core.model.data.StringData
 import org.javarosa.core.model.instance.DataInstance
+import org.javarosa.core.model.instance.TreeElement
 import org.javarosa.core.model.instance.TreeReference
 import org.javarosa.form.api.FormEntryController
 import org.javarosa.form.api.FormEntryModel
@@ -22,12 +23,13 @@ import org.javarosa.test.XFormsElement.model
 import org.javarosa.test.XFormsElement.t
 import org.javarosa.test.XFormsElement.title
 import org.javarosa.xform.parse.ExternalInstanceParser
+import org.javarosa.xform.parse.ExternalInstanceParser.InstanceProvider
 import org.javarosa.xform.util.XFormUtils
 import org.javarosa.xpath.expr.XPathExpression
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import org.odk.collect.entities.javarosa.intance.LocalEntitiesExternalInstanceParserFactory
+import org.odk.collect.entities.javarosa.intance.LocalEntitiesInstanceProvider
 import org.odk.collect.entities.storage.Entity
 import org.odk.collect.entities.storage.InMemEntitiesRepository
 import java.util.function.Supplier
@@ -36,6 +38,8 @@ class LocalEntitiesFilterStrategyTest {
 
     private val entitiesRepository = InMemEntitiesRepository()
     private val fallthroughFilterStrategy = FallthroughFilterStrategy()
+    private val instanceProvider =
+        SpyInstanceProvider(LocalEntitiesInstanceProvider(::entitiesRepository))
 
     private val controllerSupplier: (FormDef) -> FormEntryController = { formDef ->
         FormEntryController(FormEntryModel(formDef)).also {
@@ -46,9 +50,11 @@ class LocalEntitiesFilterStrategyTest {
 
     @Before
     fun setup() {
-        XFormUtils.setExternalInstanceParserFactory(
-            LocalEntitiesExternalInstanceParserFactory(::entitiesRepository)
-        )
+        XFormUtils.setExternalInstanceParserFactory {
+            ExternalInstanceParser().also {
+                it.addInstanceProvider(instanceProvider)
+            }
+        }
     }
 
     @After
@@ -88,6 +94,42 @@ class LocalEntitiesFilterStrategyTest {
 
         assertThat(fallthroughFilterStrategy.fellThrough, equalTo(false))
         assertThat(scenario.answerOf<StringData>("/data/calculate").value, equalTo("Thing"))
+    }
+
+    @Test
+    fun `replaces partial elements when entity matches name`() {
+        entitiesRepository.save(
+            Entity.New("things", "thing", "Thing"),
+            Entity.New("things", "other", "Other")
+        )
+
+        Scenario.init(
+            "Secondary instance form",
+            html(
+                head(
+                    title("Secondary instance form"),
+                    model(
+                        mainInstance(
+                            t(
+                                "data id=\"create-entity-form\"",
+                                t("question"),
+                                t("calculate")
+                            )
+                        ),
+                        t("instance id=\"things\" src=\"jr://file-csv/things.csv\""),
+                        bind("/data/question").type("string"),
+                        bind("/data/calculate").type("string")
+                            .calculate("instance('things')/root/item[name='thing']/label")
+                    )
+                ),
+                body(
+                    input("/data/calculate")
+                )
+            ),
+            controllerSupplier
+        )
+
+        assertThat(instanceProvider.fullParsePerformed, equalTo(false))
     }
 
     @Test
@@ -225,7 +267,7 @@ class LocalEntitiesFilterStrategyTest {
     }
 }
 
-class FallthroughFilterStrategy : FilterStrategy {
+private class FallthroughFilterStrategy : FilterStrategy {
 
     var fellThrough = false
         private set
@@ -240,5 +282,26 @@ class FallthroughFilterStrategy : FilterStrategy {
     ): MutableList<TreeReference> {
         fellThrough = true
         return next.get()
+    }
+}
+
+private class SpyInstanceProvider(private val wrapped: InstanceProvider) : InstanceProvider {
+    var fullParsePerformed = false
+        private set
+
+    override fun get(instanceId: String, instanceSrc: String): TreeElement {
+        return wrapped.get(instanceId, instanceSrc)
+    }
+
+    override fun get(instanceId: String, instanceSrc: String, partial: Boolean): TreeElement {
+        if (!partial) {
+            fullParsePerformed = true
+        }
+
+        return wrapped.get(instanceId, instanceSrc, partial)
+    }
+
+    override fun isSupported(instanceId: String, instanceSrc: String): Boolean {
+        return wrapped.isSupported(instanceId, instanceSrc)
     }
 }
