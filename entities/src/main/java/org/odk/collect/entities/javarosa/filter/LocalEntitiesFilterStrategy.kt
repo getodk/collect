d4d8250b43e46697003e0fb867ce02f9918147d8
Callum Stott <callum@seadowg.com>
2024-07-25 11:50:04 +0100

    Add docs for filter strategy

--- entities/src/main/java/org/odk/collect/entities/javarosa/filter/LocalEntitiesFilterStrategy.kt
+++ entities/src/main/java/org/odk/collect/entities/javarosa/filter/LocalEntitiesFilterStrategy.kt
@@ -8,9 +8,17 @@ import org.javarosa.core.model.instance.TreeReference
 import org.javarosa.xpath.expr.XPathEqExpr
 import org.javarosa.xpath.expr.XPathExpression
 import org.odk.collect.entities.javarosa.intance.LocalEntitiesInstanceAdapter
+import org.odk.collect.entities.javarosa.intance.LocalEntitiesInstanceProvider
 import org.odk.collect.entities.storage.EntitiesRepository
 import java.util.function.Supplier
 
+/**
+ * A JavaRosa [FilterStrategy] that will use an [EntitiesRepository] to perform filters. For
+ * supported expressions, this prevents JavaRosa from using it's standard [FilterStrategy] chain
+ * which requires loading the whole secondary instance into memory (assuming that
+ * [LocalEntitiesInstanceProvider] or similar is used to take advantage of JavaRosa's partial
+ * parsing).
+ */
 class LocalEntitiesFilterStrategy(entitiesRepository: EntitiesRepository) :
     FilterStrategy {
 
