b1eaa7310c3c436df6edb6f609b76ea3524689d3
Callum Stott <callum@seadowg.com>
2024-08-06 11:36:51 +0100

    Convert test to Kotlin

---
+++ collect_app/src/androidTest/java/org/odk/collect/android/feature/instancemanagement/DeleteSavedFormTest.kt
@@ -0,0 +1,55 @@
+package org.odk.collect.android.feature.instancemanagement
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+import org.junit.runner.RunWith
+import org.odk.collect.android.R
+import org.odk.collect.android.support.pages.DeleteSavedFormPage
+import org.odk.collect.android.support.pages.MainMenuPage
+import org.odk.collect.android.support.rules.CollectTestRule
+import org.odk.collect.android.support.rules.TestRuleChain.chain
+import org.odk.collect.strings.R.string
+
+@RunWith(AndroidJUnit4::class)
+class DeleteSavedFormTest {
+
+    private val rule: CollectTestRule = CollectTestRule()
+
+    @get:Rule
+    val chain: RuleChain = chain().around(rule)
+
+    @Test
+    fun deletingAForm_removesFormFromFinalizedForms() {
+        rule.startAtMainMenu()
+            .copyForm("one-question.xml")
+            .startBlankForm("One Question")
+            .answerQuestion("what is your age", "30")
+            .swipeToEndScreen()
+            .clickFinalize()
+
+            .clickDeleteSavedForm()
+            .clickForm("One Question")
+            .clickDeleteSelected(1)
+            .clickDeleteForms()
+            .checkIsSnackbarWithMessageDisplayed(string.file_deleted_ok, 1)
+            .assertTextDoesNotExist("One Question")
+            .pressBack(MainMenuPage())
+            .assertNumberOfFinalizedForms(0)
+    }
+
+    @Test
+    fun accessingSortMenuInDeleteSavedInstancesShouldNotCrashTheAppAfterRotatingTheDevice() {
+        rule.startAtMainMenu()
+            .copyForm("one-question.xml")
+            .startBlankForm("One Question")
+            .answerQuestion("what is your age", "30")
+            .swipeToEndScreen()
+            .clickFinalize()
+            .clickDeleteSavedForm()
+            .rotateToLandscape(DeleteSavedFormPage())
+            .clickOnId(R.id.menu_sort)
+            .assertText(string.sort_by)
+    }
+}
