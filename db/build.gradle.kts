8c5d1a08624fb3ea9586d2ff7bff7c9982a4650b
Callum Stott <callum@seadowg.com>
2024-08-16 11:45:11 +0100

    Create new module for db

---
+++ db/build.gradle.kts
@@ -0,0 +1,45 @@
+import dependencies.Dependencies
+import dependencies.Versions
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+}
+
+apply(from = "../config/quality.gradle")
+
+android {
+    namespace = "org.odk.collect.db"
+
+    compileSdk = Versions.android_compile_sdk
+
+    defaultConfig {
+        minSdk = Versions.android_min_sdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+}
+
+dependencies {
+    coreLibraryDesugaring(Dependencies.desugar)
+    implementation(Dependencies.kotlin_stdlib)
+}
