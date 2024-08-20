f6fe384a913c655c0c3278d0067cbccb3cbd6067
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2024-08-20 15:11:42 +0200

    Added a test to verify that forms sent manually are sent in oldest first order

--- collect_app/src/androidTest/java/org/odk/collect/android/feature/instancemanagement/SendFinalizedFormTest.kt
+++ collect_app/src/androidTest/java/org/odk/collect/android/feature/instancemanagement/SendFinalizedFormTest.kt
@@ -1,10 +1,15 @@
 package org.odk.collect.android.feature.instancemanagement
 
+import android.app.Application
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
 import org.junit.runner.RunWith
+import org.odk.collect.android.injection.DaggerUtils
 import org.odk.collect.android.support.TestDependencies
 import org.odk.collect.android.support.pages.FormEntryPage.QuestionAndAnswer
 import org.odk.collect.android.support.pages.MainMenuPage
@@ -14,6 +19,7 @@ import org.odk.collect.android.support.pages.SendFinalizedFormPage
 import org.odk.collect.android.support.rules.CollectTestRule
 import org.odk.collect.android.support.rules.TestRuleChain.chain
 import org.odk.collect.androidtest.RecordedIntentsRule
+import org.odk.collect.forms.instances.Instance
 
 @RunWith(AndroidJUnit4::class)
 class SendFinalizedFormTest {
@@ -133,5 +139,41 @@ class SendFinalizedFormTest {
             .clickSendSelected()
             .clickOK(SendFinalizedFormPage())
             .assertQuantityText(org.odk.collect.strings.R.plurals.forms_ready_to_send, 1, 1)
+    }
+
+    @Test
+    fun formsAreSentInOldestFirstOrder() {
+        var savedForms = emptyList<Instance>()
+
+        rule.withProject(testDependencies.server.url)
+            .copyForm("one-question.xml", testDependencies.server.hostName)
+            .startBlankForm("One Question")
+            .fillOutAndFinalize(QuestionAndAnswer("what is your age", "123"))
+            .startBlankForm("One Question")
+            .fillOutAndFinalize(QuestionAndAnswer("what is your age", "124")).also {
+                val application = ApplicationProvider.getApplicationContext<Application>()
+                val component = DaggerUtils.getComponent(application)
+                val currentProject = DaggerUtils.getComponent(application).currentProjectProvider().getCurrentProject()
+                val instancesRepository = component.instancesRepositoryProvider().create(currentProject.uuid)
+                savedForms = instancesRepository.all.sortedBy { it.lastStatusChangeDate }
+            }
+
+            .clickSendFinalizedForm(2)
+            .selectForm(1)
+            .selectForm(0)
+            .clickSendSelected()
+            .clickOK(SendFinalizedFormPage())
+
+        val sentForms = testDependencies.server.submissions
+
+        assertThat(
+            sentForms[0].absolutePath,
+            equalTo(savedForms[0].instanceFilePath)
+        )
+
+        assertThat(
+            sentForms[1].absolutePath,
+            equalTo(savedForms[1].instanceFilePath)
+        )
     }
 }
