5c11199d78f033baa303d15a309620c49a01bbd4
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2024-08-26 13:57:06 +0200

    Added a commnet

--- collect_app/src/main/java/org/odk/collect/android/javarosawrapper/XFormParser.kt
+++ collect_app/src/main/java/org/odk/collect/android/javarosawrapper/XFormParser.kt
@@ -9,6 +9,11 @@ import java.io.FileNotFoundException
 import java.io.InputStream
 import java.io.InputStreamReader
 
+/**
+ * This is a basic XML parser for forms, designed to read the content without
+ * the advanced parsing typically performed by JavaRosa. Currently, it provides
+ * simple functionality but can be extended to meet future requirements.
+ */
 object XFormParser {
     @JvmStatic
     @Throws(FileNotFoundException::class, XmlPullParserException::class)
