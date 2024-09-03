b3aa991fb35633c3085d50a58fd033b18c085806
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2024-09-03 20:36:05 +0200

    Added comment headers

--- google-maps/src/main/java/org/odk/collect/googlemaps/scaleview/MapScaleModel.java
+++ google-maps/src/main/java/org/odk/collect/googlemaps/scaleview/MapScaleModel.java
@@ -1,3 +1,7 @@
+/*
+ * This file includes code from MapScaleView (https://github.com/pengrad/MapScaleView),
+ * licensed under the Apache License, Version 2.0.
+ */
 package org.odk.collect.googlemaps.scaleview;
 
 class MapScaleModel {
