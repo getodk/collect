9d2ec022192cb964dce3bf3cd040ec62b74a73fa
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2024-09-13 17:19:16 +0200

    Added failing test

--- collect_app/src/androidTest/java/org/odk/collect/android/feature/instancemanagement/DeleteSavedFormTest.kt
+++ collect_app/src/androidTest/java/org/odk/collect/android/feature/instancemanagement/DeleteSavedFormTest.kt
@@ -10,6 +10,7 @@ import org.odk.collect.android.support.TestDependencies
 import org.odk.collect.android.support.pages.DeleteSavedFormPage
 import org.odk.collect.android.support.pages.FormEntryPage
 import org.odk.collect.android.support.pages.MainMenuPage
+import org.odk.collect.android.support.pages.SendFinalizedFormPage
 import org.odk.collect.android.support.rules.CollectTestRule
 import org.odk.collect.android.support.rules.TestRuleChain.chain
 import org.odk.collect.strings.R.string
@@ -44,15 +45,35 @@ class DeleteSavedFormTest {
     }
 
     @Test
-    fun whenFinalizedFormHasCreatedALocalEntity_doesNotAppearInListToDelete() {
+    fun whenFinalizedButNotSentFormHasCreatedALocalEntity_doesNotAppearInListToDelete() {
         testDependencies.server.addForm("one-question-entity-registration.xml")
 
         rule.withMatchExactlyProject(testDependencies.server.url)
+            // Drafts can be deleted
             .startBlankForm("One Question Entity Registration")
-            .fillOutAndFinalize(FormEntryPage.QuestionAndAnswer("Name", "Logan Roy"))
+            .fillOutAndSave(FormEntryPage.QuestionAndAnswer("Name", "Logan Roy"))
+            .clickDeleteSavedForm()
+            .assertText("One Question Entity Registration")
+            .pressBack(MainMenuPage())
 
+            // Finalized forms can not be deleted
+            .clickDrafts(1)
+            .clickOnForm("One Question Entity Registration")
+            .clickGoToEnd()
+            .clickFinalize()
             .clickDeleteSavedForm()
             .assertTextDoesNotExist("One Question Entity Registration")
+            .pressBack(MainMenuPage())
+
+            // Sent forms can be deleted
+            .clickSendFinalizedForm(1)
+            .clickSelectAll()
+            .clickSendSelected()
+            .clickOK(SendFinalizedFormPage())
+            .pressBack(MainMenuPage())
+            .clickDeleteSavedForm()
+            .assertText("One Question Entity Registration")
+            .pressBack(MainMenuPage())
     }
 
     @Test
