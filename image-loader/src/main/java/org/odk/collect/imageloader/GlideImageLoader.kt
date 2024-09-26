ad93179212128868d1a97971999c615cfdb27414
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2024-09-26 14:29:02 +0200

    Improved caching images with Glide

--- image-loader/src/main/java/org/odk/collect/imageloader/GlideImageLoader.kt
+++ image-loader/src/main/java/org/odk/collect/imageloader/GlideImageLoader.kt
@@ -9,6 +9,7 @@ import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.Target
+import com.bumptech.glide.signature.ObjectKey
 import org.odk.collect.imageloader.svg.SvgSoftwareLayerSetter
 import java.io.File
 
@@ -60,6 +61,7 @@ class GlideImageLoader : ImageLoader {
         } else {
             Glide.with(imageView)
                 .load(imageFile)
+                .signature(ObjectKey(imageFile.lastModified()))
                 .listener(object : RequestListener<Drawable> {
                     override fun onLoadFailed(
                         e: GlideException?,
