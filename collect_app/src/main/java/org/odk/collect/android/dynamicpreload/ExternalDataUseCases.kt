bd3fcc1b71396aeda5d4f32ceaa35190b8bd7739
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2024-10-09 09:14:37 +0200

    Determine if a form uses search or pulldata based just on the presence of DynamicPreloadExtra not its value

--- collect_app/src/main/java/org/odk/collect/android/dynamicpreload/ExternalDataUseCases.kt
+++ collect_app/src/main/java/org/odk/collect/android/dynamicpreload/ExternalDataUseCases.kt
@@ -16,7 +16,7 @@ object ExternalDataUseCases {
         isCancelled: Supplier<Boolean>,
         progressReporter: Consumer<Function<Resources, String>>
     ) {
-        if (form.extras.get(DynamicPreloadExtra::class.java)?.usesDynamicPreload != true) {
+        if (form.extras.get(DynamicPreloadExtra::class.java) == null) {
             return
         }
 
