b2e8c2d9a260281ddff42bfd46c9de527e44b0c7
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2024-10-09 10:21:41 +0200

    Fixed tests

--- collect_app/src/test/java/org/odk/collect/android/dynamicpreload/DynamicPreloadExtraTest.kt
+++ collect_app/src/test/java/org/odk/collect/android/dynamicpreload/DynamicPreloadExtraTest.kt
@@ -1,7 +1,7 @@
 package org.odk.collect.android.dynamicpreload
 
 import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.instanceOf
 import org.javarosa.core.util.externalizable.ExtUtil
 import org.junit.Test
 
@@ -9,12 +9,12 @@ class DynamicPreloadExtraTest {
 
     @Test
     fun `can be externalized`() {
-        val extra = DynamicPreloadExtra(true)
+        val extra = DynamicPreloadExtra()
 
         val external = ExtUtil.serialize(extra)
         val deserialized =
             ExtUtil.deserialize(external, DynamicPreloadExtra::class.java) as DynamicPreloadExtra
 
-        assertThat(deserialized.usesDynamicPreload, equalTo(true))
+        assertThat(deserialized, instanceOf(DynamicPreloadExtra::class.java))
     }
 }
