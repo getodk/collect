61c9dd72da9800124707749bd7ec4ed3b7f2705b
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2024-10-24 09:26:12 +0200

    Code improvements in BooleanChangeLock

--- test-shared/src/main/java/org/odk/collect/testshared/BooleanChangeLock.kt
+++ test-shared/src/main/java/org/odk/collect/testshared/BooleanChangeLock.kt
@@ -4,22 +4,28 @@ import org.odk.collect.shared.locks.ChangeLock
 import java.util.function.Function
 
 class BooleanChangeLock : ChangeLock {
-    private var isLocked = false
+    private var locked = false
 
     override fun <T> withLock(function: Function<Boolean, T>): T {
-        return function.apply(!isLocked)
+        val acquired = tryLock()
+
+        return try {
+            function.apply(acquired)
+        } finally {
+            unlock()
+        }
     }
 
     override fun tryLock(): Boolean {
-        if (!isLocked) {
-            isLocked = true
-            return true
-        } else {
+        if (locked) {
             return false
+        } else {
+            locked = true
+            return true
         }
     }
 
     override fun unlock() {
-        isLocked = false
+        locked = false
     }
 }
