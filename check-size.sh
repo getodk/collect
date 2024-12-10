89d4e9d46d60157031eb76f1e949a72fccfefb79
Callum Stott <callum@seadowg.com>
2024-12-10 15:50:38 +0000

    Bump expected APK size

--- check-size.sh
+++ check-size.sh
@@ -1,6 +1,6 @@
 set -e
 
-if [ $(ls -l collect_app/build/outputs/apk/selfSignedRelease/*.apk | awk '{print $5}') -gt 11700000 ];then
+if [ $(ls -l collect_app/build/outputs/apk/selfSignedRelease/*.apk | awk '{print $5}') -gt 11800000 ];then
   echo "APK increased to $(ls -l collect_app/build/outputs/apk/selfSignedRelease/*.apk | awk '{print $5}') bytes!"
   exit 1
 fi
