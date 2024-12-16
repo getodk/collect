cb54f901d0cbf3e61f64152fda73dced7de46c73
Callum Stott <callum@seadowg.com>
2024-12-16 14:00:06 +0000

    Don't show theme setting in access control

--- collect_app/src/main/java/org/odk/collect/android/preferences/screens/UserSettingsAccessPreferencesFragment.java
+++ collect_app/src/main/java/org/odk/collect/android/preferences/screens/UserSettingsAccessPreferencesFragment.java
@@ -2,7 +2,11 @@ package org.odk.collect.android.preferences.screens;
 
 import android.os.Bundle;
 
+import androidx.preference.PreferenceCategory;
+
 import org.odk.collect.android.R;
+import org.odk.collect.android.application.FeatureFlags;
+import org.odk.collect.settings.keys.ProtectedProjectKeys;
 
 public class UserSettingsAccessPreferencesFragment extends BaseAdminPreferencesFragment {
 
@@ -10,5 +14,10 @@ public class UserSettingsAccessPreferencesFragment extends BaseAdminPreferencesF
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         super.onCreatePreferences(savedInstanceState, rootKey);
         setPreferencesFromResource(R.xml.user_settings_access_preferences, rootKey);
+
+        if (FeatureFlags.NO_THEME_SETTING) {
+            PreferenceCategory category = (PreferenceCategory) getPreferenceScreen().getPreference(0);
+            category.removePreference(findPreference(ProtectedProjectKeys.KEY_APP_THEME));
+        }
     }
 }
