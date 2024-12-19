0d67890a71de3bd5399551e14056320c7c7f7c12
Callum Stott <callum@seadowg.com>
2024-12-19 16:01:54 +0000

    Optimize imports

--- collect_app/src/main/java/org/odk/collect/android/mainmenu/MainMenuActivity.kt
+++ collect_app/src/main/java/org/odk/collect/android/mainmenu/MainMenuActivity.kt
@@ -8,7 +8,6 @@ import org.odk.collect.android.R
 import org.odk.collect.android.activities.ActivityUtils
 import org.odk.collect.android.activities.CrashHandlerActivity
 import org.odk.collect.android.activities.FirstLaunchActivity
-import org.odk.collect.android.application.FeatureFlags
 import org.odk.collect.android.injection.DaggerUtils
 import org.odk.collect.android.projects.ProjectSettingsDialog
 import org.odk.collect.android.utilities.ThemeUtils
