05959fcc4ebd230a93f324fb3371b3d9f57063e8
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2025-01-15 01:02:01 +0100

    Use the new #zoomToCurrentLocation method

--- geo/src/main/java/org/odk/collect/geo/selection/SelectionMapFragment.kt
+++ geo/src/main/java/org/odk/collect/geo/selection/SelectionMapFragment.kt
@@ -189,7 +189,7 @@ class SelectionMapFragment(
         map = newMapFragment
 
         binding.zoomToLocation.setMultiClickSafeOnClickListener {
-            map.zoomToPoint(map.gpsLocation, true)
+            map.zoomToCurrentLocation(map.gpsLocation)
         }
 
         binding.zoomToBounds.setMultiClickSafeOnClickListener {
@@ -368,7 +368,7 @@ class SelectionMapFragment(
                 map.zoomToBoundingBox(points, 0.8, false)
             } else {
                 map.setGpsLocationListener { point ->
-                    map.zoomToPoint(point, true)
+                    map.zoomToCurrentLocation(point)
                     map.setGpsLocationListener(null)
                 }
             }
