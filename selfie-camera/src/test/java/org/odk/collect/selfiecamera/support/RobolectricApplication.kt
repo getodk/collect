abb5ddad2129f38bc6bf77aeec212e2f9f67322e
Callum Stott <callum@seadowg.com>
2025-01-28 17:56:01 +0000

    Fix module tests

--- selfie-camera/src/test/java/org/odk/collect/selfiecamera/support/RobolectricApplication.kt
+++ selfie-camera/src/test/java/org/odk/collect/selfiecamera/support/RobolectricApplication.kt
@@ -1,10 +1,16 @@
 package org.odk.collect.selfiecamera.support
 
 import android.app.Application
+import org.odk.collect.androidshared.ui.ToastUtils
 import org.odk.collect.selfiecamera.SelfieCameraDependencyComponent
 import org.odk.collect.selfiecamera.SelfieCameraDependencyComponentProvider
 
 class RobolectricApplication : Application(), SelfieCameraDependencyComponentProvider {
 
     override lateinit var selfieCameraDependencyComponent: SelfieCameraDependencyComponent
+
+    override fun onCreate() {
+        super.onCreate()
+        ToastUtils.setApplication(this)
+    }
 }
