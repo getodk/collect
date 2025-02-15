5320f9c4726a04fed1488ae8f3ed92ff48c26820
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2025-02-15 20:29:39 +0100

    Fixed touch tracking in the range slider

--- collect_app/src/main/java/org/odk/collect/android/views/TrackingTouchSlider.kt
+++ collect_app/src/main/java/org/odk/collect/android/views/TrackingTouchSlider.kt
@@ -4,8 +4,6 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.ColorStateList
 import android.util.AttributeSet
-import android.view.MotionEvent
-import android.view.View
 import com.google.android.material.R
 import com.google.android.material.color.MaterialColors
 import com.google.android.material.slider.Slider
@@ -27,24 +25,16 @@ class TrackingTouchSlider(
     init {
         addOnSliderTouchListener(this)
         setLabelFormatter(null)
-        setOnTouchListener { v: View, event: MotionEvent ->
-            val action = event.action
-            when (action) {
-                MotionEvent.ACTION_DOWN -> v.parent.requestDisallowInterceptTouchEvent(
-                    true
-                )
+        addOnSliderTouchListener(object : OnSliderTouchListener {
+            override fun onStartTrackingTouch(slider: Slider) {}
 
-                MotionEvent.ACTION_UP -> {
-                    v.parent.requestDisallowInterceptTouchEvent(false)
-                    setTickActiveTintList(defaultTickActiveTintList)
-                    setThumbWidth(defaultThumbWidth)
-                    setThumbTrackGapSize(defaultThumbTrackGapSize)
-                    listener?.onValueChange(this, value, true)
-                }
+            override fun onStopTrackingTouch(slider: Slider) {
+                setTickActiveTintList(defaultTickActiveTintList)
+                setThumbWidth(defaultThumbWidth)
+                setThumbTrackGapSize(defaultThumbTrackGapSize)
+                listener?.onValueChange(this@TrackingTouchSlider, value, true)
             }
-            v.onTouchEvent(event)
-            true
-        }
+        })
     }
 
     @SuppressLint("RestrictedApi")
