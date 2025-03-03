6b6a230b77727dc0d29ed4254cd4dba1e085b799
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2025-03-03 23:59:42 +0100

    Factored out WidgetAnswerTextView

--- collect_app/src/main/java/org/odk/collect/android/widgets/BarcodeWidgetAnswer.kt
+++ collect_app/src/main/java/org/odk/collect/android/widgets/BarcodeWidgetAnswer.kt
@@ -1,7 +1,6 @@
 package org.odk.collect.android.widgets
 
 import android.content.Context
-import android.util.TypedValue
 import android.view.LayoutInflater
 import org.odk.collect.android.databinding.BarcodeWidgetAnswerBinding
 
@@ -14,10 +13,6 @@ class BarcodeWidgetAnswer(context: Context) : WidgetAnswer(context) {
 
     override fun getAnswer(): String {
         return binding.answer.text.toString()
-    }
-
-    override fun setTextSize(textSize: Float) {
-        binding.answer.setTextSize(TypedValue.COMPLEX_UNIT_DIP, textSize)
     }
 
     // Remove control characters, invisible characters and unused code points.
