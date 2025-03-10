b6dcce9830824fe6d1ad551894ddcc9216f68325
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2025-03-10 23:16:30 +0100

    Removed redundant code

--- collect_app/src/main/java/org/odk/collect/android/injection/config/AppDependencyComponent.java
+++ collect_app/src/main/java/org/odk/collect/android/injection/config/AppDependencyComponent.java
@@ -85,7 +85,6 @@ import org.odk.collect.maps.layers.ReferenceLayerRepository;
 import org.odk.collect.openrosa.http.OpenRosaHttpInterface;
 import org.odk.collect.permissions.PermissionsChecker;
 import org.odk.collect.permissions.PermissionsProvider;
-import org.odk.collect.projects.ProjectCreator;
 import org.odk.collect.projects.ProjectsRepository;
 import org.odk.collect.settings.ODKAppSettingsImporter;
 import org.odk.collect.settings.SettingsProvider;
@@ -311,6 +310,4 @@ public interface AppDependencyComponent {
     ProjectDependencyModuleFactory projectDependencyModuleFactory();
 
     ExternalWebPageHelper externalWebPageHelper();
-
-    ProjectCreator projectCreator();
 }
