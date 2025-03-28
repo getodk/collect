380a2a89c43e6eed0e3dbc28bf92c5ecdd67da57
Callum Stott <callum@seadowg.com>
2025-03-28 12:22:24 +0000

    Add basic implementation for MLKit barcode scanning

---
+++ collect_app/src/main/java/org/odk/collect/android/fragments/MlkitBarcodeScannerViewFactory.kt
@@ -0,0 +1,67 @@
+package org.odk.collect.android.fragments
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.view.LayoutInflater
+import androidx.camera.core.ImageAnalysis.COORDINATE_SYSTEM_VIEW_REFERENCED
+import androidx.camera.mlkit.vision.MlKitAnalyzer
+import androidx.camera.view.LifecycleCameraController
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import org.odk.collect.android.databinding.MlkitBarcodeScannerLayoutBinding
+
+@SuppressLint("ViewConstructor")
+private class MlkitBarcodeScannerView(context: Context, lifecycleOwner: LifecycleOwner) : BarcodeScannerView(context) {
+
+    private val binding = MlkitBarcodeScannerLayoutBinding.inflate(LayoutInflater.from(context), this, true)
+    private val cameraController = LifecycleCameraController(context)
+
+    init {
+        cameraController.bindToLifecycle(lifecycleOwner)
+        binding.preview.setController(cameraController)
+    }
+
+    override fun decodeContinuous(callback: (String) -> Unit) {
+        val options = BarcodeScannerOptions.Builder()
+            .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
+            .build()
+        val barcodeScanner = BarcodeScanning.getClient(options)
+
+        val executor = ContextCompat.getMainExecutor(context)
+        cameraController.setImageAnalysisAnalyzer(
+            executor,
+            MlKitAnalyzer(
+                listOf(barcodeScanner),
+                COORDINATE_SYSTEM_VIEW_REFERENCED,
+                executor
+            ) { result: MlKitAnalyzer.Result ->
+                val value = result.getValue(barcodeScanner)
+                if (value!!.isNotEmpty()) {
+                    callback(value.first().rawValue!!)
+                }
+            }
+        )
+    }
+
+    override fun setTorchOn(on: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setTorchListener(torchListener: TorchListener) = Unit
+}
+
+class MlkitBarcodeScannerViewFactory : BarcodeScannerViewContainer.Factory {
+    override fun create(
+        activity: Activity,
+        lifecycleOwner: LifecycleOwner,
+        qrOnly: Boolean,
+        prompt: String,
+        useFrontCamera: Boolean
+    ): BarcodeScannerView {
+        return MlkitBarcodeScannerView(activity, lifecycleOwner)
+    }
+}
