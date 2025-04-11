eb874752993dcadfb4133c370307be81fb32971c
Callum Stott <callum@seadowg.com>
2025-04-11 16:41:44 +0100

    Add approval lists to stub server

--- collect_app/src/androidTest/java/org/odk/collect/android/support/StubOpenRosaServer.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/support/StubOpenRosaServer.java
@@ -281,9 +281,14 @@ public class StubOpenRosaServer implements OpenRosaHttpInterface {
                     mediaFileHash = Md5.getMd5Hash(getResourceAsStream("media/" + mediaFile.getFile()));
                 }
 
-                if (mediaFile instanceof EntityListItem) {
-                    stringBuilder
-                            .append("<mediaFile type=\"entityList\">");
+                if (mediaFile instanceof EntityListItem entityListItem) {
+                    if (entityListItem.isApprovalList()) {
+                        stringBuilder
+                                .append("<mediaFile type=\"approvalEntityList\">");
+                    } else {
+                        stringBuilder
+                                .append("<mediaFile type=\"entityList\">");
+                    }
                 } else {
                     stringBuilder
                             .append("<mediaFile>");
@@ -455,10 +460,16 @@ public class StubOpenRosaServer implements OpenRosaHttpInterface {
     public static class EntityListItem extends MediaFileItem {
 
         private int version;
+        private boolean approvalList;
 
         public EntityListItem(String name, String file, int version) {
+            this(name, file, version, false);
+        }
+
+        public EntityListItem(String name, String file, int version, boolean approvalList) {
             super(name, file);
             this.version = version;
+            this.approvalList = approvalList;
         }
 
         public EntityListItem(String name) {
@@ -471,6 +482,10 @@ public class StubOpenRosaServer implements OpenRosaHttpInterface {
 
         public void incrementVersion() {
             this.version++;
+        }
+
+        public boolean isApprovalList() {
+            return approvalList;
         }
     }
 
