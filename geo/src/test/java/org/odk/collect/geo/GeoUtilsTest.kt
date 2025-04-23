db2de93fb26db137b3e90adefa09bec7394e2518
Callum Stott <callum@seadowg.com>
2025-04-23 13:43:01 +0100

    Add tests for formatAccuracy

--- geo/src/test/java/org/odk/collect/geo/GeoUtilsTest.kt
+++ geo/src/test/java/org/odk/collect/geo/GeoUtilsTest.kt
@@ -1,7 +1,11 @@
 package org.odk.collect.geo
 
+import android.content.Context
 import android.location.Location
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -12,6 +16,7 @@ import org.odk.collect.testshared.LocationTestUtils.createLocation
 @RunWith(AndroidJUnit4::class)
 class GeoUtilsTest {
 
+    private val context = ApplicationProvider.getApplicationContext<Context>()
     private val points = mutableListOf(
         MapPoint(11.0, 12.0, 13.0, 14.0),
         MapPoint(21.0, 22.0, 23.0, 24.0),
@@ -44,5 +49,14 @@ class GeoUtilsTest {
     fun test_formatLocationResultString() {
         val location: Location = createLocation("GPS", 1.0, 2.0, 3.0, 4f)
         assertEquals(GeoUtils.formatLocationResultString(location), "1.0 2.0 3.0 4.0")
+    }
+
+    @Test
+    fun `#formatAccuracy formats accuracy in meters to 2 decimal places`() {
+        assertThat(GeoUtils.formatAccuracy(context, 0.000f), equalTo("0 m"))
+        assertThat(GeoUtils.formatAccuracy(context, 0.001f), equalTo("0 m"))
+        assertThat(GeoUtils.formatAccuracy(context, 0.01f), equalTo("0.01 m"))
+        assertThat(GeoUtils.formatAccuracy(context, 0.10f), equalTo("0.1 m"))
+        assertThat(GeoUtils.formatAccuracy(context, 1.1f), equalTo("1.1 m"))
     }
 }
