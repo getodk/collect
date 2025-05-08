6996ce7f99335b40934f102f3dbcd1f25a57e4b1
Callum Stott <callum@seadowg.com>
2025-05-08 14:36:33 +0100

    Use MigrationListDatabaseMigrator for save points

--- collect_app/src/main/java/org/odk/collect/android/database/savepoints/SavepointsDatabaseMigrator.kt
+++ collect_app/src/main/java/org/odk/collect/android/database/savepoints/SavepointsDatabaseMigrator.kt
@@ -7,31 +7,19 @@ import org.odk.collect.android.database.savepoints.DatabaseSavepointsColumns.FOR
 import org.odk.collect.android.database.savepoints.DatabaseSavepointsColumns.INSTANCE_DB_ID
 import org.odk.collect.android.database.savepoints.DatabaseSavepointsColumns.INSTANCE_FILE_PATH
 import org.odk.collect.android.database.savepoints.DatabaseSavepointsColumns.SAVEPOINT_FILE_PATH
-import org.odk.collect.db.sqlite.DatabaseMigrator
+import org.odk.collect.db.sqlite.MigrationListDatabaseMigrator
 
-class SavepointsDatabaseMigrator :
-    DatabaseMigrator {
-    override fun onCreate(db: SQLiteDatabase) {
-        createSavepointsTableV1(db)
-    }
-
-    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int) {
-        when (oldVersion) {
-            1 -> {
-                // Remember to bump the database version number in {@link org.odk.collect.android.database.DatabaseConstants}
-                // upgradeToVersion2(db);
-            }
+class SavepointsDatabaseMigrator(databaseVersion: Int) : MigrationListDatabaseMigrator(databaseVersion) {
+    override fun createDbForVersion(db: SQLiteDatabase, version: Int) {
+        if (version == 1) {
+            db.execSQL(
+                "CREATE TABLE IF NOT EXISTS $SAVEPOINTS_TABLE_NAME (" +
+                    "$_ID integer PRIMARY KEY, " +
+                    "$FORM_DB_ID integer NOT NULL, " +
+                    "$INSTANCE_DB_ID integer, " +
+                    "$SAVEPOINT_FILE_PATH text NOT NULL, " +
+                    "$INSTANCE_FILE_PATH text NOT NULL);"
+            )
         }
-    }
-
-    private fun createSavepointsTableV1(db: SQLiteDatabase) {
-        db.execSQL(
-            "CREATE TABLE IF NOT EXISTS $SAVEPOINTS_TABLE_NAME (" +
-                "$_ID integer PRIMARY KEY, " +
-                "$FORM_DB_ID integer NOT NULL, " +
-                "$INSTANCE_DB_ID integer, " +
-                "$SAVEPOINT_FILE_PATH text NOT NULL, " +
-                "$INSTANCE_FILE_PATH text NOT NULL);"
-        )
     }
 }
