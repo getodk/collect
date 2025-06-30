db12376059dbafd05029ded8fde8fed0a6239a69
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2025-06-30 20:59:06 +0200

    Handle deleted forms on form saving

--- collect_app/src/androidTest/java/org/odk/collect/android/support/pages/FormEndPage.java
+++ collect_app/src/androidTest/java/org/odk/collect/android/support/pages/FormEndPage.java
@@ -7,8 +7,6 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import android.os.Build;
-
 import org.odk.collect.android.R;
 
 public class FormEndPage extends Page<FormEndPage> {
