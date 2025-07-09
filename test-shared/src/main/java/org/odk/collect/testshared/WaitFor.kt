522cd49adff0a161edf45444599013db01abe9af
Callum Stott <callum@seadowg.com>
2025-07-09 09:04:20 +0100

    Lower interval for waitFor

--- test-shared/src/main/java/org/odk/collect/testshared/WaitFor.kt
+++ test-shared/src/main/java/org/odk/collect/testshared/WaitFor.kt
@@ -1,17 +1,19 @@
 package org.odk.collect.testshared
 
 import junit.framework.AssertionFailedError
+import org.odk.collect.shared.TimeInMs
+import java.sql.Time
 import java.util.concurrent.Callable
 
 object WaitFor {
 
     @JvmStatic
     fun <T> waitFor(callable: Callable<T>): T {
-        var counter = 0
         var failure: Throwable? = null
+        val startTime = System.currentTimeMillis()
 
-        // Try 20 times/for 5 seconds
-        while (counter < 20) {
+        // Try for 5 seconds
+        while ((System.currentTimeMillis() - startTime) < (5 * TimeInMs.ONE_SECOND)) {
             failure = try {
                 return callable.call()
             } catch (throwable: Exception) {
@@ -22,8 +24,7 @@ object WaitFor {
                 throwable
             }
 
-            wait250ms()
-            counter++
+            Thread.sleep(10)
         }
 
         throw failure!!
@@ -31,11 +32,7 @@ object WaitFor {
 
     @JvmStatic
     fun wait250ms() {
-        try {
-            Thread.sleep(250)
-        } catch (ignored: InterruptedException) {
-            // ignored
-        }
+        Thread.sleep(250)
     }
 
     @JvmStatic
@@ -48,7 +45,7 @@ object WaitFor {
                 return
             } catch (e: Throwable) {
                 failure = e
-                wait250ms()
+                Thread.sleep(250)
             }
         }
 
