da88a2bcc5329443d0249bf28351bb3a0e7be1dd
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2025-07-13 13:58:02 +0200

    Manage font size in WidgetAnswerView

--- collect_app/src/main/java/org/odk/collect/android/widgets/BarcodeWidgetAnswerView.kt
+++ collect_app/src/main/java/org/odk/collect/android/widgets/BarcodeWidgetAnswerView.kt
@@ -1,11 +1,21 @@
 package org.odk.collect.android.widgets
 
 import android.content.Context
+import android.util.TypedValue
 import android.view.LayoutInflater
 import org.odk.collect.android.databinding.BarcodeWidgetAnswerViewBinding
+import org.odk.collect.android.widgets.utilities.QuestionFontSizeUtils
+import org.odk.collect.settings.SettingsProvider
 
-class BarcodeWidgetAnswerView(context: Context) : WidgetAnswerView(context) {
+class BarcodeWidgetAnswerView(
+    context: Context,
+    private val settingsProvider: SettingsProvider
+) : WidgetAnswerView(context) {
     private val binding = BarcodeWidgetAnswerViewBinding.inflate(LayoutInflater.from(context), this, true)
+
+    init {
+        setFontSize()
+    }
 
     override fun setAnswer(answer: String?) {
         binding.answer.text = stripInvalidCharacters(answer)
@@ -13,6 +23,14 @@ class BarcodeWidgetAnswerView(context: Context) : WidgetAnswerView(context) {
 
     override fun getAnswer(): String {
         return binding.answer.text.toString()
+    }
+
+    override fun setFontSize() {
+        val textSize = QuestionFontSizeUtils.getFontSize(
+            settingsProvider.getUnprotectedSettings(),
+            QuestionFontSizeUtils.FontSize.HEADLINE_6
+        )
+        binding.answer.setTextSize(TypedValue.COMPLEX_UNIT_DIP, textSize.toFloat())
     }
 
     // Remove control characters, invisible characters and unused code points.
