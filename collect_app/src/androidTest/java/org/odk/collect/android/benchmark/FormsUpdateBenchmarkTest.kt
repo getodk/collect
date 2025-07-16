a84efb2d59bd8165c474fa758c761e8211bc3867
Callum Stott <callum@seadowg.com>
2025-07-16 14:42:37 +0100

    Add benchmark for 1000 media files with entities

--- collect_app/src/androidTest/java/org/odk/collect/android/benchmark/FormsUpdateBenchmarkTest.kt
+++ collect_app/src/androidTest/java/org/odk/collect/android/benchmark/FormsUpdateBenchmarkTest.kt
@@ -14,16 +14,9 @@ import org.odk.collect.android.support.TestDependencies
 import org.odk.collect.android.support.pages.MainMenuPage
 import org.odk.collect.android.support.rules.CollectTestRule
 import org.odk.collect.android.support.rules.TestRuleChain.chain
-import org.odk.collect.android.test.BuildConfig.COLLECT_BENCHMARKS_TEST_PROJECT_URL
+import org.odk.collect.android.test.BuildConfig.THOUSAND_MEDIA_FILE_ENTITY_LIST_PROJECT_URL
+import org.odk.collect.android.test.BuildConfig.THOUSAND_MEDIA_FILE_PROJECT_URL
 
-/**
- * Benchmarks the performance of updating forms. [COLLECT_BENCHMARKS_TEST_PROJECT_URL] should
- * be set to a project that contains a form with 1k media files.
- *
- * Devices that currently pass:
- * - Fairphone 3
- * - Pixel 3
- */
 @RunWith(AndroidJUnit4::class)
 class FormsUpdateBenchmarkTest {
     private val rule = CollectTestRule(useDemoProject = false)
@@ -31,11 +24,19 @@ class FormsUpdateBenchmarkTest {
     @get:Rule
     val chain: RuleChain = chain(TestDependencies(true)).around(rule)
 
+    /**
+     * Benchmarks the performance of updating forms. [THOUSAND_MEDIA_FILE_PROJECT_URL] should
+     * be set to a project that contains a form with 1k media files.
+     *
+     * Devices that currently pass:
+     * - Fairphone 3
+     * - Pixel 3
+     */
     @Test
-    fun run() {
+    fun oneThousandMediaFiles() {
         assertThat(
-            "Need to set COLLECT_BENCHMARKS_TEST_PROJECT_URL before running!",
-            COLLECT_BENCHMARKS_TEST_PROJECT_URL,
+            "Need to set THOUSAND_MEDIA_FILE_PROJECT_URL before running!",
+            THOUSAND_MEDIA_FILE_PROJECT_URL,
             not(blankOrNullString())
         )
 
@@ -43,7 +44,7 @@ class FormsUpdateBenchmarkTest {
 
         rule.startAtFirstLaunch()
             .clickManuallyEnterProjectDetails()
-            .inputUrl(COLLECT_BENCHMARKS_TEST_PROJECT_URL)
+            .inputUrl(THOUSAND_MEDIA_FILE_PROJECT_URL)
             .addProject()
 
             // Download all forms
@@ -67,6 +68,58 @@ class FormsUpdateBenchmarkTest {
                 benchmarker
             ) {
                 it
+                    .clickGetSelected()
+                    .clickOKOnDialog(MainMenuPage())
+            }
+
+        benchmarker.assertResults()
+    }
+
+    /**
+     * Benchmarks the performance of updating forms. [THOUSAND_MEDIA_FILE_PROJECT_URL] should
+     * be set to a project that contains a form with 1k media files.
+     *
+     * Devices that currently pass:
+     * - Fairphone 3
+     */
+    @Test
+    fun oneThousandMediaFilesWithEntityList() {
+        assertThat(
+            "Need to set THOUSAND_MEDIA_FILE_ENTITY_LIST_PROJECT_URL before running!",
+            THOUSAND_MEDIA_FILE_ENTITY_LIST_PROJECT_URL,
+            not(blankOrNullString())
+        )
+
+        val benchmarker = Benchmarker()
+
+        rule.startAtFirstLaunch()
+            .clickManuallyEnterProjectDetails()
+            .inputUrl(THOUSAND_MEDIA_FILE_ENTITY_LIST_PROJECT_URL)
+            .addProject()
+
+            // Download all forms
+            .clickGetBlankForm()
+            .clickGetSelected()
+            .clickOKOnDialog(MainMenuPage())
+
+            .benchmark(
+                "Fetching form list with 1k media files and entity list when there are no updates",
+                7,
+                benchmarker
+            ) {
+                it
+                    .clickGetBlankForm()
+                    .clickGetSelected()
+                    .clickOKOnDialog(MainMenuPage())
+            }
+
+            .benchmark(
+                "Redownloading a form with 1k media files and entity list when there are no updates",
+                5,
+                benchmarker
+            ) {
+                it
+                    .clickGetBlankForm()
                     .clickGetSelected()
                     .clickOKOnDialog(MainMenuPage())
             }
