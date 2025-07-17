ff19bb2d998dd1bb296f55a340efff52fd17b4f5
Callum Stott <callum@seadowg.com>
2025-07-17 14:14:08 +0100

    Disable faster form updates by default

--- collect_app/src/main/java/org/odk/collect/android/application/FeatureFlags.kt
+++ collect_app/src/main/java/org/odk/collect/android/application/FeatureFlags.kt
@@ -3,5 +3,5 @@ package org.odk.collect.android.application
 object FeatureFlags {
 
     const val NO_THEME_SETTING = true
-    const val FASTER_FORM_UPDATES = true
+    const val FASTER_FORM_UPDATES = false
 }
