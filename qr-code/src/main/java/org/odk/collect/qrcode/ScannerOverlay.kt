2c199d325987ede310047c20cae260ed0562733b
Callum Stott <callum@seadowg.com>
2025-08-22 16:23:29 +0100

    Use Material theme shape values

--- qr-code/src/main/java/org/odk/collect/qrcode/ScannerOverlay.kt
+++ qr-code/src/main/java/org/odk/collect/qrcode/ScannerOverlay.kt
@@ -3,6 +3,7 @@ package org.odk.collect.qrcode
 import android.graphics.Rect
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
@@ -11,24 +12,34 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalDensity
 
 @Composable
 fun ScannerOverlay(viewFinderRect: Rect, detectedState: DetectedState = DetectedState.None) {
+    val smallShapeCornerSize = MaterialTheme.shapes.small.topStart
+    val density = LocalDensity.current
+
     Canvas(modifier = Modifier.fillMaxSize()) {
         drawRect(
             color = Color(0x4B000000),
             size = size
         )
 
+        val viewFinderOffset = Offset(viewFinderRect.left.toFloat(), viewFinderRect.top.toFloat())
+        val viewFinderSize = Size(
+            (viewFinderRect.right - viewFinderRect.left).toFloat(),
+            (viewFinderRect.bottom - viewFinderRect.top).toFloat()
+        )
+
+        val cornerSizePx = smallShapeCornerSize.toPx(viewFinderSize, density)
+        val cornerRadius = CornerRadius(cornerSizePx, cornerSizePx)
+
         drawRoundRect(
             color = Color(0x00000000),
-            topLeft = Offset(viewFinderRect.left.toFloat(), viewFinderRect.top.toFloat()),
-            size = Size(
-                (viewFinderRect.right - viewFinderRect.left).toFloat(),
-                (viewFinderRect.bottom - viewFinderRect.top).toFloat()
-            ),
+            topLeft = viewFinderOffset,
+            size = viewFinderSize,
             blendMode = BlendMode.Clear,
-            cornerRadius = CornerRadius(5f, 5f)
+            cornerRadius = cornerRadius
         )
 
         if (detectedState != DetectedState.None) {
@@ -40,13 +51,10 @@ fun ScannerOverlay(viewFinderRect: Rect, detectedState: DetectedState = Detected
 
             drawRoundRect(
                 color = borderColor,
-                topLeft = Offset(viewFinderRect.left.toFloat(), viewFinderRect.top.toFloat()),
-                size = Size(
-                    (viewFinderRect.right - viewFinderRect.left).toFloat(),
-                    (viewFinderRect.bottom - viewFinderRect.top).toFloat()
-                ),
+                topLeft = viewFinderOffset,
+                size = viewFinderSize,
                 style = Stroke(width = 8f),
-                cornerRadius = CornerRadius(5f, 5f)
+                cornerRadius = cornerRadius
             )
         }
     }
