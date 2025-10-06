e8f93696af6731c4e379779fb7288b60f17a060a
Callum Stott <callum@seadowg.com>
2025-10-06 12:11:25 +0100

    Add onPrimaryContainer to Compose theme

--- collect_app/src/main/java/org/odk/collect/android/application/ComposeTheme.kt
+++ collect_app/src/main/java/org/odk/collect/android/application/ComposeTheme.kt
@@ -1,6 +1,5 @@
 package org.odk.collect.android.application
 
-import android.content.Context
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -9,7 +8,7 @@ import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import org.odk.collect.android.R
 import org.odk.collect.androidshared.ui.ComposeThemeProvider
@@ -18,23 +17,20 @@ import org.odk.collect.androidshared.ui.ComposeThemeProvider
  * Changes to this theme should also be made in `theme.xml`
  */
 @Composable
-fun CollectTheme(
-    context: Context,
-    content: @Composable () -> Unit
-) {
-    val resources = context.resources
-
+fun CollectTheme(content: @Composable () -> Unit) {
     val lightColors = lightColorScheme(
-        primary = Color(resources.getColor(R.color.colorPrimaryLight)),
-        onPrimary = Color(resources.getColor(R.color.colorOnPrimaryLight)),
-        surface = Color(resources.getColor(R.color.colorSurfaceLight)),
-        primaryContainer = Color(resources.getColor(R.color.colorPrimaryContainerLight))
+        primary = colorResource(R.color.colorPrimaryLight),
+        onPrimary = colorResource(R.color.colorOnPrimaryLight),
+        surface = colorResource(R.color.colorSurfaceLight),
+        primaryContainer = colorResource(R.color.colorPrimaryContainerLight),
+        onPrimaryContainer = colorResource(R.color.colorOnPrimaryContainerLight)
     )
     val darkColors = darkColorScheme(
-        primary = Color(resources.getColor(R.color.colorPrimaryDark)),
-        onPrimary = Color(resources.getColor(R.color.colorOnPrimaryDark)),
-        surface = Color(resources.getColor(R.color.colorSurfaceDark)),
-        primaryContainer = Color(resources.getColor(R.color.colorPrimaryContainerDark))
+        primary = colorResource(R.color.colorPrimaryDark),
+        onPrimary = colorResource(R.color.colorOnPrimaryDark),
+        surface = colorResource(R.color.colorSurfaceDark),
+        primaryContainer = colorResource(R.color.colorPrimaryContainerDark),
+        onPrimaryContainer = colorResource(R.color.colorOnPrimaryContainerDark)
     )
     val colorScheme = if (isSystemInDarkTheme()) darkColors else lightColors
 
@@ -59,6 +55,6 @@ fun CollectTheme(
 interface CollectComposeThemeProvider : ComposeThemeProvider {
     @Composable
     override fun Theme(content: @Composable () -> Unit) {
-        CollectTheme(this as Context) { content() }
+        CollectTheme { content() }
     }
 }
