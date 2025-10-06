3acc055416708164a52959853765b511a1a0e24d
Callum Stott <callum@seadowg.com>
2025-10-06 11:59:54 +0100

    Add shape and typography to Compose theme

--- collect_app/src/main/java/org/odk/collect/android/application/ComposeTheme.kt
+++ collect_app/src/main/java/org/odk/collect/android/application/ComposeTheme.kt
@@ -2,11 +2,15 @@ package org.odk.collect.android.application
 
 import android.content.Context
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import org.odk.collect.android.R
 import org.odk.collect.androidshared.ui.ComposeThemeProvider
 
@@ -34,8 +38,20 @@ fun CollectTheme(
     )
     val colorScheme = if (isSystemInDarkTheme()) darkColors else lightColors
 
+    val typography = Typography(
+        bodyMedium = MaterialTheme.typography.bodyMedium
+    )
+
+    val shapes = Shapes(
+        small = MaterialTheme.shapes.small,
+        medium = RoundedCornerShape(8.dp),
+        large = MaterialTheme.shapes.large
+    )
+
     MaterialTheme(
         colorScheme = colorScheme,
+        typography = typography,
+        shapes = shapes,
         content = content
     )
 }
