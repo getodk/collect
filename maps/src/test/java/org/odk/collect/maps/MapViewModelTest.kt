98026400401c1634866da3135256e421afc2e045
Callum Stott <callum@seadowg.com>
2025-10-21 09:56:07 +0100

    Merge pull request #6934 from grzesiek2010/COLLECT-6921
    Remove duplicate map points when zooming to them

--- maps/src/test/java/org/odk/collect/maps/MapViewModelTest.kt
+++ maps/src/test/java/org/odk/collect/maps/MapViewModelTest.kt
@@ -3,6 +3,7 @@ package org.odk.collect.maps
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.contains
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.odk.collect.androidtest.getOrAwaitValue
@@ -52,5 +53,16 @@ class MapViewModelTest {
 
             assertThat(settings.size, equalTo(1))
         }
+    }
+
+    @Test
+    fun `zoomTo removes duplicate MapPoints`() {
+        val point = MapPoint(43.0, 7.0)
+        viewModel.zoomTo(
+            listOf(point, point),
+            0.0,
+            false
+        )
+        assertThat((viewModel.zoom.value as Zoom.Box).box, contains(point))
     }
 }
