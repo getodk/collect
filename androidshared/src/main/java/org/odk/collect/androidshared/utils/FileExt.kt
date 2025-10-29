a7399ba5942c21a9539bd6ab76a3ce166691c87c
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2025-10-29 22:31:05 +0100

    Add ext fun for loading video thumbnail

---
+++ androidshared/src/main/java/org/odk/collect/androidshared/utils/FileExt.kt
@@ -0,0 +1,22 @@
+package org.odk.collect.androidshared.utils
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.media.MediaMetadataRetriever
+import androidx.core.net.toUri
+import timber.log.Timber
+import java.io.File
+import kotlin.use
+
+fun File.loadThumbnail(context: Context): Bitmap? {
+    return try {
+        MediaMetadataRetriever().apply {
+            setDataSource(context, this@loadThumbnail.toUri())
+        }.use { retriever ->
+            retriever.getFrameAtTime(0, MediaMetadataRetriever.OPTION_CLOSEST_SYNC)
+        }
+    } catch (e: Exception) {
+        Timber.w(e)
+        null
+    }
+}
