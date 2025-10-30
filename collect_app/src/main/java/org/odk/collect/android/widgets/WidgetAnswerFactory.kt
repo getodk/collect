641302ee333a7e66dc6413e4552be0fe0af62b90
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2025-10-30 15:53:45 +0100

    Fix confusing naming by introducing VideoWidgetAnswer.Container and Content

--- collect_app/src/main/java/org/odk/collect/android/widgets/WidgetAnswerFactory.kt
+++ collect_app/src/main/java/org/odk/collect/android/widgets/WidgetAnswerFactory.kt
@@ -1,16 +1,11 @@
 package org.odk.collect.android.widgets
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.javarosa.core.model.Constants
 import org.javarosa.form.api.FormEntryPrompt
 import org.odk.collect.android.widgets.video.VideoWidgetAnswer
-import org.odk.collect.android.widgets.video.VideoWidgetAnswerViewModel
 
 @Composable
 fun WidgetAnswer(
@@ -21,27 +16,8 @@ fun WidgetAnswer(
 ) {
     if (answer != null) {
         when (prompt.controlType) {
-            Constants.CONTROL_VIDEO_CAPTURE -> VideoWidgetAnswer(modifier, answer, viewModelProvider)
+            Constants.CONTROL_VIDEO_CAPTURE -> VideoWidgetAnswer.Container(modifier, answer, viewModelProvider)
             else -> throw IllegalArgumentException("Unsupported control type: ${prompt.controlType}")
         }
-    }
-}
-
-@Composable
-private fun VideoWidgetAnswer(
-    modifier: Modifier,
-    answer: String,
-    viewModelProvider: ViewModelProvider
-) {
-    val context = LocalContext.current
-    val viewModel = viewModelProvider[VideoWidgetAnswerViewModel::class]
-
-    val bitmapFlow = remember(answer) {
-        viewModel.getFrame(answer, context)
-    }
-    val bitmap by bitmapFlow.collectAsStateWithLifecycle()
-
-    VideoWidgetAnswer(modifier, bitmap) {
-        viewModel.playVideo(context, answer)
     }
 }
--- collect_app/src/main/java/org/odk/collect/android/widgets/video/VideoWidgetAnswer.kt
+++ collect_app/src/main/java/org/odk/collect/android/widgets/video/VideoWidgetAnswer.kt
@@ -13,50 +13,74 @@ import androidx.compose.material.icons.filled.PlayCircle
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
-@Composable
-fun VideoWidgetAnswer(
-    modifier: Modifier,
-    bitmap: ImageBitmap?,
-    onPlayClick: () -> Unit
-) {
-    Box(
-        modifier = modifier
-            .fillMaxWidth()
-            .heightIn(max = 200.dp)
-            .clip(MaterialTheme.shapes.large)
-            .clickable { onPlayClick() }
-            .testTag("video_widget_answer"),
-        contentAlignment = Alignment.Center
+object VideoWidgetAnswer {
+    @Composable
+    fun Container(
+        modifier: Modifier,
+        answer: String,
+        viewModelProvider: ViewModelProvider
     ) {
-        if (bitmap != null) {
-            Image(
-                bitmap = bitmap,
-                contentDescription = stringResource(org.odk.collect.strings.R.string.play_video),
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxSize()
-            )
-        } else {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Gray)
+        val context = LocalContext.current
+        val viewModel = viewModelProvider[VideoWidgetAnswerViewModel::class]
+
+        val bitmapFlow = remember(answer) { viewModel.getFrame(answer, context) }
+        val bitmap by bitmapFlow.collectAsStateWithLifecycle()
+
+        Content(modifier, bitmap) {
+            viewModel.playVideo(context, answer)
+        }
+    }
+
+    @Composable
+    fun Content(
+        modifier: Modifier,
+        bitmap: ImageBitmap?,
+        onPlayClick: () -> Unit
+    ) {
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .heightIn(max = 200.dp)
+                .clip(MaterialTheme.shapes.large)
+                .clickable { onPlayClick() }
+                .testTag("video_widget_answer"),
+            contentAlignment = Alignment.Center
+        ) {
+            if (bitmap != null) {
+                Image(
+                    bitmap = bitmap,
+                    contentDescription = stringResource(org.odk.collect.strings.R.string.play_video),
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize()
+                )
+            } else {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Gray)
+                )
+            }
+            Icon(
+                imageVector = Icons.Default.PlayCircle,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onPrimary,
+                modifier = Modifier.size(64.dp)
             )
         }
-        Icon(
-            imageVector = Icons.Default.PlayCircle,
-            contentDescription = null,
-            tint = MaterialTheme.colorScheme.onPrimary,
-            modifier = Modifier.size(64.dp)
-        )
     }
 }
