b31f3674e3c1c024ee4ab5c608064819f1e63883
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2025-11-05 17:14:06 +0100

    Simplify onNodeWithClickLabel

--- androidtest/src/main/java/org/odk/collect/androidtest/NodeInteractionExtensions.kt
+++ androidtest/src/main/java/org/odk/collect/androidtest/NodeInteractionExtensions.kt
@@ -6,28 +6,11 @@ import androidx.compose.ui.test.SemanticsMatcher
 import androidx.compose.ui.test.SemanticsNodeInteraction
 import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
 
-fun SemanticsNodeInteractionsProvider.onNodeWithClickLabel(
-    label: String,
-    substring: Boolean = false,
-    ignoreCase: Boolean = false,
-    useUnmergedTree: Boolean = false,
-): SemanticsNodeInteraction =
-    onNode(hasClickLabel(label, substring, ignoreCase), useUnmergedTree)
+fun SemanticsNodeInteractionsProvider.onNodeWithClickLabel(label: String): SemanticsNodeInteraction =
+    onNode(hasClickLabel(label))
 
-fun hasClickLabel(
-    label: String,
-    substring: Boolean = false,
-    ignoreCase: Boolean = false,
-): SemanticsMatcher {
-    return if (substring) {
-        SemanticsMatcher("Clickable action label contains '$label' (ignoreCase: $ignoreCase)") {
-            it.config.getOrNull(SemanticsActions.OnClick)?.label
-                ?.contains(label, ignoreCase) == true
-        }
-    } else {
-        SemanticsMatcher("Clickable action label = '$label' (ignoreCase: $ignoreCase)") {
-            it.config.getOrNull(SemanticsActions.OnClick)?.label
-                ?.equals(label, ignoreCase) == true
-        }
+fun hasClickLabel(label: String): SemanticsMatcher {
+    return SemanticsMatcher("Clickable action label = '$label'") {
+        it.config.getOrNull(SemanticsActions.OnClick)?.label?.equals(label) == true
     }
 }
