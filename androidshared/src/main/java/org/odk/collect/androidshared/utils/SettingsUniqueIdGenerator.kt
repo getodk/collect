93962b4fa274900c50f98844479bcdc82e0b25c5
Callum Stott <callum@seadowg.com>
2025-11-07 14:47:12 +0000

    Switch to keys with less clash chance

--- androidshared/src/main/java/org/odk/collect/androidshared/utils/SettingsUniqueIdGenerator.kt
+++ androidshared/src/main/java/org/odk/collect/androidshared/utils/SettingsUniqueIdGenerator.kt
@@ -6,24 +6,32 @@ class SettingsUniqueIdGenerator(private val settings: Settings) : UniqueIdGenera
 
     override fun getInt(identifier: String): Int {
         return synchronized(this) {
-            if (settings.contains(identifier)) {
-                settings.getInt(identifier)
+            val identifierKey = identifierKey(identifier)
+
+            if (settings.contains(identifierKey)) {
+                settings.getInt(identifierKey)
             } else {
                 val next = getNextInt()
-                settings.save(identifier, next)
+                settings.save(identifierKey, next)
                 next
             }
         }
     }
 
     private fun getNextInt(): Int {
-        val next = if (settings.contains("next")) {
-            settings.getInt("next")
+        val next = if (settings.contains(NEXT_ID_KEY)) {
+            settings.getInt(NEXT_ID_KEY)
         } else {
             1
         }
 
-        settings.save("next", next + 1)
+        settings.save(NEXT_ID_KEY, next + 1)
         return next
+    }
+
+    companion object {
+        private const val KEY_PREFIX = "uniqueId"
+        private const val NEXT_ID_KEY = "$KEY_PREFIX:next"
+        private fun identifierKey(identifier: String) = "$KEY_PREFIX:identifier:$identifier"
     }
 }
