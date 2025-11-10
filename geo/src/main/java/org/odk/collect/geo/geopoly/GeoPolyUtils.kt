1c5868b9e4a2ff129a63412de1b4b86c5bbbb3e0
Callum Stott <callum@seadowg.com>
2025-11-10 20:15:00 +0000

    Add intersects util

---
+++ geo/src/main/java/org/odk/collect/geo/geopoly/GeoPolyUtils.kt
@@ -0,0 +1,93 @@
+package org.odk.collect.geo.geopoly
+
+import org.odk.collect.maps.MapPoint
+
+object GeoPolyUtils {
+
+    /**
+     * Returns `true` if any segment of the trace intersects with any other and `false` otherwise.
+     */
+    fun intersects(trace: List<MapPoint>): Boolean {
+        return if (trace.size >= 3) {
+            val segments = getSegments(trace)
+            segments.any { line1 ->
+                segments.any { line2 ->
+                    intersects(line1, line2)
+                }
+            }
+        } else {
+            false
+        }
+    }
+
+    private fun getSegments(trace: List<MapPoint>): List<Pair<MapPoint, MapPoint>> {
+        return trace.flatMapIndexed { index, point ->
+            if (index != trace.size - 1) {
+                listOf(Pair(point, trace[index + 1]))
+            } else {
+                emptyList()
+            }
+        }
+    }
+
+    /**
+     * Work out whether two line segments intersect by calculating if the endpoints of one segment
+     * are on opposite sides of the other segment **and** vice versa. This is determined by finding
+     * the orientation of endpoints relative to the other line.
+     */
+    private fun intersects(
+        segment1: Pair<MapPoint, MapPoint>,
+        segment2: Pair<MapPoint, MapPoint>
+    ): Boolean {
+        return orientation(segment1.first, segment2.first, segment2.second)
+            .isOpposing(orientation(segment1.second, segment2.first, segment2.second)) &&
+            orientation(segment1.first, segment1.second, segment2.first)
+                .isOpposing(orientation(segment1.first, segment1.second, segment2.second))
+    }
+
+    /**
+     * Calculate the "orientation" (or "direction") of three points using the cross product of the
+     * vectors of the pairs of points (see
+     * [here](https://en.wikipedia.org/wiki/Cross_product#Computational_geometry)). This can
+     * either be clockwise, anticlockwise or collinear (the three points form a straight line).
+     *
+     */
+    private fun orientation(a: MapPoint, b: MapPoint, c: MapPoint): Orientation {
+        val ab = Pair(b.latitude - a.latitude, b.longitude - a.longitude)
+        val ac = Pair(c.latitude - a.latitude, c.longitude - a.longitude)
+        val crossProduct = crossProduct(ab, ac)
+
+        return if (crossProduct > 0) {
+            Orientation.AntiClockwise
+        } else if (crossProduct < 0) {
+            Orientation.Clockwise
+        } else {
+            Orientation.Collinear
+        }
+    }
+
+    /**
+     * [https://en.wikipedia.org/wiki/Cross_product](https://en.wikipedia.org/wiki/Cross_product)
+     */
+    private fun crossProduct(x: Pair<Double, Double>, y: Pair<Double, Double>): Double {
+        return (x.first * y.second) - (y.first * x.second)
+    }
+
+    private enum class Orientation {
+        Collinear,
+        Clockwise,
+        AntiClockwise;
+
+        fun isOpposing(other: Orientation): Boolean {
+            return if (this == Collinear) {
+                false
+            } else if (this == Clockwise && other == AntiClockwise) {
+                true
+            } else if (this == AntiClockwise && other == Clockwise) {
+                true
+            } else {
+                false
+            }
+        }
+    }
+}
---
+++ geo/src/test/java/org/odk/collect/geo/geopoly/GeoPolyUtilsTest.kt
@@ -0,0 +1,62 @@
+package org.odk.collect.geo.geopoly
+
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
+import org.junit.Test
+import org.odk.collect.maps.MapPoint
+
+class GeoPolyUtilsTest {
+
+    @Test
+    fun `#intersects returns false for an empty list`() {
+        assertThat(GeoPolyUtils.intersects(emptyList()), equalTo(false))
+    }
+
+    @Test
+    fun `#intersects returns false when there is only one point`() {
+        val trace = listOf(MapPoint(0.0, 0.0))
+        assertThat(GeoPolyUtils.intersects(trace), equalTo(false))
+    }
+
+    @Test
+    fun `#intersects returns false when there is only one segment`() {
+        val trace = listOf(MapPoint(0.0, 0.0), MapPoint(1.0, 0.0))
+        assertThat(GeoPolyUtils.intersects(trace), equalTo(false))
+    }
+
+    @Test
+    fun `#intersects returns false when no segment intersects with another`() {
+        val trace = listOf(
+            MapPoint(0.0, 0.0),
+            MapPoint(1.0, 1.0),
+            MapPoint(2.0, 0.0)
+        )
+
+        assertThat(GeoPolyUtils.intersects(trace), equalTo(false))
+    }
+
+    @Test
+    fun `#intersects returns false when no segment intersects with another in a closed trace`() {
+        val trace = listOf(
+            MapPoint(0.0, 0.0),
+            MapPoint(1.0, 1.0),
+            MapPoint(2.0, 0.0),
+            MapPoint(0.0, 0.0)
+        )
+
+        assertThat(GeoPolyUtils.intersects(trace), equalTo(false))
+    }
+
+    @Test
+    fun `#intersects returns true when a segment intersects with another`() {
+        val trace = listOf(
+            MapPoint(1.0, 1.0),
+            MapPoint(1.0, 3.0),
+            MapPoint(2.0, 3.0),
+            MapPoint(2.0, 2.0),
+            MapPoint(0.0, 2.0)
+        )
+
+        assertThat(GeoPolyUtils.intersects(trace), equalTo(true))
+    }
+}
