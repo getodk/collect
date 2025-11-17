3419811c10d1e27eef289404ac223dae92306aca
Callum Stott <callum@seadowg.com>
2025-11-17 14:19:32 +0000

    Add test cases for repeated segments and closed traced intersection

--- geo/src/test/java/org/odk/collect/geo/geopoly/GeoPolyUtilsTest.kt
+++ geo/src/test/java/org/odk/collect/geo/geopoly/GeoPolyUtilsTest.kt
@@ -25,6 +25,18 @@ class GeoPolyUtilsTest {
     }
 
     @Test
+    fun `#intersects returns false when there is just two of the same segment`() {
+        val trace = listOf(
+            MapPoint(0.0, 0.0),
+            MapPoint(1.0, 0.0),
+            MapPoint(1.0, 0.0),
+            MapPoint(0.0, 0.0)
+        )
+
+        assertThat(GeoPolyUtils.intersects(trace), equalTo(false))
+    }
+
+    @Test
     fun `#intersects returns false when no segment intersects with another`() {
         val trace = listOf(
             MapPoint(0.0, 0.0),
@@ -55,6 +67,20 @@ class GeoPolyUtilsTest {
             MapPoint(2.0, 3.0),
             MapPoint(2.0, 2.0),
             MapPoint(0.0, 2.0)
+        )
+
+        assertThat(GeoPolyUtils.intersects(trace), equalTo(true))
+    }
+
+    @Test
+    fun `#intersects returns true when a segment intersects with another in a closed trace`() {
+        val trace = listOf(
+            MapPoint(1.0, 1.0),
+            MapPoint(1.0, 3.0),
+            MapPoint(2.0, 3.0),
+            MapPoint(2.0, 2.0),
+            MapPoint(0.0, 2.0),
+            MapPoint(1.0, 1.0)
         )
 
         assertThat(GeoPolyUtils.intersects(trace), equalTo(true))
