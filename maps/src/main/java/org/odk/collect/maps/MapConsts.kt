736ef946d70ce62aa926ed79df3b97aaf8f029e1
Callum Stott <callum@seadowg.com>
2026-01-08 14:13:40 +0000

    Change default color for lines in maps

--- maps/src/main/java/org/odk/collect/maps/MapConsts.kt
+++ maps/src/main/java/org/odk/collect/maps/MapConsts.kt
@@ -1,7 +1,9 @@
 package org.odk.collect.maps
 
+import android.graphics.Color
+
 object MapConsts {
-    const val DEFAULT_STROKE_COLOR = -65536 // color-int representation of #ffff0000
+    val DEFAULT_STROKE_COLOR = Color.parseColor("#3e9fcc")
     const val DEFAULT_STROKE_WIDTH = 8f
     const val DEFAULT_FILL_COLOR_OPACITY = 68
 }
