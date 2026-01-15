e576fa6b20e4bb4a3111423f69d2db7e08027069
Callum Stott <callum@seadowg.com>
2026-01-15 14:55:57 +0000

    Use LocationTracker to schedule location recording

--- geo/src/main/java/org/odk/collect/geo/geopoly/GeoPolyViewModel.kt
+++ geo/src/main/java/org/odk/collect/geo/geopoly/GeoPolyViewModel.kt
@@ -1,13 +1,19 @@
 package org.odk.collect.geo.geopoly
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import org.odk.collect.geo.geopoly.GeoPolyFragment.OutputMode
+import org.odk.collect.location.tracker.LocationTracker
 import org.odk.collect.maps.MapPoint
-import kotlin.collections.plus
 
-class GeoPolyViewModel(outputMode: OutputMode, points: List<MapPoint>) :
+class GeoPolyViewModel(
+    outputMode: OutputMode,
+    points: List<MapPoint>,
+    private val locationTracker: LocationTracker
+) :
     ViewModel() {
 
     private val _points = MutableStateFlow(
@@ -22,6 +28,29 @@ class GeoPolyViewModel(outputMode: OutputMode, points: List<MapPoint>) :
         }
     )
     val points: StateFlow<List<MapPoint>> = _points
+    private var accuracyThreshold: Int = 0
+
+    init {
+        viewModelScope.launch {
+            locationTracker.getLocation().collect {
+                if (it != null) {
+                    accuracyThreshold.let { threshold ->
+                        if (threshold == 0 || it.accuracy <= threshold) {
+                            add(
+                                MapPoint(
+                                    it.latitude,
+                                    it.longitude,
+                                    it.altitude,
+                                    it.accuracy.toDouble()
+                                )
+                            )
+                        }
+                    }
+
+                }
+            }
+        }
+    }
 
     fun add(point: MapPoint) {
         val points = _points.value
@@ -36,5 +65,18 @@ class GeoPolyViewModel(outputMode: OutputMode, points: List<MapPoint>) :
 
     fun update(points: List<MapPoint>) {
         _points.value = points
+    }
+
+    fun startRecording(retainMockAccuracy: Boolean, accuracyThreshold: Int, interval: Long) {
+        this.accuracyThreshold = accuracyThreshold
+        locationTracker.start(retainMockAccuracy, interval)
+    }
+
+    fun stopRecording() {
+        locationTracker.stop()
+    }
+
+    override fun onCleared() {
+        locationTracker.stop()
     }
 }
