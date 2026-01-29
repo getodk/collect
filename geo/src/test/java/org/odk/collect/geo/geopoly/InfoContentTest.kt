69a034f9a136701ff5608238be1c223ce5e3f7f8
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2026-01-29 20:06:39 +0100

    Add test for InfoContent done action

--- geo/src/test/java/org/odk/collect/geo/geopoly/InfoContentTest.kt
+++ geo/src/test/java/org/odk/collect/geo/geopoly/InfoContentTest.kt
@@ -4,9 +4,12 @@ import android.app.Application
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollTo
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -28,7 +31,7 @@ class InfoContentTest {
         }
 
         composeTestRule.setContent {
-            InfoContent(viewModel, fromSnackbar = true, {})
+            InfoContent(viewModel, fromSnackbar = true) {}
         }
 
         assertInfo(
@@ -48,7 +51,7 @@ class InfoContentTest {
         }
 
         composeTestRule.setContent {
-            InfoContent(viewModel, fromSnackbar = false, {})
+            InfoContent(viewModel, fromSnackbar = false) {}
         }
 
         assertInfo(
@@ -68,7 +71,7 @@ class InfoContentTest {
         }
 
         composeTestRule.setContent {
-            InfoContent(viewModel, fromSnackbar = true, {})
+            InfoContent(viewModel, fromSnackbar = true) {}
         }
 
         assertInfo(
@@ -88,7 +91,7 @@ class InfoContentTest {
         }
 
         composeTestRule.setContent {
-            InfoContent(viewModel, fromSnackbar = false, {})
+            InfoContent(viewModel, fromSnackbar = false) {}
         }
 
         assertInfo(
@@ -109,7 +112,7 @@ class InfoContentTest {
         }
 
         composeTestRule.setContent {
-            InfoContent(viewModel, fromSnackbar = true, {})
+            InfoContent(viewModel, fromSnackbar = true) {}
         }
 
         assertInfo(
@@ -129,7 +132,7 @@ class InfoContentTest {
         }
 
         composeTestRule.setContent {
-            InfoContent(viewModel, fromSnackbar = false, {})
+            InfoContent(viewModel, fromSnackbar = false) {}
         }
 
         assertInfo(
@@ -141,6 +144,26 @@ class InfoContentTest {
                 org.odk.collect.strings.R.string.delete_entire_shape_info_item,
             )
         )
+    }
+
+    @Test
+    fun `calls onDone when Done button is clicked`() {
+        val viewModel = mock<GeoPolyViewModel>().apply {
+            whenever(recordingMode).thenReturn(GeoPolyViewModel.RecordingMode.PLACEMENT)
+        }
+
+        var onDoneCalled = false
+
+        composeTestRule.setContent {
+            InfoContent(viewModel, false) { onDoneCalled = true }
+        }
+
+        composeTestRule
+            .onNodeWithText(context.getString(org.odk.collect.strings.R.string.done))
+            .assertIsDisplayed()
+            .performClick()
+
+        assertThat(onDoneCalled, equalTo(true))
     }
 
     private fun assertInfo(items: List<Int>) {
