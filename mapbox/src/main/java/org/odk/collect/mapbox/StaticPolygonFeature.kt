1eb659dd58ec9887007cc63d9ef07888d4c3ad72
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2026-02-11 22:09:30 +0100

    Add lines to mapbox static polygons

--- mapbox/src/main/java/org/odk/collect/mapbox/StaticPolygonFeature.kt
+++ mapbox/src/main/java/org/odk/collect/mapbox/StaticPolygonFeature.kt
@@ -2,15 +2,20 @@ package org.odk.collect.mapbox
 
 import com.mapbox.geojson.Point
 import com.mapbox.maps.plugin.annotation.generated.OnPolygonAnnotationClickListener
+import com.mapbox.maps.plugin.annotation.generated.OnPolylineAnnotationClickListener
 import com.mapbox.maps.plugin.annotation.generated.PolygonAnnotation
 import com.mapbox.maps.plugin.annotation.generated.PolygonAnnotationManager
 import com.mapbox.maps.plugin.annotation.generated.PolygonAnnotationOptions
+import com.mapbox.maps.plugin.annotation.generated.PolylineAnnotation
+import com.mapbox.maps.plugin.annotation.generated.PolylineAnnotationManager
+import com.mapbox.maps.plugin.annotation.generated.PolylineAnnotationOptions
 import org.odk.collect.maps.MapFragment
 import org.odk.collect.maps.MapPoint
 import org.odk.collect.maps.PolygonDescription
 
 class StaticPolygonFeature(
     private val polygonAnnotationManager: PolygonAnnotationManager,
+    private val polylineAnnotationManager: PolylineAnnotationManager,
     polygonDescription: PolygonDescription,
     featureClickListener: MapFragment.FeatureListener?,
     featureId: Int
@@ -30,9 +35,23 @@ class StaticPolygonFeature(
             polygonAnnotationManager.addClickListener(it)
         }
 
+    private val polylineAnnotation = polylineAnnotationManager.create(
+        PolylineAnnotationOptions()
+            .withPoints(polygonDescription.points.map { Point.fromLngLat(it.longitude, it.latitude) })
+            .withLineColor(polygonDescription.getStrokeColor())
+            .withLineWidth(MapUtils.convertStrokeWidth(polygonDescription))
+    )
+
+    private val polylineClickListener =
+        PolylineClickListener(polylineAnnotation.id, featureClickListener, featureId).also {
+            polylineAnnotationManager.addClickListener(it)
+        }
+
     override fun dispose() {
         polygonAnnotationManager.delete(polygonAnnotation)
         polygonAnnotationManager.removeClickListener(polygonClickListener)
+        polylineAnnotationManager.delete(polylineAnnotation)
+        polylineAnnotationManager.removeClickListener(polylineClickListener)
     }
 }
 
@@ -42,6 +61,21 @@ private class PolygonClickListener(
     private val featureId: Int
 ) : OnPolygonAnnotationClickListener {
     override fun onAnnotationClick(annotation: PolygonAnnotation): Boolean {
+        return if (annotation.id == polygonId && featureClickListener != null) {
+            featureClickListener.onFeature(featureId)
+            true
+        } else {
+            false
+        }
+    }
+}
+
+private class PolylineClickListener(
+    private val polygonId: Long,
+    private val featureClickListener: MapFragment.FeatureListener?,
+    private val featureId: Int
+) : OnPolylineAnnotationClickListener {
+    override fun onAnnotationClick(annotation: PolylineAnnotation): Boolean {
         return if (annotation.id == polygonId && featureClickListener != null) {
             featureClickListener.onFeature(featureId)
             true
