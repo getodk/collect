67a6c4a35606cbf5b4054814a39bd68c819b47ab
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2026-04-23 16:08:31 +0200

    Rename fun

--- collect_app/src/main/java/org/odk/collect/android/instancemanagement/InstancesDataService.kt
+++ collect_app/src/main/java/org/odk/collect/android/instancemanagement/InstancesDataService.kt
@@ -230,7 +230,7 @@ class InstancesDataService(
 
         return projectDependencyModule.instancesLock.withLockSuspend { acquiredLock: Boolean ->
             if (acquiredLock) {
-                val result = instanceSubmitter.submitInstancesSuspend(
+                val result = instanceSubmitter.submitInstances(
                     projectId,
                     instances,
                     referrer,
