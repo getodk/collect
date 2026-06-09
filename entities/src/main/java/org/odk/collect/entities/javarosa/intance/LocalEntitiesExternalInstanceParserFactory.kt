a064714eaae3d06f4022afa49a4441696d72f95d
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2026-06-09 12:31:09 +0200

    Fix attached CSV being shadowed by entity list of the same name

--- entities/src/main/java/org/odk/collect/entities/javarosa/intance/LocalEntitiesExternalInstanceParserFactory.kt
+++ entities/src/main/java/org/odk/collect/entities/javarosa/intance/LocalEntitiesExternalInstanceParserFactory.kt
@@ -3,16 +3,20 @@ package org.odk.collect.entities.javarosa.intance
 import org.javarosa.xform.parse.ExternalInstanceParser
 import org.javarosa.xform.parse.ExternalInstanceParserFactory
 import org.odk.collect.entities.storage.EntitiesRepository
+import org.odk.collect.forms.FormMediaFileRepository
 
 class LocalEntitiesExternalInstanceParserFactory(
     private val entitiesRepositoryProvider: () -> EntitiesRepository,
+    private val mediaFileRepository: FormMediaFileRepository,
     private val enabled: () -> Boolean
 ) : ExternalInstanceParserFactory {
     override fun getExternalInstanceParser(): ExternalInstanceParser {
         val parser = ExternalInstanceParser()
 
         if (enabled()) {
-            parser.addInstanceProvider(LocalEntitiesInstanceProvider(entitiesRepositoryProvider))
+            parser.addInstanceProvider(
+                LocalEntitiesInstanceProvider(entitiesRepositoryProvider, mediaFileRepository)
+            )
         }
 
         return parser
--- entities/src/main/java/org/odk/collect/entities/javarosa/intance/LocalEntitiesInstanceProvider.kt
+++ entities/src/main/java/org/odk/collect/entities/javarosa/intance/LocalEntitiesInstanceProvider.kt
@@ -3,9 +3,12 @@ package org.odk.collect.entities.javarosa.intance
 import org.javarosa.core.model.instance.TreeElement
 import org.javarosa.xform.parse.ExternalInstanceParser
 import org.odk.collect.entities.storage.EntitiesRepository
+import org.odk.collect.forms.FormMediaFileRepository
 
-internal class LocalEntitiesInstanceProvider(private val entitiesRepositoryProvider: () -> EntitiesRepository) :
-    ExternalInstanceParser.InstanceProvider {
+internal class LocalEntitiesInstanceProvider(
+    private val entitiesRepositoryProvider: () -> EntitiesRepository,
+    private val mediaFileRepository: FormMediaFileRepository
+) : ExternalInstanceParser.InstanceProvider {
 
     override fun get(instanceId: String, instanceSrc: String): TreeElement {
         return get(instanceId, instanceSrc, false)
@@ -18,6 +21,14 @@ internal class LocalEntitiesInstanceProvider(private val entitiesRepositoryProvi
     }
 
     override fun isSupported(instanceId: String, instanceSrc: String): Boolean {
+        // Entity lists are stored in the database, not as files on disk (their seed CSV is
+        // deleted after import). So if a media file referenced by the instance's src exists,
+        // it must be a plain attached CSV, which should be used instead of any same-named
+        // entity list. Returning false lets JavaRosa parse the file directly.
+        if (mediaFileRepository.get(instanceSrc)?.exists() == true) {
+            return false
+        }
+
         return createDataAdapter().supportsInstance(instanceId)
     }
 
