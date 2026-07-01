147d6d5e3bc2dc34e83640da912d0e86a6aaf07e
Callum Stott <callum@seadowg.com>
2026-07-01 14:14:56 +0100

    Remove ProjectSwitch analytics event

--- collect_app/src/main/java/org/odk/collect/android/mainmenu/CurrentProjectViewModel.kt
+++ collect_app/src/main/java/org/odk/collect/android/mainmenu/CurrentProjectViewModel.kt
@@ -2,8 +2,6 @@ package org.odk.collect.android.mainmenu
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import org.odk.collect.analytics.Analytics
-import org.odk.collect.android.analytics.AnalyticsEvents
 import org.odk.collect.android.projects.ProjectsDataService
 import org.odk.collect.projects.Project
 
@@ -18,7 +16,6 @@ class CurrentProjectViewModel(
     val currentProject = projectsDataService.getCurrentProject().asLiveData()
 
     fun setCurrentProject(project: Project.Saved) {
-        Analytics.log(AnalyticsEvents.SWITCH_PROJECT)
         projectsDataService.setCurrentProject(project.uuid)
     }
 
