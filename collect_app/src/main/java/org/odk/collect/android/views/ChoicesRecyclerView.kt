026534283fa79ca07c83697eb47dbf8fed3cd07f
Callum Stott <callum@seadowg.com>
2026-07-21 17:35:26 +0100

    Merge pull request #7311 from grzesiek2010/COLLECT-7310
    Fix select_one/multiple list height

---
+++ collect_app/src/main/java/org/odk/collect/android/views/ChoicesRecyclerView.kt
@@ -0,0 +1,69 @@
+package org.odk.collect.android.views
+
+import android.content.Context
+import android.graphics.Rect
+import android.util.AttributeSet
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.flexbox.FlexboxLayoutManager
+import org.odk.collect.android.R
+import org.odk.collect.android.adapters.AbstractSelectListAdapter
+
+class ChoicesRecyclerView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null
+) : RecyclerView(context, attrs) {
+
+    private var maxHeight: Int? = null
+
+    @JvmOverloads
+    fun initRecyclerView(adapter: AbstractSelectListAdapter, isFlex: Boolean, maxHeight: Int? = null) {
+        this.maxHeight = maxHeight
+        if (isFlex) {
+            enableFlexboxLayout()
+        } else {
+            enableGridLayout(adapter.numColumns)
+        }
+        setAdapter(adapter)
+    }
+
+    override fun onMeasure(widthSpec: Int, heightSpec: Int) {
+        val boundedHeightSpec = maxHeight?.let {
+            MeasureSpec.makeMeasureSpec(it, MeasureSpec.AT_MOST)
+        } ?: heightSpec
+        super.onMeasure(widthSpec, boundedHeightSpec)
+    }
+
+    private fun enableFlexboxLayout() {
+        layoutManager = FlexboxLayoutManager(context)
+        val marginBetweenItems = resources.getDimensionPixelSize(org.odk.collect.androidshared.R.dimen.margin_standard)
+        addItemDecoration(FlexItemDecoration(marginBetweenItems))
+    }
+
+    private fun enableGridLayout(numColumns: Int) {
+        if (numColumns == 1) {
+            enableDivider()
+        }
+
+        layoutManager = GridLayoutManager(context, numColumns)
+    }
+
+    private fun enableDivider() {
+        val divider = DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+        val drawable = ContextCompat.getDrawable(context, R.drawable.inset_divider_64dp)
+        if (drawable != null) {
+            divider.setDrawable(drawable)
+        }
+        addItemDecoration(divider)
+    }
+
+    internal class FlexItemDecoration(private val margin: Int) : ItemDecoration() {
+        override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: State) {
+            outRect.bottom = margin
+            outRect.right = margin
+        }
+    }
+}
