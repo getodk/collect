20883013e52c63c0e87ea6c395a9569cea961080
Callum Stott <callum@seadowg.com>
2026-07-28 16:38:25 +0100

    Prevent crash if trying to append empty file

--- collect_app/src/main/java/org/odk/collect/android/audio/AMRAppender.kt
+++ collect_app/src/main/java/org/odk/collect/android/audio/AMRAppender.kt
@@ -1,32 +1,15 @@
 package org.odk.collect.android.audio
 
 import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
 import java.io.IOException
 
 class AMRAppender : AudioFileAppender {
     @Throws(IOException::class)
     override fun append(one: File, two: File) {
-        val fos = FileOutputStream(one, true)
-        val fis = FileInputStream(two)
-
-        var fileContent = ByteArray(two.length().toInt())
-        fis.read(fileContent)
-
-        val headerlessFileContent = ByteArray(fileContent.size - AMR_HEADER_BYTES)
-        if (fileContent.size - AMR_HEADER_BYTES >= 0) {
-            System.arraycopy(
-                fileContent,
-                AMR_HEADER_BYTES,
-                headerlessFileContent,
-                0,
-                fileContent.size - AMR_HEADER_BYTES
-            )
+        val twoContents = two.readBytes()
+        if (twoContents.size - AMR_HEADER_BYTES >= 0) {
+            one.appendBytes(twoContents.copyOfRange(AMR_HEADER_BYTES, twoContents.size))
         }
-
-        fileContent = headerlessFileContent
-        fos.write(fileContent)
     }
 
     companion object {
