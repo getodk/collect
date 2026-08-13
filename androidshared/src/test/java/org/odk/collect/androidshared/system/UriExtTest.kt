734a68be7969120d1d214eb54c53990ff60262c8
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2026-08-14 00:00:37 +0200

    Improve tests for copyToFile

--- androidshared/src/test/java/org/odk/collect/androidshared/system/UriExtTest.kt
+++ androidshared/src/test/java/org/odk/collect/androidshared/system/UriExtTest.kt
@@ -1,6 +1,8 @@
 package org.odk.collect.androidshared.system
 
 import android.app.Application
+import android.content.ContentResolver
+import android.content.Context
 import android.net.Uri
 import androidx.core.net.toUri
 import androidx.test.core.app.ApplicationProvider
@@ -9,22 +11,73 @@ import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.kotlin.doAnswer
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
 import org.odk.collect.shared.TempFiles
+import java.io.ByteArrayInputStream
+import java.io.File
+import java.io.IOException
+import java.io.InputStream
 
 @RunWith(AndroidJUnit4::class)
 class UriExtTest {
     private val context = ApplicationProvider.getApplicationContext<Application>()
 
     @Test
-    fun `copyToFile copies the source file to the target file`() {
-        val sourceFile = TempFiles.createTempFile().also {
-            it.writeText("blah")
-        }
-        val sourceFileUri = sourceFile.toUri()
-        val targetFile = TempFiles.createTempFile()
+    fun `copyToFile creates the target file with the contents of the uri`() {
+        val uri = Uri.parse("content://media/external/downloads/64711")
+        val targetFile = File(TempFiles.createTempDir(), "target")
+        val context = contextWhereOpeningUri(uri) { ByteArrayInputStream("blah".toByteArray()) }
 
-        sourceFileUri.copyToFile(context, targetFile)
-        assertThat(targetFile.readText(), equalTo(sourceFile.readText()))
+        uri.copyToFile(context, targetFile)
+
+        assertThat(targetFile.readText(), equalTo("blah"))
+    }
+
+    @Test
+    fun `copyToFile creates no file when the uri cannot be opened`() {
+        val uri = Uri.parse("content://media/external/downloads/64711")
+        val targetFile = File(TempFiles.createTempDir(), "target")
+        val context = contextWhereOpeningUri(uri) {
+            throw IllegalStateException("Only owner is able to interact with pending media")
+        }
+
+        uri.copyToFile(context, targetFile)
+
+        assertThat(targetFile.exists(), equalTo(false))
+    }
+
+    @Test
+    fun `copyToFile creates no file when there is nothing to read`() {
+        val uri = Uri.parse("content://media/external/downloads/64711")
+        val targetFile = File(TempFiles.createTempDir(), "target")
+        val context = contextWhereOpeningUri(uri) { null }
+
+        uri.copyToFile(context, targetFile)
+
+        assertThat(targetFile.exists(), equalTo(false))
+    }
+
+    @Test
+    fun `copyToFile leaves no partial file when reading fails part way through`() {
+        val uri = Uri.parse("content://media/external/downloads/64711")
+        val targetFile = File(TempFiles.createTempDir(), "target")
+        val context = contextWhereOpeningUri(uri) {
+            object : ByteArrayInputStream("blah".toByteArray()) {
+                override fun read(b: ByteArray, off: Int, len: Int): Int {
+                    return if (available() > 0) {
+                        super.read(b, off, len)
+                    } else {
+                        throw IOException("connection lost")
+                    }
+                }
+            }
+        }
+
+        uri.copyToFile(context, targetFile)
+
+        assertThat(targetFile.exists(), equalTo(false))
     }
 
     @Test
@@ -73,5 +126,13 @@ class UriExtTest {
         val result = uri.addQueryParam("id", "123")
 
         assertThat(result.toString(), equalTo("https://example.com/path/subpath?id=123"))
+    }
+
+    private fun contextWhereOpeningUri(uri: Uri, open: () -> InputStream?): Context {
+        val contentResolver = mock<ContentResolver> {
+            on { openInputStream(uri) } doAnswer { open() }
+        }
+
+        return mock<Context> { on { getContentResolver() } doReturn contentResolver }
     }
 }
