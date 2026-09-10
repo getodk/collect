b5b0c6828ad5bc8e75a1686933464055165a3688
Callum Stott <callum@seadowg.com>
2026-09-10 17:16:09 +0100

    Grab crash reports from BigQuery

--- tools/crash_reports.py
+++ tools/crash_reports.py
@@ -1,19 +1,48 @@
 #!/usr/bin/env -S uv run --script
 # /// script
 # requires-python = ">=3.10"
+# dependencies = ["google-cloud-bigquery"]
 # ///
 
-import csv
 import sys
+from google.cloud import bigquery
+from google.auth.exceptions import DefaultCredentialsError
+
+QUERY = """
+    SELECT t.issue_id,
+           COUNT(DISTINCT t.installation_uuid) as users
+    FROM `api-project-322300403941.firebase_crashlytics.org_odk_collect_android_ANDROID` as t,
+         UNNEST(exceptions) as e
+    WHERE is_fatal = true
+      AND t.issue_title != "java.lang.OutOfMemoryError"
+      AND t.issue_subtitle != "java.lang.OutOfMemoryError"
+      AND t.issue_subtitle != "com.android.internal.os.BinderInternal$GcWatcher.finalize"
+      AND t.issue_subtitle !=
+          "java.util.concurrent.TimeoutException - com.android.internal.os.BinderInternal$GcWatcher.finalize() timed out after 10 seconds"
+      AND t.issue_subtitle != "android.os.ThreadLocalWorkSource.setUid"
+      AND t.issue_subtitle != "java.lang.Object.wait"
+      AND e.type != "java.lang.OutOfMemoryError"
+      AND STARTS_WITH(t.application.display_version, "v2026.3.")
+    GROUP BY t.issue_id
+    ORDER BY users DESC
+        LIMIT 10
+"""
 
 def main() -> int:
-    input("Run the query at https://console.cloud.google.com/bigquery?sq=322300403941:b378914d4b4e4580a74bca23575fe7cd, download the CSV results to crash_reports.csv and then press enter...\n")
+    try:
+        client = bigquery.Client()
+    except DefaultCredentialsError:
+        print("Not authenticated with Google Cloud! Run the follow and then try again:\n")
+        print("    gcloud auth application-default login\n")
+        return 0
 
-    print("\nFirebase links:\n\n")
-    with open("crash_reports.csv") as csv_file:
-        reader = csv.DictReader(csv_file)
-        for row in reader:
-            print(f"- {row['issue_title']}: https://console.firebase.google.com/project/api-project-322300403941/crashlytics/app/android:org.odk.collect.android/issues/{row['issue_id']}\n\n")
+    query_job = client.query(QUERY)
+    results = query_job.result()
+
+    print("\nTop 10 crashes by users affected:\n\n")
+    for row in results:
+        print(f"https://console.firebase.google.com/project/api-project-322300403941/crashlytics/app/android:org.odk.collect.android/issues/{row.issue_id}\n")
+
     return 0
 
 if __name__ == "__main__":
