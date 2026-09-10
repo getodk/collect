29adddfec127033a45467a786119320d512b9db3
Callum Stott <callum@seadowg.com>
2026-09-10 15:44:22 +0100

    Add support for encoded values to getQueryParamater

--- shared/src/main/java/org/odk/collect/shared/strings/URIExt.kt
+++ shared/src/main/java/org/odk/collect/shared/strings/URIExt.kt
@@ -1,6 +1,7 @@
 package org.odk.collect.shared.strings
 
 import java.net.URI
+import java.net.URLDecoder
 
 fun URI.getQueryParameter(param: String): String? {
     val query = this.query
@@ -13,7 +14,7 @@ fun URI.getQueryParameter(param: String): String? {
                 if (matchResult != null) {
                     val groups = matchResult.groupValues.drop(1)
                     when (groups.size) {
-                        2 -> groups[0] to groups[1]
+                        2 -> groups[0] to URLDecoder.decode(groups[1], "UTF-8")
                         else -> null
                     }
                 } else {
