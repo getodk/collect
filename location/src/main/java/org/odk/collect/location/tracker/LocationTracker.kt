052d4c67507d76f105d4aae64781be8b948993e2
Callum Stott <callum@seadowg.com>
2026-09-14 09:10:40 +0100

    Spike out background service runner

--- location/src/main/java/org/odk/collect/location/tracker/LocationTracker.kt
+++ location/src/main/java/org/odk/collect/location/tracker/LocationTracker.kt
@@ -1,9 +1,18 @@
 package org.odk.collect.location.tracker
 
+import android.app.ActivityManager
+import android.content.Context
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import org.odk.collect.location.Location
+import kotlin.time.Duration.Companion.milliseconds
 
 /**
  * Provides a way to track the location of a device.
@@ -40,20 +49,55 @@ fun LocationTracker.getCurrentLocation(): Location? {
 }
 
 fun LocationTracker.bindToLifecycle(
-    lifecycleOwner: LifecycleOwner,
+    fragment: Fragment,
     retainMockAccuracy: Boolean = false
 ) {
-    lifecycleOwner.lifecycle.addObserver(object : DefaultLifecycleObserver {
-        override fun onResume(owner: LifecycleOwner) {
+    fragment.bindToForeground(
+        onForeground = {
             start(
                 retainMockAccuracy = retainMockAccuracy,
                 updateInterval = null,
                 notification = false
             )
+        },
+        onBackground = {
+            stop()
+        }
+    )
+}
+
+fun Fragment.bindToForeground(onForeground: () -> Unit, onBackground: () -> Unit) {
+    val delayedCheckScope = CoroutineScope(Dispatchers.Main)
+
+    lifecycle.addObserver(object : DefaultLifecycleObserver {
+        override fun onResume(owner: LifecycleOwner) {
+            delayedCheckScope.launch {
+                while (!isAppInForeground(requireContext())) {
+                    delay(100.milliseconds)
+                }
+
+                onForeground()
+            }
         }
 
         override fun onPause(owner: LifecycleOwner) {
-            stop()
+            delayedCheckScope.cancel()
+            onBackground()
         }
     })
+}
+
+private fun isAppInForeground(context: Context): Boolean {
+    val activityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+    val appProcesses = activityManager.runningAppProcesses ?: return false
+
+    val packageName = context.packageName
+    for (appProcess in appProcesses) {
+        if (appProcess.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND &&
+            appProcess.processName == packageName) {
+            return true
+        }
+    }
+
+    return false
 }
