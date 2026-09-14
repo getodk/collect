4a0e7eb6e844d20cf83cbfaec73f6860f22a9d4a
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2026-09-14 22:24:47 +0200

    Add failing test for a node test without a name

--- entities/src/test/java/org/odk/collect/entities/javarosa/parse/XPathExpressionExtTest.kt
+++ entities/src/test/java/org/odk/collect/entities/javarosa/parse/XPathExpressionExtTest.kt
@@ -58,4 +58,16 @@ class XPathExpressionExtTest {
             equalTo(null)
         )
     }
+
+    @Test
+    fun `#toQuery returns null when node side is self expression`() {
+        val sourceInstance = ExternalDataInstance()
+        val evaluationContext = EvaluationContext(sourceInstance)
+
+        val expression = XPathParseTool.parseXPath(". = 'blah'")
+        assertThat(
+            expression.toQuery(sourceInstance, evaluationContext),
+            equalTo(null)
+        )
+    }
 }
