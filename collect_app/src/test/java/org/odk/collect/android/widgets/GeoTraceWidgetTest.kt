abb1f0aa7fd97aede5f35b56db5db7ed2f0f4949
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2026-09-22 22:59:57 +0200

    Migrate GeoTraceWidget to Compose

--- collect_app/src/test/java/org/odk/collect/android/widgets/GeoTraceWidgetTest.kt
+++ collect_app/src/test/java/org/odk/collect/android/widgets/GeoTraceWidgetTest.kt
@@ -1,26 +1,44 @@
 package org.odk.collect.android.widgets
 
-import android.view.View
+import android.app.Application
+import android.content.ComponentName
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
 import org.javarosa.core.model.data.StringData
 import org.javarosa.form.api.FormEntryPrompt
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.odk.collect.android.formentry.questions.QuestionDetails
+import org.odk.collect.android.support.WidgetTestActivity
 import org.odk.collect.android.widgets.interfaces.GeoDataRequester
 import org.odk.collect.android.widgets.support.GeoWidgetHelpers.stringFromDoubleList
 import org.odk.collect.android.widgets.support.QuestionWidgetHelpers.promptWithAnswer
 import org.odk.collect.android.widgets.support.QuestionWidgetHelpers.promptWithReadOnly
 import org.odk.collect.android.widgets.support.QuestionWidgetHelpers.promptWithReadOnlyAndAnswer
 import org.odk.collect.android.widgets.support.QuestionWidgetHelpers.widgetDependencies
-import org.odk.collect.android.widgets.support.QuestionWidgetHelpers.widgetTestActivity
+import org.odk.collect.androidtest.onNodeWithClickLabel
+import org.odk.collect.strings.R.string
+import org.robolectric.Shadows.shadowOf
 
 @RunWith(AndroidJUnit4::class)
 class GeoTraceWidgetTest {
+    init {
+        shadowOf(getApplicationContext<Application>().packageManager)
+            .addActivityIfNotPresent(ComponentName(getApplicationContext(), WidgetTestActivity::class.java))
+    }
+
+    @get:Rule
+    val composeRule = createAndroidComposeRule<WidgetTestActivity>()
+
     private val answer = stringFromDoubleList()
 
     private val geoDataRequester = mock<GeoDataRequester>()
@@ -38,68 +56,47 @@ class GeoTraceWidgetTest {
     }
 
     @Test
-    fun `the answer text is empty when there is no answer`() {
-        val widget = createWidget(promptWithAnswer(null))
-        assertThat(widget.binding.geoAnswerText.text.toString(), equalTo(""))
+    fun `the answer is not displayed when there is no answer`() {
+        createWidget(promptWithAnswer(null))
+        composeRule.onNodeWithText(answer).assertDoesNotExist()
     }
 
     @Test
-    fun `the answer text displays the answer when there is one`() {
-        val widget = createWidget(promptWithAnswer(StringData(answer)))
-        assertThat(widget.binding.geoAnswerText.text.toString(), equalTo(answer))
+    fun `the answer is displayed when there is one`() {
+        createWidget(promptWithAnswer(StringData(answer)))
+        composeRule.onNodeWithText(answer).assertIsDisplayed()
     }
 
     @Test
     fun `the button is hidden when the question is read-only and there is no answer`() {
-        val widget = createWidget(promptWithReadOnly())
-        assertThat(widget.binding.simpleButton.visibility, equalTo(View.GONE))
+        createWidget(promptWithReadOnly())
+        composeRule.onNodeWithClickLabel(string.view_line).assertDoesNotExist()
     }
 
     @Test
     fun `the button has correct text when the question is read-only and there is an answer`() {
-        val widget = createWidget(promptWithReadOnlyAndAnswer(StringData(answer)))
-        assertThat(
-            widget.binding.simpleButton.text.toString(),
-            equalTo(widget.context.getString(org.odk.collect.strings.R.string.view_line))
-        )
+        createWidget(promptWithReadOnlyAndAnswer(StringData(answer)))
+        composeRule.onNodeWithClickLabel(string.view_line).assertIsDisplayed()
     }
 
     @Test
     fun `the button has correct text when the question is not read-only and there is no answer`() {
-        val widget = createWidget(promptWithAnswer(null))
-        assertThat(
-            widget.binding.simpleButton.text.toString(),
-            equalTo(widget.context.getString(org.odk.collect.strings.R.string.get_line))
-        )
+        createWidget(promptWithAnswer(null))
+        composeRule.onNodeWithClickLabel(string.get_line).assertIsDisplayed()
     }
 
     @Test
     fun `the button has correct text when the question is not read-only and there is an answer`() {
-        val widget = createWidget(promptWithAnswer(StringData(answer)))
-        assertThat(
-            widget.binding.simpleButton.text.toString(),
-            equalTo(widget.context.getString(org.odk.collect.strings.R.string.view_or_change_line))
-        )
-    }
-
-    @Test
-    fun `long clicking the button and the answer text calls the long click listener`() {
-        val listener = mock<View.OnLongClickListener>()
-        val widget = createWidget(promptWithAnswer(null))
-
-        widget.setOnLongClickListener(listener)
-        widget.binding.simpleButton.performLongClick()
-        widget.binding.geoAnswerText.performLongClick()
-
-        verify(listener).onLongClick(widget.binding.simpleButton)
-        verify(listener).onLongClick(widget.binding.geoAnswerText)
+        createWidget(promptWithAnswer(StringData(answer)))
+        composeRule.onNodeWithClickLabel(string.view_or_change_line).assertIsDisplayed()
     }
 
     @Test
     fun `clicking the button requests a geotrace`() {
         val prompt = promptWithAnswer(null)
-        val widget = createWidget(prompt)
-        widget.binding.simpleButton.performClick()
+        createWidget(prompt)
+
+        composeRule.onNodeWithClickLabel(string.get_line).performClick()
 
         verify(geoDataRequester).requestGeoPoly(prompt)
     }
@@ -109,15 +106,18 @@ class GeoTraceWidgetTest {
         val prompt = promptWithAnswer(StringData(answer))
         val widget = createWidget(prompt)
         widget.clearAnswer()
-        widget.binding.simpleButton.performClick()
+
+        composeRule.onNodeWithClickLabel(string.view_or_change_line).performClick()
 
         verify(geoDataRequester).requestGeoPoly(prompt)
     }
 
     private fun createWidget(prompt: FormEntryPrompt) = GeoTraceWidget(
-        widgetTestActivity(),
+        composeRule.activity,
         QuestionDetails(prompt),
         geoDataRequester,
         widgetDependencies()
-    )
+    ).also {
+        composeRule.activity.setContentView(it)
+    }
 }
