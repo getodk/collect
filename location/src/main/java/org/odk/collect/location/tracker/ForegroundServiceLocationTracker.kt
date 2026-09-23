898f70c960a1e5379bf5c00018ccef5e097e17d4
Callum Stott <callum@seadowg.com>
2026-09-23 12:12:18 +0100

    runningAppProcesses only returns this app's processes

--- location/src/main/java/org/odk/collect/location/tracker/ForegroundServiceLocationTracker.kt
+++ location/src/main/java/org/odk/collect/location/tracker/ForegroundServiceLocationTracker.kt
@@ -202,10 +202,8 @@ private fun isAppInForeground(context: Context): Boolean {
     val activityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
     val appProcesses = activityManager.runningAppProcesses ?: return false
 
-    val packageName = context.packageName
     for (appProcess in appProcesses) {
-        if (appProcess.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND &&
-            appProcess.processName == packageName) {
+        if (appProcess.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
             return true
         }
     }
